b406b7910d6fad59bf28a94b4dcc4e29bb07421f
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-07-20 05:00:09 +0200

    have to completely rework

--- crates/poirot-core/src/parser_stats.rs
+++ crates/poirot-core/src/parser_stats.rs
@@ -5,17 +5,19 @@ use reth_primitives::{BlockNumberOrTag, H256, U256};
 /// Custom error type for trace parsing
 #[derive(Debug)]
 pub enum TraceParseError {
-    NotCallAction(H256), // Added field for transaction hash
-    EmptyInput(H256),    // Added field for transaction hash
+    TraceMissing,
+    NotRecognizedAction(H256), 
+    EmptyInput(H256),          
     EtherscanError(EtherscanError),
     AbiParseError(serde_json::Error),
     InvalidFunctionSelector(H256),
     AbiDecodingFailed(H256),
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct ParserStats {
     pub block_number: BlockNumberOrTag,
+    pub total_tx: usize,
     pub total_traces: usize,
     pub successful_parses: usize,
     pub not_call_action_errors: usize,
@@ -24,12 +26,13 @@ pub struct ParserStats {
     pub abi_parse_errors: usize,
     pub invalid_function_selector_errors: usize,
     pub abi_decoding_failed_errors: usize,
+    pub trace_missing_errors: usize,
 }
 
 impl ParserStats {
     pub fn increment_error(&mut self, error: TraceParseError) {
         match error {
-            TraceParseError::NotCallAction(_) => self.not_call_action_errors += 1,
+            TraceParseError::NotRecognizedAction(_) => self.not_call_action_errors += 1,
             TraceParseError::EmptyInput(_) => self.empty_input_errors += 1,
             TraceParseError::EtherscanError(_) => self.etherscan_errors += 1,
             TraceParseError::AbiParseError(_) => self.abi_parse_errors += 1,
@@ -37,7 +40,21 @@ impl ParserStats {
                 self.invalid_function_selector_errors += 1
             }
             TraceParseError::AbiDecodingFailed(_) => self.abi_parse_errors += 1,
+            TraceParseError::TraceMissing => self.trace_missing_errors += 1,
         };
+    }
+
+    pub fn merge(&mut self, other: &ParserStats) {
+        self.total_tx += other.total_tx;
+        self.total_traces += other.total_traces;
+        self.successful_parses += other.successful_parses;
+        self.not_call_action_errors += other.not_call_action_errors;
+        self.empty_input_errors += other.empty_input_errors;
+        self.etherscan_errors += other.etherscan_errors;
+        self.abi_parse_errors += other.abi_parse_errors;
+        self.invalid_function_selector_errors += other.invalid_function_selector_errors;
+        self.abi_decoding_failed_errors += other.abi_decoding_failed_errors;
+        self.trace_missing_errors += other.trace_missing_errors;
     }
 
     pub fn increment_success(&mut self) {
