51b3c8963696e54e87bf20fe9d9e8c81d6cf0ff4
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-07-25 20:25:05 +0200

    debugging tracing
    Former-commit-id: 795445649597ee31dd220c00c38408933c5499ef

--- crates/poirot-core/src/lib.rs
+++ crates/poirot-core/src/lib.rs
@@ -1,5 +1,16 @@
+use colored::{Colorize, ColoredString};
+
 pub mod decode;
 pub mod errors;
 pub mod normalize;
 pub mod stats;
 pub mod structured_trace;
+
+/// formats a stat with a color based on its value
+pub fn format_color(stat: &str, val: usize) -> ColoredString {
+    if val == 0 {
+        format!("{}", stat).bright_green()
+    } else {
+        format!("{}", stat).bright_red()
+    }
+}
