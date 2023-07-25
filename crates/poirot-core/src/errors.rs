ce58957ad1f196530fe015532f3bced371012cbc
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-07-25 04:54:00 +0200

    finished tracing, need to test

--- crates/poirot-core/src/errors.rs
+++ crates/poirot-core/src/errors.rs
@@ -1,12 +1,13 @@
-use alloy_etherscan::errors::EtherscanError;
 use ethers_core::types::H256;
 use thiserror::Error;
+use alloy_etherscan::errors::EtherscanError;
+
 
 /// Custom error type for trace parsing
 #[derive(Debug, Error)]
 pub enum TraceParseError {
     #[error("trace missing")]
-    TraceMissing, // must be returned
+    TraceMissing,
     #[error("empty input: {0}")]
     EmptyInput(H256),
     #[error("etherscan error: {0}")]
