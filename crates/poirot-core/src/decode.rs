188f78923e65c72d447f7cdb33e87e2b974ca222
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-07-25 22:19:42 +0200

    debugging tracing

--- crates/poirot-core/src/decode.rs
+++ crates/poirot-core/src/decode.rs
@@ -107,21 +107,21 @@ impl Parser {
         let tx_hash = &trace.transaction_hash;
 
         for (idx, transaction_trace) in transaction_traces.iter().enumerate() {
-            info!(message = format!("Starting Trace {}", format!("{}/{}", idx, transaction_traces.len()).bright_cyan()), tx_hash = format!("{:#x}", trace.transaction_hash));
+            info!(message = format!("Starting Trace {}", format!("{}/{}", idx, transaction_traces.len()).bright_cyan()));
             let (action, trace_address) = match &transaction_trace.action {
                 RethAction::Call(call) => (call, transaction_trace.trace_address.clone()),
                 RethAction::Create(create_action) => {
-                    info!(message = "Successfully Parsed Trace", tx_hash = format!("{:#x}", trace.transaction_hash), trace_action = "Create");
+                    info!(message = "Successfully Parsed Trace", trace_action = format!("{}", "CREATE".bold()), creator_addr = format!("{:#x}", create_action.from));
                     structured_traces.push(StructuredTrace::CREATE(create_action.clone()));
                     continue
                 }
                 RethAction::Selfdestruct(self_destruct) => {
-                    info!(message = "Successfully Parsed Trace", tx_hash = format!("{:#x}", trace.transaction_hash), trace_action = "Self Destruct");
+                    info!(message = "Successfully Parsed Trace", trace_action = format!("{}", "SELFDESTRUCT".bold()), contract_addr = format!("{:#x}", self_destruct.address));
                     structured_traces.push(StructuredTrace::SELFDESTRUCT(self_destruct.clone()));
                     continue
                 }
                 RethAction::Reward(reward) => {
-                    info!(message = "Successfully Parsed Trace", tx_hash = format!("{:#x}", trace.transaction_hash), trace_action = "Reward");
+                    info!(message = "Successfully Parsed Trace", trace_action = format!("{}", "REWARD".bold()), reward_type = format!("{:?}", reward.reward_type), reward_author = format!("{:#x}", reward.author));
                     structured_traces.push(StructuredTrace::REWARD(reward.clone()));
                     continue
                 }
@@ -140,7 +140,7 @@ impl Parser {
             if action.input.is_empty() {
                 match handle_empty_input(&abi, action, &trace_address, tx_hash) {
                     Ok(structured_trace) => {
-                        info!(message = "Successfully Parsed Trace", tx_hash = format!("{:#x}", trace.transaction_hash), trace_action = str_trace_action(&structured_trace));
+                        info!(message = "Successfully Parsed Trace", trace_action = format!("{}", "CALL".bold()), call_type = format!("{:?}", action.call_type));
                         structured_traces.push(structured_trace);
                         continue;
                     }
@@ -193,7 +193,7 @@ impl Parser {
                     }
                 }
             };
-            info!(message = "Successfully Parsed Trace", tx_hash = format!("{:#x}", trace.transaction_hash), trace_action = str_trace_action(&structured_trace));
+            info!(message = "Successfully Parsed Trace", trace_action = format!("{}", "CALL".bold()), call_type = format!("{:?}", action.call_type));
             structured_traces.push(structured_trace);
         }
 
