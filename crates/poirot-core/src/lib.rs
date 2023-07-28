ebdf9ac835978aeb693483c22037657fdefc9dde
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-07-28 02:11:50 +0200

    tracing

--- crates/poirot-core/src/lib.rs
+++ crates/poirot-core/src/lib.rs
@@ -16,19 +16,3 @@ pub static SUCCESSFUL_TX_PARSE: &'static str = "Successfully Parsed Transaction"
 pub static TRANSACTION_COUNTER: AtomicUsize = AtomicUsize::new(0);
 pub static TRACE_COUNTER: AtomicUsize = AtomicUsize::new(0);
 pub static SUCCESSFUL_PARSE_COUNTER: AtomicUsize = AtomicUsize::new(0);
-
-
-/// formats a stat with a color based on its value + kind
-pub fn format_color(stat: &str, val: impl Debug, color: Color) -> ColoredString {
-    format!("{}: {:?}", stat, val).color(color)
-}
-
-
-pub fn str_trace_action(trace: &StructuredTrace) -> String {
-    match trace {
-        StructuredTrace::CALL(_) => "Call".to_string(),
-        StructuredTrace::CREATE(_) => "Create".to_string(),
-        StructuredTrace::SELFDESTRUCT(_) => "Self Destruct".to_string(),
-        StructuredTrace::REWARD(_) => "Reward".to_string(),
-    }
-}
--- crates/poirot-core/src/stats/mod.rs
+++ crates/poirot-core/src/stats/mod.rs
@@ -1,2 +1,12 @@
+use colored::{ColoredString, Color, Colorize};
+use std::fmt::Debug;
+
 pub mod macros;
 pub mod stats;
+pub mod display;
+
+
+/// formats a stat with a color based on its value + kind
+pub fn format_color(stat: &str, val: impl Debug, color: Color) -> ColoredString {
+    format!("{}: {:?}", stat, val).color(color)
+}
