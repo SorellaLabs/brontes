07f828cd71cd90a011a1c68cd2c0bf8433e563eb
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-08-23 19:10:02 -0700

    updated prometheus exporter

--- crates/poirot-core/src/stats/metrics.rs
+++ crates/poirot-core/src/stats/metrics.rs
@@ -27,9 +27,9 @@ pub(crate) struct TransactionTracingMetrics {
     /// The transaction index in the block
     pub(crate) tx_idx: Gauge,
     /// The trace index in the transaction
-    pub(crate) tx_trace_idx: Gauge, 
+    pub(crate) tx_trace_idx: Gauge,
     /// The total amount of successful traces for this Transaction hash
-    pub(crate) success_traces: Counter, 
+    pub(crate) success_traces: Counter,
     /// The total amount of trace errors for this Transaction hash
     pub(crate) error_traces: Counter,
     /// Empty Input Errors
