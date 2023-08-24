e7d9aae996c2e870707c834f004051f8fd2f5b7c
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-08-23 19:10:02 -0700

    updated prometheus exporter

--- crates/bin/src/prometheus_exporter.rs
+++ crates/bin/src/prometheus_exporter.rs
@@ -1,45 +1,55 @@
-use eyre::{Result, WrapErr};
+//! Prometheus exporter
+use eyre::WrapErr;
 use hyper::{
     service::{make_service_fn, service_fn},
     Body, Request, Response, Server,
 };
 use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
 use metrics_util::layers::{PrefixLayer, Stack};
-use metrics_process::Collector;
-use std::{convert::Infallible, net::SocketAddr};
-use std::sync::Arc;
+use std::{convert::Infallible, net::SocketAddr, sync::Arc};
 
-/// Installs Prometheus as the metrics recorder and serves it over HTTP.
-pub async fn initialize(listen_addr: SocketAddr, prometheus_collector: Collector) -> eyre::Result<()> {
+pub(crate) trait Hook: Fn() + Send + Sync {}
+impl<T: Fn() + Send + Sync> Hook for T {}
 
-    let clone_collector = prometheus_collector.clone();
-    let collector: Box<dyn Fn() + Send + Sync> = Box::new(move || clone_collector.collect());
-
+/// Installs Prometheus as the metrics recorder and serves it over HTTP with hooks.
+///
+/// The hooks are called every time the metrics are requested at the given endpoint, and can be used
+/// to record values for pull-style metrics, i.e. metrics that are not automatically updated.
+pub(crate) async fn initialize_with_hooks<F: Hook + 'static>(
+    listen_addr: SocketAddr,
+    hooks: impl IntoIterator<Item = F>,
+) -> eyre::Result<()> {
     let recorder = PrometheusBuilder::new().build_recorder();
     let handle = recorder.handle();
 
+    let hooks: Vec<_> = hooks.into_iter().collect();
+
     // Start endpoint
-    start_endpoint(listen_addr, handle, Arc::new(move || collector())).await.wrap_err("Could not start Prometheus endpoint")?;
+    start_endpoint(listen_addr, handle, Arc::new(move || hooks.iter().for_each(|hook| hook())))
+        .await
+        .wrap_err("Could not start Prometheus endpoint")?;
 
     // Build metrics stack
     Stack::new(recorder)
-        .push(PrefixLayer::new("mev-tracing"))
+        .push(PrefixLayer::new("sorella-db"))
         .install()
         .wrap_err("Couldn't set metrics recorder.")?;
-
-    prometheus_collector.collect();
 
     Ok(())
 }
 
 /// Starts an endpoint at the given address to serve Prometheus metrics.
-async fn start_endpoint<T: 'static + Fn() + Sync + Send>(listen_addr: SocketAddr, handle: PrometheusHandle, collector: Arc<T>) -> Result<()> {
+async fn start_endpoint<F: Hook + 'static>(
+    listen_addr: SocketAddr,
+    handle: PrometheusHandle,
+    hook: Arc<F>,
+) -> eyre::Result<()> {
     let make_svc = make_service_fn(move |_| {
         let handle = handle.clone();
-        let collector = Arc::clone(&collector);
+        let hook = Arc::clone(&hook);
         async move {
             Ok::<_, Infallible>(service_fn(move |_: Request<Body>| {
-                (collector)();
+                (hook)();
                 let metrics = handle.render();
                 async move { Ok::<_, Infallible>(Response::new(Body::from(metrics))) }
             }))
@@ -49,6 +59,24 @@ async fn start_endpoint<T: 'static + Fn() + Sync + Send>(listen_addr: SocketAddr
         Server::try_bind(&listen_addr).wrap_err("Could not bind to address")?.serve(make_svc);
 
     tokio::spawn(async move { server.await.expect("Metrics endpoint crashed") });
+
+    Ok(())
+}
+
+/// Installs Prometheus as the metrics recorder and serves it over HTTP with database and process
+/// metrics.
+pub async fn initialize(
+    listen_addr: SocketAddr,
+    process: metrics_process::Collector,
+) -> eyre::Result<()> {
+    // Clone `process` to move it into the hook and use the original `process` for describe below.
+    let cloned_process = process.clone();
+    let hooks: Vec<Box<dyn Hook<Output = ()>>> = vec![Box::new(move || cloned_process.collect())];
+    initialize_with_hooks(listen_addr, hooks).await?;
+
+    // We describe the metrics after the recorder is installed, otherwise this information is not
+    // registered
+    process.describe();
 
     Ok(())
 }
