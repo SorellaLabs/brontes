c16bf840b42efe7bd70067a22b49edcddda60b3c
lagrange.eth <lagrange@sorellacapital.com>
2023-09-06 14:19:29 -0700

    finished parsing

---
+++ crates/poirot-core/src/bindings.rs
@@ -0,0 +1,10 @@
+use alloy_sol_types::sol;
+
+
+sol! (Curve_TriCrypto_Factory_V2, "./abis/Curve_TriCrypto_Factory_V2.json");
+sol! (Curve_Factory1_V1, "./abis/Curve_Factory1_V1.json");
+sol! (Curve_Meta_Factory1_V1, "./abis/Curve_Meta_Factory1_V1.json");
+sol! (Curve_Factory2_V1, "./abis/Curve_Factory2_V1.json");
+sol! (Curve_Meta_V1, "./abis/Curve_Meta_V1.json");
+sol! (Curve_Crypto_V2, "./abis/Curve_Crypto_V2.json");
+sol! (Curve_Crypto_Factory_V2, "./abis/Curve_Crypto_Factory_V2.json");
