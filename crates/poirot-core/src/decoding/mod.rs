87b29f959da8f92856155b0ebc430c7de4c45a54
lagrange.eth <lagrange@sorellacapital.com>
2023-09-06 16:02:50 -0700

    updated types

--- crates/poirot-core/src/decoding/mod.rs
+++ crates/poirot-core/src/decoding/mod.rs
@@ -47,7 +47,7 @@ type TraceIndex = u16;
 
 pub struct Parser {
     executor: Executor,
-    parser_fut: FuturesUnordered<JoinHandle<Option<(Vec<TxTrace>, BlockNumber)>>>,
+    parser_fut: FuturesUnordered<JoinHandle<Option<Vec<TxTrace>>>>,
     parser: Arc<TraceParser>,
 }
 
@@ -88,10 +88,7 @@ impl Parser {
     }
 
     /// executes the tracing of a given block
-    async fn execute_block(
-        this: self::Arc<TraceParser>,
-        block_num: u64,
-    ) -> Option<(Vec<TxTrace>, BlockNumber)> {
+    async fn execute_block(this: self::Arc<TraceParser>, block_num: u64) -> Option<Vec<TxTrace>> {
         let parity_trace = this.trace_block(block_num).await;
 
         if parity_trace.0.is_none() {
@@ -101,12 +98,12 @@ impl Parser {
 
         let traces = this.parse_block(parity_trace.0.unwrap(), block_num).await;
         this.metrics_tx.send(TraceMetricEvent::BlockMetricRecieved(traces.1)).unwrap();
-        Some((traces.0, block_num))
+        Some(traces.0)
     }
 }
 
 impl Stream for Parser {
-    type Item = Option<(Vec<TxTrace>, BlockNumber)>;
+    type Item = Option<Vec<TxTrace>>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.get_mut();
