50afce7fd2ea8a7aed2907fd2e2fcc702b15ec6f
Will Smith <williamqsmith11@gmail.com>
2023-09-06 15:47:17 -0700

    feat: add dotenv

--- crates/poirot-core/src/bindings.rs
+++ crates/poirot-core/src/bindings.rs
@@ -1,12 +1,11 @@
 use alloy_sol_types::sol;
 
-
-sol! (Curve_TriCrypto_Factory_V2, "./abis/Curve_TriCrypto_Factory_V2.json");
-sol! (Curve_Factory1_V1, "./abis/Curve_Factory1_V1.json");
-sol! (Curve_Meta_Factory1_V1, "./abis/Curve_Meta_Factory1_V1.json");
-sol! (Curve_Factory2_V1, "./abis/Curve_Factory2_V1.json");
-sol! (Uniswap_V3, "./abis/Uniswap_V3.json");
-sol! (Curve_Meta_V1, "./abis/Curve_Meta_V1.json");
-sol! (Curve_Crypto_V2, "./abis/Curve_Crypto_V2.json");
-sol! (SushiSwap_V2, "./abis/SushiSwap_V2.json");
-sol! (Curve_Crypto_Factory_V2, "./abis/Curve_Crypto_Factory_V2.json");
+sol!(Curve_TriCrypto_Factory_V2, "./abis/Curve_TriCrypto_Factory_V2.json");
+sol!(Curve_Factory1_V1, "./abis/Curve_Factory1_V1.json");
+sol!(Curve_Meta_Factory1_V1, "./abis/Curve_Meta_Factory1_V1.json");
+sol!(Curve_Factory2_V1, "./abis/Curve_Factory2_V1.json");
+sol!(Uniswap_V3, "./abis/Uniswap_V3.json");
+sol!(Curve_Meta_V1, "./abis/Curve_Meta_V1.json");
+sol!(Curve_Crypto_V2, "./abis/Curve_Crypto_V2.json");
+sol!(SushiSwap_V2, "./abis/SushiSwap_V2.json");
+sol!(Curve_Crypto_Factory_V2, "./abis/Curve_Crypto_Factory_V2.json");
