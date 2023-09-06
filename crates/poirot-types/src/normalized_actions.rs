4a7750a1b915b7fb7729bbd38c7b7f21e133d00c
Will Smith <williamqsmith11@gmail.com>
2023-09-06 16:15:33 -0700

    push template

--- crates/poirot-types/src/normalized_actions.rs
+++ crates/poirot-types/src/normalized_actions.rs
@@ -1,15 +1,44 @@
+use reth_primitives::{Address, U256};
+
+use crate::structured_trace::StructuredTrace;
+
 #[derive(Debug, Clone)]
 pub enum Actions {
     Swap(NormalizedSwap),
-    Transfer,
-    Mint,
-    Burn,
-    Unclassified,
+    Transfer(NormalizedTransfer),
+    Mint(NormalizedMint),
+    Burn(NormalizedBurn),
+    Unclassified(StructuredTrace),
 }
 
 #[derive(Debug, Clone)]
 pub struct NormalizedSwap {
-    fn_name: String,
+    pub token_in: Address,
+    pub token_out: Address,
+    pub amount_in: U256,
+    pub amount_out: U256,
+}
+
+#[derive(Debug, Clone)]
+pub struct NormalizedTransfer {
+    pub from: Address,
+    pub to: Address,
+    pub token: Address,
+    pub amount: U256,
+}
+
+#[derive(Debug, Clone)]
+pub struct NormalizedMint {
+    pub from: Address,
+    pub token: Address,
+    pub amount: U256,
+}
+
+#[derive(Debug, Clone)]
+pub struct NormalizedBurn {
+    pub from: Address,
+    pub token: Address,
+    pub amount: U256,
 }
 
 pub trait NormalizedAction: Clone {
