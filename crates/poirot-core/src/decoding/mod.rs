c29730324e9e8d5deaa0240788b5c905bbfb8fb5
lagrange.eth <lagrange@sorellacapital.com>
2023-09-05 20:54:13 -0700

    building bindings

--- crates/poirot-core/src/decoding/mod.rs
+++ crates/poirot-core/src/decoding/mod.rs
@@ -29,7 +29,7 @@ mod utils;
 pub(crate) const UNKNOWN: &str = "unknown";
 pub(crate) const RECEIVE: &str = "receive";
 pub(crate) const FALLBACK: &str = "fallback";
-pub(crate) const CACHE_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(10_000);
+const CACHE_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(10_000);
 const CACHE_DIRECTORY: &str = "./abi_cache";
 
 type BlockNumber = u64;
