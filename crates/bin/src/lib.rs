92a2f23a29ad45403474345697a73fa09a750d05
lagrange.eth <lagrange@sorellacapital.com>
2023-09-06 17:36:08 -0700

    finished tree building

--- crates/bin/src/lib.rs
+++ crates/bin/src/lib.rs
@@ -2,22 +2,18 @@ use poirot_core::decoding::{Parser, TypeToParse};
 use std::task::Poll;
 pub mod prometheus_exporter;
 use futures::{Future, StreamExt};
-use poirot_types::{
-    normalized_actions::NormalizedAction, structured_trace::TxTrace, tree::TimeTree,
-};
 use std::{pin::Pin, task::Context};
 
 pub const PROMETHEUS_ENDPOINT_IP: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
 pub const PROMETHEUS_ENDPOINT_PORT: u16 = 6423;
 
-pub(crate) struct Poirot<V: NormalizedAction> {
+pub(crate) struct Poirot {
     parser: Parser,
-    tree: TimeTree<V>,
 }
 
-impl<V: NormalizedAction> Poirot<V> {
-    pub(crate) fn new(parser: Parser, tree: TimeTree<V>) -> Self {
-        Self { parser, tree }
+impl Poirot {
+    pub(crate) fn new(parser: Parser) -> Self {
+        Self { parser }
     }
 
     pub(crate) fn trace_block(&self, block_num: u64) {
@@ -25,10 +21,7 @@ impl<V: NormalizedAction> Poirot<V> {
     }
 }
 
-impl<V> Future for Poirot<V>
-where
-    V: NormalizedAction + Unpin,
-{
+impl Future for Poirot {
     type Output = ();
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
@@ -37,14 +30,7 @@ where
         let mut iters = 1024;
         loop {
             while let Poll::Ready(val) = this.parser.poll_next_unpin(cx) {
-                if val.is_none() {
-                    return Poll::Ready(());
-                }
-
-                match val.unwrap() {
-                    Some(block_traces) => (),
-                    None => (),
-                }
+                let tree = if let Some(t) = val { t } else { return Poll::Ready(()) };
             }
             iters -= 1;
             if iters == 0 {
