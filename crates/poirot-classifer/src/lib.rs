ccbcf36dda495b68f31b18d69226b9ec10deb907
Will Smith <williamqsmith11@gmail.com>
2023-09-07 17:37:29 -0700

    wip
    Former-commit-id: 20d9a009f9b56e647dd3664cad6e700495e5689d

--- crates/poirot-classifer/src/lib.rs
+++ crates/poirot-classifer/src/lib.rs
@@ -1,7 +1,11 @@
 use reth_primitives::Bytes;
 
 pub mod classifer;
+pub use classifer::Classifier;
+
+mod impls;
+pub use impls::*;
 
 pub trait IntoAction {
-    pub fn decode_calldata_results(&self, calldata: Bytes, return_data: Bytes) -> Self;
+    pub fn decode_trace_data(&self, calldata: Bytes, return_data: Bytes) -> Actions;
 }
