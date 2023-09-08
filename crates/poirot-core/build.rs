4b41527f0861256ac56e990f1d1954392a3c9410
lagrange.eth <lagrange@sorellacapital.com>
2023-09-07 22:50:41 -0700

    finished build

--- crates/poirot-core/build.rs
+++ crates/poirot-core/build.rs
@@ -1,21 +1,19 @@
-use alloy_json_abi::JsonAbi;
 use clickhouse::{Client, Row};
 use ethers_core::types::{Chain, H160};
 use hyper_tls::HttpsConnector;
-use phf_codegen::Map;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::{
     env,
     fs::{self, File},
-    io::{BufReader, BufWriter, Write},
+    io::{BufWriter, Write},
     path::{Path, PathBuf},
     str::FromStr,
 };
 
-const BINDINGS_DIRECTORY: &str = "./src/";
 const ABI_DIRECTORY: &str = "./abis/";
 const PROTOCOL_ADDRESS_MAPPING_PATH: &str = "protocol_addr_mapping.rs";
+const BINDINGS_PATH: &str = "bindings.rs";
 const CACHE_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(10_000);
 const CACHE_DIRECTORY: &str = "../../abi_cache";
 const PROTOCOL_ADDRESSES: &str =
@@ -49,12 +47,16 @@ async fn run() {
 
     let protocol_abis = query_db::<ProtocolAbis>(&clickhouse_client, PROTOCOL_ABIS).await;
 
-    let abis = write_all_abis(etherscan_client, protocol_abis.clone()).await;
+    write_all_abis(etherscan_client, protocol_abis.clone()).await;
 
     let protocol_address_map =
         query_db::<AddressToProtocolMapping>(&clickhouse_client, PROTOCOL_ADDRESSES).await;
 
-    generate("./src/bindings.rs", protocol_abis.clone(), abis).await;
+    generate(
+        Path::new(&env::var("OUT_DIR").unwrap()).join(BINDINGS_PATH).to_str().unwrap(),
+        protocol_abis.clone(),
+    )
+    .await;
     address_abi_mapping(protocol_address_map)
 }
 
@@ -65,28 +67,35 @@ async fn run() {
 //
 
 /// generates all bindings and enums for them and writes them to a file
-async fn generate(bindings_file_path: &str, addresses: Vec<ProtocolAbis>, abis: Vec<Value>) {
+async fn generate(bindings_file_path: &str, addresses: Vec<ProtocolAbis>) {
     let mut file = write_file(bindings_file_path, true);
 
-    let mut bindings = init_bindings();
+    let mut addr_bindings = init_bindings();
     let mut binding_enums = init_enum("StaticBindings");
+    let mut return_binding_enums = init_enum("StaticReturnBindings");
     let mut mod_enums = Vec::new();
 
-    for (protocol_addr, abi) in addresses.into_iter().zip(abis) {
+    for protocol_addr in addresses {
         let abi_file_path = get_file_path(ABI_DIRECTORY, &protocol_addr.protocol, ".json");
-        bindings.push(binding_string(&abi_file_path, &protocol_addr.protocol));
+        addr_bindings.push(binding_string(&abi_file_path, &protocol_addr.protocol));
 
-        binding_enums.push(enum_binding_string(&protocol_addr.protocol));
-
-        individual_sub_enums(&mut mod_enums, &abi_file_path, &protocol_addr.protocol);
+        binding_enums.push(enum_binding_string(&protocol_addr.protocol, Some("_Enum")));
+        return_binding_enums.push(enum_binding_string(
+            &protocol_addr.protocol,
+            Some(&format!("::{}Calls", &protocol_addr.protocol)),
+        ));
+        individual_sub_enums(&mut mod_enums, &protocol_addr.protocol);
+        enum_impl_macro(&mut mod_enums, &protocol_addr.protocol);
     }
 
     binding_enums.push("}".to_string());
+    return_binding_enums.push("}".to_string());
 
     let aggr = format!(
-        "{}\n\n{}\n{}",
-        bindings.join("\n"),
+        "{}\n\n{}\n\n{}\n{}",
+        addr_bindings.join("\n"),
         binding_enums.join("\n"),
+        return_binding_enums.join("\n"),
         mod_enums.join("\n")
     );
 
@@ -96,7 +105,7 @@ async fn generate(bindings_file_path: &str, addresses: Vec<ProtocolAbis>, abis: 
 /// generates the bindings for the given abis
 fn init_bindings() -> Vec<String> {
     let mut bindings = Vec::new();
-    bindings.push("use alloy_sol_types::sol;\n\n".to_string());
+    bindings.push("\n\n".to_string());
     bindings
 }
 
@@ -112,63 +121,33 @@ fn init_enum(name: &str) -> Vec<String> {
 /// generates the string of an individual binding
 fn binding_string(file_path: &str, protocol_name: &str) -> String {
     let binding = format!("sol! ({}, \"{}\");", protocol_name, file_path);
-    //let enum_binding = format!("   {},\n", protocol_name);
-    //(binding, enum_binding)
     binding
 }
 
 /// generates the string of an enum for a binding
-fn enum_binding_string(protocol_name: &str) -> String {
-    let binding = format!("   {},", protocol_name);
+fn enum_binding_string(protocol_name: &str, other: Option<&str>) -> String {
+    let binding = format!("   {}({}{}),", protocol_name, protocol_name, other.unwrap_or(""));
     binding
 }
 
 /// generates the mapping of function selector to decodable type
-fn function_selector_mapping(map: &mut Map<[u8; 4]>, abi_file_path: &str, protocol_name: &str) {
-    //let abi_file_path = get_file_path(ABI_DIRECTORY, &protocol_addr.protocol, ".json");
-    let reader = BufReader::new(File::open(abi_file_path).unwrap());
-    let json_abi: JsonAbi = serde_json::from_reader(reader).unwrap();
-
-    for functions in json_abi.functions.values() {
-        for function in functions {
-            let val =
-                format!("StaticBindings::{}({}::{})", protocol_name, protocol_name, function.name);
-            let map = map.entry(function.selector(), &val);
-        }
-    }
-}
-
-/// generates the mapping of function selector to decodable type
-fn individual_sub_enums(mod_enum: &mut Vec<String>, abi_file_path: &str, protocol_name: &str) {
-    let input = fs::read_to_string(abi_file_path).unwrap();
-    let json_abi: JsonAbi = serde_json::from_str(&input).unwrap();
-
+fn individual_sub_enums(mod_enum: &mut Vec<String>, protocol_name: &str) {
     let mut enum_protocol_name = protocol_name.to_string();
     enum_protocol_name.push_str("_Enum");
     mod_enum.extend(init_enum(&enum_protocol_name));
-    for functions in json_abi.functions.values() {
-        if functions.len() > 1 {
-            for (idx, function) in functions.into_iter().enumerate() {
-                let val = format!(
-                    "   {}_{}({}::{}_{}Call),",
-                    &function.name, idx, protocol_name, &function.name, idx
-                );
-                mod_enum.push(val)
-            }
-        } else {
-            let val = format!(
-                "   {}({}::{}Call),",
-                &functions[0].name, protocol_name, &functions[0].name
-            );
-            mod_enum.push(val);
-        }
-    }
-
+    mod_enum.push(" None".to_string());
     mod_enum.push("}".to_string());
 }
 
-/// generates the matching of function selector to type to decode
-fn fn_sig_protocol_mapping() {}
+/// generates the mapping of function selector to decodable type
+fn enum_impl_macro(mod_enum: &mut Vec<String>, protocol_name: &str) {
+    let macro_impl = format!(
+        "impl_decode_sol!({}_Enum, {}::{}Calls);",
+        protocol_name, protocol_name, protocol_name
+    );
+    mod_enum.push(macro_impl);
+    mod_enum.push("\n".to_string());
+}
 
 //
 //
@@ -177,32 +156,28 @@ fn fn_sig_protocol_mapping() {}
 //
 
 /// writes the provider json abis to files given the protocol name
-async fn write_all_abis(
-    client: alloy_etherscan::Client,
-    addresses: Vec<ProtocolAbis>,
-) -> Vec<Value> {
-    let mut abis = Vec::new();
+async fn write_all_abis(client: alloy_etherscan::Client, addresses: Vec<ProtocolAbis>) {
     for protocol_addr in addresses {
         let abi = get_abi(client.clone(), &protocol_addr.address).await;
-        abis.push(abi.clone());
         let abi_file_path = get_file_path(ABI_DIRECTORY, &protocol_addr.protocol, ".json");
         let mut file = write_file(&abi_file_path, true);
         file.write_all(serde_json::to_string(&abi).unwrap().as_bytes()).unwrap();
     }
-
-    abis
 }
 
 /// creates a mapping of each address to an abi binding
 fn address_abi_mapping(mapping: Vec<AddressToProtocolMapping>) {
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join(PROTOCOL_ADDRESS_MAPPING_PATH);
     let mut file = BufWriter::new(File::create(&path).unwrap());
-    file.write_all("use crate::bindings::StaticBindings;\n\n".as_bytes()).unwrap();
+    //file.write_all("use crate::bindings::*;\n\n".as_bytes()).unwrap();
 
     let mut phf_map = phf_codegen::Map::new();
     for map in &mapping {
         for address in &map.addresses {
-            phf_map.entry(address, &format!("StaticBindings::{}", &map.protocol));
+            phf_map.entry(
+                address,
+                &format!("StaticBindings::{}({}_Enum::None)", &map.protocol, &map.protocol),
+            );
         }
     }
 
