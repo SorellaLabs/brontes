00f71a99d7be2d780a016e4068dc31f693e4c704
Will Smith <williamqsmith11@gmail.com>
2023-09-07 19:06:36 -0700

    wire in poirot lib

--- crates/poirot-inspect/src/backrun.rs
+++ crates/poirot-inspect/src/backrun.rs
@@ -1,1 +1,12 @@
+use crate::{database::InspectorDataClient, Inspector};
+use poirot_types::{normalized_actions::Actions, tree::TimeTree};
+use std::sync::Arc;
 
+pub struct BackrunInspector {
+    db: Arc<InspectorDataClient>,
+}
+
+#[async_trait::async_trait]
+impl Inspector for BackrunInspector {
+    async fn process_tree(&self, tree: Arc<TimeTree<Actions>>) {}
+}
