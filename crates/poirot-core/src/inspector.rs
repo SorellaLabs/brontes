9e0a81a29b068338f72a7ddb92af4b5bc058e8d6
Will Smith <williamqsmith11@gmail.com>
2023-09-07 21:39:44 -0700

    delete old files
    Former-commit-id: 6ada86105af3dccdfbc96123ae71a4fa2af1b229

--- crates/poirot-core/src/inspector.rs
+++
@@ -1,8 +0,0 @@
-use crate::normalize::Structure;
-
-/// Represents the amount of MEV extracted for a particular type of strategy.
-pub struct Report;
-
-pub trait Inspector {
-    fn inspect(&self, inspection: Vec<Structure>) -> Report;
-}
