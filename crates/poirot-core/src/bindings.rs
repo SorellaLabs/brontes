86bea43008301e7208770cd96f7f12b50c3fdb85
Will Smith <williamqsmith11@gmail.com>
2023-09-07 17:24:20 -0700

    push

--- crates/poirot-core/src/bindings.rs
+++ crates/poirot-core/src/bindings.rs
@@ -1,24 +1,23 @@
 use alloy_sol_types::sol;
 
-
-sol! (Curve_TriCrypto_Factory_V2, "./abis/Curve_TriCrypto_Factory_V2.json");
-sol! (Curve_Factory1_V1, "./abis/Curve_Factory1_V1.json");
-sol! (Curve_Meta_Factory1_V1, "./abis/Curve_Meta_Factory1_V1.json");
-sol! (Curve_Factory2_V1, "./abis/Curve_Factory2_V1.json");
-sol! (Uniswap_V3, "./abis/Uniswap_V3.json");
-sol! (Curve_Meta_V1, "./abis/Curve_Meta_V1.json");
-sol! (Curve_Crypto_V2, "./abis/Curve_Crypto_V2.json");
-sol! (SushiSwap_V2, "./abis/SushiSwap_V2.json");
-sol! (Curve_Crypto_Factory_V2, "./abis/Curve_Crypto_Factory_V2.json");
+sol!(Curve_TriCrypto_Factory_V2, "./abis/Curve_TriCrypto_Factory_V2.json");
+sol!(Curve_Factory1_V1, "./abis/Curve_Factory1_V1.json");
+sol!(Curve_Meta_Factory1_V1, "./abis/Curve_Meta_Factory1_V1.json");
+sol!(Curve_Factory2_V1, "./abis/Curve_Factory2_V1.json");
+sol!(Uniswap_V3, "./abis/Uniswap_V3.json");
+sol!(Curve_Meta_V1, "./abis/Curve_Meta_V1.json");
+sol!(Curve_Crypto_V2, "./abis/Curve_Crypto_V2.json");
+sol!(SushiSwap_V2, "./abis/SushiSwap_V2.json");
+sol!(Curve_Crypto_Factory_V2, "./abis/Curve_Crypto_Factory_V2.json");
 
 pub enum StaticBindings {
-   Curve_TriCrypto_Factory_V2,
-   Curve_Factory1_V1,
-   Curve_Meta_Factory1_V1,
-   Curve_Factory2_V1,
-   Uniswap_V3,
-   Curve_Meta_V1,
-   Curve_Crypto_V2,
-   SushiSwap_V2,
-   Curve_Crypto_Factory_V2,
+    Curve_TriCrypto_Factory_V2,
+    Curve_Factory1_V1,
+    Curve_Meta_Factory1_V1,
+    Curve_Factory2_V1,
+    Uniswap_V3,
+    Curve_Meta_V1,
+    Curve_Crypto_V2,
+    SushiSwap_V2,
+    Curve_Crypto_Factory_V2,
 }
