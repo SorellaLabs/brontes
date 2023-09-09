79fa3bb708103435590c0945b41fc33d34bece70
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-09-08 22:44:53 -0700

    todo list for classifiers

--- crates/poirot-classifer/src/impls/uniswap_v3.rs
+++ crates/poirot-classifer/src/impls/uniswap_v3.rs
@@ -1,8 +1,9 @@
 use crate::{yoink_decoded_type, IntoAction};
 use poirot_core::{
     StaticReturnBindings,
-    Uniswap_V3::{swapCall, Uniswap_V3Calls},
+    Uniswap_V3::{swapCall, Uniswap_V3Calls, swapReturn},
 };
+use alloy_sol_types::SolCall;
 use poirot_types::normalized_actions::Actions;
 use reth_primitives::Bytes;
 
@@ -12,6 +13,7 @@ impl IntoAction for V3SwapImpl {
     fn decode_trace_data(&self, data: StaticReturnBindings, return_data: Bytes) -> Actions {
         let res = yoink_decoded_type!(data, Uniswap_V3, swapCall);
 
+        let r = swapCall::decode_returns(&mut &return_data[..], true).unwrap();
         todo!()
     }
 }
