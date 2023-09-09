118d076da60f2d5949297bcd5055fae99eb41220
Will Smith <williamqsmith11@gmail.com>
2023-09-09 13:25:45 -0700

    add v3 impls

--- crates/poirot-classifer/src/lib.rs
+++ crates/poirot-classifer/src/lib.rs
@@ -1,4 +1,4 @@
-use reth_primitives::Bytes;
+use reth_primitives::{Bytes, Address};
 
 use poirot_core::StaticReturnBindings;
 
@@ -11,5 +11,5 @@ pub use impls::*;
 use poirot_types::normalized_actions::Actions;
 
 pub trait IntoAction: Send + Sync {
-    fn decode_trace_data(&self, data: StaticReturnBindings, return_data: Bytes) -> Actions;
+    fn decode_trace_data(&self, data: StaticReturnBindings, return_data: Bytes, address: Address) -> Actions;
 }
