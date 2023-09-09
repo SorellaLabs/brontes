45e5b0b064988671d28aa5ba7b55622f0e943773
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2023-09-09 15:16:34 -0700

    feat: v2 impl (#1)
    * feat: finish v2 impl
    
    * fix ci
    
    * fix ci
    
    * fix names
    
    * fix spacing
    
    * remove this
    
    Former-commit-id: 80e1c1b4e26548af1c176988fc1cb2662e4eaacc

--- crates/poirot-classifer/src/lib.rs
+++ crates/poirot-classifer/src/lib.rs
@@ -1,4 +1,4 @@
-use reth_primitives::{Bytes, Address};
+use reth_primitives::{Address, Bytes, Log};
 
 use poirot_core::StaticReturnBindings;
 
@@ -11,5 +11,11 @@ pub use impls::*;
 use poirot_types::normalized_actions::Actions;
 
 pub trait IntoAction: Send + Sync {
-    fn decode_trace_data(&self, data: StaticReturnBindings, return_data: Bytes, address: Address) -> Actions;
+    fn decode_trace_data(
+        &self,
+        data: StaticReturnBindings,
+        return_data: Bytes,
+        address: Address,
+        logs: &Vec<Log>,
+    ) -> Actions;
 }
