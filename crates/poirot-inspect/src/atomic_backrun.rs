566c98e0cedec8eb34c4a4d82feeeb3c1cb8d25f
Will Smith <williamqsmith11@gmail.com>
2023-09-08 17:42:35 -0700

    feat: add get hashes

--- crates/poirot-inspect/src/atomic_backrun.rs
+++ crates/poirot-inspect/src/atomic_backrun.rs
@@ -2,11 +2,16 @@ use crate::{database::InspectorDataClient, Inspector};
 use poirot_types::{normalized_actions::Actions, tree::TimeTree};
 use std::sync::Arc;
 
-pub struct BackrunInspector {
+pub struct AtomicBackrunInspector {
     db: Arc<InspectorDataClient>,
 }
 
+impl AtomicBackrunInspector {
+
+}
+
 #[async_trait::async_trait]
-impl Inspector for BackrunInspector {
-    async fn process_tree(&self, tree: Arc<TimeTree<Actions>>) {}
+impl Inspector for AtomicBackrunInspector {
+    async fn process_tree(&self, tree: Arc<TimeTree<Actions>>) {
+    }
 }
