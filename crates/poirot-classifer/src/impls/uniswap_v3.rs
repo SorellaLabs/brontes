947125ba5cbd0577c95d3f8e154b2c4e2087ea9a
Will Smith <williamqsmith11@gmail.com>
2023-09-08 20:36:55 -0700

    add epic macro

--- crates/poirot-classifer/src/impls/uniswap_v3.rs
+++ crates/poirot-classifer/src/impls/uniswap_v3.rs
@@ -1,1 +1,17 @@
-// TODO: impl IntoAction here
+use crate::{yoink_decoded_type, IntoAction};
+use poirot_core::{
+    StaticReturnBindings,
+    Uniswap_V3::{swapCall, Uniswap_V3Calls},
+};
+use poirot_types::normalized_actions::Actions;
+use reth_primitives::Bytes;
+
+pub struct V3SwapImpl;
+
+impl IntoAction for V3SwapImpl {
+    fn decode_trace_data(&self, data: StaticReturnBindings, return_data: Bytes) -> Actions {
+        let res = yoink_decoded_type!(data, Uniswap_V3, swapCall);
+
+        todo!()
+    }
+}
