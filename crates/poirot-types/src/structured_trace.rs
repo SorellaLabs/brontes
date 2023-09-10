3c69a2cff4646b1fe4f1ec39b2b15530644d712c
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2023-09-10 00:32:12 -0700

    fix main (#10)

--- crates/poirot-types/src/structured_trace.rs
+++ crates/poirot-types/src/structured_trace.rs
@@ -44,17 +44,11 @@ pub struct TxTrace {
     pub trace:    Vec<TransactionTrace>,
     pub logs:     Vec<Log>,
     pub tx_hash:  H256,
-    pub tx_index: u64 /* pub gas_used: u64, */
-                      /* pub gas_used: U256, */
+    pub tx_index: u64
 }
 
 impl TxTrace {
-    pub fn new(
-        trace: Vec<TransactionTrace>,
-        tx_hash: H256,
-        logs: Vec<Log>,
-        tx_index: u64 /* gas_used: U256, */
-    ) -> Self {
-        Self { trace, tx_hash, tx_index, logs /* , gas_used */ }
+    pub fn new(trace: Vec<TransactionTrace>, tx_hash: H256, logs: Vec<Log>, tx_index: u64) -> Self {
+        Self { trace, tx_hash, tx_index, logs }
     }
 }
