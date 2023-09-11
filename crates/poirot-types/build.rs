c20268a1361697d3f3da1072b1aa383fec3e3c57
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2023-09-10 20:03:41 -0700

    Feat/decimal build script (#14)
    * add build script for decimals
    
    * don't blow up lsp

--- crates/poirot-types/build.rs
+++ crates/poirot-types/build.rs
@@ -4,14 +4,61 @@ use std::{
     fs::{self, File},
     hash::Hash,
     io::{BufWriter, Write},
-    path::Path
+    path::Path,
+    str::FromStr
 };
 
-use ethers_core::types::Address;
+use clickhouse::{Client, Row};
+use ethers_core::types::{Address, Chain, H160};
+use hyper_tls::HttpsConnector;
 use serde::{Deserialize, Serialize};
 use strum::Display;
 
 const TOKEN_MAPPING_FILE: &str = "token_mapping.rs";
+const TOKEN_QUERIES: &str = "SELECT toString(address),decimals FROM tokens";
+
+fn main() {
+    dotenv::dotenv().ok();
+    println!("cargo:rerun-if-env-changed=RUN_BUILD_SCRIPT");
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    runtime.block_on(async move {
+        let path = Path::new(&env::var("OUT_DIR").unwrap()).join(TOKEN_MAPPING_FILE);
+        let mut file = BufWriter::new(File::create(&path).unwrap());
+        build_token_details_map(&mut file).await;
+        build_asset_map(&mut file);
+    });
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Row)]
+pub struct TokenDetails {
+    address:  String,
+    decimals: u8
+}
+
+async fn build_token_details_map(file: &mut BufWriter<File>) {
+    let mut phf_map: phf_codegen::Map<[u8; 20]> = phf_codegen::Map::new();
+    #[cfg(feature = "server")]
+    {
+        let client = build_db();
+        let rows = query_db::<TokenDetails>(&client, TOKEN_QUERIES).await;
+
+        for row in rows {
+            phf_map
+                .entry(H160::from_str(&row.address).unwrap().0, row.decimals.to_string().as_str());
+        }
+    }
+
+    writeln!(
+        file,
+        "pub static TOKEN_TO_DECIMALS: phf::Map<[u8; 20], u8> = \n{};\n",
+        phf_map.build()
+    )
+    .unwrap();
+}
 
 #[derive(
     Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, Serialize, Deserialize, Display,
@@ -47,14 +94,12 @@ impl Hash for Token {
         self.global_id.hash(state)
     }
 }
-fn main() {
+
+fn build_asset_map(file: &mut BufWriter<File>) {
     let tokens: TokenList = serde_json::from_str(
         &fs::read_to_string("../../ticker_address_mapping/assets.json").unwrap()
     )
     .unwrap();
-
-    let path = Path::new(&env::var("OUT_DIR").unwrap()).join(TOKEN_MAPPING_FILE);
-    let mut file = BufWriter::new(File::create(&path).unwrap());
 
     let mut phf_map = phf_codegen::Map::new();
 
@@ -66,9 +111,37 @@ fn main() {
     }
 
     writeln!(
-        &mut file,
+        file,
         "pub static TOKEN_ADDRESS_TO_TICKER: phf::Map<[u8; 20], &'static str> = \n{};\n",
         phf_map.build()
     )
     .unwrap();
+}
+
+/// builds the clickhouse database client
+fn build_db() -> Client {
+    // clickhouse path
+    let clickhouse_path = format!(
+        "{}:{}",
+        &env::var("CLICKHOUSE_URL").expect("CLICKHOUSE_URL not found in .env"),
+        &env::var("CLICKHOUSE_PORT").expect("CLICKHOUSE_PORT not found in .env")
+    );
+
+    // builds the https connector
+    let https = HttpsConnector::new();
+    let https_client = hyper::Client::builder().build::<_, hyper::Body>(https);
+
+    // builds the clickhouse client
+
+    Client::with_http_client(https_client)
+        .with_url(clickhouse_path)
+        .with_user(env::var("CLICKHOUSE_USER").expect("CLICKHOUSE_USER not found in .env"))
+        .with_password(env::var("CLICKHOUSE_PASS").expect("CLICKHOUSE_PASS not found in .env"))
+        .with_database(
+            env::var("CLICKHOUSE_DATABASE").expect("CLICKHOUSE_DATABASE not found in .env")
+        )
+}
+
+async fn query_db<T: Row + for<'a> Deserialize<'a>>(db: &Client, query: &str) -> Vec<T> {
+    db.query(query).fetch_all::<T>().await.unwrap()
 }
