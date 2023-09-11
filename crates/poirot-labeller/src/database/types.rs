c41540f222c90f59a26c18f5263e6e212bddc26b
lagrange.eth <lagrange@sorellacapital.com>
2023-09-11 01:46:42 -0700

    label queries
    Former-commit-id: 01052cb691d651536a1b009bb69253d7780be865

--- crates/poirot-labeller/src/database/types.rs
+++ crates/poirot-labeller/src/database/types.rs
@@ -1,4 +1,4 @@
-use clickhouse::Row;
+use clickhouse::row::Row;
 use malachite::Rational;
 use serde::Deserialize;
 
@@ -7,70 +7,71 @@ use crate::database::serialize::as_rational;
 #[derive(Debug, Clone, Row, Deserialize)]
 pub struct DBMempool {
     timestamp: u64,
-    tx_hash:   String,
-    node_id:   String,
-    source:    String,
-    vpc:       bool
+    tx_hash: String,
+    node_id: String,
+    source: String,
+    vpc: bool,
 }
 
 #[derive(Debug, Clone, Row, Deserialize)]
 pub struct DBRelays {
-    epoch:          u32,
-    slot:           u32,
-    timestamp:      u64,
-    block_number:   u32,
-    parent_hash:    String,
-    block_hash:     String,
-    relay:          String,
-    builder_name:   String,
+    epoch: u32,
+    slot: u32,
+    timestamp: u64,
+    block_number: u32,
+    parent_hash: String,
+    block_hash: String,
+    relay: String,
+    builder_name: String,
     builder_pubkey: String,
-    fee_recipient:  String,
-    gas_limit:      u64,
-    gas_used:       u64,
-    value:          u128,
-    tx_num:         u16
+    fee_recipient: String,
+    gas_limit: u64,
+    gas_used: u64,
+    value: u128,
+    tx_num: u16,
 }
 
 #[derive(Debug, Clone, Row, Deserialize)]
 pub struct DBTardisQuotes {
-    exchange:        String,
-    symbol:          String,
-    timestamp:       u64,
+    exchange: String,
+    symbol: String,
+    timestamp: u64,
     local_timestamp: u64,
     #[serde(with = "as_rational")]
-    ask_amt:         Rational,
+    ask_amt: Rational,
     #[serde(with = "as_rational")]
-    ask_price:       Rational,
+    ask_price: Rational,
     #[serde(with = "as_rational")]
-    bid_price:       Rational,
+    bid_price: Rational,
     #[serde(with = "as_rational")]
-    bid_amt:         Rational
+    bid_amt: Rational,
 }
 
 #[derive(Debug, Clone, Row, Deserialize)]
 pub struct DBTardisL2 {
-    exchange:        String,
-    symbol:          String,
-    timestamp:       u64,
+    exchange: String,
+    symbol: String,
+    timestamp: u64,
     local_timestamp: u64,
-    is_snapshot:     bool,
-    side:            String,
+    is_snapshot: bool,
+    side: String,
     #[serde(with = "as_rational")]
-    price:           Rational,
+    price: Rational,
     #[serde(with = "as_rational")]
-    amt:             Rational
+    amt: Rational,
+}
+
+#[derive(Debug, Clone, Row, Deserialize)]
+pub struct DBP2PRelayTimes {
+    pub relay_timestamp: u64,
+    pub p2p_timestamp: u64,
 }
 
 #[derive(Debug, Clone, Row, Deserialize)]
 pub struct DBTardisTrades {
-    exchange:        String,
-    symbol:          String,
-    timestamp:       u64,
-    local_timestamp: u64,
-    id:              String,
-    side:            String,
+    timestamp: u64,
+    exchange: String,
+    symbol: String,
     #[serde(with = "as_rational")]
-    price:           Rational,
-    #[serde(with = "as_rational")]
-    amt:             Rational
+    price: Rational,
 }
