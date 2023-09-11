2302b252d44b6009ba2c48c8fc289f3d9acadb35
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2023-09-10 18:39:18 -0700

    cleanup (#13)

--- crates/poirot-labeller/src/database/mod.rs
+++ crates/poirot-labeller/src/database/mod.rs
@@ -9,6 +9,8 @@ use malachite::Rational;
 use reth_primitives::{Address, TxHash, U256};
 use sorella_db_clients::{databases::clickhouse::ClickhouseClient, errors::DatabaseError};
 
+use super::Metadata;
+
 const RELAYS_TABLE: &str = "relays";
 const MEMPOOL_TABLE: &str = "chainbound_mempool";
 const TARDIS_QUOTES_L2: &str = "tardis_l2";
@@ -26,18 +28,8 @@ impl Default for Database {
     }
 }
 
-pub struct Metadata {
-    pub block_num:       u64,
-    pub block_hash:      U256,
-    pub relay_timestamp: u64,
-    pub p2p_timestamp:   u64,
-    pub token_prices:    HashMap<Address, (Rational, Rational)>,
-    pub eth_prices:      (Rational, Rational),
-    pub mempool:         HashSet<TxHash>
-}
-
 impl Database {
-    pub async fn get_metadata<'a>(&'a self, block_num: u64, block_hash: U256) -> Metadata {
+    pub async fn get_metadata(&self, block_num: u64, block_hash: U256) -> Metadata {
         let query = format!("SELECT * FROM {} LIMIT 1", RELAYS_TABLE);
         //let res = self.client.query_all::<types::Relay>(&query).await?;
         //println!("{:?}", res);
