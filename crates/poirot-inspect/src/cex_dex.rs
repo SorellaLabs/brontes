7419758c63a554749082a1d41349bdf6a627c3d9
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-09-13 02:58:59 -0700

    fmt

--- crates/poirot-inspect/src/cex_dex.rs
+++ crates/poirot-inspect/src/cex_dex.rs
@@ -1,15 +1,14 @@
-use std::sync::Arc;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
+
 use malachite::{num::conversion::traits::RoundingFrom, rounding_modes::RoundingMode, Rational};
 use poirot_labeller::Metadata;
 use poirot_types::{
     normalized_actions::{Actions, NormalizedSwap},
-    TOKEN_TO_DECIMALS,
-    tree::{GasDetails, TimeTree}, ToScaledRational,
+    tree::{GasDetails, TimeTree},
+    ToScaledRational, TOKEN_TO_DECIMALS
 };
-
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use reth_primitives::{H256, Address, U256};
+use reth_primitives::{Address, H256, U256};
 use tracing::error;
 
 use crate::{ClassifiedMev, Inspector};
@@ -26,7 +25,6 @@ impl CexDexInspector {
         swaps: Vec<Vec<Actions>>
     ) -> Option<ClassifiedMev> {
         let cex_dex_d = self.cex_dex_profit_no_gas(swaps, metadata);
-
 
         let appearance_usd_deltas = self.get_best_usd_delta(
             deltas.clone(),
@@ -74,48 +72,57 @@ impl CexDexInspector {
         })
     }
 
-
     pub fn cex_dex_profit_no_gas(
         &self,
         swaps: Vec<Vec<Actions>>,
-        metadata: Arc<Metadata>,
+        metadata: Arc<Metadata>
     ) -> HashMap<Address, HashMap<Actions, Rational>> {
         let mut deltas = HashMap::new();
-    
+
         for actions in swaps.iter() {
-            for action in actions.iter().map(|action| ) {
+            for action in actions
+                .iter()
+                .map(|action| self.get_cex_dex(swaps, metadata))
+            {
                 if let Actions::Swap(swap) = action {
-                    
-                    // think it should be fine to unwrap here because it should never fail but we never know
-                    
-                    
-    
-                    
-    
+                    // think it should be fine to unwrap here because it should never fail but we
+                    // never know
+
                     let (cex_price1, cex_price2) = centralized_prices.unwrap();
-    
+
                     let dex_price = dex_price.unwrap();
                     if *cex_price1 > dex_price && *cex_price2 > dex_price {
-                        //TODO: very terroristic, will finish tmrw should be chill 
-                        
-                        
-
+                        //TODO: very terroristic, will finish tmrw should be
+                        // chill
                     }
                 }
             }
         }
-    
+
         deltas
     }
 
-    pub fn get_cex_dex(&self, swap: &NormalizedSwap, metadata: &Metadata, dex_price: Rational) -> Option<(&Rational, &Rational)> {
-        let arb = &self.rational_dex_price(&swap, metadata).and_then(| (dex_price, cex_price1, cex_price2 )| {
-            self.profit_classifier(swap, dex_price, cex_price1, cex_price2)
-        });
+    pub fn get_cex_dex(
+        &self,
+        swap: &NormalizedSwap,
+        metadata: &Metadata,
+        dex_price: Rational
+    ) -> Option<(&Rational, &Rational)> {
+        let arb = &self.rational_dex_price(&swap, metadata).and_then(
+            |(dex_price, cex_price1, cex_price2)| {
+                self.profit_classifier(swap, dex_price, cex_price1, cex_price2)
+            }
+        );
     }
 
-    fn profit_classifier(&self,swap: &NormalizedSwap, dex_price: &Rational, cex_price1: &Rational, cex_price2: &Rational, ) -> Option<(Option<Rational>, Option<Rational>)> {
-       // Calculate the price differences between DEX and CEX
+    fn profit_classifier(
+        &self,
+        swap: &NormalizedSwap,
+        dex_price: &Rational,
+        cex_price1: &Rational,
+        cex_price2: &Rational
+    ) -> Option<(Option<Rational>, Option<Rational>)> {
+        // Calculate the price differences between DEX and CEX
         let delta_price1 = cex_price1 - dex_price;
         let delta_price2 = cex_price2 - dex_price;
 
@@ -123,26 +130,26 @@ impl CexDexInspector {
         let profit1 = delta_price1 * swap.amount_in.to_scaled_rational(18);
         let profit2 = delta_price2 * swap.amount_in.to_scaled_rational(18);
 
-        // Check if the profit is positive 
+        // Check if the profit is positive
         match (profit1 > Rational::from(0), profit2 > Rational::from(0)) {
             (true, true) => Some((Some(profit1), Some(profit2))),
             (true, false) => Some((Some(profit1), None)),
             (false, true) => Some((None, Some(profit2))),
-            (false, false) => None,
+            (false, false) => None
         }
     }
-
 
     pub fn rational_dex_price(
         &self,
         swap: &NormalizedSwap,
-        metadata: &Metadata,
+        metadata: &Metadata
     ) -> Option<(&Rational, &Rational, &Rational)> {
         let Some(decimals_in) = TOKEN_TO_DECIMALS.get(&swap.token_in.0) else {
             error!(missing_token=?swap.token_in, "missing token in token to decimal map");
             return None
         };
-        //TODO(JOE): this is ugly asf, but we should have some metrics shit so we can log it
+        //TODO(JOE): this is ugly asf, but we should have some metrics shit so we can
+        // log it
         let Some(decimals_out) = TOKEN_TO_DECIMALS.get(&swap.token_out.0) else {
             error!(missing_token=?swap.token_in, "missing token in token to decimal map");
             return None
@@ -157,8 +164,10 @@ impl CexDexInspector {
     }
 }
 
-//TODO(WILL) I think we should reorganise the way we do priority fee, becuase why force a unecessary call on all inspectos when we can just
-//TODO calc & store it when we build the tree, because all inspectors need it (same as bribe amount)
+//TODO(WILL) I think we should reorganise the way we do priority fee, becuase
+// why force a unecessary call on all inspectos when we can just TODO calc &
+// store it when we build the tree, because all inspectors need it (same as
+// bribe amount)
 #[async_trait::async_trait]
 impl Inspector for CexDexInspector {
     async fn process_tree(
@@ -166,11 +175,8 @@ impl Inspector for CexDexInspector {
         tree: Arc<TimeTree<Actions>>,
         meta_data: Arc<Metadata>
     ) -> Vec<ClassifiedMev> {
-        let intersting_state = tree.inspect_all(|node| {
-            node.subactions
-                .iter()
-                .any(|action| action.is_swap())
-        });
+        let intersting_state =
+            tree.inspect_all(|node| node.subactions.iter().any(|action| action.is_swap()));
 
         intersting_state
             .into_par_iter()
