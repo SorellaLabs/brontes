3f7316f03b40b7a3beae61fab78071c6e1f21a5c
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-09-13 19:06:58 -0700

    will finish cex_dex later

--- crates/poirot-inspect/src/cex_dex.rs
+++ crates/poirot-inspect/src/cex_dex.rs
@@ -1,6 +1,10 @@
 use std::{collections::HashMap, sync::Arc};
 
-use malachite::{num::conversion::traits::RoundingFrom, rounding_modes::RoundingMode, Rational};
+use malachite::{
+    num::{basic::traits::Zero, conversion::traits::RoundingFrom},
+    rounding_modes::RoundingMode,
+    Rational
+};
 use poirot_labeller::Metadata;
 use poirot_types::{
     normalized_actions::{Actions, NormalizedSwap},
@@ -16,134 +20,87 @@ use crate::{ClassifiedMev, Inspector};
 pub struct CexDexInspector;
 
 impl CexDexInspector {
-    fn process_swaps(
+    fn process_swap(
         &self,
         hash: H256,
-        priority_fee: u64,
         metadata: Arc<Metadata>,
         gas_details: &GasDetails,
-        swaps: Vec<Vec<Actions>>
+        swaps: Vec<Actions>
     ) -> Option<ClassifiedMev> {
-        let cex_dex_d = self.cex_dex_profit_no_gas(swaps, metadata);
+        let (pre, post) = swaps
+            .into_iter()
+            .filter_map(|action| {
+                if let Actions::Swap(normalized_swap) = action {
+                    // Call get_cex_dex for each normalized swap
+                    Some(self.get_cex_dex(&normalized_swap, metadata.as_ref()))
+                } else {
+                    None
+                }
+            })
+            .unzip();
 
-        let appearance_usd_deltas = self.get_best_usd_delta(
-            deltas.clone(),
-            metadata.clone(),
-            Box::new(|(appearance, _)| appearance)
-        );
+        let profit_pre = self.arb_gas(pre, gas_details, metadata.eth_prices.0);
+        let profit_post = self.arb_gas(post, gas_details, metadata.eth_prices.1);
 
-        let finalized_usd_deltas =
-            self.get_best_usd_delta(deltas, metadata.clone(), Box::new(|(_, finalized)| finalized));
-
-        if finalized_usd_deltas.is_none() || appearance_usd_deltas.is_none() {
-            return None
+        if profit_pre.is_some() || profit_post.is_some() {
+            let mev = Some(ClassifiedMev {
+                tx_hash:     vec![hash],
+                gas_details: vec![gas_details.clone()] // TODO: add other fields
+            });
+            return mev
         }
-        let (finalized, appearance) =
-            (finalized_usd_deltas.unwrap(), appearance_usd_deltas.unwrap());
-
-        if finalized.0 != appearance.0 {
-            error!("finalized addr != appearance addr");
-            return None
-        }
-
-        let gas_used = gas_details.gas_paid();
-        let (gas_used_usd_appearance, gas_used_usd_finalized) = (
-            Rational::from(gas_used) * &metadata.eth_prices.0,
-            Rational::from(gas_used) * &metadata.eth_prices.1
-        );
-
-        Some(ClassifiedMev {
-            contract: finalized.0,
-            gas_details: vec![gas_details.clone()],
-            tx_hash: vec![hash],
-            priority_fee: vec![priority_fee],
-            block_finalized_profit_usd: f64::rounding_from(
-                &finalized.1 - gas_used_usd_finalized,
-                RoundingMode::Nearest
-            )
-            .0,
-            block_appearance_profit_usd: f64::rounding_from(
-                &appearance.1 - gas_used_usd_appearance,
-                RoundingMode::Nearest
-            )
-            .0,
-            block_finalized_revenue_usd: f64::rounding_from(finalized.1, RoundingMode::Nearest).0,
-            block_appearance_revenue_usd: f64::rounding_from(appearance.1, RoundingMode::Nearest).0
-        })
+        None
     }
 
-    pub fn cex_dex_profit_no_gas(
+    fn arb_gas(
         &self,
-        swaps: Vec<Vec<Actions>>,
-        metadata: Arc<Metadata>
-    ) -> HashMap<Address, HashMap<Actions, Rational>> {
-        let mut deltas = HashMap::new();
-
-        for actions in swaps.iter() {
-            for action in actions
-                .iter()
-                .map(|action| self.get_cex_dex(swaps, metadata))
-            {
-                if let Actions::Swap(swap) = action {
-                    // think it should be fine to unwrap here because it should never fail but we
-                    // never know
-
-                    let (cex_price1, cex_price2) = centralized_prices.unwrap();
-
-                    let dex_price = dex_price.unwrap();
-                    if *cex_price1 > dex_price && *cex_price2 > dex_price {
-                        //TODO: very terroristic, will finish tmrw should be
-                        // chill
-                    }
-                }
-            }
-        }
-
-        deltas
+        arbs: Vec<Option<Rational>>,
+        gas_details: &GasDetails,
+        eth_price: Rational
+    ) -> Option<Rational> {
+        Some(
+            arbs.into_iter().flatten().sum::<Rational>()
+                - Rational::from(gas_details.gas_paid()) * eth_price
+        )
+        .filter(|&p| p > Rational::ZERO)
     }
 
     pub fn get_cex_dex(
         &self,
         swap: &NormalizedSwap,
-        metadata: &Metadata,
-        dex_price: Rational
-    ) -> Option<(&Rational, &Rational)> {
-        let arb = &self.rational_dex_price(&swap, metadata).and_then(
-            |(dex_price, cex_price1, cex_price2)| {
-                self.profit_classifier(swap, dex_price, cex_price1, cex_price2)
-            }
-        );
+        metadata: &Metadata
+    ) -> (Option<Rational>, Option<Rational>) {
+        self.rational_dex_price(&swap, metadata)
+            .map(|(dex_price, cex_price1, cex_price2)| {
+                let profit1 = self.profit_classifier(swap, &dex_price, &cex_price1);
+                let profit2 = self.profit_classifier(swap, &dex_price, &cex_price2);
+
+                (
+                    Some(profit1).filter(|p| Rational::ZERO.lt(p)),
+                    Some(profit2).filter(|p| Rational::ZERO.lt(p))
+                )
+            })
+            .unwrap_or((None, None))
     }
 
     fn profit_classifier(
         &self,
         swap: &NormalizedSwap,
         dex_price: &Rational,
-        cex_price1: &Rational,
-        cex_price2: &Rational
-    ) -> Option<(Option<Rational>, Option<Rational>)> {
+        cex_price: &Rational
+    ) -> Rational {
         // Calculate the price differences between DEX and CEX
-        let delta_price1 = cex_price1 - dex_price;
-        let delta_price2 = cex_price2 - dex_price;
+        let delta_price = cex_price - dex_price;
 
         // Calculate the potential profit
-        let profit1 = delta_price1 * swap.amount_in.to_scaled_rational(18);
-        let profit2 = delta_price2 * swap.amount_in.to_scaled_rational(18);
-
-        // Check if the profit is positive
-        match (profit1 > Rational::from(0), profit2 > Rational::from(0)) {
-            (true, true) => Some((Some(profit1), Some(profit2))),
-            (true, false) => Some((Some(profit1), None)),
-            (false, true) => Some((None, Some(profit2))),
-            (false, false) => None
-        }
+        delta_price * swap.amount_in.to_scaled_rational(18)
     }
 
     pub fn rational_dex_price(
         &self,
         swap: &NormalizedSwap,
         metadata: &Metadata
-    ) -> Option<(&Rational, &Rational, &Rational)> {
+    ) -> Option<(Rational, Rational, Rational)> {
         let Some(decimals_in) = TOKEN_TO_DECIMALS.get(&swap.token_in.0) else {
             error!(missing_token=?swap.token_in, "missing token in token to decimal map");
             return None
@@ -160,14 +117,10 @@ impl CexDexInspector {
 
         let centralized_prices = metadata.token_prices.get(&swap.token_out)?;
 
-        Some((&(adjusted_out / adjusted_in), &centralized_prices.0, &centralized_prices.1))
+        Some(((adjusted_out / adjusted_in), centralized_prices.0, centralized_prices.1))
     }
 }
 
-//TODO(WILL) I think we should reorganise the way we do priority fee, becuase
-// why force a unecessary call on all inspectos when we can just TODO calc &
-// store it when we build the tree, because all inspectors need it (same as
-// bribe amount)
 #[async_trait::async_trait]
 impl Inspector for CexDexInspector {
     async fn process_tree(
@@ -180,18 +133,12 @@ impl Inspector for CexDexInspector {
 
         intersting_state
             .into_par_iter()
-            .filter_map(|(tx, swaps)| {
+            .filter_map(|(tx, nested_swaps)| {
                 let gas_details = tree.get_gas_details(tx)?;
-                self.process_swaps(
-                    tx,
-                    tree.get_priority_fee_for_transaction(tx).unwrap(),
-                    meta_data.clone(),
-                    gas_details,
-                    swaps
-                )
+                // Flatten the nested Vec<Vec<V>> into a Vec<V>
+                let swaps = nested_swaps.into_iter().flatten().collect::<Vec<_>>();
+                self.process_swap(tx, meta_data.clone(), gas_details, swaps)
             })
             .collect::<Vec<_>>()
     }
 }
-
-pub struct AtomicArb {}
