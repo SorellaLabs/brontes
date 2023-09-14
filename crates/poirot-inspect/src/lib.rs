8db4f1079b18ccf07cd5b3aa3686b503140812d9
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-09-14 00:28:05 -0700

    db insertion types for mev block & tx. Need to clean up inspectors in consequence

--- crates/poirot-inspect/src/lib.rs
+++ crates/poirot-inspect/src/lib.rs
@@ -13,28 +13,14 @@ use poirot_labeller::Metadata;
 use poirot_types::{
     normalized_actions::Actions,
     tree::{GasDetails, TimeTree},
-    ToScaledRational, TOKEN_TO_DECIMALS
+    ToScaledRational, TOKEN_TO_DECIMALS,
+    classified_mev::{ClassifiedMev, MevBlock},
 };
 use reth_primitives::{Address, H256};
 use serde::{Deserialize, Serialize};
 use tracing::error;
 
-#[derive(Debug, Serialize, Deserialize, Row)]
-pub struct ClassifiedMev {
-    // can be multiple for sandwich
-    pub tx_hash:     Vec<H256>,
-    pub mev_bot:     Address,
-    pub gas_details: Vec<GasDetails>,
-    pub tokens:      Vec<Address>,
-    pub protocols:   Vec<(String, Address)>,
 
-    pub block_number:                 u64,
-    // results
-    pub submission_profit_usd:        f64,
-    pub finalized_profit_usd:         f64,
-    pub submission_bribe_percentage_: f64,
-    pub finalized_bribe_percentage_:  f64
-}
 
 #[async_trait::async_trait]
 pub trait Inspector: Send + Sync {
