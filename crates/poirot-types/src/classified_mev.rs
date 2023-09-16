e8e685c25d19c328153575c00563303b9eb5460c
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-09-16 14:11:43 -0700

    working on fixing types

--- crates/poirot-types/src/classified_mev.rs
+++ crates/poirot-types/src/classified_mev.rs
@@ -7,35 +7,71 @@ use crate::tree::GasDetails;
 
 #[derive(Debug, Serialize, Deserialize, Row)]
 pub struct MevBlock {
-    block_hash:            H256,
-    block_number:          u64,
-    builder_address:       Address,
-    builder_name:          String,
-    relays:                Vec<String>,
-    proposer:              Address,
-    proposer_rewards:      u64,
-    mev_count:             u64,
-    cumulative_gas_used:   u64,
-    cumulative_gas_paid:   u64,
-    bribe_amount:          u64,
-    submission_eth_price:  u64,
-    finalized_eth_price:   u64,
+    block_hash: H256,
+    block_number: u64,
+    builder_address: Address,
+    proposer: Address,
+    proposer_rewards: u64,
+    mev_count: u64,
+    cumulative_gas_used: u64,
+    cumulative_gas_paid: u64,
+    bribe_amount: u64,
+    submission_eth_price: u64,
+    finalized_eth_price: u64,
     submission_profit_usd: u64,
-    finalized_profit_usd:  u64
+    finalized_profit_usd: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize, Row)]
 pub struct ClassifiedMev {
     // can be multiple for sandwich
-    pub tx_hash:               Vec<H256>,
-    pub block_number:          u64,
-    pub mev_bot:               Address,
-    pub gas_details:           Vec<GasDetails>,
-    pub tokens:                Vec<Address>,
-    pub protocols:             Vec<(String, Address)>,
+    pub block_number: u64,
+    pub tx_hash: H256,
+    pub mev_type: String,
+}
+
+#[derive(Debug)]
+
+pub enum MevType {
+    Sandwich,
+    AtomicBackrun,
+    CexDex,
+    DexDex,
+    DexCex,
+    Dex,
+    Cex,
+    Unknown,
+}
+
+#[derive(Debug, Serialize, Row)]
+pub struct Sandwich<A>
+where
+    A: Row + Serialize,
+{
+    pub front_run: (H256, GasDetails),
+    pub victim: Vec<H256>,
+    pub back_run: H256,
+    pub mev_bot: Address,
+    pub gas_details: Vec<GasDetails>,
+    pub tokens: Vec<Address>,
+    pub contracts: Vec<Address>,
+    pub action: Vec<A>,
     // results
     pub submission_profit_usd: f64,
-    pub finalized_profit_usd:  f64,
-    pub submission_bribe_uds:  f64,
-    pub finalized_bribe_usd:   f64
+    pub finalized_profit_usd: f64,
+    pub submission_bribe_uds: f64,
+    pub finalized_bribe_usd: f64,
+}
+
+#[derive(Debug, Serialize)]
+pub struct CexDex {
+    pub mev_bot: Address,
+    pub gas_details: Vec<GasDetails>,
+    pub tokens: Vec<Address>,
+    pub contracts: Vec<Address>,
+    // results
+    pub submission_profit_usd: f64,
+    pub finalized_profit_usd: f64,
+    pub submission_bribe_uds: f64,
+    pub finalized_bribe_usd: f64,
 }
