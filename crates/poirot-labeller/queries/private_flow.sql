7a82d39d9d3193da7622f6101369b6702a85246e
lagrange.eth <lagrange@sorellacapital.com>
2023-09-16 15:37:25 -0700

    fixed queries
    Former-commit-id: 169c8cb2a501353c632c0fc2293f4d261cd21853

--- crates/poirot-labeller/queries/private_flow.sql
+++ crates/poirot-labeller/queries/private_flow.sql
@@ -2,10 +2,10 @@ SELECT tx_hash
 FROM
 (
     SELECT arrayJoin(transaction_hashes) AS tx_hash
-    FROM blocks
+    FROM ethereum.blocks
     WHERE (block_number = ?) AND (block_hash = ?) AND (valid = 1)
 ) AS subquery
 WHERE tx_hash NOT IN (
     SELECT tx_hash
-    FROM unique_mempool
+    FROM ethereum.unique_mempool
 )
--- crates/poirot-labeller/queries/relays_p2p_time.sql
+++
@@ -1,5 +0,0 @@
-SELECT max(relays.timestamp) as relay_timestamp, max(cb.timestamp) as p2p_timestamp
-FROM relays 
-INNER JOIN chainbound_block_observations_remote as cb
-ON relays.block_number = cb.block_number
-WHERE  block_number = ? AND block_hash = ?
