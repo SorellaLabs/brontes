beb75bbd4e3dc4b2e02973a04faa6d717113367e
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-09-16 15:55:58 -0700

    fmt

--- crates/poirot-labeller/src/database/types.rs
+++ crates/poirot-labeller/src/database/types.rs
@@ -4,12 +4,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Row, Serialize, Deserialize)]
 pub struct DBP2PRelayTimes {
     pub relay_timestamp: u64,
-    pub p2p_timestamp: u64,
+    pub p2p_timestamp:   u64
 }
 
 #[derive(Debug, Row, Serialize, Deserialize)]
 pub struct DBTardisTrades {
     pub address: String,
-    pub price1: f64,
-    pub price0: f64,
+    pub price1:  f64,
+    pub price0:  f64
 }
