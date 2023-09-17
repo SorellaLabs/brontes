12448ac38be2bbc1f00e9cfb34927c71a18551ba
Will Smith <williamqsmith11@gmail.com>
2023-09-16 18:29:03 -0700

    wip

--- crates/poirot-labeller/src/database/const_sql.rs
+++ crates/poirot-labeller/src/database/const_sql.rs
@@ -1,21 +1,3 @@
-pub const PRIVATE_FLOW: &str = r#"SELECT tx_hash
-FROM
-(
-    SELECT arrayJoin(transaction_hashes) AS tx_hash
-    FROM blocks
-    WHERE (block_number = ?) AND (block_hash = ?) AND (valid = 1)
-) AS subquery
-WHERE tx_hash NOT IN (
-    SELECT tx_hash
-    FROM unique_mempool
-)"#;
-
-pub const RELAY_P2P_TIMES: &str = r#"SELECT max(relays.timestamp) as relay_timestamp, max(cb.timestamp) as p2p_timestamp
-FROM relays 
-INNER JOIN chainbound_block_observations_remote as cb
-ON relays.block_number = cb.block_number
-WHERE  block_number = ? AND block_hash = ?"#;
-
 pub const PRICES: &str = r#"SELECT 
     any(bt.timestamp) as timestamp, 
     substring(bt.symbol, 1, length(bt.symbol) - 4) as symbol, 
@@ -31,3 +13,22 @@ WHERE
     AND substring(bt.symbol, -4) = 'USDT'
 GROUP BY 
     bt.symbol;"#;
+
+pub const PRIVATE_FLOW: &str = r#"SELECT tx_hash
+FROM
+(
+    SELECT arrayJoin(transaction_hashes) AS tx_hash
+    FROM ethereum.blocks
+    WHERE (block_number = ?) AND (block_hash = ?) AND (valid = 1)
+) AS subquery
+WHERE tx_hash NOT IN (
+    SELECT tx_hash
+    FROM ethereum.unique_mempool
+)"#;
+
+pub const RELAY_P2P_TIMES: &str = r#"SELECT max(relays.timestamp) as relay_timestamp, max(cb.timestamp) as p2p_timestamp
+FROM ethereum.relays 
+INNER JOIN ethereum.chainbound_block_observations_remote as cb
+ON ethereum.relays.block_number = cb.block_number
+WHERE  block_number = ? AND block_hash = ?"#;
+
