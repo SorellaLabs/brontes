f276f4a16c9b2879e521c43bb11e089429c39760
Will Smith <williamqsmith11@gmail.com>
2023-09-17 16:52:47 -0700

    finalize shit
    Former-commit-id: 6ece8cbfa528391a9374c541fca97f7015ace95a

--- crates/poirot-types/src/classified_mev.rs
+++ crates/poirot-types/src/classified_mev.rs
@@ -1,3 +1,5 @@
+use std::any::Any;
+
 use clickhouse::Row;
 use reth_primitives::{Address, H256};
 use serde::{Deserialize, Serialize};
@@ -5,7 +7,7 @@ use strum::EnumIter;
 
 use crate::{
     normalized_actions::{NormalizedBurn, NormalizedLiquidation, NormalizedMint, NormalizedSwap},
-    tree::GasDetails,
+    tree::GasDetails
 };
 
 #[derive(Debug, Serialize, Deserialize, Row)]
@@ -33,21 +35,21 @@ pub struct MevBlock {
     // gas used * (effective gas price - base fee) for all Classified MEV txs
     /// Mev profit
     pub cumulative_mev_submission_profit_usd: u64,
-    pub cumulative_mev_finalized_profit_usd: u64,
+    pub cumulative_mev_finalized_profit_usd: u64
 }
 
 #[derive(Debug, Serialize, Deserialize, Row)]
 pub struct ClassifiedMev {
     // can be multiple for sandwich
-    pub block_number: u64,
-    pub tx_hash: H256,
-    pub mev_executor: Address,
-    pub mev_collector: Address,
-    pub mev_type: MevType,
+    pub block_number:          u64,
+    pub tx_hash:               H256,
+    pub mev_executor:          Address,
+    pub mev_collector:         Address,
+    pub mev_type:              MevType,
     pub submission_profit_usd: f64,
-    pub finalized_profit_usd: f64,
-    pub submission_bribe_usd: f64,
-    pub finalized_bribe_usd: f64,
+    pub finalized_profit_usd:  f64,
+    pub submission_bribe_usd:  f64,
+    pub finalized_bribe_usd:   f64
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, EnumIter)]
@@ -58,42 +60,54 @@ pub enum MevType {
     Jit,
     CexDex,
     Liquidation,
-    Unknown,
+    Unknown
 }
 
-impl Row for MevType {
-    const COLUMN_NAMES: &'static [&'static str] = &["mev_type"];
+pub enum MevResult {
+    Sandwich(Sandwich),
+    Jit(JitLiquidity),
+    JitSandwich(JitLiquiditySandwich),
+    CexDex(CexDex),
+    Liquidation(Liquidation)
 }
 
-pub trait SpecificMev: Serialize + Row {
-    const MEV_TYPE: MevType;
-    type ComposableResult: SpecificMev;
-    type ComposableType: SpecificMev;
-
+/// Because of annoying trait requirements. we do some degenerate shit here.
+pub trait SpecificMev {
+    fn mev_type(&self) -> MevType;
     fn bribe(&self) -> u64;
     fn priority_fee_paid(&self) -> u64;
     fn mev_transaction_hashes(&self) -> Vec<H256>;
-    fn compose(&mut self, other: Box<Self::ComposableType>) -> Option<Box<Self::ComposableResult>>;
 }
 
 #[derive(Debug, Serialize, Row)]
 pub struct Sandwich {
-    pub front_run: H256,
+    pub front_run:             H256,
     pub front_run_gas_details: GasDetails,
-    pub front_run_swaps: Vec<NormalizedSwap>,
-    pub victim: Vec<H256>,
-    pub victim_gas_details: Vec<GasDetails>,
-    pub victim_swaps: Vec<Vec<NormalizedSwap>>,
-    pub back_run: H256,
-    pub back_run_gas_details: GasDetails,
-    pub back_run_swaps: Vec<NormalizedSwap>,
+    pub front_run_swaps:       Vec<NormalizedSwap>,
+    pub victim:                Vec<H256>,
+    pub victim_gas_details:    Vec<GasDetails>,
+    pub victim_swaps:          Vec<Vec<NormalizedSwap>>,
+    pub back_run:              H256,
+    pub back_run_gas_details:  GasDetails,
+    pub back_run_swaps:        Vec<NormalizedSwap>
+}
+
+pub fn compose_sandwich_jit(
+    sandwich: Box<dyn Any + 'static>,
+    jit: Box<dyn Any + 'static>,
+    sandwich_classified: ClassifiedMev,
+    jit_classified: ClassifiedMev
+) -> (ClassifiedMev, Box<dyn SpecificMev>) {
+    let sandwich: Sandwich = *sandwich.downcast().unwrap();
+    let jit: JitLiquidity = *jit.downcast().unwrap();
+
+    todo!()
 }
 
 impl SpecificMev for Sandwich {
-    type ComposableResult = JitLiquiditySandwich;
-    type ComposableType = JitLiquidity;
-
-    const MEV_TYPE: MevType = MevType::Sandwich;
+    fn mev_type(&self) -> MevType {
+        MevType::Sandwich
+    }
 
     fn bribe(&self) -> u64 {
         todo!()
@@ -104,34 +118,29 @@ impl SpecificMev for Sandwich {
     }
 
     fn mev_transaction_hashes(&self) -> Vec<H256> {
-        todo!()
-    }
-
-    fn compose(&mut self, other: Box<Self::ComposableType>) -> Option<Box<Self::ComposableResult>> {
         todo!()
     }
 }
 
 #[derive(Debug, Serialize, Row)]
 pub struct JitLiquiditySandwich {
-    pub front_run: H256,
+    pub front_run:             H256,
     pub front_run_gas_details: GasDetails,
-    pub front_run_swaps: Vec<NormalizedSwap>,
-    pub front_run_mint: Vec<NormalizedMint>,
-    pub victim: Vec<H256>,
-    pub victim_gas_details: Vec<GasDetails>,
-    pub victim_swaps: Vec<Vec<NormalizedSwap>>,
-    pub back_run: H256,
-    pub back_run_gas_details: GasDetails,
-    pub back_run_burn: Vec<NormalizedBurn>,
-    pub back_run_swaps: Vec<NormalizedSwap>,
+    pub front_run_swaps:       Vec<NormalizedSwap>,
+    pub front_run_mint:        Vec<NormalizedMint>,
+    pub victim:                Vec<H256>,
+    pub victim_gas_details:    Vec<GasDetails>,
+    pub victim_swaps:          Vec<Vec<NormalizedSwap>>,
+    pub back_run:              H256,
+    pub back_run_gas_details:  GasDetails,
+    pub back_run_burn:         Vec<NormalizedBurn>,
+    pub back_run_swaps:        Vec<NormalizedSwap>
 }
 
 impl SpecificMev for JitLiquiditySandwich {
-    type ComposableResult = Self;
-    type ComposableType = Self;
-
-    const MEV_TYPE: MevType = MevType::JitSandwich;
+    fn mev_type(&self) -> MevType {
+        MevType::JitSandwich
+    }
 
     fn bribe(&self) -> u64 {
         todo!()
@@ -143,27 +152,22 @@ impl SpecificMev for JitLiquiditySandwich {
 
     fn mev_transaction_hashes(&self) -> Vec<H256> {
         todo!()
-    }
-
-    fn compose(&mut self, other: Box<Self::ComposableType>) -> Option<Box<Self::ComposableResult>> {
-        None
     }
 }
 
 #[derive(Debug, Serialize, Row)]
 pub struct CexDex {
-    pub tx_hash: H256,
-    pub swaps: Vec<NormalizedSwap>,
-    pub cex_prices: Vec<f64>,
-    pub dex_prices: Vec<f64>,
-    pub gas_details: Vec<GasDetails>,
+    pub tx_hash:     H256,
+    pub swaps:       Vec<NormalizedSwap>,
+    pub cex_prices:  Vec<f64>,
+    pub dex_prices:  Vec<f64>,
+    pub gas_details: Vec<GasDetails>
 }
 
 impl SpecificMev for CexDex {
-    type ComposableResult = Self;
-    type ComposableType = Self;
-
-    const MEV_TYPE: MevType = MevType::CexDex;
+    fn mev_type(&self) -> MevType {
+        MevType::CexDex
+    }
 
     fn bribe(&self) -> u64 {
         todo!()
@@ -175,27 +179,22 @@ impl SpecificMev for CexDex {
 
     fn mev_transaction_hashes(&self) -> Vec<H256> {
         todo!()
-    }
-
-    fn compose(&mut self, other: Box<Self::ComposableType>) -> Option<Box<Self::ComposableResult>> {
-        None
     }
 }
 
 #[derive(Debug, Serialize, Row)]
 pub struct Liquidation {
-    pub trigger: H256,
-    pub liquidation_tx_hash: H256,
+    pub trigger:                 H256,
+    pub liquidation_tx_hash:     H256,
     pub liquidation_gas_details: GasDetails,
-    pub liquidation_swaps: Vec<NormalizedSwap>,
-    pub liquidation: Vec<NormalizedLiquidation>,
+    pub liquidation_swaps:       Vec<NormalizedSwap>,
+    pub liquidation:             Vec<NormalizedLiquidation>
 }
 
 impl SpecificMev for Liquidation {
-    type ComposableResult = Self;
-    type ComposableType = Self;
-
-    const MEV_TYPE: MevType = MevType::Liquidation;
+    fn mev_type(&self) -> MevType {
+        MevType::Liquidation
+    }
 
     fn bribe(&self) -> u64 {
         todo!()
@@ -207,31 +206,26 @@ impl SpecificMev for Liquidation {
 
     fn mev_transaction_hashes(&self) -> Vec<H256> {
         todo!()
-    }
-
-    fn compose(&mut self, other: Box<Self::ComposableType>) -> Option<Box<Self::ComposableResult>> {
-        None
     }
 }
 
 #[derive(Debug, Serialize, Row)]
 pub struct JitLiquidity {
-    pub mint_tx_hash: H256,
+    pub mint_tx_hash:     H256,
     pub mint_gas_details: GasDetails,
-    pub jit_mints: Vec<NormalizedMint>,
-    pub swap_tx_hash: H256,
+    pub jit_mints:        Vec<NormalizedMint>,
+    pub swap_tx_hash:     H256,
     pub swap_gas_details: GasDetails,
-    pub swaps: Vec<NormalizedSwap>,
-    pub burn_tx_hash: H256,
+    pub swaps:            Vec<NormalizedSwap>,
+    pub burn_tx_hash:     H256,
     pub burn_gas_details: GasDetails,
-    pub jit_burns: Vec<NormalizedBurn>,
+    pub jit_burns:        Vec<NormalizedBurn>
 }
 
 impl SpecificMev for JitLiquidity {
-    type ComposableResult = JitLiquiditySandwich;
-    type ComposableType = Sandwich;
-
-    const MEV_TYPE: MevType = MevType::Jit;
+    fn mev_type(&self) -> MevType {
+        MevType::Jit
+    }
 
     fn bribe(&self) -> u64 {
         todo!()
@@ -242,10 +236,6 @@ impl SpecificMev for JitLiquidity {
     }
 
     fn mev_transaction_hashes(&self) -> Vec<H256> {
-        todo!()
-    }
-
-    fn compose(&mut self, other: Box<Self::ComposableType>) -> Option<Box<Self::ComposableResult>> {
         todo!()
     }
 }
