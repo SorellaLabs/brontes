27e60e702782f12825dcfe6249c65dab5ad31712
Will Smith <williamqsmith11@gmail.com>
2023-09-19 14:54:14 -0700

    fix macro:

--- crates/poirot-inspect/src/daddy_inspector.rs
+++ crates/poirot-inspect/src/daddy_inspector.rs
@@ -25,28 +25,33 @@ use reth_primitives::Address;
 
 use crate::Inspector;
 
-pub struct BlockPreprocessing {
-    meta_data:           Arc<Metadata>,
-    cumulative_gas_used: u64,
-    cumulative_gas_paid: u64,
-    builder_address:     Address
-}
+type ComposeFunction = Option<
+    Box<
+        dyn Fn(
+                Box<dyn Any + 'static>,
+                Box<dyn Any + 'static>,
+                ClassifiedMev,
+                ClassifiedMev
+            ) -> (ClassifiedMev, Box<dyn SpecificMev>)
+            + Send
+            + Sync
+    >
+>;
 
 /// we use this to define a filter that we can iterate over such that
 /// everything is ordered properly and we have already composed lower level
 /// actions that could effect the higher level composing.
 macro_rules! mev_composability {
 
-    ($($mev_type:ident => $($deps:ident),+ => $replace:expr;)+) => {
+    ($($mev_type:ident => $($deps:ident),+;)+) => {
         lazy_static! {
         static ref MEV_FILTER: &'static [(
                 MevType,
-                Option<Box<dyn Fn(Box<dyn Any + 'static>, Box<dyn Any + 'static>, ClassifiedMev, ClassifiedMev) ->
-                (ClassifiedMev, Box<dyn SpecificMev>) + Send + Sync>>,
-                &'static[MevType])] = {
-            &[
-                $((MevType::$mev_type, $replace, [$(MevType::$deps,)+].as_slice()),)+
-            ]
+                ComposeFunction,
+                Vec<MevType>)] = {
+            &*Box::leak(Box::new([
+                $((MevType::$mev_type, get_compose_fn(MevType::$mev_type), [$(MevType::$deps,)+].to_vec()),)+
+            ]))
         };
     }
     };
@@ -54,16 +59,32 @@ macro_rules! mev_composability {
 
 mev_composability!(
     // reduce first
-    Sandwich => Backrun, Jit => None;
+    Sandwich => Backrun, Jit;
     // try compose
-    JitSandwich => Sandwich, Jit => Some(Box::new(compose_sandwich_jit));
+    JitSandwich => Sandwich, Jit;
 );
+
+/// the compose function is used in order to be able to properly be able to cast
+/// in the lazy static
+fn get_compose_fn(mev_type: MevType) -> ComposeFunction {
+    match mev_type {
+        MevType::JitSandwich => Some(Box::new(compose_sandwich_jit)),
+        _ => None
+    }
+}
+
+pub struct BlockPreprocessing {
+    meta_data:           Arc<Metadata>,
+    cumulative_gas_used: u64,
+    cumulative_gas_paid: u64,
+    builder_address:     Address
+}
 
 type InspectorFut<'a> =
     JoinAll<Pin<Box<dyn Future<Output = Vec<(ClassifiedMev, Box<dyn SpecificMev>)>> + Send + 'a>>>;
 
 /// the results downcast using any in order to be able to serialize and
-/// impliment row trait due to the abosulte autism that the db library
+/// impliment row trait due to the abosulte autism that the db library   
 /// requirements
 pub type DaddyInspectorResults = (MevBlock, HashMap<MevType, Vec<(ClassifiedMev, MevResult)>>);
 
@@ -75,9 +96,7 @@ pub struct DaddyInspector<'a, const N: usize> {
 
 impl<'a, const N: usize> DaddyInspector<'a, N> {
     pub fn new(baby_inspectors: &'a [&'a Box<dyn Inspector>; N]) -> Self {
-        let a = [1,2].as_slice();
         Self { baby_inspectors, inspectors_execution: None, pre_processing: None }
-
     }
 
     pub fn is_processing(&self) -> bool {
@@ -118,11 +137,11 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
 
     fn build_mev_header(
         &mut self,
-        baby_data: Arc<impl Iterator<Item = (ClassifiedMev, Box<dyn SpecificMev>)>>
+        baby_data: &Vec<(ClassifiedMev, Box<dyn SpecificMev>)>
     ) -> MevBlock {
         let pre_processing = self.pre_processing.take().unwrap();
         let cum_mev_priority_fee_paid = baby_data
-            .clone()
+            .iter()
             .map(|(_, mev)| mev.priority_fee_paid())
             .sum::<u64>();
 
@@ -133,30 +152,30 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
         MevBlock {
             block_hash: pre_processing.meta_data.block_hash.into(),
             block_number: pre_processing.meta_data.block_num,
-            mev_count: baby_data.count() as u64,
+            mev_count: baby_data.len() as u64,
             submission_eth_price: f64::rounding_from(
-                pre_processing.meta_data.eth_prices.0,
+                &pre_processing.meta_data.eth_prices.0,
                 RoundingMode::Nearest
             )
             .0,
             finalized_eth_price: f64::rounding_from(
-                pre_processing.meta_data.eth_prices.1,
+                &pre_processing.meta_data.eth_prices.1,
                 RoundingMode::Nearest
             )
             .0,
             cumulative_gas_used: pre_processing.cumulative_gas_used,
             cumulative_gas_paid: pre_processing.cumulative_gas_paid,
-            total_bribe: baby_data.map(|(_, mev)| mev.bribe()).sum::<u64>(),
+            total_bribe: baby_data.iter().map(|(_, mev)| mev.bribe()).sum::<u64>(),
             cumulative_mev_priority_fee_paid: cum_mev_priority_fee_paid,
             builder_address: pre_processing.builder_address,
             builder_eth_profit,
             builder_submission_profit_usd: f64::rounding_from(
-                Rational::from(builder_eth_profit) * pre_processing.meta_data.eth_prices.0,
+                Rational::from(builder_eth_profit) * &pre_processing.meta_data.eth_prices.0,
                 RoundingMode::Nearest
             )
             .0,
             builder_finalized_profit_usd: f64::rounding_from(
-                Rational::from(builder_eth_profit) * pre_processing.meta_data.eth_prices.1,
+                Rational::from(builder_eth_profit) * &pre_processing.meta_data.eth_prices.1,
                 RoundingMode::Nearest
             )
             .0,
@@ -164,23 +183,23 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
             proposer_mev_reward: pre_processing.meta_data.proposer_mev_reward,
             proposer_submission_mev_reward_usd: f64::rounding_from(
                 Rational::from(pre_processing.meta_data.proposer_mev_reward)
-                    * pre_processing.meta_data.eth_prices.0,
+                    * &pre_processing.meta_data.eth_prices.0,
                 RoundingMode::Nearest
             )
             .0,
             proposer_finalized_mev_reward_usd: f64::rounding_from(
                 Rational::from(pre_processing.meta_data.proposer_mev_reward)
-                    * pre_processing.meta_data.eth_prices.1,
+                    * &pre_processing.meta_data.eth_prices.1,
                 RoundingMode::Nearest
             )
             .0,
             cumulative_mev_submission_profit_usd: f64::rounding_from(
-                Rational::from(cum_mev_priority_fee_paid) * pre_processing.meta_data.eth_prices.0,
+                Rational::from(cum_mev_priority_fee_paid) * &pre_processing.meta_data.eth_prices.0,
                 RoundingMode::Nearest
             )
             .0,
             cumulative_mev_finalized_profit_usd: f64::rounding_from(
-                Rational::from(cum_mev_priority_fee_paid) * pre_processing.meta_data.eth_prices.1,
+                Rational::from(cum_mev_priority_fee_paid) * &pre_processing.meta_data.eth_prices.1,
                 RoundingMode::Nearest
             )
             .0
@@ -189,11 +208,12 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
 
     fn on_baby_resolution(
         &mut self,
-        baby_data: impl Iterator<Item = (ClassifiedMev, Box<dyn SpecificMev>)>
+        baby_data: Vec<(ClassifiedMev, Box<dyn SpecificMev>)>
     ) -> Poll<Option<DaddyInspectorResults>> {
-        let header = self.build_mev_header(Arc::new(baby_data));
+        let header = self.build_mev_header(&baby_data);
 
         let mut sorted_mev = baby_data
+            .into_iter()
             .map(|(classified_mev, specific)| (classified_mev.mev_type, (classified_mev, specific)))
             .fold(
                 HashMap::default(),
@@ -227,26 +247,24 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
                 .map(|(k, v)| {
                     let new_v = v
                         .into_iter()
-                        .map(|(class, mut other)| {
-                            let mut a_ref = other.as_mut();
-                            let any_cast = &mut a_ref as &mut dyn Any;
+                        .map(|(class, other)| {
+                            let any_cast = other.into_any();
 
                             let res = match k {
-                                MevType::Jit => {
-                                    MevResult::Jit(any_cast.downcast_mut().cloned().unwrap())
-                                }
-                                MevType::CexDex => {
-                                    MevResult::CexDex(any_cast.downcast_mut().cloned().unwrap())
-                                }
+                                MevType::Jit => MevResult::Jit(*any_cast.downcast().unwrap()),
+                                MevType::CexDex => MevResult::CexDex(*any_cast.downcast().unwrap()),
                                 MevType::Sandwich => {
-                                    MevResult::Sandwich(any_cast.downcast_mut().cloned().unwrap())
+                                    MevResult::Sandwich(*any_cast.downcast().unwrap())
                                 }
-                                MevType::JitSandwich => MevResult::JitSandwich(
-                                    any_cast.downcast_mut().cloned().unwrap()
-                                ),
-                                MevType::Liquidation => MevResult::Liquidation(
-                                    any_cast.downcast_mut().cloned().unwrap()
-                                ),
+                                MevType::JitSandwich => {
+                                    MevResult::JitSandwich(*any_cast.downcast().unwrap())
+                                }
+                                MevType::Liquidation => {
+                                    MevResult::Liquidation(*any_cast.downcast().unwrap())
+                                }
+                                MevType::Backrun => {
+                                    MevResult::Backrun(*any_cast.downcast().unwrap())
+                                }
                                 _ => todo!("add other downcasts for different types")
                             };
                             (class, res)
@@ -274,14 +292,14 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
 
         let mut remove_count: HashMap<MevType, usize> = HashMap::new();
 
-        let mut flattend_indexes = head_mev
+        let flattend_indexes = head_mev
             .iter()
-            .flat_map(|(classified, specific)| {
+            .flat_map(|(_, specific)| {
                 let hashes = specific.mev_transaction_hashes();
                 let mut remove_data: Vec<(MevType, usize)> = Vec::new();
                 for dep in deps {
                     let Some(dep_mev) = sorted_mev.get(dep) else { continue };
-                    for (i, (class, specific)) in dep_mev.iter().enumerate() {
+                    for (i, (_, specific)) in dep_mev.iter().enumerate() {
                         let dep_hashes = specific.mev_transaction_hashes();
                         // verify both match
                         if dep_hashes == hashes {
@@ -318,11 +336,13 @@ impl<'a, const N: usize> DaddyInspector<'a, N> {
         composable_types: &[MevType],
         compose: &Box<
             dyn Fn(
-                Box<dyn Any>,
-                Box<dyn Any>,
-                ClassifiedMev,
-                ClassifiedMev
-            ) -> (ClassifiedMev, Box<dyn SpecificMev>) + Send +Sync
+                    Box<dyn Any>,
+                    Box<dyn Any>,
+                    ClassifiedMev,
+                    ClassifiedMev
+                ) -> (ClassifiedMev, Box<dyn SpecificMev>)
+                + Send
+                + Sync
         >,
         sorted_mev: &mut HashMap<MevType, Vec<(ClassifiedMev, Box<dyn SpecificMev>)>>
     ) {
@@ -377,7 +397,7 @@ impl<const N: usize> Stream for DaddyInspector<'_, N> {
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         if let Some(mut calculations) = self.inspectors_execution.take() {
             return match calculations.poll_unpin(cx) {
-                Poll::Ready(data) => self.on_baby_resolution(data.into_iter().flatten()),
+                Poll::Ready(data) => self.on_baby_resolution(data.into_iter().flatten().collect()),
                 Poll::Pending => {
                     self.inspectors_execution = Some(calculations);
                     Poll::Pending
