02155fb485107a057a18f72c4b1b7e0a1fb77f35
lagrange.eth <lagrange@sorellacapital.com>
2023-09-20 20:02:08 -0500

    fixed price queries
    Former-commit-id: 516942d3996c8258058b7d87658d8c2c38a3073e

--- crates/poirot-database/queries/prices.sql
+++ crates/poirot-database/queries/prices.sql
@@ -1,15 +1,40 @@
+
 SELECT 
-    any(bt.timestamp) as timestamp, 
-    substring(bt.symbol, 1, length(bt.symbol) - 4) as symbol, 
-    avg(bt.price) as price
-FROM 
-    cex.binance_trades as bt
-WHERE 
-    (
-        (bt.timestamp < ? + 10000 AND bt.timestamp > ? - 10000) 
-        OR 
-        (bt.timestamp < ? + 10000 AND bt.timestamp > ? - 10000)
-    )
-    AND substring(bt.symbol, -4) = 'USDT'
-GROUP BY 
-    bt.symbol;
+    sub1.address AS address,
+    sub1.price AS relay_price,
+    sub2.price AS p2p_price
+FROM
+(
+    SELECT 
+        any(bt.timestamp) as timestamp, 
+        et.address as address, 
+        avg(bt.price) as price
+    FROM 
+        cex.binance_trades as bt
+    INNER JOIN ethereum.tokens AS et 
+    ON et.symbol = substring(bt.symbol, 1, length(bt.symbol) - 4)
+    WHERE 
+        (
+            (bt.timestamp < ? + 10000 AND bt.timestamp > ? - 10000)
+        )
+        AND substring(bt.symbol, -4) = 'USDT'
+    GROUP BY 
+        address
+) as sub1 INNER JOIN (
+    SELECT 
+        any(bt.timestamp) as timestamp, 
+        et.address as address, 
+        avg(bt.price) as price
+    FROM 
+        cex.binance_trades as bt
+    INNER JOIN ethereum.tokens AS et 
+    ON et.symbol = substring(bt.symbol, 1, length(bt.symbol) - 4)
+    WHERE 
+        (
+            (bt.timestamp < ? + 10000 AND bt.timestamp > ? - 10000) 
+        )
+        AND substring(bt.symbol, -4) = 'USDT'
+    GROUP BY 
+        address
+) AS sub2 ON sub2.address = sub1.address
+
