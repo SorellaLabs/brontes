75a4558714c08a8ee83997eb7e17fdb81a5d7313
Will Smith <williamqsmith11@gmail.com>
2023-10-02 19:21:37 -0700

    wip:

--- crates/brontes-classifier/src/impls/mod.rs
+++ crates/brontes-classifier/src/impls/mod.rs
@@ -4,22 +4,20 @@ pub mod uniswap_v3;
 #[macro_export]
 macro_rules! enum_unwrap {
     ($data:ident, $exchange:ident, $return:ty) => {{
-        use paste::paste;
-
         unsafe {
             let a = &$data as *const _ as *mut u8;
             let ptr = a.add(2);
-            let inner = ptr.cast() as *mut paste!([<$exchange Calls>]);
+            let inner = ptr.cast() as *mut $exchange;
             let ptr = inner.add(1);
 
             &*(ptr.cast() as *mut $return)
-        }}
-    };
+        }
+    }};
 }
 
 #[macro_export]
 /// all variants for swap implementation
-macro_rules! action_impl{
+macro_rules! action_impl_all {
     // everything
     (
      $exchange:ident,
@@ -27,12 +25,12 @@ macro_rules! action_impl{
      $call_type:ident,
      $exchange_mod:ident,
      $event_struct:ident,
-     $return_calldata:expr,
      $fn:expr
      ) => {
-        pub struct paste![<$exchange $impl_type>];
+        #[derive(Debug, Default)]
+        pub struct $exchange;
 
-        impl IntoAction for paste![<$exchange $impl_type>] {
+        impl IntoAction for $exchange {
             fn get_signature(&self) -> [u8; 4] {
                 $call_type::SELECTOR
             }
@@ -48,35 +46,35 @@ macro_rules! action_impl{
                 let call_data = enum_unwrap!(data, $exchange_mod, $call_type);
                 let log_data = logs
                     .into_iter()
-                    .filter_map(|log|$event_struct::decode_log(log.
-                                                                topics
-                                                                .iter()
-                                                                .map(|h| h.0)
-                                                                , &log.data,
-                                                                true).ok())
-                    .collect::<Vec<_>>().first().unwrap();
+                    .filter_map(|log| {
+                        $event_struct::decode_log(log.topics.iter().map(|h| h.0), &log.data, true)
+                            .ok()
+                    })
+                    .collect::<Vec<_>>()
+                    .first()
+                    .unwrap();
 
-                if $return_calldata {
-                    let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
-                    Actions::$impl_type($fn(index, address, call_data, return_data, log_data))
-                } else {
-                    Actions::$impl_type($fn(index, address, call_data, log_data))
-                }
+                let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                Actions::$impl_type($fn(index, address, call_data, return_data, log_data))
             }
         }
     };
+}
+
+#[macro_export]
+macro_rules! action_impl_calldata {
     // calldata
     (
         $exchange:ident,
         $impl_type:ident,
         $call_type:ident,
         $exchange_mod:ident,
-        $return_calldata:expr,
         $fn:expr
         ) => {
-        pub struct paste![<$exchange $impl_type>];
+        #[derive(Debug, Default)]
+        pub struct $exchange;
 
-        impl IntoAction for paste![<$exchange $impl_type>] {
+        impl IntoAction for $exchange {
             fn get_signature(&self) -> [u8; 4] {
                 $call_type::SELECTOR
             }
@@ -90,28 +88,27 @@ macro_rules! action_impl{
                 logs: &Vec<Log>,
             ) -> Actions {
                 let call_data = enum_unwrap!(data, $exchange_mod, $call_type);
-                if $return_calldata  {
-                    let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
-                    Actions::$impl_type($fn(index, address, call_data, return_data))
-                } else {
-                    Actions::$impl_types($fn(index, address, call_data))
-                }
+                let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                Actions::$impl_type($fn(index, address, call_data, return_data))
             }
         }
     };
+}
 
-    // return data
+#[macro_export]
+macro_rules! action_impl_log {
+    // log
     (
         $exchange:ident,
         $impl_type:ident,
         $call_type:ident,
         $event_struct:ident,
-        $return_calldata:expr,
         $fn:expr
         ) => {
-        pub struct paste![<$exchange $impl_type>];
+        #[derive(Debug, Default)]
+        pub struct $exchange;
 
-        impl IntoAction for paste![<$exchange $impl_type>] {
+        impl IntoAction for $exchange {
             fn get_signature(&self) -> [u8; 4] {
                 $call_type::SELECTOR
             }
@@ -124,22 +121,136 @@ macro_rules! action_impl{
                 address: Address,
                 logs: &Vec<Log>,
             ) -> Actions {
-                let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
                 let log_data = logs
                     .into_iter()
-                    .filter_map(|log|$event_struct::decode_log(log.
-                                                                topics
-                                                                .iter()
-                                                                .map(|h| h.0),
-                                                                &log.data,
-                                                                true).ok())
-                    .collect::<Vec<_>>().first().unwrap();
-                if $return_calldata  {
-                    let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
-                    Actions::$impl_type($fn(index, address, return_data, log_data))
-                } else {
-                    Actions::$impl_type($fn(index, address, return_data))
-                }
+                    .filter_map(|log| {
+                        $event_struct::decode_log(log.topics.iter().map(|h| h.0), &log.data, true)
+                            .ok()
+                    })
+                    .collect::<Vec<_>>()
+                    .first()
+                    .unwrap();
+                let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                Actions::$impl_type($fn(index, address, log_data, return_data))
+            }
+        }
+    };
+}
+
+#[macro_export]
+/// all variants for swap implementation
+macro_rules! action_impl_all_no_return {
+    // everything
+    (
+     $exchange:ident,
+     $impl_type:ident,
+     $call_type:ident,
+     $exchange_mod:ident,
+     $event_struct:ident,
+     $fn:expr
+     ) => {
+        #[derive(Debug, Default)]
+        pub struct $exchange;
+
+        impl IntoAction for $exchange {
+            fn get_signature(&self) -> [u8; 4] {
+                $call_type::SELECTOR
+            }
+
+            fn decode_trace_data(
+                &self,
+                index: u64,
+                data: StaticReturnBindings,
+                return_data: Bytes,
+                address: Address,
+                logs: &Vec<Log>,
+            ) -> Actions {
+                let call_data = enum_unwrap!(data, $exchange_mod, $call_type);
+                let log_data = logs
+                    .into_iter()
+                    .filter_map(|log| {
+                        $event_struct::decode_log(log.topics.iter().map(|h| h.0), &log.data, true)
+                            .ok()
+                    })
+                    .collect::<Vec<_>>()
+                    .first()
+                    .unwrap();
+
+                Actions::$impl_type($fn(index, address, call_data, log_data))
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! action_impl_calldata_no_return {
+    // calldata
+    (
+        $exchange:ident,
+        $impl_type:ident,
+        $call_type:ident,
+        $exchange_mod:ident,
+        $fn:expr
+        ) => {
+        #[derive(Debug, Default)]
+        pub struct $exchange;
+
+        impl IntoAction for $exchange {
+            fn get_signature(&self) -> [u8; 4] {
+                $call_type::SELECTOR
+            }
+
+            fn decode_trace_data(
+                &self,
+                index: u64,
+                data: StaticReturnBindings,
+                return_data: Bytes,
+                address: Address,
+                logs: &Vec<Log>,
+            ) -> Actions {
+                let call_data = enum_unwrap!(data, $exchange_mod, $call_type);
+                Actions::$impl_type($fn(index, address, call_data))
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! action_impl_log_no_return {
+    // log
+    (
+        $exchange:ident,
+        $impl_type:ident,
+        $call_type:ident,
+        $event_struct:ident,
+        $fn:expr
+        ) => {
+        #[derive(Debug, Default)]
+        pub struct $exchange;
+
+        impl IntoAction for $exchange {
+            fn get_signature(&self) -> [u8; 4] {
+                $call_type::SELECTOR
+            }
+
+            fn decode_trace_data(
+                &self,
+                index: u64,
+                data: StaticReturnBindings,
+                return_data: Bytes,
+                address: Address,
+                logs: &Vec<Log>,
+            ) -> Actions {
+                let log_data = logs
+                    .into_iter()
+                    .filter_map(|log| {
+                        $event_struct::decode_log(log.topics.iter().map(|h| h.0), &log.data, true)
+                            .ok()
+                    })
+                    .collect::<Vec<_>>()
+                    .first()
+                    .unwrap();
+                Actions::$impl_type($fn(index, address, log_data))
             }
         }
     };
