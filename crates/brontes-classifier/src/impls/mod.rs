60d78337134e1c952ad835a9e214c09e7c0d41d0
Will Smith <williamqsmith11@gmail.com>
2023-10-02 18:49:57 -0700

    rip macros

--- crates/brontes-classifier/src/impls/mod.rs
+++ crates/brontes-classifier/src/impls/mod.rs
@@ -16,3 +16,131 @@ macro_rules! enum_unwrap {
         }}
     };
 }
+
+#[macro_export]
+/// all variants for swap implementation
+macro_rules! action_impl{
+    // everything
+    (
+     $exchange:ident,
+     $impl_type:ident,
+     $call_type:ident,
+     $exchange_mod:ident,
+     $event_struct:ident,
+     $return_calldata:expr,
+     $fn:expr
+     ) => {
+        pub struct paste![<$exchange $impl_type>];
+
+        impl IntoAction for paste![<$exchange $impl_type>] {
+            fn get_signature(&self) -> [u8; 4] {
+                $call_type::SELECTOR
+            }
+
+            fn decode_trace_data(
+                &self,
+                index: u64,
+                data: StaticReturnBindings,
+                return_data: Bytes,
+                address: Address,
+                logs: &Vec<Log>,
+            ) -> Actions {
+                let call_data = enum_unwrap!(data, $exchange_mod, $call_type);
+                let log_data = logs
+                    .into_iter()
+                    .filter_map(|log|$event_struct::decode_log(log.
+                                                                topics
+                                                                .iter()
+                                                                .map(|h| h.0)
+                                                                , &log.data,
+                                                                true).ok())
+                    .collect::<Vec<_>>().first().unwrap();
+
+                if $return_calldata {
+                    let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                    Actions::$impl_type($fn(index, address, call_data, return_data, log_data))
+                } else {
+                    Actions::$impl_type($fn(index, address, call_data, log_data))
+                }
+            }
+        }
+    };
+    // calldata
+    (
+        $exchange:ident,
+        $impl_type:ident,
+        $call_type:ident,
+        $exchange_mod:ident,
+        $return_calldata:expr,
+        $fn:expr
+        ) => {
+        pub struct paste![<$exchange $impl_type>];
+
+        impl IntoAction for paste![<$exchange $impl_type>] {
+            fn get_signature(&self) -> [u8; 4] {
+                $call_type::SELECTOR
+            }
+
+            fn decode_trace_data(
+                &self,
+                index: u64,
+                data: StaticReturnBindings,
+                return_data: Bytes,
+                address: Address,
+                logs: &Vec<Log>,
+            ) -> Actions {
+                let call_data = enum_unwrap!(data, $exchange_mod, $call_type);
+                if $return_calldata  {
+                    let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                    Actions::$impl_type($fn(index, address, call_data, return_data))
+                } else {
+                    Actions::$impl_types($fn(index, address, call_data))
+                }
+            }
+        }
+    };
+
+    // return data
+    (
+        $exchange:ident,
+        $impl_type:ident,
+        $call_type:ident,
+        $event_struct:ident,
+        $return_calldata:expr,
+        $fn:expr
+        ) => {
+        pub struct paste![<$exchange $impl_type>];
+
+        impl IntoAction for paste![<$exchange $impl_type>] {
+            fn get_signature(&self) -> [u8; 4] {
+                $call_type::SELECTOR
+            }
+
+            fn decode_trace_data(
+                &self,
+                index: u64,
+                data: StaticReturnBindings,
+                return_data: Bytes,
+                address: Address,
+                logs: &Vec<Log>,
+            ) -> Actions {
+                let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                let log_data = logs
+                    .into_iter()
+                    .filter_map(|log|$event_struct::decode_log(log.
+                                                                topics
+                                                                .iter()
+                                                                .map(|h| h.0),
+                                                                &log.data,
+                                                                true).ok())
+                    .collect::<Vec<_>>().first().unwrap();
+                if $return_calldata  {
+                    let return_data = $call_type::abi_decode_returns(&return_data, true).unwrap();
+                    Actions::$impl_type($fn(index, address, return_data, log_data))
+                } else {
+                    Actions::$impl_type($fn(index, address, return_data))
+                }
+            }
+        }
+    };
+}
