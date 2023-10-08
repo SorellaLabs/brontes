58c283f468354b7d7800cc6ed3685e866434d077
Will Smith <williamqsmith11@gmail.com>
2023-10-07 23:45:36 -0700

    seperate out

--- crates/brontes-core/build.rs
+++ crates/brontes-core/build.rs
@@ -119,48 +119,51 @@ async fn get_all_touched_addresses(start_block: u64, end_block: u64) -> HashSet<
     let db_path = env::var("DB_PATH").expect("DB_PATH not found in env");
     let tracer = TracingClient::new(Path::new(&db_path), tokio::runtime::Handle::current());
 
-    let mut trace_type = HashSet::new();
-    trace_type.insert(TraceType::Trace);
-    trace_type.insert(TraceType::VmTrace);
-
     let range = (start_block..end_block).into_iter().collect::<Vec<_>>();
     let mut res = HashSet::new();
 
     for chunk in range.as_slice().chunks(10) {
-        res.extend(
-            join_all(chunk.into_iter().map(|block_num| {
-                tracer
-                    .trace
-                    .replay_block_transactions(
-                        BlockId::Number(BlockNumberOrTag::Number(*block_num)),
-                        trace_type.clone(),
-                    )
-                    .map(|trace| {
-                        trace.unwrap().unwrap().into_iter().flat_map(|trace| {
-                            trace
-                                .full_trace
-                                .trace
-                                .into_iter()
-                                .filter_map(|call_frame| match call_frame.action {
-                                    reth_rpc_types::trace::parity::Action::Call(c) => Some(c.to),
-                                    reth_rpc_types::trace::parity::Action::Create(_)
-                                    | reth_rpc_types::trace::parity::Action::Reward(_) => None,
-                                    reth_rpc_types::trace::parity::Action::Selfdestruct(s) => {
-                                        Some(s.address)
-                                    }
-                                })
-                                .collect::<HashSet<_>>()
-                        })
-                    })
-            }))
-            .await
-            .into_iter()
-            .flatten()
-            .collect::<HashSet<_>>(),
-        );
+        res.extend(get_addresses_for_chunk(&tracer, chunk).await)
     }
 
     res
+}
+
+#[cfg(feature = "test_run")]
+async fn get_addresses_for_chunk(client: &TracingClient, chunk: &[u64]) -> HashSet<Address> {
+    let mut trace_type = HashSet::new();
+    trace_type.insert(TraceType::Trace);
+    trace_type.insert(TraceType::VmTrace);
+
+    join_all(chunk.into_iter().map(|block_num| {
+        client
+            .trace
+            .replay_block_transactions(
+                BlockId::Number(BlockNumberOrTag::Number(*block_num)),
+                trace_type.clone(),
+            )
+            .map(|trace| {
+                trace.unwrap().unwrap().into_iter().flat_map(|trace| {
+                    trace
+                        .full_trace
+                        .trace
+                        .into_iter()
+                        .filter_map(|call_frame| match call_frame.action {
+                            reth_rpc_types::trace::parity::Action::Call(c) => Some(c.to),
+                            reth_rpc_types::trace::parity::Action::Create(_)
+                            | reth_rpc_types::trace::parity::Action::Reward(_) => None,
+                            reth_rpc_types::trace::parity::Action::Selfdestruct(s) => {
+                                Some(s.address)
+                            }
+                        })
+                        .collect::<HashSet<_>>()
+                })
+            })
+    }))
+    .await
+    .into_iter()
+    .flatten()
+    .collect::<HashSet<_>>()
 }
 
 //
