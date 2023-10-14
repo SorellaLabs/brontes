f8ca36cafdcfca0cd76d337aaa473cb8b8a84830
Will Smith <williamqsmith11@gmail.com>
2023-10-14 16:41:37 -0700

    wip

--- collect_errors.sh
+++ collect_errors.sh
@@ -11,7 +11,7 @@ do
   echo "Running block $OUR_START_BLOCK to $OUR_END_BLOCK"
   export START_BLOCK=$OUR_START_BLOCK
   export END_BLOCK=$OUR_END_BLOCK
-  exec "cargo build --features test_run,server"
+  cargo build --features test_run,server
 done 
 
 
