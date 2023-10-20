63764363e99423999ce347af0dd21dfa3ed26281
lagrange.eth <lagrange@sorellacapital.com>
2023-10-20 01:36:02 -0700

    updated toml
    Former-commit-id: 75cd46e070f3a814a44dbcce5f0b461c0b5a6efb

--- crates/brontes-classifier/build.rs
+++ crates/brontes-classifier/build.rs
@@ -21,7 +21,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 const TOKEN_MAPPING: &str = "token_mappings.rs";
-const TOKEN_QUERIES: &str = "SELECT toString(address), arrayMap(x -> toString(x), tokens) AS \
+const TOKEN_QUERIES: &str = "SELECT toString(address), arrayMap(x -> toString(x), tokens) AS 
                              tokens FROM pools WHERE length(tokens) = ";
 
 const ABI_DIRECTORY: &str = "./abis/";
