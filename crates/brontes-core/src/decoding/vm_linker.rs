43c81c77c625b34b3894c83a77f4e3679ff549e1
lagrange.eth <lagrange@sorellacapital.com>
2023-10-23 17:58:39 -0700

    finished parsing

--- crates/brontes-core/src/decoding/vm_linker.rs
+++ crates/brontes-core/src/decoding/vm_linker.rs
@@ -1,5 +1,4 @@
 use brontes_types::structured_trace::TransactionTraceWithLogs;
-use ethers_core::types::H256;
 use reth_rpc_types::{
     trace::parity::{TransactionTrace, VmInstruction, VmTrace},
     Log,
@@ -11,14 +10,8 @@ pub fn link_vm_to_trace(
     mut logs: Vec<Log>,
 ) -> Vec<TransactionTraceWithLogs> {
     let mut res = Vec::new();
-
-    // println!("tx_trace: {}\nlogs: {}", tx_trace.len(), logs.len());
-
     recursive_parsing(&mut res, vm, &mut tx_trace, &mut logs);
 
-    for r in &res {
-        //println!("{:?}, {}", r.trace.trace_address, !r.logs.is_empty())
-    }
     res
 }
 
