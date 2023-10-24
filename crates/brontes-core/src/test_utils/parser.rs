040b9cd1fd13d942e94be64d82e5d794910ab978
lagrange.eth <lagrange@sorellacapital.com>
2023-10-23 17:28:11 -0700

    debugging test parser

--- crates/brontes-core/src/test_utils/parser.rs
+++ crates/brontes-core/src/test_utils/parser.rs
@@ -136,7 +136,7 @@ async fn test_execute_block() {
                 let full_trace = get_full_tx_trace(t.tx_hash.clone()).await;
                 let receipt = get_tx_reciept(t.tx_hash.clone()).await;
 
-                println!("logs: {:?}", receipt.logs);
+                println!("trace: {:?}", t.tx_hash);
                 let traces_with_logs =
                     link_vm_to_trace(full_trace.vm_trace.unwrap(), full_trace.trace, receipt.logs);
 
