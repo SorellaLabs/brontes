dec8e95447c5c7f3c0977d33eacd97b520160977
lagrange.eth <lagrange@sorellacapital.com>
2023-10-23 17:11:15 -0700

    fixing test parser

--- crates/brontes-core/src/test_utils/parser.rs
+++ crates/brontes-core/src/test_utils/parser.rs
@@ -122,6 +122,7 @@ async fn test_execute_block() {
 
     let block_1 = tracer.execute_block(17000000).await;
     assert!(block_1.is_some());
+    println!("assert made");
 
     let traces = block_1.unwrap().0;
     assert_eq!(traces.len(), 676);
