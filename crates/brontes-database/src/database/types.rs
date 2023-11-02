17e09c6fbf245a45d2394004cf71cdd585ad6718
lagrange.eth <lagrange@sorellacapital.com>
2023-11-02 17:21:39 -0400

    fixing db relay query

--- crates/brontes-database/src/database/types.rs
+++ crates/brontes-database/src/database/types.rs
@@ -65,7 +65,7 @@ pub struct RelayInfo {
 
 impl From<RelayInfoDB> for RelayInfo {
     fn from(value: RelayInfoDB) -> Self {
-        println!("{:?}", value);
+        println!("RELAY info calling into: {:?}", value);
         RelayInfo {
             block_hash: H256::from_str(&value.block_hash).unwrap(),
             relay_time: value.relay_time,
