3fc44118d7877179b0f2f63d4a0cc7b295ef6ac8
lagrange.eth <lagrange@sorellacapital.com>
2023-11-02 18:02:46 -0400

    debugging inserts

--- crates/brontes-database/queries/relay_p2p_times.sql
+++ crates/brontes-database/queries/relay_p2p_times.sql
@@ -9,3 +9,6 @@ INNER JOIN ethereum.blocks AS blocks ON blocks.block_hash = relays.block_hash
 INNER JOIN ethereum.block_observations AS cb
 ON ethereum.relays.block_number = cb.block_number 
 WHERE (relays.block_number = ?) AND blocks.valid = 1
+
+
+
