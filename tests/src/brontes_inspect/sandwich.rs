cf618b3bfda0ef334dcd8f3995b3c64bcf07f00d
lagrange.eth <lagrange@sorellacapital.com>
2023-11-06 16:08:40 -0500

    testing sandwich inspector
    Former-commit-id: 2a6cabe86362e452a9037802d73d7f660d709dbd

--- tests/src/brontes_inspect/sandwich.rs
+++ tests/src/brontes_inspect/sandwich.rs
@@ -23,7 +23,7 @@ async fn process_tree() {
     let metadata = db.get_metadata(block_num).await;
     let tree = Arc::new(classifier.build_tree(block.0, block.1, &metadata));
 
-    //write_tree_as_json(&tree, "./tree.json").await;
+    write_tree_as_json(&tree, "./tree.json").await;
 
     let inspector = SandwichInspector::default();
 
