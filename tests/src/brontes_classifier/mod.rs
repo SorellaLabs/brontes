ea9bb243e9ffb02404803ed4e7038ecf8db81a3c
lagrange.eth <lagrange@sorellacapital.com>
2023-11-06 11:23:18 -0500

    testing test_try_classify_unknown_exchanges

--- tests/src/brontes_classifier/mod.rs
+++ tests/src/brontes_classifier/mod.rs
@@ -1,2 +1,9 @@
+use alloy_sol_macro::sol;
+
 mod classified_tree;
 mod decoding;
+
+mod normalized_actions;
+
+sol!(UniswapV2, "../crates/brontes-classifier/abis/UniswapV2.json");
+sol!(UniswapV3, "../crates/brontes-classifier/abis/UniswapV3.json");
