8a040a50c797100388b83369188f3942191392dc
lagrange.eth <lagrange@sorellacapital.com>
2023-11-06 15:46:30 -0500

    testing sandwich inspector
    Former-commit-id: f71b57cbcf53c9a9eda1ad7c05d3327813697931

--- tests/src/brontes_inspect/sandwich.rs
+++ tests/src/brontes_inspect/sandwich.rs
@@ -1,10 +1,11 @@
-use std::sync::Arc;
+use std::{str::FromStr, sync::Arc};
 
 use brontes_classifier::{test_utils::build_raw_test_tree, Classifier};
 use brontes_core::test_utils::init_trace_parser;
 use brontes_database::database::Database;
 use brontes_inspect::{sandwich::SandwichInspector, Inspector};
 use brontes_types::test_utils::write_tree_as_json;
+use reth_primitives::H256;
 use tokio::sync::mpsc::unbounded_channel;
 
 #[tokio::test]
@@ -22,11 +23,22 @@ async fn process_tree() {
     let metadata = db.get_metadata(block_num).await;
     let tree = Arc::new(classifier.build_tree(block.0, block.1, &metadata));
 
-    write_tree_as_json(&tree, "./tree.json").await;
+    //write_tree_as_json(&tree, "./tree.json").await;
 
     let inspector = SandwichInspector::default();
 
-    let mev = inspector.process_tree(tree, metadata.into()).await;
+    let mev = inspector.process_tree(tree.clone(), metadata.into()).await;
 
     println!("{:?}", mev);
+
+    let actions = tree
+        .inspect(
+            H256::from_str("0xd8d45bdcb25ba4cb2ecb357a5505d03fa2e67fe6e6cc032ca6c05de75d14f5b5")
+                .unwrap(),
+            |node| node.subactions.iter().any(|action| action.is_swap()),
+        )
+        .into_iter()
+        .collect::<Vec<_>>();
+
+    println!("ACTIONSSS: {:?}", actions);
 }
