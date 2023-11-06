8dd27380b6a68bedf0df18750d04d7e381a97105
lagrange.eth <lagrange@sorellacapital.com>
2023-11-06 16:08:24 -0500

    fixed subactions
    Former-commit-id: 13ed80fe5fc9396b3b5fda118b824eb93687c26e

--- crates/brontes-types/src/tree.rs
+++ crates/brontes-types/src/tree.rs
@@ -14,11 +14,11 @@ use crate::normalized_actions::NormalizedAction;
 
 #[derive(Serialize)]
 pub struct TimeTree<V: NormalizedAction> {
-    pub roots:            Vec<Root<V>>,
-    pub header:           Header,
+    pub roots: Vec<Root<V>>,
+    pub header: Header,
     pub avg_priority_fee: u64,
     /// first is on block submission, second is when the block gets accepted
-    pub eth_prices:       (Rational, Rational),
+    pub eth_prices: (Rational, Rational),
 }
 
 impl<V: NormalizedAction> TimeTree<V> {
@@ -112,9 +112,9 @@ impl<V: NormalizedAction> TimeTree<V> {
 
 #[derive(Serialize)]
 pub struct Root<V: NormalizedAction> {
-    pub head:        Node<V>,
-    pub tx_hash:     H256,
-    pub private:     bool,
+    pub head: Node<V>,
+    pub tx_hash: H256,
+    pub private: bool,
     pub gas_details: GasDetails,
 }
 
@@ -166,9 +166,9 @@ impl<V: NormalizedAction> Root<V> {
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, Row, Default)]
 pub struct GasDetails {
-    pub coinbase_transfer:   Option<u64>,
-    pub priority_fee:        u64,
-    pub gas_used:            u64,
+    pub coinbase_transfer: Option<u64>,
+    pub priority_fee: u64,
+    pub gas_used: u64,
     pub effective_gas_price: u64,
 }
 
@@ -190,15 +190,15 @@ impl GasDetails {
 
 #[derive(Serialize)]
 pub struct Node<V: NormalizedAction> {
-    pub inner:     Vec<Node<V>>,
+    pub inner: Vec<Node<V>>,
     pub finalized: bool,
-    pub index:     u64,
+    pub index: u64,
 
     /// This only has values when the node is frozen
-    pub subactions:    Vec<V>,
+    pub subactions: Vec<V>,
     pub trace_address: Vec<usize>,
-    pub address:       Address,
-    pub data:          V,
+    pub address: Address,
+    pub data: V,
 }
 
 impl<V: NormalizedAction> Node<V> {
@@ -216,7 +216,7 @@ impl<V: NormalizedAction> Node<V> {
     /// The address here is the from address for the trace
     pub fn insert(&mut self, n: Node<V>) {
         if self.finalized {
-            return
+            return;
         }
 
         let trace_addr = n.trace_address.clone();
@@ -233,14 +233,19 @@ impl<V: NormalizedAction> Node<V> {
     }
 
     pub fn get_all_sub_actions(&self) -> Vec<V> {
+        let mut subactions = vec![];
         if self.finalized {
-            self.subactions.clone()
+            subactions.push(self.data.clone());
+            subactions.extend(self.subactions.clone());
         } else {
-            self.inner
-                .iter()
-                .flat_map(|inner| inner.get_all_sub_actions())
-                .collect()
+            subactions.extend(
+                self.inner
+                    .iter()
+                    .flat_map(|inner| inner.get_all_sub_actions())
+                    .collect::<Vec<_>>(),
+            );
         }
+        subactions
     }
 
     pub fn tree_right_path(&self) -> Vec<Address> {
@@ -264,7 +269,7 @@ impl<V: NormalizedAction> Node<V> {
 
     pub fn current_call_stack(&self) -> Vec<Address> {
         let Some(mut stack) = self.inner.last().map(|n| n.current_call_stack()) else {
-            return vec![self.address]
+            return vec![self.address];
         };
 
         stack.push(self.address);
@@ -286,7 +291,7 @@ impl<V: NormalizedAction> Node<V> {
     {
         // prev better
         if !find(self) {
-            return false
+            return false;
         }
         let lower_has_better = self
             .inner
@@ -301,7 +306,7 @@ impl<V: NormalizedAction> Node<V> {
             indexes.extend(classified_indexes);
         }
 
-        return true
+        return true;
     }
 
     pub fn get_bounded_info<F, R>(&self, lower: u64, upper: u64, res: &mut Vec<R>, info_fn: &F)
@@ -309,7 +314,7 @@ impl<V: NormalizedAction> Node<V> {
         F: Fn(&Node<V>) -> R,
     {
         if self.inner.is_empty() {
-            return
+            return;
         }
 
         let last = self.inner.last().unwrap();
@@ -321,7 +326,7 @@ impl<V: NormalizedAction> Node<V> {
                 .iter()
                 .for_each(|node| node.get_bounded_info(lower, upper, res, info_fn));
 
-            return
+            return;
         }
 
         // find bounded limit
@@ -338,7 +343,7 @@ impl<V: NormalizedAction> Node<V> {
                     end = end.or(Some(our_index).filter(|_| peek.index > upper));
                 }
             } else {
-                break
+                break;
             }
         }
 
@@ -359,7 +364,7 @@ impl<V: NormalizedAction> Node<V> {
 
     pub fn remove_index_and_childs(&mut self, index: u64) {
         if self.inner.is_empty() {
-            return
+            return;
         }
 
         let mut iter = self.inner.iter_mut().enumerate().peekable();
@@ -367,16 +372,16 @@ impl<V: NormalizedAction> Node<V> {
         let val = loop {
             if let Some((our_index, next)) = iter.next() {
                 if index == next.index {
-                    break Some(our_index)
+                    break Some(our_index);
                 }
 
                 if let Some(peek) = iter.peek() {
                     if index > next.index && index < peek.1.index {
                         next.remove_index_and_childs(index);
-                        break None
+                        break None;
                     }
                 } else {
-                    break None
+                    break None;
                 }
             }
         };
@@ -392,7 +397,8 @@ impl<V: NormalizedAction> Node<V> {
     {
         // the previous sub-action was the last one to meet the criteria
         if !call(self) {
-            return false
+            println!("calling false??");
+            return false;
         }
         let lower_has_better = self
             .inner
@@ -421,7 +427,7 @@ impl<V: NormalizedAction> Node<V> {
     {
         let works = find(self.address, self.get_all_sub_actions());
         if !works {
-            return false
+            return false;
         }
 
         let lower_has_better = self
