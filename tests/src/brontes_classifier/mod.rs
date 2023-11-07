3b273f289726209d589ee970e955679ef7e269f3
lagrange.eth <lagrange@sorellacapital.com>
2023-11-07 10:31:14 -0500

    added tests

--- tests/src/brontes_classifier/mod.rs
+++ tests/src/brontes_classifier/mod.rs
@@ -1,6 +1,1 @@
-mod uni_v3;
 
-use alloy_sol_macro::sol;
-
-sol!(UniswapV2, "../crates/brontes-classifier/abis/UniswapV2.json");
-sol!(UniswapV3, "../crates/brontes-classifier/abis/UniswapV3.json");
