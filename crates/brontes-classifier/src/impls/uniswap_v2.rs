87578f3447057d4752b36b1eeb93b69c4c61b7f2
Will Smith <williamqsmith11@gmail.com>
2023-11-07 16:48:24 -0500

    unwrap

--- crates/brontes-classifier/src/impls/uniswap_v2.rs
+++ crates/brontes-classifier/src/impls/uniswap_v2.rs
@@ -317,7 +317,7 @@ mod tests {
         assert_eq!(swap, expected_swap);
     }
 
-    /* 
+    /*
     #[test]
     fn t() {
         println!("{:?}", H160::from_str("0x6Aa46141f1789C6BEc54651690312F754B05a99e").unwrap().0);
