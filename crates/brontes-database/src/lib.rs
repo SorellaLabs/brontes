dbd831ee88fc509caa66fd49dd66046fb227b6f9
Will Smith <williamqsmith11@gmail.com>
2023-11-07 14:20:15 -0500

    fix
    Former-commit-id: c40735b8738128c9c74e2fba69db9c582016935d

--- crates/brontes-database/src/lib.rs
+++ crates/brontes-database/src/lib.rs
@@ -46,8 +46,7 @@ impl Metadata {
 
 impl Metadata {
     pub fn get_gas_price_usd(&self, gas_used: u64) -> (Rational, Rational) {
-        let gas_used_rational =
-            Rational::from(gas_used) * Rational::from_unsigneds(gas_used, 10u64.pow(-9));
+        let gas_used_rational = Rational::from_unsigneds(gas_used, 10u64.pow(9));
 
         (&self.eth_prices.0 * &gas_used_rational, &self.eth_prices.1 * gas_used_rational)
     }
