d0816555da44ad7cd7387419263741ea3fa43171
Will Smith <williamqsmith11@gmail.com>
2023-11-07 12:38:12 -0500

    comment the delta fetcher

--- crates/brontes-inspect/src/shared_utils.rs
+++ crates/brontes-inspect/src/shared_utils.rs
@@ -23,9 +23,11 @@ impl SharedInspectorUtils {
     ) -> HashMap<Address, HashMap<Address, Rational>> {
         let mut transfers = Vec::new();
 
-        // address and there token delta's
+        // Address and there token delta's
         let mut deltas = HashMap::new();
+
         for action in actions.into_iter().flatten() {
+            // If the action is a swap, get the decimals to scale the amount in and out properly.
             if let Actions::Swap(swap) = action {
                 let Some(decimals_in) = TOKEN_TO_DECIMALS.get(&swap.token_in.0) else {
                     error!(missing_token=?swap.token_in, "missing token in token to decimal map");
@@ -40,6 +42,8 @@ impl SharedInspectorUtils {
                 let adjusted_in = -swap.amount_in.to_scaled_rational(*decimals_in);
                 let adjusted_out = swap.amount_out.to_scaled_rational(*decimals_out);
 
+
+                // Store the amount_in amount_out deltas for a given from address
                 match deltas.entry(swap.from) {
                     Entry::Occupied(mut o) => {
                         let inner: &mut HashMap<Address, Rational> = o.get_mut();
@@ -55,23 +59,30 @@ impl SharedInspectorUtils {
                         v.insert(default);
                     }
                 }
+            // If there is a transfer, push to the given transfer addresses.
             } else if let Actions::Transfer(transfer) = action {
                 transfers.push(transfer);
             }
         }
 
+        // Now that all swap deltas have been calculated for a given from address we need to 
+        // apply all transfers that occurred. This is to move all the funds to there end account to
+        // ensure for a given address what the exact delta's are.
         loop {
             let mut changed = false;
 
             transfers = transfers
                 .into_iter()
                 .filter_map(|transfer| {
+                    // normalize token decimals
                     let Some(decimals) = TOKEN_TO_DECIMALS.get(&transfer.token.0) else {
                         error!(missing_token=?transfer.token, "missing token in token to decimal map");
                         return None;
                     };
                     let adjusted_amount = transfer.amount.to_scaled_rational(*decimals);
 
+
+                    // subtract value from the from address
                     if let Some(from_token_map) = deltas.get_mut(&transfer.from) {
                         changed = true;
                         apply_entry(transfer.token, -adjusted_amount.clone(), from_token_map);
@@ -79,6 +90,7 @@ impl SharedInspectorUtils {
                         return Some(transfer)
                     }
 
+                    // add value to the destination address
                     let to_token_map = deltas.entry(transfer.to).or_default();
                     apply_entry(transfer.token, adjusted_amount, to_token_map);
 
