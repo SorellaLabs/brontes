ceda15de4d1d463930d4aa3bc52ccf224c59fdd0
lagrange.eth <lagrange@sorellacapital.com>
2023-11-07 16:45:08 -0500

    testing sandwich inspector
    Former-commit-id: 0613b7e159c3a957635efdc5922ce00e60ef4c10

--- crates/brontes-inspect/src/sandwich.rs
+++ crates/brontes-inspect/src/sandwich.rs
@@ -127,7 +127,7 @@ impl Inspector for SandwichInspector {
                 println!("Victim actions - {:#?}", victim_actions);
 
                 let searcher_actions = vec![ps.tx0, ps.tx1]
-                    .iter()
+                    .into_iter()
                     .flat_map(|tx| tree.inspect(tx, search_fn.clone()))
                     .collect::<Vec<Vec<Actions>>>();
                 println!("Searcher actions - {:#?}", searcher_actions);
