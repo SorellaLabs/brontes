3522e3c7a9c1f5a61989499cd40e2a78ed9d844e
lagrange.eth <lagrange@sorellacapital.com>
2023-11-07 19:36:46 -0500

    u256
    Former-commit-id: 42e308706372f821f73bd669e97ad2c7e39b4a0b

--- crates/brontes-types/src/lib.rs
+++ crates/brontes-types/src/lib.rs
@@ -37,10 +37,51 @@ pub use queries::make_call_request;
 pub mod structured_trace;
 pub mod traits;
 pub mod tree;
-pub use tree::*;
-pub mod utils;
-pub use utils::*;
-pub mod protocol;
-pub use protocol::*;
-pub mod channel_alerts;
-pub use channel_alerts::*;
+
+#[cfg(feature = "tests")]
+pub mod test_utils;
+
+include!(concat!(env!("ABI_BUILD_DIR"), "/token_mapping.rs"));
+
+pub trait ToScaledRational {
+    fn to_scaled_rational(self, decimals: u8) -> Rational;
+}
+
+impl ToScaledRational for U256 {
+    fn to_scaled_rational(self, decimals: u8) -> Rational {
+        let top = Natural::from_limbs_asc(&self.into_limbs());
+
+        Rational::from_naturals(top, Natural::from(10u8).pow(decimals as u64))
+    }
+}
+
+pub trait ToFloatNearest {
+    fn to_float(self) -> f64;
+}
+
+impl ToFloatNearest for Rational {
+    fn to_float(self) -> f64 {
+        f64::rounding_from(self, RoundingMode::Nearest).0
+    }
+}
+
+pub(crate) mod u256 {
+    use reth_primitives::U256;
+    use serde::{
+        de::{Deserialize, Deserializer},
+        ser::{Serialize, Serializer},
+    };
+
+    pub fn serialize<S: Serializer>(u: &U256, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut buf: [u8; 32] = u.to_le_bytes();
+        buf.serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<U256, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let u: [u8; 32] = Deserialize::deserialize(deserializer)?;
+        Ok(U256::from_le_bytes(u))
+    }
+}
