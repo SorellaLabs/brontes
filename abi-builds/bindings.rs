16230bbc54e10744eb5d405dd25165e9b27c092a
Will Smith <williamqsmith11@gmail.com>
2023-11-08 16:28:45 -0500

    un 32 it
    Former-commit-id: e0e6a8b51dec8bf46287c34ddf93e7b7ceb2ff40

--- abi-builds/bindings.rs
+++ abi-builds/bindings.rs
@@ -4,8 +4,7 @@ sol! (SushiSwapV2, "./abis/SushiSwapV2.json");
 
 
 #[allow(non_camel_case_types)]
-#[repr(u32)]
- pub enum StaticBindings {
+pub enum StaticBindings {
    UniswapV3(UniswapV3_Enum),
    UniswapV2(UniswapV2_Enum),
    SushiSwapV2(SushiSwapV2_Enum),
@@ -22,16 +21,14 @@ _=> panic!("no binding match found")}
 
 
 #[allow(non_camel_case_types)]
-#[repr(u32)]
- pub enum StaticReturnBindings {
+pub enum StaticReturnBindings {
    UniswapV3(UniswapV3::UniswapV3Calls),
    UniswapV2(UniswapV2::UniswapV2Calls),
    SushiSwapV2(SushiSwapV2::SushiSwapV2Calls),
 }
 
 #[allow(non_camel_case_types)]
-#[repr(u32)]
- pub enum UniswapV3_Enum {
+pub enum UniswapV3_Enum {
  None
 }
 impl_decode_sol!(UniswapV3_Enum, UniswapV3::UniswapV3Calls);
@@ -39,8 +36,7 @@ impl_decode_sol!(UniswapV3_Enum, UniswapV3::UniswapV3Calls);
 
 
 #[allow(non_camel_case_types)]
-#[repr(u32)]
- pub enum UniswapV2_Enum {
+pub enum UniswapV2_Enum {
  None
 }
 impl_decode_sol!(UniswapV2_Enum, UniswapV2::UniswapV2Calls);
@@ -48,8 +44,7 @@ impl_decode_sol!(UniswapV2_Enum, UniswapV2::UniswapV2Calls);
 
 
 #[allow(non_camel_case_types)]
-#[repr(u32)]
- pub enum SushiSwapV2_Enum {
+pub enum SushiSwapV2_Enum {
  None
 }
 impl_decode_sol!(SushiSwapV2_Enum, SushiSwapV2::SushiSwapV2Calls);
