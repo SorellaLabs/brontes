49a0a722d9decef08f04993113a948dde6240eda
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-11-07 22:09:11 -0500

    comment out annoying prints
    Former-commit-id: a2f5a6534e193f33e7b19595afac19d6bb485aa5

--- crates/brontes-classifier/src/classifier.rs
+++ crates/brontes-classifier/src/classifier.rs
@@ -79,13 +79,14 @@ impl Classifier {
                         self.get_coinbase_transfer(header.beneficiary, &trace.trace.action);
 
                     let from_addr = trace.get_from_addr();
-                    println!(
+                    /*println!(
                         "NODE - PRE-CLASSIFICATION FROM ADDRESS: {:?}, DATA: {:?}\n",
                         from_addr,
                         trace.get_calldata()
-                    );
+                    );*/
                     let classification = self.classify_node(trace.clone(), (index + 1) as u64);
-                    println!("NODE - FROM ADDRESS: {:?}, DATA: {:?}\n", from_addr, classification);
+                    //println!("NODE - FROM ADDRESS: {:?}, DATA: {:?}\n", from_addr,
+                    // classification);
 
                     let subactions = if !classification.is_unclassified() {
                         vec![classification.clone()]
@@ -189,7 +190,7 @@ impl Classifier {
             println!("target address bytes: {:?}", &target_address.0);
         } */
         if let Some(protocol) = PROTOCOL_ADDRESS_MAPPING.get(&target_address.0) {
-            println!("\n\n\n CALLDATA {:?}", trace.get_calldata());
+            //println!("\n\n\n CALLDATA {:?}", trace.get_calldata());
             if let Some(classifier) = &protocol.0 {
                 let calldata = trace.get_calldata();
                 let return_bytes = trace.get_return_calldata();
