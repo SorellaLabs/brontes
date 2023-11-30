080ddcb641af7b213a04ef4cbab09989be421bca
Will Smith <williamqsmith11@gmail.com>
2023-11-30 15:26:40 -0500

    wip
    Former-commit-id: 04fcc2c4159a308690972e794a6549c1918cb810

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -131,6 +131,8 @@ async fn run(handle: tokio::runtime::Handle) -> Result<(), Box<dyn Error>> {
     )
     .await;
 
+    drop(parser);
+
     // you have a intermediate parse function for the range of blocks you want to
     // parse it collects the aggregate stats of each block stats
     // the block stats collect the aggregate stats of each tx
