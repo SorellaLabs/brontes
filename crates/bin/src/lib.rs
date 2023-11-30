48ea130b814fc1ba3b5365544670edadf1e61f6b
Will Smith <williamqsmith11@gmail.com>
2023-11-30 17:52:29 -0500

    fix: run single block
    Former-commit-id: e84aac7ff0623184b5d0e74b5560096289d68530

--- crates/bin/src/lib.rs
+++ crates/bin/src/lib.rs
@@ -55,12 +55,13 @@ impl<'inspector, const N: usize, T: TracingProvider> Brontes<'inspector, N, T> {
             block_inspectors: FuturesUnordered::new(),
         };
 
+
         let max_blocks = match end_block {
             Some(end_block) => end_block.min(init_block + max_tasks),
             None => init_block + max_tasks,
         };
 
-        for _ in init_block..max_blocks {
+        for _ in init_block..=max_blocks {
             brontes.spawn_block_inspector();
         }
 
