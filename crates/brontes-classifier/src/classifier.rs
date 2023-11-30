3b297698f6de86d8fdecf448627c191aa52f3e69
Will Smith <williamqsmith11@gmail.com>
2023-11-30 00:13:26 -0500

    wip
    Former-commit-id: 278e471dd09cc692bf4f6ff692f629649f0f05f0

--- crates/brontes-classifier/src/classifier.rs
+++ crates/brontes-classifier/src/classifier.rs
@@ -187,7 +187,7 @@ impl Classifier {
                 if let Some(res) = d {
                     return res
                 } else {
-                    tracing::error!(contract_addr = ?target_address.0, trace=?trace, "classification failed on the given address");
+                    tracing::warn!(contract_addr = ?target_address.0, trace=?trace, "classification failed on the given address");
                 }
             }
         }
