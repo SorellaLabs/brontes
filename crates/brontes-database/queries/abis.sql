9e5f4ff3af659244d0193277f03706121dca5a62
Will Smith <williamqsmith11@gmail.com>
2023-11-30 20:00:38 -0500

    wip
    Former-commit-id: 9647ad571cc9222c22110060d31a3e221c101281

--- crates/brontes-database/queries/abis.sql
+++ crates/brontes-database/queries/abis.sql
@@ -1,5 +1,5 @@
 SELECT 
- distinct(a.address), any(c.abi)
+ distinct(a.address), c.abi
 FROM ethereum.addresses a 
 INNER JOIN ethereum.contracts c ON a.hashed_bytecode = c.hashed_bytecode
 WHERE c.abi IS NOT NULL AND has([?], address)
