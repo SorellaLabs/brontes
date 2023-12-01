e70a8c23f3c0ddfeceb63bb1de0c5b2335227d33
Will Smith <williamqsmith11@gmail.com>
2023-11-30 19:50:42 -0500

    fix query
    Former-commit-id: 03058f3b7fb46b605380344d5e6d1b948c156df0

--- crates/brontes-database/queries/abis.sql
+++ crates/brontes-database/queries/abis.sql
@@ -1,5 +1,7 @@
 SELECT 
-	address, abi 
-FROM brontes.protocol_details
-WHERE has([?], address)
+ a.address, c.abi 
+FROM ethereum.addresses a 
+INNER JOIN ethereum.contracts c ON a.hashed_bytecode = c.hashed_bytecode
+WHERE c.abi IS NOT NULL
+AND has([?], address)
 
