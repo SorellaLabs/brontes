06547b285f9cc40ead79a320f3d9028dca13e9c5
Will Smith <williamqsmith11@gmail.com>
2023-12-01 23:17:47 -0500

    make derive
    Former-commit-id: b481530e5a5af7917d372095e7b6a1fd72bf130c

--- crates/brontes-classifier/build.rs
+++ crates/brontes-classifier/build.rs
@@ -210,7 +210,12 @@ fn init_enum(name: &str, is_empty: bool) -> Vec<String> {
 
 /// generates the string of an individual binding
 fn binding_string(file_path: &str, protocol_name: &str) -> String {
-    let binding = format!("sol! ({}, \"{}\");", protocol_name, file_path);
+    let binding = format!(
+        "sol! (
+            #![sol(all_derives = true)]
+            {}, \"{}\");",
+        protocol_name, file_path
+    );
     binding
 }
 
