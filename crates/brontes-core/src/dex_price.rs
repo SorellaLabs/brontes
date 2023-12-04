4579b659c77d3c4c0cdcef46d5907f22646c910e
Will Smith <williamqsmith11@gmail.com>
2023-12-03 22:08:42 -0500

    wip

--- crates/brontes-core/src/dex_price.rs
+++ crates/brontes-core/src/dex_price.rs
@@ -1,5 +1,20 @@
-use alloy_primitives::Address;
-use reth_primitives::revm_primitives::{HashMap, Output};
+use std::{collections::HashMap, pin::Pin, task::Poll};
+
+use alloy_primitives::{Address, Bytes, FixedBytes};
+use alloy_providers::provider::Provider;
+use alloy_rpc_types::TransactionRequest;
+use alloy_sol_macro::sol;
+use alloy_sol_types::SolCall;
+use alloy_transport::TransportResult;
+use alloy_transport_http::Http;
+use brontes_database::database::Database;
+use brontes_types::cache_decimals;
+use futures::{future::join, join, stream::FuturesUnordered, Future, StreamExt};
+
+pub struct TransactionPoolSwappedTokens {
+    pairs:      Vec<(Address, Address)>,
+    state_diff: StateDiff,
+}
 
 pub trait DexPrice {
     fn get_price(
@@ -7,22 +22,18 @@ pub trait DexPrice {
         provider: &Provider<Http<reqwest::Client>>,
         address: Address,
         zto: bool,
+        // for overrides
+        state_diff: StateDiff,
     ) -> Pin<Box<dyn Future<Output = (Rational, Rational)> + Send + Sync>>;
 }
-
-
-// some static map
-//"MAP: Map<([u8;20],[u8;20]), Vec<(bool, Address, Box<dyn DexPrice>)>>"
 
 // we will have a static map for (token0, token1) => Vec<address, exchange type>
 // this will then process using async, grab the reserves and process the price.
 // and return that with tvl. with this we can calculate weighted price
-pub struct DexPricing {
-    provider: Provider<Http<reqwest::Client>>,
+pub struct DexPricing<'p> {
+    provider: &'p Provider<Http<reqwest::Client>>,
 }
 
 impl DexPricing {
-    pub fn need_prices_for(&mut self, pools_tokens_type: Vec<(Address, Address)>) {
-
-    }
+    pub fn need_prices_for(&mut self, pools_tokens_type: Vec<TransactionPoolSwappedTokens>) {}
 }
