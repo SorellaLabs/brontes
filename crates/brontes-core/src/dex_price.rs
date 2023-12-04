55520506a3bb663c18cf5cb6db3b0916d12708c0
Will Smith <williamqsmith11@gmail.com>
2023-12-03 23:24:39 -0500

    add state diff

--- crates/brontes-core/src/dex_price.rs
+++ crates/brontes-core/src/dex_price.rs
@@ -10,10 +10,11 @@ use alloy_transport_http::Http;
 use brontes_database::database::Database;
 use brontes_types::cache_decimals;
 use futures::{future::join, join, stream::FuturesUnordered, Future, StreamExt};
-use reth_rpc_types::trace::parity::StateDiff;
 use malachite::Rational;
+use reth_rpc_types::trace::parity::StateDiff;
 
 pub struct TransactionPoolSwappedTokens {
+    tx_idx:     usize,
     pairs:      Vec<(Address, Address)>,
     state_diff: StateDiff,
 }
@@ -24,9 +25,22 @@ pub trait DexPrice {
         provider: &Provider<Http<reqwest::Client>>,
         address: Address,
         zto: bool,
-        // for overrides
         state_diff: StateDiff,
     ) -> Pin<Box<dyn Future<Output = (Rational, Rational)> + Send + Sync>>;
+}
+
+struct V2Pricing;
+
+impl DexPrice for V2Pricing {
+    fn get_price(
+        &self,
+        provider: &Provider<Http<reqwest::Client>>,
+        address: Address,
+        zto: bool,
+        state_diff: StateDiff,
+    ) -> Pin<Box<dyn Future<Output = (Rational, Rational)> + Send + Sync>> {
+        Box::pin(async { todo!() })
+    }
 }
 
 // we will have a static map for (token0, token1) => Vec<address, exchange type>
@@ -34,8 +48,24 @@ pub trait DexPrice {
 // and return that with tvl. with this we can calculate weighted price
 pub struct DexPricing<'p> {
     provider: &'p Provider<Http<reqwest::Client>>,
+    futures: FuturesUnordered<Pin<Box<dyn Future<Output = (Rational, Rational)> + Send + Sync>>>,
+    
 }
 
 impl DexPricing<'_> {
-    pub fn need_prices_for(&mut self, pools_tokens_type: Vec<TransactionPoolSwappedTokens>) {}
+    pub fn need_prices_for(&mut self, pools_tokens_type: Vec<TransactionPoolSwappedTokens>) {
+
+    }
+}
+
+impl Future for DexPricing<'_> {
+    type Output = HashMap<usize, HashMap<(Address, Address)>, Rational>>;
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        todo!()
+    }
+
 }
