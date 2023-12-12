1afbfd1462a7a90ec26c3b06852455cdbcdd75de
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-12-12 14:53:06 -0500

    Will pls fix shared utils, to take into account new pricing model that is pair based

--- crates/brontes-core/src/missing_decimals.rs
+++ crates/brontes-core/src/missing_decimals.rs
@@ -10,7 +10,7 @@ use alloy_transport_http::Http;
 use brontes_database::database::Database;
 use brontes_types::cache_decimals;
 use futures::{future::join, join, stream::FuturesUnordered, Future, StreamExt};
-use tracing::{info, warn};
+use tracing::{debug, info, warn};
 
 sol!(
     function decimals() public view returns (uint8);
@@ -32,12 +32,13 @@ impl<'db> MissingDecimals<'db> {
         db: &'db Database,
         missing: Vec<Address>,
     ) -> Self {
-        let mut this = Self {
-            provider,
-            pending_decimals: FuturesUnordered::default(),
-            db_future: FuturesUnordered::default(),
-            database: db,
-        };
+        let mut this =
+            Self {
+                provider,
+                pending_decimals: FuturesUnordered::default(),
+                db_future: FuturesUnordered::default(),
+                database: db,
+            };
         this.missing_decimals(missing);
 
         this
