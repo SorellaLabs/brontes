0ef487e08f8d56d2d018e5ca8297696f0f27bf3e
lagrange.eth <lagrange@sorellacapital.com>
2023-12-12 00:06:25 -0500

    fixed queries
    Former-commit-id: 57cfc9b5a6f805a1b4efa32c0425c013a8d1c3e7

--- crates/brontes-database/queries/private_flow.sql
+++ crates/brontes-database/queries/private_flow.sql
@@ -13,87 +13,83 @@ WHERE tx_hash NOT IN (
 
 
 
+WITH
+    18000000 AS block_number,
+    private_flow AS (
+            SELECT toString(bt.tx_hash) as tx_hash
+            FROM
+            (
+                SELECT toString(arrayJoin(transaction_hashes)) AS tx_hash
+                FROM ethereum.blocks
+                WHERE (blocks.block_number = block_number) AND (valid = 1)
+            ) AS bt
+            WHERE bt.tx_hash NOT IN (
+                SELECT DISTINCT um.tx_hash
+                FROM ethereum.unique_mempool AS um
+                WHERE um.tx_hash IN (
+                    SELECT toString(arrayJoin(transaction_hashes)) AS tx_hash
+                    FROM ethereum.blocks
+                    WHERE (blocks.block_number = block_number) AND (valid = 1)
+                )
+            )
+        ),
+        grouped_private_flow AS (
+        SELECT 
+            groupArray(tx_hash) AS private_flow
+        FROM private_flow
+    )
+SELECT
+    length(grouped_private_flow.private_flow),
+    grouped_private_flow.private_flow
+FROM grouped_private_flow
 
-SELECT toString(tx_hash) as tx_hash
-FROM
-(
-    SELECT arrayJoin(transaction_hashes) AS tx_hash
-    FROM ethereum.blocks
-    WHERE (block_number = 18180900) AND (valid = 1)
-) AS subquery
-WHERE tx_hash NOT IN (
-    SELECT tx_hash
-    FROM ethereum.unique_mempool
-)
+
 
 
 WITH
-    relay_block AS
+    18000000 AS block_number,
+    private_flow AS
     (
-        SELECT
-            relays.block_number AS block_number,
-            toString(blocks.block_hash) AS block_hash,
-            relays.timestamp AS relay_time,
-            toString(relays.proposer_fee_recipient) AS proposer_addr,
-            relays.value AS proposer_reward
-        FROM ethereum.relays
-        INNER JOIN ethereum.blocks AS blocks ON (blocks.block_number = 18180900) AND (blocks.block_hash = ethereum.relays.block_hash) AND (blocks.valid = 1)
-        WHERE relays.block_number = 18180900
-    ),
-    relay_p2p AS
-    (
-        SELECT
-            any(relay_block.block_hash) AS block_hash,
-            any(relay_block.relay_time) AS relay_time,
-            toUInt64(round(max(cb.timestamp) / 1000)) AS p2p_time,
-            any(relay_block.proposer_addr) AS proposer_addr,
-            any(relay_block.proposer_reward) AS proposer_reward
-        FROM relay_block
-        INNER JOIN ethereum.block_observations AS cb ON relay_block.block_number = cb.block_number
-    ),
-    relay_prices AS (
-        SELECT 
-            max(bt.timestamp) as timestamp, 
-            et.address as address, 
-            round(avg(bt.ask_price + bt.bid_price)/2, 6) as price
-        FROM 
-            relay_p2p,
-            cex.normalized_quotes as bt
-        INNER JOIN ethereum.tokens AS et 
-        ON et.symbol = substring(bt.symbol, 1, length(bt.symbol) - 4)
-        WHERE 
+        SELECT groupArray(a.tx_hash)
+        FROM
+        (
+            SELECT DISTINCT arrayJoin(transaction_hashes) AS tx_hash
+            FROM ethereum.blocks
+            WHERE (blocks.block_number = block_number) AND (valid = 1)
+        ) AS a
+        LEFT JOIN
+        (
+            SELECT a.tx_hash AS tx_hash
+            FROM ethereum.unique_mempool AS um
+            INNER JOIN
             (
-                (bt.timestamp <= relay_p2p.relay_time) AND (bt.timestamp > relay_p2p.relay_time - 1000000)
-            )
-            AND substring(bt.symbol, -4) = 'USDT'
-        GROUP BY 
-            address
-    ),
-    p2p_prices AS (
-        SELECT 
-            max(bt.timestamp) as timestamp, 
-            et.address as address, 
-            round(avg(bt.ask_price + bt.bid_price)/2, 6) as price
-        FROM 
-            relay_p2p,
-            cex.normalized_quotes as bt
-        INNER JOIN ethereum.tokens AS et 
-        ON et.symbol = substring(bt.symbol, 1, length(bt.symbol) - 4)
-        WHERE 
-            (
-                (bt.timestamp <= relay_p2p.p2p_time) AND (bt.timestamp > relay_p2p.p2p_time - 1000000)
-            )
-            AND substring(bt.symbol, -4) = 'USDT'
-        GROUP BY 
-            address
-    ),
-    prices AS (
-        SELECT 
-            substring(toString(r.address), 3) AS address,
-            r.price AS relay_price,
-            p.price AS p2p_price
-        FROM relay_prices r
-        INNER JOIN p2p_prices p ON r.address = p.address
+                SELECT DISTINCT arrayJoin(transaction_hashes) AS tx_hash
+                FROM ethereum.blocks
+                WHERE (blocks.block_number = block_number) AND (valid = 1)
+            ) AS a ON a.tx_hash = um.tx_hash
+        ) AS pub ON a.tx_hash = pub.tx_hash
+        WHERE (pub.tx_hash = '') OR (pub.tx_hash IS NULL)
     )
-SELECT * FROM relay_p2p
-SETTINGS distributed_product_mode = 'allow'
+SELECT *
+FROM private_flow
+
+
+
+SELECT sub.tx_hash FROM (
+    SELECT distinct(arrayJoin(b.transaction_hashes)) AS tx_hash
+    FROM ethereum.blocks b
+    WHERE (b.block_number = 18000000) AND (b.valid = 1)
+) sub
+LEFT JOIN ethereum.unique_mempool AS um ON sub.tx_hash = um.tx_hash
+WHERE um.tx_hash IS NULL OR um.tx_hash = ''
+
+
+
+SELECT sub.tx_hash as tx FROM ethereum.unique_mempool AS um
+RIGHT JOIN (
+    SELECT distinct(arrayJoin(b.transaction_hashes)) AS tx_hash
+    FROM ethereum.blocks b
+    WHERE (b.block_number = 18000000) AND (b.valid = 1)
+) sub ON sub.tx_hash = um.tx_hash
+WHERE um.tx_hash IS NULL OR um.tx_hash = ''
+ORDER BY tx
