881cfaaf8509090c6507619aa1b7a8d074836764
Will Smith <williamqsmith11@gmail.com>
2023-12-12 21:58:58 -0500

    restructure
    Former-commit-id: 6cb2fcf8efd807d7533ac3bfcb2e81a3aa5720d7

--- crates/brontes-inspect/src/composer.rs
+++ crates/brontes-inspect/src/composer.rs
@@ -211,7 +211,8 @@ impl<'a, const N: usize> Composer<'a, N> {
             )
             .0,
             cumulative_mev_finalized_profit_usd: f64::rounding_from(
-                cum_mev_priority_fee_paid.to_scaled_rational(18) * &pre_processing.meta_data.eth_prices,
+                cum_mev_priority_fee_paid.to_scaled_rational(18)
+                    * &pre_processing.meta_data.eth_prices,
                 RoundingMode::Nearest,
             )
             .0,
