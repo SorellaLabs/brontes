79108b7dd1eceec6da57790bf1ecf93e1dbaadc0
Will Smith <williamqsmith11@gmail.com>
2023-12-15 13:35:41 -0500

    wip

--- crates/brontes-core/benches/parsing.rs
+++ crates/brontes-core/benches/parsing.rs
@@ -13,7 +13,7 @@ fn bench_tx_trace_parse(c: &mut Criterion) {
     let block = 18793182;
     let (tx, _rx) = unbounded_channel();
     let tracer = init_trace_parser(tokio::runtime::Handle::current().clone(), tx);
-    c.bench_function("29,995,104 gas block", |b| {
+    c.bench_function("29,995,104 gas block", move |b| {
         b.to_async(rt)
             .iter(|| black_box(tracer.execute_block(block)))
     });
