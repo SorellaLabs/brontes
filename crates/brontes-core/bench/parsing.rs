bed6fdb50e333ea3b4833943985bccfe123e3626
Will Smith <williamqsmith11@gmail.com>
2023-12-15 13:21:54 -0500

    wip

---
+++ crates/brontes-core/bench/parsing.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+
+use brontes_core::{decoding::parser::TraceParser, init_tracing, test_utils::init_trace_parser};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn bench_tx_trace_parse(c: &mut Criterion) {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let block = 18793182;
+    let tracer = init_trace_parser(tokio::runtime::Handle::current().clone(), tx);
+    c.bench_function("29,995,104 gas block", |b| {
+        b.to_async(rt).iter(|| black_box(tracer.execute_block(block)))
+    );
+}
+
+criterion_group!(parsing, bench_tx_trace_parse);
+criterion_main!(parsing);
