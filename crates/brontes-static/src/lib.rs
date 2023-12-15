2f343f1ff4c98e653718a146bd9a8138bfc4ee0e
Will Smith <williamqsmith11@gmail.com>
2023-12-15 00:44:33 -0500

    wip

--- crates/brontes-static/src/lib.rs
+++ crates/brontes-static/src/lib.rs
@@ -2,6 +2,8 @@ include!(concat!(env!("ABI_BUILD_DIR"), "/token_to_addresses.rs"));
 include!(concat!(env!("ABI_BUILD_DIR"), "/protocol_addr_set.rs"));
 include!(concat!(env!("ABI_BUILD_DIR"), "/bindings.rs"));
 
+use alloy_sol_macro::sol;
+
 pub trait TryDecodeSol {
     type DecodingType;
 
