e2ef6f84b0e539633d9f13334b3295f55feaef81
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-12-15 05:39:08 -0500

    bruh
    Former-commit-id: 96ab3f3a4caaa4954624be509346695c775933de

--- crates/brontes-classifier/src/impls/curve/curve_crypto_swap.rs
+++ crates/brontes-classifier/src/impls/curve/curve_crypto_swap.rs
@@ -3,24 +3,25 @@ use brontes_macros::{action_dispatch, action_impl};
 use brontes_types::normalized_actions::{Actions, NormalizedBurn, NormalizedMint, NormalizedSwap};
 use reth_primitives::{Address, Bytes, U256};
 use reth_rpc_types::Log;
-
+use std::str::FromStr;
 use crate::{
     enum_unwrap, ActionCollection,
-    CurveCryptoSwap::{exchange_0Call, exchange_1Call, exchange_2Call, TokenExchange},
+    CurveCryptoSwap::{exchange_0Call, exchange_1Call, exchange_2Call, exchange_underlying_0Call,exchange_underlying_1Call, TokenExchange, CurveCryptoSwapCalls},
     IntoAction, StaticReturnBindings, ADDRESS_TO_TOKENS_2_POOL,
 };
 
+pub const ETH: Address = Address::from_str("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE").unwrap();
+pub const WETH: Address = Address::from_str("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").unwrap();
 action_impl!(
     CurveCryptoExchange0,
     Swap,
-    TokenExchange,
     exchange_0Call,
+    TokenExchange,
     CurveCryptoSwap,
     logs: true,
     |index, from_address: Address, target_address: Address, log_data: Option<TokenExchange> | {
         let log = log_data?;
         let [token_0, token_1] = ADDRESS_TO_TOKENS_2_POOL.get(&*target_address.0).copied()?;
-
 
         if log.sold_id ==  U256::ZERO {
             return Some(NormalizedSwap {
@@ -49,15 +50,28 @@ action_impl!(
 action_impl!(
     CurveCryptoExchange1,
     Swap,
+    exchange_1Call, 
     TokenExchange,
-    exchange_1Call,
     CurveCryptoSwap,
     logs: true,
-    |index, from_address: Address, target_address: Address, log_data: Option<TokenExchange> | {
+    call_data: true,
+    |index, from_address: Address, target_address: Address, log_data: Option<TokenExchange>, call_data: exchange_1Call | {
+
         let log = log_data?;
         let [token_0, token_1] = ADDRESS_TO_TOKENS_2_POOL.get(&*target_address.0).copied()?;
 
-        if log.sold_id ==  U256::ZERO {
+        let is_eth = call_data.use_eth;
+
+        // Check if ETH is used and adjust token_in or token_out accordingly
+        if is_eth {
+            if log.sold_id == U256::ZERO && token_0 == WETH {
+                token_0 = ETH;
+            } else if log.sold_id != U256::ZERO && token_1 == WETH {
+                token_1 = ETH;
+            }
+        }
+
+        if log.sold_id == U256::ZERO {
             return Some(NormalizedSwap {
                 pool: target_address,
                 index,
@@ -81,18 +95,26 @@ action_impl!(
     }
 );
 
-/// I don't know who coded this contract, but I wish them great harm.
+// I don't know who coded this contract, but I wish them great harm.
 action_impl!(
 CurveCryptoExchangeUnderlying,
 Swap,
 TokenExchange,
-exchange_underlyingCall,
+exchange_underlying_0Call,
 CurveCryptoSwap,
 logs: true,
-|index, from_address: Address, target_address: Address, log_data: Option<TokenExchange> | {
+|index, from_address: Address, target_address: Address, log_data: Option<TokenExchange>| {
         let log = log_data?;
         let [token_0, token_1] = ADDRESS_TO_TOKENS_2_POOL.get(&*target_address.0).copied()?;
 
+
+         // Replace WETH with ETH for token_in or token_out
+         if token_0 == WETH {
+            token_0 = ETH;
+        }
+        if token_1 == WETH {
+            token_1 = ETH;
+        }
 
         if log.sold_id ==  U256::ZERO {
             return Some(NormalizedSwap {
