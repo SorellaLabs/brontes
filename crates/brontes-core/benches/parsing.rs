6b75855900a846bab30af425a762775e10b9ffc3
Will Smith <williamqsmith11@gmail.com>
2023-12-15 13:31:06 -0500

    wip

--- crates/brontes-core/benches/parsing.rs
+++ crates/brontes-core/benches/parsing.rs
@@ -1,8 +1,8 @@
 use std::collections::HashSet;
 
-use tokio::sync::mpsc::unbounded_channel;
 use brontes_core::{decoding::parser::TraceParser, init_tracing, test_utils::init_trace_parser};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use tokio::sync::mpsc::unbounded_channel;
 
 fn bench_tx_trace_parse(c: &mut Criterion) {
     let rt = tokio::runtime::Builder::new_multi_thread()
@@ -14,9 +14,9 @@ fn bench_tx_trace_parse(c: &mut Criterion) {
     let (tx, _rx) = unbounded_channel();
     let tracer = init_trace_parser(tokio::runtime::Handle::current().clone(), tx);
     c.bench_function("29,995,104 gas block", |b| {
-        b.to_async(rt).iter(|| black_box(tracer.execute_block(block)))
-    }
-    );
+        b.to_async(rt)
+            .iter(|| black_box(tracer.execute_block(block)))
+    });
 }
 
 criterion_group!(parsing, bench_tx_trace_parse);
