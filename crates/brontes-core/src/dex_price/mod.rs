23e601f6cdc9286f2a6009fcdc7469563f3cd6aa
Will Smith <williamqsmith11@gmail.com>
2023-12-15 11:09:07 -0500

    wip
    Former-commit-id: a0f5d99dc207a4da95175175c78856c418186895

--- crates/brontes-core/src/dex_price/mod.rs
+++ crates/brontes-core/src/dex_price/mod.rs
@@ -7,16 +7,21 @@ use alloy_sol_macro::sol;
 use alloy_sol_types::SolCall;
 use alloy_transport::TransportResult;
 use alloy_transport_http::Http;
-use brontes_database::{database::Database, graph::PriceGraph, DexQuote, Pair, QuotesMap};
+use brontes_database::{
+    database::Database,
+    graph::{PriceGraph, TrackableGraph},
+    DexQuote, Pair, QuotesMap,
+};
 use brontes_types::cache_decimals;
 use futures::{future::join, join, stream::FuturesUnordered, Future, StreamExt};
 use itertools::Itertools;
 use malachite::Rational;
 use once_cell::sync::Lazy;
 use phf::phf_map;
-use reth_primitives::revm_primitives::HashMap;
+use reth_primitives::revm_primitives::{HashMap, HashSet};
 use reth_rpc_types::trace::parity::StateDiff;
 use tokio::sync::futures;
+use tracing::error;
 
 use crate::TracingProvider;
 
@@ -114,15 +119,43 @@ impl<T: TracingProvider> DexPricing<T> {
                 (k, DexQuote(res))
             })
             .collect();
-        let map = QuotesMap::wrap(res);
 
+        let map = QuotesMap::wrap(res);
         let (disjointed, graph) = PriceGraph::from_quotes_disjoint(map);
-        let dummy_graph =
-            PriceGraph::from_quotes(QuotesMap::wrap(DEX_TOKEN_MAP.into_iter().collect()));
+        if disjointed.is_empty() {
+            // TODO: quick return here
+        }
+
+        // make graph of all known tokens
+        let dummy_graph = TrackableGraph::from_hash_map(
+            DEX_TOKEN_MAP
+                .into_iter()
+                .map(|(k, _)| ((k.0, k.1), ()))
+                .collect(),
+        );
 
         let combos = disjointed.into_iter().combinations(2).collect::<Vec<_>>();
+        let mut needed_pairs = HashSet::new();
 
-        for combo in combos {}
+        for mut combo in combos {
+            let start = combo.remove(0);
+            let end = combo.remove(0);
+
+            if let Some(path) = dummy_graph.get_path(start, end) {
+                needed_pairs.extend(
+                    path.into_iter()
+                        .tuple_windows()
+                        .map(|(t0, t1)| Pair(t0, t1)),
+                )
+            } else {
+                error!(
+                    ?start,
+                    ?end,
+                    "no path between tokens over all known dexes... if you see this its prob \
+                     worth a screenshot bc of how rare this would be"
+                );
+            }
+        }
     }
 }
 
