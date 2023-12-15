65745cf043b950576c0a29eb57c4c6a3e3ba5c0f
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-12-14 23:10:03 -0500

    testing

--- crates/brontes-inspect/src/cex_dex.rs
+++ crates/brontes-inspect/src/cex_dex.rs
@@ -290,7 +290,7 @@ mod tests {
         let block = tracer.execute_block(block_num).await.unwrap();
         let metadata = db.get_metadata(block_num).await;
 
-        let tx = block.0.clone().into_iter().take(40).collect::<Vec<_>>();
+        let tx = block.0.clone().into_iter().take(1).collect::<Vec<_>>();
         let (missing_token_decimals, tree) = classifier.build_tree(tx, block.1);
         let tree = Arc::new(tree);
 
