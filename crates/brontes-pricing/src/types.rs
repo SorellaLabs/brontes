c8a3f6afd7d95d861e3f740de329da3b294a13e1
Will Smith <williamqsmith11@gmail.com>
2023-12-16 12:02:15 -0500

    wip

--- crates/brontes-pricing/src/types.rs
+++ crates/brontes-pricing/src/types.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use alloy_primitives::Address;
 use brontes_types::{extra_processing::Pair, normalized_actions::Actions};
+// use crate::exchanges::{uniswap_v2::UniswapV2Pool, uniswap_v3::UniswapV3Pool};
+use malachite::Rational;
 
 #[derive(Debug, Clone)]
 pub struct PoolKey {
@@ -11,12 +13,47 @@ pub struct PoolKey {
     pub tx_idx: usize,
 }
 
+pub struct DexPrices {
+    quotes: DexQuotes,
+    state:  Arc<HashMap<PoolKey, PoolState>>,
+}
+
+impl DexPrices {
+    pub fn price_after(&self, pair: Pair, tx: usize) -> Rational {
+        // self.quotes
+        todo!()
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct DexQuotes(Vec<Option<HashMap<Pair, Vec<Vec<PoolKey>>>>>);
 
+impl DexQuotes {
+    pub fn get_pair_keys(&self, pair: Pair, tx: usize) -> &Vec<Vec<PoolKey>> {
+        self.0
+            .get(tx)
+            .expect("this should never be reached")
+            .as_ref()
+            .expect("unreachable")
+            .get(&pair)
+            .unwrap()
+    }
+}
+
 pub enum PoolState {
-    UniswapV2(),
-    UniswapV3(),
+    UniswapV2(()),
+    UniswapV3(()),
+}
+
+pub enum PairPriceMessage {
+    Update(PoolUpdate),
+    Finished(u64),
+}
+
+pub struct PoolUpdate {
+    block:  u64,
+    tx_idx: usize,
+    action: Actions,
 }
 
 impl PoolState {
