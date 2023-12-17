8b1617a4781a4baeaa4bfc657e534e4d11372f4d
Will Smith <williamqsmith11@gmail.com>
2023-12-16 19:24:51 -0500

    wip

--- crates/bin/src/data_batching.rs
+++ crates/bin/src/data_batching.rs
@@ -8,6 +8,7 @@ use alloy_providers::provider::Provider;
 use alloy_transport_http::Http;
 use brontes_classifier::Classifier;
 use brontes_core::decoding::{Parser, TracingProvider};
+use brontes_database_libmdbx::Libmdbx;
 use brontes_pricing::{types::DexPrices, BrontesBatchPricer};
 use futures::Future;
 
@@ -15,13 +16,14 @@ pub struct DataBatching<'db, T: TracingProvider> {
     parser:        &'db Parser<'db, T>,
     provider:      &'db Provider<Http<reqwest::Client>>,
     classifier:    &'db Classifier<'db>,
+    libmdbx:       &'db Libmdbx,
     dex_price_map: BrontesBatchPricer,
 }
 
 impl<T: TracingProvider> Future for DataBatching<'_, T> {
     type Output = HashMap<u64, DexPrices>;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         Poll::Pending
     }
 }
