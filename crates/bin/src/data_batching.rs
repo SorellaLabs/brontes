82241993956a599f148d3c390d9a979e5d9329c7
Will Smith <williamqsmith11@gmail.com>
2023-12-16 19:13:46 -0500

    finish pricing core logic
    Former-commit-id: a966ced965b8cc6c7b53e721856f32998e0f0617

--- crates/bin/src/data_batching.rs
+++ crates/bin/src/data_batching.rs
@@ -1,3 +1,27 @@
-// pub struct DataBatching<'db> {
-//     parser: &'d Parser<'db, T>,
-// }
+use std::{
+    collections::HashMap,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use alloy_providers::provider::Provider;
+use alloy_transport_http::Http;
+use brontes_classifier::Classifier;
+use brontes_core::decoding::{Parser, TracingProvider};
+use brontes_pricing::{types::DexPrices, BrontesBatchPricer};
+use futures::Future;
+
+pub struct DataBatching<'db, T: TracingProvider> {
+    parser:        &'db Parser<'db, T>,
+    provider:      &'db Provider<Http<reqwest::Client>>,
+    classifier:    &'db Classifier<'db>,
+    dex_price_map: BrontesBatchPricer,
+}
+
+impl<T: TracingProvider> Future for DataBatching<'_, T> {
+    type Output = HashMap<u64, DexPrices>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Pending
+    }
+}
