482463f38e7d28051237fe01cb84567a15bb9d6b
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-12-18 05:23:14 -0500

    remove warnings

--- crates/brontes-types/src/lib.rs
+++ crates/brontes-types/src/lib.rs
@@ -21,7 +21,7 @@ pub mod test_utils;
 
 static DYN_MAP: OnceLock<RwLock<HashMap<[u8; 20], u8>>> = OnceLock::new();
 
-pub fn try_get_decimals(address: &[u8; 20]) -> Option<u8> {
+pub fn try_get_decimals(_address: &[u8; 20]) -> Option<u8> {
     // if let Some(value) = TOKEN_TO_DECIMALS.get(address) {
     //     Some(*value)
     // } else {
