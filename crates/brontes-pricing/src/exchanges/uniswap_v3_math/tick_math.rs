7d42c970d9439225f0481b5210c0a997fd3785b9
Will Smith <williamqsmith11@gmail.com>
2023-12-18 05:09:52 -0500

    wip

--- crates/brontes-pricing/src/exchanges/uniswap_v3_math/tick_math.rs
+++ crates/brontes-pricing/src/exchanges/uniswap_v3_math/tick_math.rs
@@ -26,67 +26,67 @@ pub fn get_sqrt_ratio_at_tick(tick: i32) -> Result<U256, UniswapV3MathError> {
     }
 
     let mut ratio = if abs_tick & (U256::from(0x1)) != U256::ZERO {
-        U256::from(0xfffcb933bd6fad37aa2d162d1a594001)
+        U256::from(0xfffcb933bd6fad37aa2d162d1a594001 as u128)
     } else {
         U256::from_str_radix("100000000000000000000000000000000", 16).unwrap()
     };
 
     if !(abs_tick & (U256::from(0x2))).is_zero() {
-        ratio = (ratio * U256::from(0xfff97272373d413259a46990580e213a)) >> 128
+        ratio = (ratio * U256::from(0xfff97272373d413259a46990580e213a as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x4))).is_zero() {
-        ratio = (ratio * U256::from(0xfff2e50f5f656932ef12357cf3c7fdcc)) >> 128
+        ratio = (ratio * U256::from(0xfff2e50f5f656932ef12357cf3c7fdcc as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x8))).is_zero() {
-        ratio = (ratio * U256::from(0xffe5caca7e10e4e61c3624eaa0941cd0)) >> 128
+        ratio = (ratio * U256::from(0xffe5caca7e10e4e61c3624eaa0941cd0 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x10))).is_zero() {
-        ratio = (ratio * U256::from(0xffcb9843d60f6159c9db58835c926644)) >> 128
+        ratio = (ratio * U256::from(0xffcb9843d60f6159c9db58835c926644 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x20))).is_zero() {
-        ratio = (ratio * U256::from(0xff973b41fa98c081472e6896dfb254c0)) >> 128
+        ratio = (ratio * U256::from(0xff973b41fa98c081472e6896dfb254c0 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x40))).is_zero() {
-        ratio = (ratio * U256::from(0xff2ea16466c96a3843ec78b326b52861)) >> 128
+        ratio = (ratio * U256::from(0xff2ea16466c96a3843ec78b326b52861 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x80))).is_zero() {
-        ratio = (ratio * U256::from(0xfe5dee046a99a2a811c461f1969c3053)) >> 128
+        ratio = (ratio * U256::from(0xfe5dee046a99a2a811c461f1969c3053 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x100))).is_zero() {
-        ratio = (ratio * U256::from(0xfcbe86c7900a88aedcffc83b479aa3a4)) >> 128
+        ratio = (ratio * U256::from(0xfcbe86c7900a88aedcffc83b479aa3a4 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x200))).is_zero() {
-        ratio = (ratio * U256::from(0xf987a7253ac413176f2b074cf7815e54)) >> 128
+        ratio = (ratio * U256::from(0xf987a7253ac413176f2b074cf7815e54 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x400))).is_zero() {
-        ratio = (ratio * U256::from(0xf3392b0822b70005940c7a398e4b70f3)) >> 128
+        ratio = (ratio * U256::from(0xf3392b0822b70005940c7a398e4b70f3 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x800))).is_zero() {
-        ratio = (ratio * U256::from(0xe7159475a2c29b7443b29c7fa6e889d9)) >> 128
+        ratio = (ratio * U256::from(0xe7159475a2c29b7443b29c7fa6e889d9 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x1000))).is_zero() {
-        ratio = (ratio * U256::from(0xd097f3bdfd2022b8845ad8f792aa5825)) >> 128
+        ratio = (ratio * U256::from(0xd097f3bdfd2022b8845ad8f792aa5825 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x2000))).is_zero() {
-        ratio = (ratio * U256::from(0xa9f746462d870fdf8a65dc1f90e061e5)) >> 128
+        ratio = (ratio * U256::from(0xa9f746462d870fdf8a65dc1f90e061e5 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x4000))).is_zero() {
-        ratio = (ratio * U256::from(0x70d869a156d2a1b890bb3df62baf32f7)) >> 128
+        ratio = (ratio * U256::from(0x70d869a156d2a1b890bb3df62baf32f7 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x8000))).is_zero() {
-        ratio = (ratio * U256::from(0x31be135f97d08fd981231505542fcfa6)) >> 128
+        ratio = (ratio * U256::from(0x31be135f97d08fd981231505542fcfa6 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x10000))).is_zero() {
-        ratio = (ratio * U256::from(0x9aa508b5b7a84e1c677de54f3e99bc9)) >> 128
+        ratio = (ratio * U256::from(0x9aa508b5b7a84e1c677de54f3e99bc9 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x20000))).is_zero() {
-        ratio = (ratio * U256::from(0x5d6af8dedb81196699c329225ee604)) >> 128
+        ratio = (ratio * U256::from(0x5d6af8dedb81196699c329225ee604 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x40000))).is_zero() {
-        ratio = (ratio * U256::from(0x2216e584f5fa1ea926041bedfe98)) >> 128
+        ratio = (ratio * U256::from(0x2216e584f5fa1ea926041bedfe98 as u128)) >> 128
     }
     if !(abs_tick & (U256::from(0x80000))).is_zero() {
-        ratio = (ratio * U256::from(0x48a170391f7dc42444e8fa2)) >> 128
+        ratio = (ratio * U256::from(0x48a170391f7dc42444e8fa2 as u128)) >> 128
     }
 
     if tick > 0 {
@@ -110,7 +110,7 @@ pub fn get_tick_at_sqrt_ratio(sqrt_price_x_96: U256) -> Result<i32, UniswapV3Mat
     let mut r = ratio;
     let mut msb = U256::ZERO;
 
-    let mut f: U256 = if r > U256::from(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) {
+    let mut f: U256 = if r > U256::from(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF as u128) {
         U256::from(1).shl(U256::from(7))
     } else {
         U256::ZERO
@@ -118,7 +118,7 @@ pub fn get_tick_at_sqrt_ratio(sqrt_price_x_96: U256) -> Result<i32, UniswapV3Mat
     msb = msb.bitor(f);
     r = r.shr(f);
 
-    f = if r > U256::from(0xFFFFFFFFFFFFFFFF) {
+    f = if r > U256::from(0xFFFFFFFFFFFFFFFF as u128) {
         U256::from(1).shl(U256::from(6))
     } else {
         U256::ZERO
@@ -127,7 +127,11 @@ pub fn get_tick_at_sqrt_ratio(sqrt_price_x_96: U256) -> Result<i32, UniswapV3Mat
     msb = msb.bitor(f);
     r = r.shr(f);
 
-    f = if r > U256::from(0xFFFFFFFF) { U256::from(1).shl(U256::from(5)) } else { U256::ZERO };
+    f = if r > U256::from(0xFFFFFFFF as u128) {
+        U256::from(1).shl(U256::from(5))
+    } else {
+        U256::ZERO
+    };
     msb = msb.bitor(f);
     r = r.shr(f);
 
