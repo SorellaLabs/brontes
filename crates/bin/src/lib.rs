f5ee2a5baeb3bd0858ba36b88f88d89e63b9f8ba
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-12-18 04:11:34 -0500

    tip tracker, wip waiting on dex pricing task to be finisished
    Former-commit-id: 8ab9415713508931662001d41566c4944b0749d5

--- crates/bin/src/lib.rs
+++ crates/bin/src/lib.rs
@@ -7,27 +7,36 @@ use brontes_classifier::Classifier;
 use brontes_core::decoding::{Parser, TracingProvider};
 use brontes_database_libmdbx::Libmdbx;
 use brontes_inspect::Inspector;
-use futures::{stream::FuturesUnordered, Future, StreamExt};
+use futures::{stream::FuturesUnordered, Future, FutureExt, StreamExt};
 use tracing::info;
 
 mod banner;
 mod block_inspector;
+mod tip_inspector;
 
 use block_inspector::BlockInspector;
+use tip_inspector::TipInspector;
 
 pub const PROMETHEUS_ENDPOINT_IP: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
 pub const PROMETHEUS_ENDPOINT_PORT: u16 = 6423;
+
+enum Mode {
+    Historical,
+    Tip,
+}
 
 pub struct Brontes<'inspector, const N: usize, T: TracingProvider> {
     current_block:    u64,
     end_block:        Option<u64>,
     chain_tip:        u64,
+    mode:             Mode,
     max_tasks:        u64,
     parser:           &'inspector Parser<'inspector, T>,
     classifier:       &'inspector Classifier<'inspector>,
     inspectors:       &'inspector [&'inspector Box<dyn Inspector>; N],
     database:         &'inspector Libmdbx,
     block_inspectors: FuturesUnordered<BlockInspector<'inspector, N, T>>,
+    tip_inspector:    Option<TipInspector<'inspector, N, T>>,
 }
 
 impl<'inspector, const N: usize, T: TracingProvider> Brontes<'inspector, N, T> {
@@ -45,12 +54,14 @@ impl<'inspector, const N: usize, T: TracingProvider> Brontes<'inspector, N, T> {
             current_block: init_block,
             end_block,
             chain_tip,
+            mode: Mode::Historical,
             max_tasks,
             parser,
             database,
             classifier,
             inspectors,
             block_inspectors: FuturesUnordered::new(),
+            tip_inspector: None,
         };
 
         let max_blocks = match end_block {
@@ -78,6 +89,18 @@ impl<'inspector, const N: usize, T: TracingProvider> Brontes<'inspector, N, T> {
         self.block_inspectors.push(inspector);
     }
 
+    fn spawn_tip_inspector(&mut self) {
+        let inspector = TipInspector::new(
+            self.parser,
+            self.database,
+            self.classifier,
+            self.inspectors,
+            self.chain_tip,
+        );
+        info!(block_number = self.chain_tip, "Finished historical inspectors, now tracking tip");
+        self.tip_inspector = Some(inspector);
+    }
+
     fn start_block_inspector(&mut self) -> bool {
         // reached end of line
         if self.block_inspectors.len() >= self.max_tasks as usize
@@ -89,10 +112,13 @@ impl<'inspector, const N: usize, T: TracingProvider> Brontes<'inspector, N, T> {
         #[cfg(not(feature = "local"))]
         if self.current_block >= self.chain_tip {
             if let Ok(chain_tip) = self.parser.get_latest_block_number() {
-                self.chain_tip = chain_tip;
-            } else {
-                // no new block ready
-                return false
+                if chain_tip > self.chain_tip {
+                    self.chain_tip = chain_tip;
+                } else {
+                    self.mode = Mode::Tip;
+                    self.spawn_tip_inspector();
+                    return false
+                }
             }
         }
 
@@ -137,15 +163,29 @@ impl<const N: usize, T: TracingProvider> Future for Brontes<'_, N, T> {
         // And tokio's docs on cooperative scheduling <https://docs.rs/tokio/latest/tokio/task/#cooperative-scheduling>
         let mut iters = 1024;
         loop {
-            if Some(self.current_block) >= self.end_block && self.block_inspectors.is_empty() {
-                return Poll::Ready(())
-            }
+            match self.mode {
+                Mode::Historical => {
+                    if Some(self.current_block) >= self.end_block
+                        && self.block_inspectors.is_empty()
+                    {
+                        return Poll::Ready(())
+                    }
 
-            if self.start_block_inspector() {
-                self.spawn_block_inspector();
-            }
+                    if self.start_block_inspector() {
+                        self.spawn_block_inspector();
+                    }
 
-            self.progress_block_inspectors(cx);
+                    self.progress_block_inspectors(cx);
+                }
+                Mode::Tip => {
+                    if let Some(tip_inspector) = self.tip_inspector.as_mut() {
+                        match tip_inspector.poll_unpin(cx) {
+                            Poll::Ready(()) => return Poll::Ready(()),
+                            Poll::Pending => {}
+                        }
+                    }
+                }
+            }
 
             iters -= 1;
             if iters == 0 {
---
+++ crates/bin/src/tip_inspector.rs
@@ -0,0 +1,143 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use brontes_classifier::Classifier;
+use brontes_core::decoding::{Parser, TracingProvider};
+use brontes_database::Metadata;
+use brontes_database_libmdbx::Libmdbx;
+use brontes_inspect::{composer::Composer, Inspector};
+use brontes_pricing::types::DexPrices;
+use brontes_types::{
+    classified_mev::{ClassifiedMev, MevBlock, SpecificMev},
+    normalized_actions::Actions,
+    tree::TimeTree,
+};
+use futures::{Future, FutureExt};
+use tracing::info;
+
+type CollectionFut<'a> = Pin<Box<dyn Future<Output = (Metadata, TimeTree<Actions>)> + Send + 'a>>;
+
+pub struct TipInspector<'inspector, const N: usize, T: TracingProvider> {
+    block_number: u64,
+
+    parser:            &'inspector Parser<'inspector, T>,
+    classifier:        &'inspector Classifier<'inspector>,
+    database:          &'inspector Libmdbx,
+    composer:          Composer<'inspector, N>,
+    // pending future data
+    classifier_future: Option<CollectionFut<'inspector>>,
+    // pending insertion data
+    insertion_future:  Option<Pin<Box<dyn Future<Output = ()> + Send + Sync + 'inspector>>>,
+}
+
+impl<'inspector, const N: usize, T: TracingProvider> TipInspector<'inspector, N, T> {
+    pub fn new(
+        parser: &'inspector Parser<'inspector, T>,
+        database: &'inspector Libmdbx,
+        classifier: &'inspector Classifier,
+        inspectors: &'inspector [&'inspector Box<dyn Inspector>; N],
+        block_number: u64,
+    ) -> Self {
+        Self {
+            block_number,
+            parser,
+            database,
+            classifier,
+            composer: Composer::new(inspectors),
+            classifier_future: None,
+            insertion_future: None,
+        }
+    }
+
+    fn start_collection(&mut self) {
+        info!(block_number = self.block_number, "starting collection of data");
+        let parser_fut = self.parser.execute(self.block_number);
+        let labeller_fut = self.database.get_metadata(self.block_number);
+
+        let classifier_fut = Box::pin(async {
+            let (traces, header) = parser_fut.await.unwrap().unwrap();
+            info!("Got {} traces + header", traces.len());
+            let (_extra_data, mut tree) = self.classifier.build_tree(traces, header);
+
+            let meta = labeller_fut.await;
+            tree.eth_price = meta.eth_prices.clone();
+            let tmp_meta = meta.into_finalized_metadata(DexPrices::new());
+
+            (tmp_meta, tree)
+        });
+
+        self.classifier_future = Some(classifier_fut);
+    }
+
+    fn on_inspectors_finish(
+        &mut self,
+        results: (MevBlock, Vec<(ClassifiedMev, Box<dyn SpecificMev>)>),
+    ) {
+        info!(
+            block_number = self.block_number,
+            "inserting the collected results \n {:#?}", results
+        );
+        self.insertion_future =
+            Some(Box::pin(self.database.insert_classified_data(results.0, results.1)));
+    }
+
+    fn progress_futures(&mut self, cx: &mut Context<'_>) {
+        if let Some(mut collection_fut) = self.classifier_future.take() {
+            match collection_fut.poll_unpin(cx) {
+                Poll::Ready((meta_data, tree)) => {
+                    self.composer.on_new_tree(tree.into(), meta_data.into());
+                }
+                Poll::Pending => {
+                    self.classifier_future = Some(collection_fut);
+                    return
+                }
+            }
+        }
+
+        if !self.composer.is_finished() {
+            if let Poll::Ready(data) = self.composer.poll_unpin(cx) {
+                self.on_inspectors_finish(data);
+            }
+        }
+
+        if let Some(mut insertion_future) = self.insertion_future.take() {
+            match insertion_future.poll_unpin(cx) {
+                Poll::Ready(_) => {}
+                Poll::Pending => {
+                    self.insertion_future = Some(insertion_future);
+                }
+            }
+        }
+    }
+}
+
+impl<const N: usize, T: TracingProvider> Future for TipInspector<'_, N, T> {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // If the classifier_future is None (not started yet), start the collection
+        // phase
+        if self.classifier_future.is_none()
+            && !self.composer.is_processing()
+            && self.insertion_future.is_none()
+        {
+            self.start_collection();
+        }
+
+        self.progress_futures(cx);
+
+        // Decide when to finish the BlockInspector's future.
+        // Finish when both classifier and insertion futures are done.
+        if self.classifier_future.is_none()
+            && !self.composer.is_processing()
+            && self.insertion_future.is_none()
+        {
+            info!(block_number = self.block_number, "finished inspecting block");
+            Poll::Ready(())
+        } else {
+            Poll::Pending
+        }
+    }
+}
