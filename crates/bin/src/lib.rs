7a1c6248d18931b185594bdbf71057b033a19d8b
Will Smith <williamqsmith11@gmail.com>
2023-12-19 03:01:34 -0500

    finish default
    Former-commit-id: 8163de38a89116cc8db31c483bc3f7e799fbcb8e

--- crates/bin/src/lib.rs
+++ crates/bin/src/lib.rs
@@ -17,6 +17,7 @@ mod tip_inspector;
 
 mod data_batching;
 use block_inspector::BlockInspector;
+pub use data_batching::DataBatching;
 use tip_inspector::TipInspector;
 
 pub const PROMETHEUS_ENDPOINT_IP: [u8; 4] = [127u8, 0u8, 0u8, 1u8];
