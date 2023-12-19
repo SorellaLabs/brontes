87392f1dc65224dfa781a3cb827d4151ae881da1
Will Smith <williamqsmith11@gmail.com>
2023-12-19 06:53:24 -0500

    wip

--- crates/brontes-pricing/src/exchanges/lazy.rs
+++ crates/brontes-pricing/src/exchanges/lazy.rs
@@ -10,13 +10,17 @@ use brontes_types::{exchanges::StaticBindingsDb, traits::TracingProvider};
 use futures::{stream::FuturesUnordered, Future, Stream, StreamExt};
 use tracing::{error, info};
 
-use crate::{types::PoolState, uniswap_v2::UniswapV2Pool, uniswap_v3::UniswapV3Pool, PoolUpdate};
+use crate::{
+    errors::AmmError, types::PoolState, uniswap_v2::UniswapV2Pool, uniswap_v3::UniswapV3Pool,
+    PoolUpdate,
+};
 
 pub struct LazyExchangeLoader<T: TracingProvider> {
     provider:          Arc<T>,
     pool_buf:          HashMap<Address, Vec<PoolUpdate>>,
-    pool_load_futures:
-        FuturesUnordered<Pin<Box<dyn Future<Output = (u64, Address, PoolState)> + Send>>>,
+    pool_load_futures: FuturesUnordered<
+        Pin<Box<dyn Future<Output = Result<(u64, Address, PoolState), (u64, AmmError)>> + Send>>,
+    >,
     // the different blocks that we are currently fetching
     req_per_block:     HashMap<u64, u64>,
 }
@@ -50,24 +54,24 @@ impl<T: TracingProvider> LazyExchangeLoader<T> {
                 self.pool_load_futures.push(Box::pin(async move {
                     let pool = UniswapV2Pool::new_load_on_block(address, provider, block_number)
                         .await
-                        .unwrap();
-                    (
+                        .map_err(|e| (block_number, e))?;
+                    Ok((
                         block_number,
                         address,
                         PoolState::new(crate::types::PoolVariants::UniswapV2(pool)),
-                    )
+                    ))
                 }))
             }
             StaticBindingsDb::UniswapV3 | StaticBindingsDb::SushiSwapV3 => {
                 self.pool_load_futures.push(Box::pin(async move {
                     let pool = UniswapV3Pool::new_from_address(address, block_number, provider)
                         .await
-                        .unwrap();
-                    (
+                        .map_err(|e| (block_number, e))?;
+                    Ok((
                         block_number,
                         address,
                         PoolState::new(crate::types::PoolVariants::UniswapV3(pool)),
-                    )
+                    ))
                 }));
             }
             rest @ _ => {
@@ -99,17 +103,31 @@ impl<T: TracingProvider> Stream for LazyExchangeLoader<T> {
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
-        if let Poll::Ready(Some((block, pool, state))) = self.pool_load_futures.poll_next_unpin(cx)
-        {
-            info!("loaded pool");
-            if let Entry::Occupied(mut o) = self.req_per_block.entry(block) {
-                *(o.get_mut()) -= 1;
-            } else {
-                unreachable!()
-            }
+        if let Poll::Ready(Some((result))) = self.pool_load_futures.poll_next_unpin(cx) {
+            match result {
+                Ok((block, addr, state)) => {
+                    info!("loaded pool");
+                    if let Entry::Occupied(mut o) = self.req_per_block.entry(block) {
+                        *(o.get_mut()) -= 1;
+                    } else {
+                        unreachable!()
+                    }
 
-            let buf = self.pool_buf.remove(&pool).unwrap_or(vec![]);
-            return Poll::Ready(Some((state, buf)))
+                    let buf = self.pool_buf.remove(&addr).unwrap_or(vec![]);
+                    return Poll::Ready(Some((state, buf)))
+                }
+                Err((block, e)) => {
+                    error!(?e, "failed to load pool");
+
+                    if let Entry::Occupied(mut o) = self.req_per_block.entry(block) {
+                        *(o.get_mut()) -= 1;
+                    } else {
+                        unreachable!()
+                    }
+
+                    return Poll::Pending
+                }
+            }
         } else {
             Poll::Pending
         }
