10c198ef4e427a5dbebb0acdc47c83430bb3f243
lagrange.eth <lagrange@sorellacapital.com>
2023-12-21 06:19:49 -0500

    hope fix
    Former-commit-id: 7672d32af5add452465f1a6bf20f7dd205e6db93

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -204,7 +204,7 @@ async fn init_brontes(init_config: Init) -> Result<(), Box<dyn Error>> {
                     .tables_to_init
                     .unwrap_or({
                         if init_config.download_dex_pricing {
-                            Tables::ALL.to_vec()
+                            vec![Tables::CexPrice] //Tables::ALL.to_vec()
                         } else {
                             Tables::ALL_NO_DEX.to_vec()
                         }
