c14256c7a73fda3d5e5f748257862338705ed74a
Will Smith <williamqsmith11@gmail.com>
2023-12-26 23:43:53 -0500

    add spammy log back
    Former-commit-id: db3326e2b47e5d8b0a185ada90d766bf028a803b

--- crates/bin/src/data_batching.rs
+++ crates/bin/src/data_batching.rs
@@ -314,6 +314,8 @@ impl<const N: usize> Future for ResultProcessing<'_, N> {
                 block_details.cumulative_mev_finalized_profit_usd
             );
 
+            println!("{:#?}", mev_details);
+
             self.database
                 .insert_classified_data(block_details, mev_details);
 
