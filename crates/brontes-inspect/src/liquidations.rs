eb6e08862e12bedc5c20d38c3350edad80ad16d1
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-02 21:47:17 -0500

    going to write the liquidation inspector when i'm back, need chainlink classifier for oracle updates as well
    Former-commit-id: 90d02f0366b3bd989153eecd5650403816bf0e03

---
+++ crates/brontes-inspect/src/liquidations.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+
+use brontes_database::{Metadata, Pair};
+use brontes_database_libmdbx::Libmdbx;
+use brontes_types::{
+    classified_mev::{ClassifiedMev, Liquidation, MevType, SpecificMev},
+    normalized_actions::{Actions, NormalizedLiquidation, NormalizedSwap},
+    tree::TimeTree,
+};
+use reth_primitives::Address;
+
+use crate::{shared_utils::SharedInspectorUtils, Inspector};
+
+pub struct LiquidationInspector<'db> {
+    inner: SharedInspectorUtils<'db>,
+}
+
+impl<'db> LiquidationInspector<'db> {
+    pub fn new(quote: Address, db: &'db Libmdbx) -> Self {
+        Self { inner: SharedInspectorUtils::new(quote, db) }
+    }
+}
+
+#[async_trait::async_trait]
+impl Inspector for LiquidationInspector<'_> {
+    async fn process_tree(
+        &self,
+        tree: Arc<TimeTree<Actions>>,
+        metadata: Arc<Metadata>,
+    ) -> Vec<(ClassifiedMev, Box<dyn SpecificMev>)> {
+        todo!()
+    }
+}
