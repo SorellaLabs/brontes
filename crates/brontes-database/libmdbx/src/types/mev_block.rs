ff764cf2791e2f07aa44ad706f87f228d0febbe2
Will Smith <williamqsmith11@gmail.com>
2024-01-08 14:18:53 -0500

    remove debug prints

--- crates/brontes-database/libmdbx/src/types/mev_block.rs
+++ crates/brontes-database/libmdbx/src/types/mev_block.rs
@@ -19,7 +19,6 @@ impl LibmdbxData<MevBlocks> for MevBlocksData {
         &self,
     ) -> (<MevBlocks as reth_db::table::Table>::Key, <MevBlocks as reth_db::table::Table>::Value)
     {
-        println!("into key val");
         (self.block_number, self.mev_blocks.clone())
     }
 }
