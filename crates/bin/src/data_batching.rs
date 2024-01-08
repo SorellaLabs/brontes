cedd9a16376e9916399632276dcc8eebeaeae700
Will Smith <williamqsmith11@gmail.com>
2024-01-08 17:48:59 -0500

    fix
    Former-commit-id: b68c1524ce366d5b6776a82747cefc1b97f384a6

--- crates/bin/src/data_batching.rs
+++ crates/bin/src/data_batching.rs
@@ -156,6 +156,9 @@ impl<T: TracingProvider, const N: usize> Future for DataBatching<'_, T, N> {
                     "batch finished completed"
                 );
             }
+            if meta.block_num + 1 == self.end_block {
+                self.classifier.close();
+            }
 
             self.on_price_finish(tree, meta);
         }
@@ -172,10 +175,7 @@ impl<T: TracingProvider, const N: usize> Future for DataBatching<'_, T, N> {
         } else if self.current_block != self.end_block {
             self.current_block += 1;
             self.start_next_block();
-        } else {
-            self.classifier.close();
         }
-
         // poll insertion
         while let Poll::Ready(Some(_)) = self.processing_futures.poll_next_unpin(cx) {}
 
