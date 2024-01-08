ed686ed29c3016788fa4f1efa2bc556886ecaa6d
Will Smith <williamqsmith11@gmail.com>
2024-01-08 14:21:46 -0500

    wip

--- crates/brontes-database/libmdbx/src/lib.rs
+++ crates/brontes-database/libmdbx/src/lib.rs
@@ -416,7 +416,6 @@ impl Libmdbx {
         block: MevBlock,
         mev: Vec<(ClassifiedMev, Box<dyn SpecificMev>)>,
     ) -> eyre::Result<()> {
-        println!("inserting classified data");
         self.write_table(&vec![MevBlocksData {
             block_number: block.block_number,
             mev_blocks:   MevBlockWithClassified { block, mev },
