cab192a08a8c25a4d9d0d0e891a33f9cb697c156
Will Smith <williamqsmith11@gmail.com>
2024-01-12 13:59:21 -0500

    wip

--- crates/brontes-pricing/src/dijkstras.rs
+++ crates/brontes-pricing/src/dijkstras.rs
@@ -113,6 +113,7 @@ where
     FS: FnMut(&N) -> bool,
 {
     let (parents, reached) = run_dijkstra(start, successors, path_value, success);
+    println!("reverse pathing");
     reached.map(|target| {
         (
             reverse_path(&parents, |&(p, ..)| p, |_, (_, _, e)| e, target),
