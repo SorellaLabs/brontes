d9e6bbd6a5652486dd868347614d70c967f09f18
Will Smith <williamqsmith11@gmail.com>
2024-01-12 22:00:43 -0500

    fix
    Former-commit-id: 652185aad5b9551223c0598f74a681d2c645e4e9

--- crates/brontes-pricing/src/graphs/all_pair_graph.rs
+++ crates/brontes-pricing/src/graphs/all_pair_graph.rs
@@ -142,7 +142,7 @@ impl AllPairGraph {
                     .collect_vec()
             },
             |node| node == end_idx,
-            |node0, node1| (*node1, *node0),
+            |node0, node1| (*node0, *node1),
             4,
         )
         .into_iter()
@@ -166,7 +166,7 @@ impl AllPairGraph {
                         .map(|info| {
                             let index = *self.token_to_index.get(&info.token_0).unwrap();
                             SubGraphEdge::new(
-                                PoolPairInfoDirection { info, token_0_in: node0 != index },
+                                PoolPairInfoDirection { info, token_0_in: node0 == index },
                                 i,
                                 path_length - i,
                             )
