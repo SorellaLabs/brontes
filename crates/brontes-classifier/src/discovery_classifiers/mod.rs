552864a945739b28f470c849f41982f1dece32de
Will Smith <williamqsmith11@gmail.com>
2024-01-16 17:11:00 -0500

    wip
    Former-commit-id: 141739179987a597dc0551a5759a8c46501dd04f

--- crates/brontes-classifier/src/discovery_classifiers/mod.rs
+++ crates/brontes-classifier/src/discovery_classifiers/mod.rs
@@ -1,5 +1,4 @@
 pub mod curve;
-pub mod sushiswap;
 pub mod uniswap;
 
 use std::sync::Arc;
@@ -8,14 +7,13 @@ use alloy_primitives::Log;
 use brontes_pricing::types::DiscoveredPool;
 use brontes_types::{exchanges::StaticBindingsDb, traits::TracingProvider};
 pub use curve::*;
-pub use sushiswap::*;
 pub use uniswap::*;
 
 pub trait FactoryDecoder<T: TracingProvider> {
     fn get_signature(&self) -> [u8; 32];
 
     #[allow(unused)]
-    async fn decode_new_pool<'a>(
+    async fn decode_new_pool(
         &self,
         node_handle: Arc<T>,
         protocol: StaticBindingsDb,
--- crates/brontes-classifier/src/discovery_classifiers/sushiswap/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod v2;
-pub mod v3;
