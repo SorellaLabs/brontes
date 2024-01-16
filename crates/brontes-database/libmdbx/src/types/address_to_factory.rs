b8f18af8560966e55beb2f68f9d495e0b6694f7d
Will Smith <williamqsmith11@gmail.com>
2024-01-16 16:25:34 -0500

    wip

--- crates/brontes-database/libmdbx/src/types/address_to_factory.rs
+++ crates/brontes-database/libmdbx/src/types/address_to_factory.rs
@@ -1,16 +1,22 @@
 use alloy_primitives::Address;
-use alloy_rlp::{ RlpDecodable, RlpEncodable};
+use alloy_rlp::{RlpDecodable, RlpEncodable};
 use brontes_types::exchanges::StaticBindingsDb;
 use serde::{Deserialize, Serialize};
 use sorella_db_databases::{clickhouse, Row};
 
-use super::{LibmdbxData};
+use super::LibmdbxData;
 use crate::AddressToFactory;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Row, RlpDecodable, RlpEncodable)]
 pub struct AddressToFactoryData {
     pub address:      Address,
     pub factory_type: StaticBindingsDb,
+}
+
+impl AddressToFactoryData {
+    pub fn new(address: Address, factory_type: StaticBindingsDb) -> Self {
+        Self { factory_type, address }
+    }
 }
 
 impl LibmdbxData<AddressToFactory> for AddressToFactoryData {
