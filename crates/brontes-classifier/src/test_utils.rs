35c9809e7b7d0a59e7bdf11e59999b7c2c1339e9
Will Smith <williamqsmith11@gmail.com>
2024-01-18 20:12:56 -0500

    finish classifier test utils
    Former-commit-id: e9bb5c6cd6aa21abf44b695778697b12e6ae361b

--- crates/brontes-classifier/src/test_utils.rs
+++ crates/brontes-classifier/src/test_utils.rs
@@ -1,22 +1,97 @@
-use brontes_core::{
-    decoding::{parser::TraceParser, TracingProvider},
-    TraceLoader,
-};
-use brontes_database::{clickhouse::Clickhouse, Metadata};
-use brontes_database_libmdbx::Libmdbx;
-use brontes_pricing::types::DexPriceMsg;
-use brontes_types::{normalized_actions::Actions, structured_trace::TxTrace, tree::BlockTree};
-use reth_primitives::Header;
-use tokio::sync::mpsc::UnboundedReceiver;
+use std::ops::Deref;
 
-use crate::Classifier;
+use alloy_primitives::TxHash;
+use brontes_core::{
+    decoding::TracingProvider, TraceLoader, TraceLoaderError, TxTracesWithHeaderAnd,
+};
+use brontes_pricing::types::DexPriceMsg;
+use brontes_types::tree::BlockTree;
+use futures::future::join_all;
+use thiserror::Error;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
+
+use crate::{Actions, Classifier};
 
 /// Classifier specific functionality
 pub struct ClassifierTestUtils {
     trace_loader: TraceLoader,
+    classifier:   Classifier<'static, Box<dyn TracingProvider>>,
 
     dex_pricing_receiver: UnboundedReceiver<DexPriceMsg>,
-    classifier: Classifier<'static, Box<dyn TracingProvider>>
+}
 
+impl ClassifierTestUtils {
+    pub fn new() -> Self {
+        let trace_loader = TraceLoader::new();
+        let (tx, rx) = unbounded_channel();
+        let classifier = Classifier::new(trace_loader.libmdbx, tx, trace_loader.get_provider());
 
+        Self { classifier, trace_loader, dex_pricing_receiver: rx }
+    }
+
+    pub fn get_pricing_receiver(&mut self) -> &mut UnboundedReceiver<DexPriceMsg> {
+        &mut self.dex_pricing_receiver
+    }
+
+    pub async fn build_raw_tree_tx(
+        &self,
+        tx_hash: TxHash,
+    ) -> Result<BlockTree<Actions>, ClassifierTestUtilsError> {
+        let TxTracesWithHeaderAnd { trace, header, .. } =
+            self.trace_loader.get_tx_trace_with_header(tx_hash).await?;
+
+        let tx_roots = self
+            .classifier
+            .build_all_tx_trees(vec![trace], &header)
+            .await;
+
+        let mut tree = BlockTree::new(header, tx_roots.len(), Default::default());
+
+        tx_roots.into_iter().for_each(|root_data| {
+            tree.insert_root(root_data.root);
+        });
+
+        Ok(tree)
+    }
+
+    pub async fn build_raw_tree_txes(
+        &self,
+        tx_hashes: Vec<TxHash>,
+    ) -> Result<Vec<BlockTree<Actions>>, ClassifierTestUtilsError> {
+        Ok(join_all(
+            self.trace_loader
+                .get_tx_traces_with_header(tx_hashes)
+                .await?
+                .into_iter()
+                .map(|data| async move {
+                    let tx_roots = self
+                        .classifier
+                        .build_all_tx_trees(vec![data.trace], &data.header)
+                        .await;
+
+                    let mut tree = BlockTree::new(data.header, tx_roots.len(), Default::default());
+
+                    tx_roots.into_iter().for_each(|root_data| {
+                        tree.insert_root(root_data.root);
+                    });
+
+                    tree
+                }),
+        )
+        .await)
+    }
+}
+
+impl Deref for ClassifierTestUtils {
+    type Target = TraceLoader;
+
+    fn deref(&self) -> &Self::Target {
+        &self.trace_loader
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ClassifierTestUtilsError {
+    #[error(transparent)]
+    TraceLoaderError(#[from] TraceLoaderError),
 }
