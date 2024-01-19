8b523d6505dec792cd86f0009c7137e25a2b435c
Will Smith <williamqsmith11@gmail.com>
2024-01-18 19:47:29 -0500

    clean classifier
    Former-commit-id: 9e53b8c16b82eaf4bf5518f6bc7ab7f2468aa1a0

--- crates/brontes-classifier/src/test_utils.rs
+++ crates/brontes-classifier/src/test_utils.rs
@@ -1,40 +1,22 @@
-use brontes_core::decoding::{parser::TraceParser, TracingProvider};
+use brontes_core::{
+    decoding::{parser::TraceParser, TracingProvider},
+    TraceLoader,
+};
 use brontes_database::{clickhouse::Clickhouse, Metadata};
 use brontes_database_libmdbx::Libmdbx;
+use brontes_pricing::types::DexPriceMsg;
 use brontes_types::{normalized_actions::Actions, structured_trace::TxTrace, tree::BlockTree};
 use reth_primitives::Header;
+use tokio::sync::mpsc::UnboundedReceiver;
 
 use crate::Classifier;
 
-pub async fn helper_build_block_tree<T: TracingProvider>(
-    classifier: &Classifier<'_, T>,
-    traces: Vec<TxTrace>,
-    header: Header,
-    metadata: &Metadata,
-) -> BlockTree<Actions> {
-    let (_, mut tree) = classifier.build_block_tree(traces, header).await;
-    tree.eth_price = metadata.eth_prices.clone();
-    tree
-}
+/// Classifier specific functionality
+pub struct ClassifierTestUtils {
+    trace_loader: TraceLoader,
 
-pub async fn build_raw_test_tree<T: TracingProvider>(
-    tracer: &TraceParser<'_, T>,
-    db: &Clickhouse,
-    lib: &Libmdbx,
-    block_number: u64,
-) -> BlockTree<Actions> {
-    let (traces, header) = get_traces_with_meta(tracer, db, block_number).await;
-    let (tx, _rx) = tokio::sync::mpsc::unbounded_channel();
-    let classifier = Classifier::new(&lib, tx, tracer.get_tracer());
-    let (_, tree) = classifier.build_block_tree(traces, header).await;
-    tree
-}
+    dex_pricing_receiver: UnboundedReceiver<DexPriceMsg>,
+    classifier: Classifier<'static, Box<dyn TracingProvider>>
 
-pub async fn get_traces_with_meta<T: TracingProvider>(
-    tracer: &TraceParser<'_, T>,
-    _db: &Clickhouse,
-    block_number: u64,
-) -> (Vec<TxTrace>, Header) {
-    let (traces, header) = tracer.execute_block(block_number).await.unwrap();
-    (traces, header)
+
 }
