63a412cb222736507baa16aea49286e9f99553a1
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-18 20:57:59 -0500

    fix camel case & incorrect sandwich inspector name

--- crates/brontes-classifier/src/classifiers/sushiswap/mod.rs
+++ crates/brontes-classifier/src/classifiers/sushiswap/mod.rs
@@ -1,4 +1,6 @@
+#[allow(non_snake_case)]
 pub mod sushiswap_v2;
+#[allow(non_snake_case)]
 pub mod sushiswap_v3;
 
 pub use sushiswap_v2::SushiSwapV2Classifier;
--- crates/brontes-classifier/src/classifiers/uniswap/mod.rs
+++ crates/brontes-classifier/src/classifiers/uniswap/mod.rs
@@ -1,6 +1,9 @@
 pub mod discovery;
+#[allow(non_snake_case)]
 pub mod uniswap_v2;
+#[allow(non_snake_case)]
 pub mod uniswap_v3;
+#[allow(non_snake_case)]
 pub mod uniswap_x;
 
 pub use discovery::*;
