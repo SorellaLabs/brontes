3db5e033f8313ee27e002a292827012cecbc35ba
Will Smith <williamqsmith11@gmail.com>
2024-01-20 23:29:13 -0500

    will: graceful shutdown

---
+++ crates/bin/src/cli/init.rs
@@ -0,0 +1,75 @@
+use std::{env, error::Error, sync::Arc};
+
+use brontes_database::libmdbx::{
+    cursor::CompressedCursor,
+    tables::{AddressToProtocol, CompressedTable, IntoTableKey, Tables},
+    Libmdbx,
+};
+use clap::Parser;
+use reth_db::mdbx::RO;
+#[derive(Debug, Parser)]
+pub struct Init {
+    /// Initialize the local Libmdbx DB
+    #[arg(long, short, default_value = "true")]
+    pub init_libmdbx:         bool,
+    /// Libmdbx tables to init:
+    ///     TokenDecimals
+    ///     AddressToTokens
+    ///     AddressToProtocol
+    ///     CexPrice
+    ///     Metadata
+    ///     PoolState
+    ///     DexPrice
+    #[arg(long, short, requires = "init_libmdbx", value_delimiter = ',')]
+    pub tables_to_init:       Option<Vec<Tables>>,
+    /// Start Block to download metadata from Sorella's MEV DB
+    #[arg(long, short, default_value = "0")]
+    pub start_block:          Option<u64>,
+    /// End Block to download metadata from Sorella's MEV DB
+    #[arg(long, short, default_value = "0")]
+    pub end_block:            Option<u64>,
+    /// Download Dex Prices from Sorella's MEV DB for the given block range. If
+    /// false it will run the dex pricing locally using raw on-chain data
+    #[arg(long, short, default_value = "false")]
+    pub download_dex_pricing: bool,
+}
+
+impl Init {
+    pub async fn execute(self) -> Result<(), Box<dyn Error>> {
+        let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect("No BRONTES_DB_PATH in .env");
+
+        let clickhouse = Arc::new(Clickhouse::default());
+
+        let libmdbx = Arc::new(Libmdbx::init_db(brontes_db_endpoint, None)?);
+        if self.init_libmdbx {
+            // currently inits all tables
+            let range = if let (Some(start), Some(end)) = (self.start_block, self.end_block) {
+                Some((start, end))
+            } else {
+                None
+            };
+            libmdbx
+                .init_tables(
+                    clickhouse.clone(),
+                    self.tables_to_init
+                        .unwrap_or({
+                            if self.download_dex_pricing {
+                                let tables = Tables::ALL.to_vec();
+                                //tables.retain(|table| table != &Tables::CexPrice);
+                                //println!("TABLES: {:?}", tables);
+                                tables
+                            } else {
+                                Tables::ALL_NO_DEX.to_vec()
+                            }
+                        })
+                        .as_slice(),
+                    range,
+                )
+                .await?;
+        }
+
+        //TODO: Joe, have it download the full range of metadata from the MEV DB so
+        // they can run everything in parallel
+        Ok(())
+    }
+}
--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -3,7 +3,10 @@ use std::{
     error::Error,
     net::{IpAddr, Ipv4Addr, SocketAddr},
     path::Path,
-    sync::Arc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
 };
 
 use alloy_primitives::Address;
@@ -38,7 +41,7 @@ mod cli;
 use banner::print_banner;
 #[cfg(feature = "tests")]
 use cli::TraceArg;
-use cli::{AddToDb, Args, Commands, DatabaseQuery, DexPricingArgs, Init, RunArgs};
+use cli::{Args, Commands};
 
 type Inspectors<'a> = &'a [&'a Box<dyn Inspector>];
 
@@ -109,171 +112,14 @@ async fn run() -> Result<(), Box<dyn Error>> {
     let opt = Args::parse();
 
     match opt.command {
-        Commands::Run(command) => run_brontes(command).await,
-        Commands::Init(command) => init_brontes(command).await,
-        Commands::RunBatchWithPricing(command) => run_batch_with_pricing(command).await,
-        Commands::QueryDb(command) => query_db(command).await,
-        Commands::AddToDb(command) => add_to_db(command).await,
+        Commands::Run(command) => command.execute().await,
+        Commands::Init(command) => command.execute().await,
+        Commands::RunBatchWithPricing(command) => command.execute().await,
+        Commands::QueryDb(command) => command.execute().await,
+        Commands::AddToDb(command) => command.execute().await,
         #[cfg(feature = "tests")]
-        Commands::Traces(args) => save_trace(args).await,
+        Commands::Traces(args) => command.execute().await,
     }
-}
-
-#[cfg(feature = "tests")]
-async fn save_trace(req: TraceArg) -> Result<(), Box<dyn Error>> {
-    brontes_core::store_traces_for_block(req.block_num).await;
-
-    Ok(())
-}
-
-async fn add_to_db(req: AddToDb) -> Result<(), Box<dyn Error>> {
-    let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect("No BRONTES_DB_PATH in .env");
-    let db = Libmdbx::init_db(brontes_db_endpoint, None)?;
-
-    macro_rules! write_to_table {
-        ($table:expr, $($tables:ident),+ = $arg0:expr, $arg1:expr) => {
-            match $table {
-                $(
-                    Tables::$tables => {
-                        db.write_table(
-                            &vec![
-                            brontes_database::libmdbx::tables::$tables::into_table_data(
-                                    $arg0,
-                                    $arg1
-                                )
-                            ]
-                        ).unwrap();
-                    }
-                )+
-            }
-        };
-    }
-
-    write_to_table!(
-        req.table,
-        CexPrice,
-        Metadata,
-        DexPrice,
-        MevBlocks,
-        TokenDecimals,
-        AddressToTokens,
-        AddressToProtocol,
-        AddressToFactory,
-        SubGraphs,
-        PoolCreationBlocks = &req.key,
-        &req.value
-    );
-
-    Ok(())
-}
-
-fn process_range_query<T, E>(
-    mut cursor: CompressedCursor<T, RO>,
-    command: DatabaseQuery,
-) -> Result<Vec<T::DecompressedValue>, Box<dyn Error>>
-where
-    T: CompressedTable,
-    T: for<'a> IntoTableKey<&'a str, T::Key, E>,
-    T::Value: From<T::DecompressedValue> + Into<T::DecompressedValue>,
-{
-    let range = command.key.split("..").collect_vec();
-    let start = range[0];
-    let end = range[1];
-
-    let start = T::into_key(start);
-    let end = T::into_key(end);
-
-    let mut res = Vec::new();
-    for entry in cursor.walk_range(start..end)? {
-        if let Ok(entry) = entry {
-            res.push(entry.1)
-        }
-    }
-
-    Ok(res)
-}
-
-#[inline(always)]
-fn process_single_query<T>(res: Option<T>, _: DatabaseQuery) -> Result<T, Box<dyn Error>> {
-    Ok(res.ok_or_else(|| reth_db::DatabaseError::Read(-1))?)
-}
-
-async fn query_db(command: DatabaseQuery) -> Result<(), Box<dyn Error>> {
-    let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect("No BRONTES_DB_PATH in .env");
-    let db = Libmdbx::init_db(brontes_db_endpoint, None)?;
-
-    let tx = db.ro_tx()?;
-
-    macro_rules! match_table {
-        ($table:expr, $fn:expr, $query:ident, $($tables:ident),+ = $args:expr) => {
-            match $table {
-                $(
-                    Tables::$tables => {
-                        println!(
-                            "{:#?}",
-                            $fn(
-                                tx.$query::<brontes_database::libmdbx::tables::$tables>(
-                                    brontes_database::libmdbx::tables::$tables::into_key($args)
-                                    ).unwrap(),
-                                command
-                            ).unwrap()
-                        )
-                    }
-                )+
-            }
-        };
-        ($table:expr, $fn:expr, $query:ident, $($tables:ident),+) => {
-            match $table {
-                $(
-                    Tables::$tables => {
-                        println!(
-                            "{:#?}",
-                            $fn(
-                                tx.$query::<brontes_database::libmdbx::tables::$tables>()?,
-                                command
-                            )?
-                        )
-                    }
-                )+
-            }
-        };
-    }
-
-    if command.key.contains("..") {
-        match_table!(
-            command.table,
-            process_range_query,
-            new_cursor,
-            CexPrice,
-            Metadata,
-            DexPrice,
-            MevBlocks,
-            TokenDecimals,
-            AddressToTokens,
-            AddressToProtocol,
-            PoolCreationBlocks,
-            AddressToFactory,
-            SubGraphs
-        );
-    } else {
-        match_table!(
-            command.table,
-            process_single_query,
-            get,
-            CexPrice,
-            Metadata,
-            DexPrice,
-            MevBlocks,
-            TokenDecimals,
-            AddressToTokens,
-            AddressToProtocol,
-            AddressToFactory,
-            SubGraphs,
-            PoolCreationBlocks = &command.key
-        );
-    }
-
-    Ok(())
 }
 
 async fn run_brontes(run_config: RunArgs) -> Result<(), Box<dyn Error>> {
@@ -338,159 +184,6 @@ async fn run_brontes(run_config: RunArgs) -> Result<(), Box<dyn Error>> {
     });
 
     Ok(())
-}
-
-async fn init_brontes(init_config: Init) -> Result<(), Box<dyn Error>> {
-    let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect("No BRONTES_DB_PATH in .env");
-
-    let clickhouse = Arc::new(Clickhouse::default());
-
-    let libmdbx = Arc::new(Libmdbx::init_db(brontes_db_endpoint, None)?);
-    if init_config.init_libmdbx {
-        // currently inits all tables
-        let range =
-            if let (Some(start), Some(end)) = (init_config.start_block, init_config.end_block) {
-                Some((start, end))
-            } else {
-                None
-            };
-        libmdbx
-            .init_tables(
-                clickhouse.clone(),
-                init_config
-                    .tables_to_init
-                    .unwrap_or({
-                        if init_config.download_dex_pricing {
-                            let tables = Tables::ALL.to_vec();
-                            //tables.retain(|table| table != &Tables::CexPrice);
-                            //println!("TABLES: {:?}", tables);
-                            tables
-                        } else {
-                            Tables::ALL_NO_DEX.to_vec()
-                        }
-                    })
-                    .as_slice(),
-                range,
-            )
-            .await?;
-    }
-
-    //TODO: Joe, have it download the full range of metadata from the MEV DB so
-    // they can run everything in parallel
-    Ok(())
-}
-
-async fn run_batch_with_pricing(config: DexPricingArgs) -> Result<(), Box<dyn Error>> {
-    assert!(config.start_block <= config.end_block);
-    info!(?config);
-
-    let db_path = get_env_vars()?;
-
-    let tracing_max_tasks = determine_max_tasks(config.max_tasks);
-    let (metrics_tx, metrics_rx) = unbounded_channel();
-
-    let metrics_listener = PoirotMetricsListener::new(metrics_rx);
-    tokio::spawn(metrics_listener);
-
-    let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect("No BRONTES_DB_PATH in .env");
-    let libmdbx =
-        Box::leak(Box::new(Libmdbx::init_db(brontes_db_endpoint, None)?)) as &'static Libmdbx;
-
-    let inspector_holder =
-        Box::leak(Box::new(InspectorHolder::new(config.quote_asset.parse().unwrap(), &libmdbx)));
-    let inspectors: Inspectors = inspector_holder.get_inspectors();
-
-    let (manager, tracer) = TracingClient::new(
-        Path::new(&db_path),
-        tokio::runtime::Handle::current(),
-        tracing_max_tasks,
-    );
-
-    tokio::spawn(manager);
-
-    let parser = DParser::new(
-        metrics_tx,
-        &libmdbx,
-        tracer,
-        Box::new(|address, db_tx| db_tx.get::<AddressToProtocol>(*address).unwrap().is_none()),
-    );
-
-    let mut scope: TokioScope<'_, ()> = unsafe { Scope::create() };
-
-    // calculate the chunk size using min batch size and max_tasks.
-    // max tasks defaults to 50% of physical threads of the system if not set
-    let cpus = determine_max_tasks(config.max_tasks);
-    let range = config.end_block - config.start_block;
-    let cpus_min = range / config.min_batch_size;
-
-    let cpus = std::cmp::min(cpus_min, cpus);
-    let chunk_size = if cpus == 0 { range + 1 } else { (range / cpus) + 1 };
-
-    let remaining_cpus = if config.max_tasks.is_some() {
-        determine_max_tasks(None) * 2 - config.max_tasks.unwrap()
-    } else {
-        determine_max_tasks(None)
-    };
-
-    let chunks_amount = (range / chunk_size) + 1;
-    // because these are lightweight tasks, we can stack them pretty easily without
-    // much overhead concern
-    let max_pool_loading_tasks = (remaining_cpus / chunks_amount + 1) * 3;
-
-    for (i, mut chunk) in (config.start_block..=config.end_block)
-        .chunks(chunk_size.try_into().unwrap())
-        .into_iter()
-        .enumerate()
-    {
-        let start_block = chunk.next().unwrap();
-        let end_block = chunk.last().unwrap_or(start_block);
-
-        info!(batch_id = i, start_block, end_block, "starting batch");
-
-        scope.spawn(spawn_batches(
-            config.quote_asset.parse().unwrap(),
-            max_pool_loading_tasks as usize,
-            i as u64,
-            start_block,
-            end_block,
-            &parser,
-            libmdbx,
-            &inspectors,
-        ));
-    }
-
-    // collect and wait
-    scope.collect().await;
-    info!("finnished running all batch , shutting down");
-    drop(scope);
-    std::thread::spawn(move || {
-        drop(parser);
-    });
-
-    Ok(())
-}
-
-async fn spawn_batches(
-    quote_asset: Address,
-    max_pool_loading_tasks: usize,
-    batch_id: u64,
-    start_block: u64,
-    end_block: u64,
-    parser: &DParser<'_, TracingClient>,
-    libmdbx: &'static Libmdbx,
-    inspectors: &Inspectors<'_>,
-) {
-    DataBatching::new(
-        quote_asset,
-        max_pool_loading_tasks,
-        batch_id,
-        start_block,
-        end_block,
-        &parser,
-        &libmdbx,
-        &inspectors,
-    )
-    .await
 }
 
 fn determine_max_tasks(max_tasks: Option<u64>) -> u64 {
