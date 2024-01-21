400eac11afff4fd3d4657bf1bdc21800e2ef6fb1
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-21 16:36:31 -0500

    sorry joe pls fix db types, I'm fixing the composer rn

--- crates/brontes-inspect/src/sandwich.rs
+++ crates/brontes-inspect/src/sandwich.rs
@@ -131,185 +131,189 @@ impl SandwichInspector<'_> {
         victim_actions: Vec<Vec<Actions>>,
         victim_gas: Vec<GasDetails>,
     ) -> Option<(ClassifiedMev, SpecificMev)> {
-        let frontrun_swaps = searcher_actions
-            .get(0)?
-            .into_iter()
-            .filter(|s| s.is_swap())
-            .map(|s| s.clone().force_swap())
-            .collect_vec();
+        todo!()
+        /*
 
-        let backrun_swaps = searcher_actions
-            .get(searcher_actions.len() - 1)?
-            .into_iter()
-            .filter(|s| s.is_swap())
-            .map(|s| s.clone().force_swap())
-            .collect_vec();
+            let frontrun_swaps = searcher_actions
+                .get(0)?
+                .into_iter()
+                .filter(|s| s.is_swap())
+                .map(|s| s.clone().force_swap())
+                .collect_vec();
 
-        let mut pools = HashSet::new();
+            let backrun_swaps = searcher_actions
+                .get(searcher_actions.len() - 1)?
+                .into_iter()
+                .filter(|s| s.is_swap())
+                .map(|s| s.clone().force_swap())
+                .collect_vec();
 
-        for swap in &frontrun_swaps {
-            pools.insert(swap.pool);
-        }
+            let mut pools = HashSet::new();
 
-        let has_victim = victim_actions
-            .iter()
-            .flatten()
-            .filter(|action| action.is_swap())
-            .map(|f| f.force_swap_ref().pool)
-            .filter(|f| pools.contains(f))
-            .collect::<HashSet<_>>();
-
-        let victim_swaps = victim_actions
-            .iter()
-            .map(|tx_actions| {
-                tx_actions
-                    .iter()
-                    .filter(|action| action.is_swap())
-                    .map(|f| f.clone().force_swap())
-                    .collect::<Vec<_>>()
-            })
-            .collect();
-
-        if !backrun_swaps
-            .iter()
-            .any(|inner| pools.contains(&inner.pool) && has_victim.contains(&inner.pool))
-        {
-            return None
-        }
-
-        let deltas = self.inner.calculate_token_deltas(&searcher_actions);
-
-        let addr_usd_deltas =
-            self.inner
-                .usd_delta_by_address(idx, deltas, metadata.clone(), false)?;
-
-        let mev_profit_collector = self.inner.profit_collectors(&addr_usd_deltas);
-
-        let rev_usd = addr_usd_deltas
-            .values()
-            .fold(Rational::ZERO, |acc, delta| acc + delta);
-
-        let gas_used = searcher_gas_details
-            .iter()
-            .map(|g| g.gas_paid())
-            .sum::<u128>();
-
-        let gas_used = metadata.get_gas_price_usd(gas_used);
-
-        let sandwich = Sandwich {
-            frontrun_tx_hash: txes[0],
-            frontrun_gas_details: searcher_gas_details[0],
-            frontrun_swaps,
-            victim_swaps_tx_hashes: victim_txes,
-            victim_swaps,
-            victim_swaps_gas_details: victim_gas,
-            backrun_tx_hash: txes[1],
-            backrun_swaps,
-            backrun_gas_details: searcher_gas_details[1],
-        };
-
-        let classified_mev = ClassifiedMev {
-            mev_tx_index: idx as u64,
-            eoa,
-            mev_profit_collector,
-            tx_hash: txes[0],
-            mev_contract: mev_executor_contract,
-            block_number: metadata.block_num,
-            mev_type: MevType::Sandwich,
-            finalized_profit_usd: (rev_usd - &gas_used).to_float(),
-            finalized_bribe_usd: gas_used.to_float(),
-        };
-
-        Some((classified_mev, SpecificMev::Sandwich(sandwich)))
-    }
-
-    fn get_possible_sandwich(&self, tree: Arc<BlockTree<Actions>>) -> Vec<PossibleSandwich> {
-        let iter = tree.tx_roots.iter();
-        if iter.len() < 3 {
-            return vec![]
-        }
-
-        let mut set: HashSet<PossibleSandwich> = HashSet::new();
-        let mut duplicate_mev_contracts: HashMap<Address, Vec<B256>> = HashMap::new();
-        let mut duplicate_senders: HashMap<Address, Vec<B256>> = HashMap::new();
-        let mut possible_victims: HashMap<B256, Vec<B256>> = HashMap::new();
-
-        for root in iter {
-            if root.head.data.is_revert() {
-                continue
+            for swap in &frontrun_swaps {
+                pools.insert(swap.pool);
             }
 
-            match duplicate_mev_contracts.entry(root.head.data.get_to_address()) {
-                // If we have not seen this sender before, we insert the tx hash into the map
-                Entry::Vacant(v) => {
-                    v.insert(vec![root.tx_hash]);
-                    possible_victims.insert(root.tx_hash, vec![]);
-                }
-                Entry::Occupied(mut o) => {
-                    let prev_tx_hashes = o.get();
+            let has_victim = victim_actions
+                .iter()
+                .flatten()
+                .filter(|action| action.is_swap())
+                .map(|f| f.force_swap_ref().pool)
+                .filter(|f| pools.contains(f))
+                .collect::<HashSet<_>>();
 
-                    for prev_tx_hash in prev_tx_hashes {
-                        // Find the victims between the previous and the current transaction
-                        if let Some(victims) = possible_victims.get(prev_tx_hash) {
-                            if victims.len() >= 1 {
-                                // Create
-                                set.insert(PossibleSandwich {
-                                    eoa:                   root.head.address,
-                                    tx0:                   *prev_tx_hash,
-                                    tx1:                   root.tx_hash,
-                                    mev_executor_contract: root.head.data.get_to_address(),
-                                    victims:               victims.clone(),
-                                });
+            let victim_swaps = victim_actions
+                .iter()
+                .map(|tx_actions| {
+                    tx_actions
+                        .iter()
+                        .filter(|action| action.is_swap())
+                        .map(|f| f.clone().force_swap())
+                        .collect::<Vec<_>>()
+                })
+                .collect();
+
+            if !backrun_swaps
+                .iter()
+                .any(|inner| pools.contains(&inner.pool) && has_victim.contains(&inner.pool))
+            {
+                return None
+            }
+
+            let deltas = self.inner.calculate_token_deltas(&searcher_actions);
+
+            let addr_usd_deltas =
+                self.inner
+                    .usd_delta_by_address(idx, deltas, metadata.clone(), false)?;
+
+            let mev_profit_collector = self.inner.profit_collectors(&addr_usd_deltas);
+
+            let rev_usd = addr_usd_deltas
+                .values()
+                .fold(Rational::ZERO, |acc, delta| acc + delta);
+
+            let gas_used = searcher_gas_details
+                .iter()
+                .map(|g| g.gas_paid())
+                .sum::<u128>();
+
+            let gas_used = metadata.get_gas_price_usd(gas_used);
+
+            let sandwich = Sandwich {
+                frontrun_tx_hash: txes[0],
+                frontrun_gas_details: searcher_gas_details[0],
+                frontrun_swaps,
+                victim_swaps_tx_hashes: victim_txes,
+                victim_swaps,
+                victim_swaps_gas_details: victim_gas,
+                backrun_tx_hash: txes[1],
+                backrun_swaps,
+                backrun_gas_details: searcher_gas_details[1],
+            };
+
+            let classified_mev = ClassifiedMev {
+                mev_tx_index: idx as u64,
+                eoa,
+                mev_profit_collector,
+                tx_hash: txes[0],
+                mev_contract: mev_executor_contract,
+                block_number: metadata.block_num,
+                mev_type: MevType::Sandwich,
+                finalized_profit_usd: (rev_usd - &gas_used).to_float(),
+                finalized_bribe_usd: gas_used.to_float(),
+            };
+
+            Some((classified_mev, SpecificMev::Sandwich(sandwich)))
+        }
+
+        fn get_possible_sandwich(&self, tree: Arc<BlockTree<Actions>>) -> Vec<PossibleSandwich> {
+            let iter = tree.tx_roots.iter();
+            if iter.len() < 3 {
+                return vec![]
+            }
+
+            let mut set: HashSet<PossibleSandwich> = HashSet::new();
+            let mut duplicate_mev_contracts: HashMap<Address, Vec<B256>> = HashMap::new();
+            let mut duplicate_senders: HashMap<Address, Vec<B256>> = HashMap::new();
+            let mut possible_victims: HashMap<B256, Vec<B256>> = HashMap::new();
+
+            for root in iter {
+                if root.head.data.is_revert() {
+                    continue
+                }
+
+                match duplicate_mev_contracts.entry(root.head.data.get_to_address()) {
+                    // If we have not seen this sender before, we insert the tx hash into the map
+                    Entry::Vacant(v) => {
+                        v.insert(vec![root.tx_hash]);
+                        possible_victims.insert(root.tx_hash, vec![]);
+                    }
+                    Entry::Occupied(mut o) => {
+                        let prev_tx_hashes = o.get();
+
+                        for prev_tx_hash in prev_tx_hashes {
+                            // Find the victims between the previous and the current transaction
+                            if let Some(victims) = possible_victims.get(prev_tx_hash) {
+                                if victims.len() >= 1 {
+                                    // Create
+                                    set.insert(PossibleSandwich {
+                                        eoa:                   root.head.address,
+                                        tx0:                   *prev_tx_hash,
+                                        tx1:                   root.tx_hash,
+                                        mev_executor_contract: root.head.data.get_to_address(),
+                                        victims:               victims.clone(),
+                                    });
+                                }
                             }
                         }
+                        // Add current transaction hash to the list of transactions for this sender
+                        o.get_mut().push(root.tx_hash);
+                        possible_victims.insert(root.tx_hash, vec![]);
                     }
-                    // Add current transaction hash to the list of transactions for this sender
-                    o.get_mut().push(root.tx_hash);
-                    possible_victims.insert(root.tx_hash, vec![]);
                 }
-            }
 
-            match duplicate_senders.entry(root.head.address) {
-                // If we have not seen this sender before, we insert the tx hash into the map
-                Entry::Vacant(v) => {
-                    v.insert(vec![root.tx_hash]);
-                    possible_victims.insert(root.tx_hash, vec![]);
-                }
-                Entry::Occupied(mut o) => {
-                    let prev_tx_hashes = o.get();
+                match duplicate_senders.entry(root.head.address) {
+                    // If we have not seen this sender before, we insert the tx hash into the map
+                    Entry::Vacant(v) => {
+                        v.insert(vec![root.tx_hash]);
+                        possible_victims.insert(root.tx_hash, vec![]);
+                    }
+                    Entry::Occupied(mut o) => {
+                        let prev_tx_hashes = o.get();
 
-                    for prev_tx_hash in prev_tx_hashes {
-                        // Find the victims between the previous and the current transaction
-                        if let Some(victims) = possible_victims.get(prev_tx_hash) {
-                            if victims.len() >= 1 {
-                                // Create
-                                set.insert(PossibleSandwich {
-                                    eoa:                   root.head.address,
-                                    tx0:                   *prev_tx_hash,
-                                    tx1:                   root.tx_hash,
-                                    mev_executor_contract: root.head.data.get_to_address(),
-                                    victims:               victims.clone(),
-                                });
+                        for prev_tx_hash in prev_tx_hashes {
+                            // Find the victims between the previous and the current transaction
+                            if let Some(victims) = possible_victims.get(prev_tx_hash) {
+                                if victims.len() >= 1 {
+                                    // Create
+                                    set.insert(PossibleSandwich {
+                                        eoa:                   root.head.address,
+                                        tx0:                   *prev_tx_hash,
+                                        tx1:                   root.tx_hash,
+                                        mev_executor_contract: root.head.data.get_to_address(),
+                                        victims:               victims.clone(),
+                                    });
+                                }
                             }
                         }
+                        // Add current transaction hash to the list of transactions for this sender
+                        o.get_mut().push(root.tx_hash);
+                        possible_victims.insert(root.tx_hash, vec![]);
                     }
-                    // Add current transaction hash to the list of transactions for this sender
-                    o.get_mut().push(root.tx_hash);
-                    possible_victims.insert(root.tx_hash, vec![]);
+                }
+
+                // Now, for each existing entry in possible_victims, we add the current
+                // transaction hash as a potential victim, if it is not the same as
+                // the key (which represents another transaction hash)
+                for (k, v) in possible_victims.iter_mut() {
+                    if k != &root.tx_hash {
+                        v.push(root.tx_hash);
+                    }
                 }
             }
 
-            // Now, for each existing entry in possible_victims, we add the current
-            // transaction hash as a potential victim, if it is not the same as
-            // the key (which represents another transaction hash)
-            for (k, v) in possible_victims.iter_mut() {
-                if k != &root.tx_hash {
-                    v.push(root.tx_hash);
-                }
-            }
-        }
-
-        set.into_iter().collect()
+            set.into_iter().collect()
+                */
     }
 }
 
