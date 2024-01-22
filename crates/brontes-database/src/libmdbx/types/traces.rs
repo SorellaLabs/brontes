e9f793f1600dc45b4290a209202cc670e54c3bd7
lagrange.eth <lagrange@sorellacapital.com>
2024-01-21 20:28:43 -0500

    updated init

--- crates/brontes-database/src/libmdbx/types/traces.rs
+++ crates/brontes-database/src/libmdbx/types/traces.rs
@@ -1,27 +1,34 @@
-
 use alloy_primitives::{Log, LogData};
-use brontes_types::{structured_trace::{TxTrace, DecodedCallData, TransactionTraceWithLogs}, db::redefined_types::primitives::{Redefined_FixedBytes, Redefined_Address, Redefined_Alloy_Bytes, Redefined_U256, Redefined_U64}};
+use alloy_rlp::{Decodable, Encodable};
+use brontes_types::{
+    db::{
+        redefined_types::primitives::{
+            Redefined_Address, Redefined_Alloy_Bytes, Redefined_FixedBytes, Redefined_U256,
+            Redefined_U64,
+        },
+        traces::TxTracesInner,
+    },
+    structured_trace::{DecodedCallData, TransactionTraceWithLogs, TxTrace},
+};
 use bytes::BufMut;
-use redefined::Redefined;
+use redefined::{Redefined, RedefinedConvert};
 use reth_db::{
     table::{Compress, Decompress},
     DatabaseError,
 };
+use reth_rpc_types::trace::parity::{
+    Action, CallAction, CallOutput, CallType, CreateAction, CreateOutput, RewardAction, RewardType,
+    SelfdestructAction, TraceOutput, TransactionTrace,
+};
 use rkyv::Deserialize;
-use alloy_rlp::{Encodable, Decodable};
-use reth_rpc_types::trace::parity::{TransactionTrace, Action, CallAction, CallType, CreateAction, SelfdestructAction, RewardType, RewardAction, CallOutput, CreateOutput, TraceOutput};
-use redefined::RedefinedConvert;
-use brontes_types::db::traces::TxTracesInner;
-
-use serde_with::{serde_as};
+use serde_with::serde_as;
 use sorella_db_databases::{clickhouse, clickhouse::Row};
-
-use crate::libmdbx::{TxTraces, CompressedTable};
 
 use super::{
     utils::{option_address, u256},
     LibmdbxData,
 };
+use crate::libmdbx::{CompressedTable, TxTraces};
 
 #[serde_as]
 #[derive(Debug, Clone, Row, serde::Serialize, serde::Deserialize)]
@@ -29,7 +36,6 @@ pub struct TxTracesData {
     pub block_number: u64,
     pub inner:        TxTracesInner,
 }
-
 
 impl LibmdbxData<TxTraces> for TxTracesData {
     fn into_key_val(
@@ -41,7 +47,17 @@ impl LibmdbxData<TxTraces> for TxTracesData {
 }
 
 #[serde_as]
-#[derive(Debug, Default, Clone, Redefined, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive)]
+#[derive(
+    Debug,
+    Default,
+    Clone,
+    Redefined,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+)]
 #[redefined(TxTracesInner)]
 pub struct LibmdbxTxTracesInner {
     pub traces: Option<Vec<LibmdbxTxTrace>>,
@@ -88,8 +104,16 @@ impl Decompress for LibmdbxTxTracesInner {
     }
 }
 
-
-#[derive(Debug, Clone, Redefined, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive)]
+#[derive(
+    Debug,
+    Clone,
+    Redefined,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+)]
 #[redefined(TxTrace)]
 pub struct LibmdbxTxTrace {
     pub trace:           Vec<LibmdbxTransactionTraceWithLogs>,
@@ -101,52 +125,93 @@ pub struct LibmdbxTxTrace {
     pub is_success:      bool,
 }
 
-
-
-#[derive(Debug, Clone, Redefined, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive)]
+#[derive(
+    Debug,
+    Clone,
+    Redefined,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+)]
 #[redefined(TransactionTraceWithLogs)]
 pub struct LibmdbxTransactionTraceWithLogs {
-    pub trace: LibmdbxTransactionTrace,
-    pub logs: Vec<LibmdbxLog>,
-    pub msg_sender: Redefined_Address,
-    pub trace_idx: u64,
+    pub trace:        LibmdbxTransactionTrace,
+    pub logs:         Vec<LibmdbxLog>,
+    pub msg_sender:   Redefined_Address,
+    pub trace_idx:    u64,
     pub decoded_data: Option<DecodedCallData>,
 }
 
-
-#[derive(Debug, Clone, Redefined, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive)]
+#[derive(
+    Debug,
+    Clone,
+    Redefined,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+)]
 #[redefined(Log)]
 pub struct LibmdbxLog {
     pub address: Redefined_Address,
-    pub data: LibmdbxLogData,
+    pub data:    LibmdbxLogData,
 }
 
-
-
-#[derive(Debug, Clone, Redefined, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive)]
+#[derive(
+    Debug,
+    Clone,
+    Redefined,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+)]
 #[redefined(LogData)]
-#[redefined_attr(to_source = "LogData::new_unchecked(self.topics.into_iter().map(Into::into).collect(), self.data.into())")]
+#[redefined_attr(to_source = "LogData::new_unchecked(self.topics.into_iter().map(Into::into).\
+                              collect(), self.data.into())")]
 pub struct LibmdbxLogData {
     #[redefined_attr(func = "src.topics().into_iter().map(|t| t.clone().into()).collect()")]
     pub topics: Vec<Redefined_FixedBytes<32>>,
-    pub data: Redefined_Alloy_Bytes,
+    pub data:   Redefined_Alloy_Bytes,
 }
 
-
-
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(TransactionTrace)]
 pub struct LibmdbxTransactionTrace {
-    pub action: LibmdbxAction,
-    pub error: Option<String>,
-    pub result: Option<LibmdbxTraceOutput>,
-    pub subtraces: usize,
+    pub action:        LibmdbxAction,
+    pub error:         Option<String>,
+    pub result:        Option<LibmdbxTraceOutput>,
+    pub subtraces:     usize,
     pub trace_address: Vec<usize>,
 }
 
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(Action)]
 pub enum LibmdbxAction {
     Call(LibmdbxCallAction),
@@ -155,59 +220,116 @@ pub enum LibmdbxAction {
     Reward(LibmdbxRewardAction),
 }
 
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(CallAction)]
 pub struct LibmdbxCallAction {
-    pub from: Redefined_Address,
+    pub from:      Redefined_Address,
     pub call_type: LibmdbxCallType,
-    pub gas: Redefined_U64,
-    pub input: Redefined_Alloy_Bytes,
-    pub to: Redefined_Address,
-    pub value: Redefined_U256,
+    pub gas:       Redefined_U64,
+    pub input:     Redefined_Alloy_Bytes,
+    pub to:        Redefined_Address,
+    pub value:     Redefined_U256,
 }
 
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(CreateAction)]
 pub struct LibmdbxCreateAction {
-    pub from: Redefined_Address,
-    pub gas: Redefined_U64,
-    pub init: Redefined_Alloy_Bytes,
+    pub from:  Redefined_Address,
+    pub gas:   Redefined_U64,
+    pub init:  Redefined_Alloy_Bytes,
     pub value: Redefined_U256,
 }
 
-
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(SelfdestructAction)]
 pub struct LibmdbxSelfdestructAction {
-    pub address: Redefined_Address,
-    pub balance: Redefined_U256,
+    pub address:        Redefined_Address,
+    pub balance:        Redefined_U256,
     pub refund_address: Redefined_Address,
 }
 
-
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(RewardAction)]
 pub struct LibmdbxRewardAction {
-    pub author: Redefined_Address,
+    pub author:      Redefined_Address,
     pub reward_type: LibmdbxRewardType,
-    pub value: Redefined_U256,
+    pub value:       Redefined_U256,
 }
 
-
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(RewardType)]
 pub enum LibmdbxRewardType {
     Block,
     Uncle,
 }
 
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(CallType)]
 pub enum LibmdbxCallType {
     None,
@@ -217,29 +339,58 @@ pub enum LibmdbxCallType {
     StaticCall,
 }
 
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(TraceOutput)]
 pub enum LibmdbxTraceOutput {
     Call(LibmdbxCallOutput),
     Create(LibmdbxCreateOutput),
 }
 
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(CallOutput)]
 pub struct LibmdbxCallOutput {
     pub gas_used: Redefined_U64,
-    pub output: Redefined_Alloy_Bytes,
+    pub output:   Redefined_Alloy_Bytes,
 }
 
-
-
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, rkyv::Serialize, rkyv::Deserialize, rkyv::Archive, Redefined)]
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    serde::Serialize,
+    serde::Deserialize,
+    rkyv::Serialize,
+    rkyv::Deserialize,
+    rkyv::Archive,
+    Redefined,
+)]
 #[redefined(CreateOutput)]
 pub struct LibmdbxCreateOutput {
-    pub address: Redefined_Address,
-    pub code: Redefined_Alloy_Bytes,
+    pub address:  Redefined_Address,
+    pub code:     Redefined_Alloy_Bytes,
     pub gas_used: Redefined_U64,
 }
 
