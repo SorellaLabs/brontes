8911c1b440205baf4ac25874baf050494abfa4d7
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-24 16:24:37 -0500

    improving formatting'
    Former-commit-id: 8ba3e289dc56543ce9bb09616b80be17e6eafd9b

--- crates/bin/src/executors/utils.rs
+++ crates/bin/src/executors/utils.rs
@@ -12,6 +12,7 @@ use brontes_types::{
     normalized_actions::Actions,
     tree::BlockTree,
 };
+use colored::Colorize;
 use tracing::{error, info};
 
 pub async fn process_results<DB: LibmdbxWriter>(
@@ -36,21 +37,36 @@ fn insert_mev_results<DB: LibmdbxWriter>(
     block_details: MevBlock,
     mev_details: Vec<(BundleHeader, BundleData)>,
 ) {
+    let mev_summary = block_details
+        .possible_missed_arbs
+        .iter()
+        .map(|possible_mev| {
+            let eth_paid = possible_mev.gas_paid as f64 * 1e-18;
+            let tx_url = format!("https://etherscan.io/tx/{:?}", possible_mev.tx_hash);
+            format!(
+                "{} paid {} ETH in gas\nEtherscan link: {}",
+                format!("Tx number {}", possible_mev.position_in_block).blue(),
+                eth_paid.to_string().green(),
+                tx_url.underline().blue()
+            )
+        })
+        .fold(String::new(), |acc, line| acc + &line + "\n");
+
     info!(
         target:"brontes",
         "\n Finished processing block: {} \n- MEV Count: {}\n- Finalized ETH Price: \
          ${:.2}\n- Cumulative Gas Used: {}\n- Cumulative Gas Paid: {}\n- Total Bribe: \
-         {}\n- Cumulative MEV Priority Fee Paid: {}\n- Builder Address: {:?}\n- Builder \
-         ETH Profit: {}\n- Builder Finalized Profit (USD): ${:.2}\n- Proposer Fee \
-         Recipient: {:?}\n- Proposer MEV Reward: {:?}\n- Proposer Finalized Profit (USD): \
+         {}\n- Cumulative MEV Priority Fee Paid: {} ETH \n- Builder Address: {:?}\n- Builder \
+         ETH Profit: {} ETH\n- Builder Finalized Profit (USD): ${:.2}\n- Proposer Fee \
+         Recipient: {:?}\n- Proposer MEV Reward: {:?} ETH \n- Proposer Finalized Profit (USD): \
         {:?}\n- Cumulative MEV Finalized Profit (USD): ${:.2}\n- Possibly Missed Mev:\n{}",
         block_details.block_number,
-        block_details.mev_count,
+        block_details.mev_count.to_string().bold().red(),
         block_details.finalized_eth_price,
-        block_details.cumulative_gas_used,
-        block_details.cumulative_gas_paid,
-        block_details.total_bribe,
-        block_details.cumulative_mev_priority_fee_paid,
+        block_details.cumulative_gas_used as f64 * 1e-18,
+        block_details.cumulative_gas_paid as f64 * 1e-18,
+        block_details.total_bribe as f64 * 1e-18,
+        block_details.cumulative_mev_priority_fee_paid as f64 * 1e-18,
         block_details.builder_address,
         block_details.builder_eth_profit,
         block_details.builder_finalized_profit_usd,
@@ -59,24 +75,12 @@ fn insert_mev_results<DB: LibmdbxWriter>(
             .unwrap_or(Address::ZERO),
         block_details
             .proposer_mev_reward
-            .map_or("None".to_string(), |v| v.to_string()),
+            .map_or(0.0, |v| v as f64 * 1e-18),
         block_details
             .proposer_finalized_profit_usd
             .map_or("None".to_string(), |v| format!("{:.2}", v)),
         block_details.cumulative_mev_finalized_profit_usd,
-        block_details.possible_missed_arbs
-        .iter()
-        .map(|possible_mev| {
-            let eth_paid = possible_mev.gas_paid as f64 * 1e-18;
-            let tx_url = format!("https://etherscan.io/tx/{:?}", possible_mev.tx_hash);
-            format!(
-                "Tx number {}, paid {} ETH in gas\nEtherscan link: {}",
-                possible_mev.position_in_block,
-                eth_paid,
-                tx_url
-            )
-        })
-        .fold(String::new(), |acc, line| acc + &line + "\n")
+        mev_summary
     );
 
     info!("{:#?}", mev_details);
