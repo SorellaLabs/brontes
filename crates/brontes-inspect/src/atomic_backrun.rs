6e21891667ecdd57b0a25b79f90864f02a7425dd
Will Smith <williamqsmith11@gmail.com>
2024-01-24 10:56:29 -0500

    wip

--- crates/brontes-inspect/src/atomic_backrun.rs
+++ crates/brontes-inspect/src/atomic_backrun.rs
@@ -94,6 +94,7 @@ impl<DB: LibmdbxReader> AtomicBackrunInspector<'_, DB> {
 
         let deltas = self.inner.calculate_token_deltas(&searcher_actions);
 
+        tracing::info!("{:#?}", deltas);
         let addr_usd_deltas =
             self.inner
                 .usd_delta_by_address(idx, deltas, metadata.clone(), false)?;
