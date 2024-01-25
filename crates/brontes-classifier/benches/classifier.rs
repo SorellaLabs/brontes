a77dd0e20ba2d33f66ce9c10e6a0dac91586028e
Will Smith <williamqsmith11@gmail.com>
2024-01-25 13:09:46 -0500

    fix classifer bench

--- crates/brontes-classifier/benches/classifier.rs
+++ crates/brontes-classifier/benches/classifier.rs
@@ -4,9 +4,11 @@ use criterion::{criterion_group, criterion_main, Criterion};
 fn bench_tree_building(c: &mut Criterion) {
     let utils = ClassifierBenchUtils::new();
     utils
-        .bench_block_classification("build 28m gas tree", 18672183, c)
+        .bench_block_tree_building("build 28m gas tree", 18672183, c)
         .unwrap();
 }
+
+
 
 criterion_group!(tree, bench_tree_building);
 criterion_main!(tree);
