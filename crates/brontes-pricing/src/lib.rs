b4dc75b2727f18b545d0bad93e1df7eb687b30f1
Will Smith <williamqsmith11@gmail.com>
2024-01-25 23:13:55 -0500

    test

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -429,7 +429,7 @@ impl<T: TracingProvider> BrontesBatchPricer<T> {
 
         let requery_pairs = self
             .graph_manager
-            .verify_subgraph(self.lazy_loader.get_completed_pairs(block), self.quote_asset)
+            .verify_subgraph(self.lazy_loader.get_completed_pairs(self.completed_block), self.quote_asset)
             .into_iter()
             .filter_map(|(failed, pair, cache_pairs)| {
                 cache_pairs.into_iter().for_each(|(pair, address)| {
@@ -445,7 +445,7 @@ impl<T: TracingProvider> BrontesBatchPricer<T> {
             })
             .collect_vec();
 
-        self.requery_bad_state_par(requery_pairs, block);
+        self.requery_bad_state_par(requery_pairs, self.completed_block);
 
         if let Some(state) = state {
             let addr = state.address();
@@ -464,7 +464,7 @@ impl<T: TracingProvider> BrontesBatchPricer<T> {
     fn on_state_load_error(&mut self, pool_pair: Pair, pool_address: Address, block: u64) {
         let requery_pairs = self
             .graph_manager
-            .verify_subgraph(self.lazy_loader.get_completed_pairs(block), self.quote_asset)
+            .verify_subgraph(self.lazy_loader.get_completed_pairs(self.completed_block), self.quote_asset)
             .into_iter()
             .filter_map(|(failed, pair, cache_pairs)| {
                 cache_pairs.into_iter().for_each(|(pair, address)| {
@@ -480,7 +480,7 @@ impl<T: TracingProvider> BrontesBatchPricer<T> {
             })
             .collect_vec();
 
-        self.requery_bad_state_par(requery_pairs, block);
+        self.requery_bad_state_par(requery_pairs, self.completed_block);
 
         self.lazy_loader
             .remove_protocol_parents(block, &pool_address)
