b4002ebee77724aec79b9f8e4904c3a9473b613b
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-29 02:20:31 -0500

    fixed incorrect priority fee calculation

--- crates/brontes-types/src/mev/backrun.rs
+++ crates/brontes-types/src/mev/backrun.rs
@@ -25,8 +25,12 @@ pub struct AtomicBackrun {
 }
 
 impl Mev for AtomicBackrun {
-    fn priority_fee_paid(&self) -> u128 {
+    fn total_gas_paid(&self) -> u128 {
         self.gas_details.gas_paid()
+    }
+
+    fn total_priority_fee_paid(&self, base_fee: u128) -> u128 {
+        self.gas_details.priority_fee(base_fee) * self.gas_details.gas_used
     }
 
     fn bribe(&self) -> u128 {
