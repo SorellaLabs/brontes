82d72aff09b90cc6f5878cd0339736227b53c920
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-30 12:32:19 -0500

    sorry joe
    Former-commit-id: cd1a38a55121cb348a8842d6997c0c2211d516ca

--- crates/brontes-types/src/mev/cex_dex.rs
+++ crates/brontes-types/src/mev/cex_dex.rs
@@ -28,6 +28,7 @@ pub struct CexDex {
     pub tx_hash:     B256,
     pub swaps:       Vec<NormalizedSwap>,
     pub prices:      Vec<StatArbDetails>,
+    pub pnl:         StatArbPnl,
     pub gas_details: GasDetails,
 }
 
@@ -81,8 +82,10 @@ impl Serialize for CexDex {
         ser_struct
             .serialize_field("stat_arb_details.dex_exchange", &stat_arb_details.dex_exchange)?;
         ser_struct.serialize_field("stat_arb_details.dex_price", &stat_arb_details.dex_price)?;
-        ser_struct
-            .serialize_field("stat_arb_details.profit_pre_gas", &stat_arb_details.profit_pre_gas)?;
+        ser_struct.serialize_field(
+            "stat_arb_details.pnl_pre_gas.taker_profit",
+            &stat_arb_details.pnl_pre_gas.taker,
+        )?;
 
         let gas_details = (
             self.gas_details.coinbase_transfer,
@@ -90,6 +93,9 @@ impl Serialize for CexDex {
             self.gas_details.gas_used,
             self.gas_details.effective_gas_price,
         );
+
+        ser_struct.serialize_field("pnl.taker_profit", &self.pnl.taker_profit)?;
+        ser_struct.serialize_field("pnl.maker_profit", &self.pnl.maker_profit)?;
 
         ser_struct.serialize_field("gas_details", &(gas_details))?;
 
@@ -112,7 +118,10 @@ impl DbRow for CexDex {
         "stat_arb_details.cex_price",
         "stat_arb_details.dex_exchange",
         "stat_arb_details.dex_price",
-        "stat_arb_details.profit_pre_gas",
+        "stat_arb_details.pnl_pre_gas.taker_profit",
+        "stat_arb_details.pnl_pre_gas.maker_profit",
+        "pnl.taker_profit",
+        "pnl.maker_profit",
         "gas_details",
     ];
 }
@@ -120,10 +129,17 @@ impl DbRow for CexDex {
 #[serde_as]
 #[derive(Debug, Deserialize, Clone, Default)]
 pub struct StatArbDetails {
-    pub cex_exchange:   CexExchange,
-    pub cex_price:      Rational,
-    pub dex_exchange:   Protocol,
-    pub dex_price:      Rational,
+    pub cex_exchange: CexExchange,
+    pub cex_price:    Rational,
+    pub dex_exchange: Protocol,
+    pub dex_price:    Rational,
     // Arbitrage profit considering both CEX and DEX swap fees, before applying gas fees
-    pub profit_pre_gas: Rational,
+    pub pnl_pre_gas:  StatArbPnl,
+}
+
+#[serde_as]
+#[derive(Debug, Deserialize, Clone, Default)]
+pub struct StatArbPnl {
+    pub taker_profit: Rational,
+    pub maker_profit: Rational,
 }
