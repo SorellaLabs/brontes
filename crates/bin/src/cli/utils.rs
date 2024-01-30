42d83dba8f37514355410ebab963330a5f6e7544
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-01-30 10:35:36 -0500

    refactored cex-dex

--- crates/bin/src/cli/utils.rs
+++ crates/bin/src/cli/utils.rs
@@ -3,8 +3,9 @@ use std::{env, path::Path};
 use alloy_primitives::Address;
 #[cfg(feature = "local")]
 use brontes_core::local_provider::LocalProvider;
-use brontes_database::{cex::CexExchange, libmdbx::LibmdbxReadWriter};
+use brontes_database::libmdbx::LibmdbxReadWriter;
 use brontes_inspect::{Inspector, Inspectors};
+use brontes_types::db::cex::CexExchange;
 use itertools::Itertools;
 use reth_tasks::TaskExecutor;
 use reth_tracing_ext::TracingClient;
@@ -31,16 +32,18 @@ pub fn init_inspectors(
     inspectors: Option<Vec<Inspectors>>,
     cex_exchanges: Option<Vec<String>>,
 ) -> &'static [&'static Box<dyn Inspector>] {
-    if let Some(cex_exchanges) = cex_exchanges {
-        let cex_exchanges: Vec<CexExchange> = cex_exchanges.into_iter().map(|s| s.into()).collect();
-    }
+    let cex_exchanges: Vec<CexExchange> = cex_exchanges
+        .unwrap_or_else(Vec::new)
+        .into_iter()
+        .map(|s| s.into())
+        .collect();
 
     let mut res = Vec::new();
     for inspector in inspectors
         .map(|i| i.into_iter())
         .unwrap_or_else(|| Inspectors::iter().collect_vec().into_iter())
     {
-        res.push(inspector.init_inspector(quote_token, db, cex_exchanges.clone()));
+        res.push(inspector.init_inspector(quote_token, db, &cex_exchanges));
     }
 
     &*Box::leak(res.into_boxed_slice())
--- crates/brontes-types/src/constants.rs
+++ crates/brontes-types/src/constants.rs
@@ -3,6 +3,9 @@ use alloy_primitives::{hex, Address};
 pub const WETH_ADDRESS: Address = Address::new(hex!("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"));
 pub const USDT_ADDRESS: Address = Address::new(hex!("dAC17F958D2ee523a2206206994597C13D831ec7"));
 pub const USDC_ADDRESS: Address = Address::new(hex!("A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"));
+pub const BUSD_ADDRESS: Address = Address::new(hex!("4fabb145d64652a948d72533023f6e7a623c7c53"));
+pub const WBTC_ADDRESS: Address = Address::new(hex!("2260fac5e5542a773aa44fbcfedf7c193bc2c599"));
+pub const DAI_ADDRESS: Address = Address::new(hex!("6b175474e89094c44da98b954eedeac495271d0f"));
 
 /// The first block where the chainbound mempool data is available.
 pub const START_OF_CHAINBOUND_MEMPOOL_DATA: u64 = 17193367;
