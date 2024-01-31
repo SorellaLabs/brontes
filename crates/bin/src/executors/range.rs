5b8f58696437e92402a6cea6c70350f95cecd0d5
Will Smith <williamqsmith11@gmail.com>
2024-01-31 01:00:03 -0500

    dont panic on missing meta
    Former-commit-id: 67209b5e52a1e2adf2dfa5b36b916aa77f4f925a

--- crates/bin/src/executors/range.rs
+++ crates/bin/src/executors/range.rs
@@ -186,10 +186,12 @@ impl<'db, T: TracingProvider + Clone, DB: LibmdbxReader + LibmdbxWriter>
 
     fn start_next_block(&mut self) {
         let parser = self.parser.execute(self.current_block);
-        let meta = self
+        let Some(meta) = self
             .libmdbx
-            .get_metadata_no_dex_price(self.current_block)
-            .unwrap();
+            .get_metadata_no_dex_price(self.current_block) else {
+                error!(?self.current_block, "failed to load metadata for block");
+                return
+            };
 
         let fut = Box::pin(parser.then(|x| {
             let (traces, header) = x.unwrap().unwrap();
