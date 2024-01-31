a6330bb79330201e8feb70aa65fe336b1a2d8bf5
Will Smith <williamqsmith11@gmail.com>
2024-01-31 01:38:10 -0500

    add just logs command

--- crates/bin/src/cli/trace_range.rs
+++ crates/bin/src/cli/trace_range.rs
@@ -33,15 +33,13 @@ pub struct TraceArgs {
 impl TraceArgs {
     pub async fn execute(self, ctx: CliContext) -> eyre::Result<()> {
         let db_path = get_env_vars()?;
-        let quote_asset = self.quote_asset.parse()?;
-        let task_executor = ctx.task_executor;
 
-        let max_tasks = determine_max_tasks(self.max_tasks);
-
+        let max_tasks = determine_max_tasks(None);
         let (metrics_tx, metrics_rx) = unbounded_channel();
 
         let metrics_listener = PoirotMetricsListener::new(metrics_rx);
-        task_executor.spawn_critical("metrics", metrics_listener);
+        ctx.task_executor
+            .spawn_critical("metrics", metrics_listener);
 
         let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect(
             "No
@@ -51,10 +49,8 @@ impl TraceArgs {
         let libmdbx = static_object(LibmdbxReadWriter::init_db(brontes_db_endpoint, None)?);
         let clickhouse = static_object(Clickhouse::default());
 
-        let inspectors =
-            init_inspectors(quote_asset, libmdbx, self.inspectors_to_run, self.cex_exchanges);
-
-        let tracer = get_tracing_provider(&Path::new(&db_path), max_tasks, task_executor.clone());
+        let tracer =
+            get_tracing_provider(&Path::new(&db_path), max_tasks, ctx.task_executor.clone());
 
         let parser = static_object(DParser::new(
             metrics_tx,
@@ -66,11 +62,14 @@ impl TraceArgs {
         let chunk_size = (self.end_block - self.start_block) / max_tasks + 1;
 
         let mut handles = FuturesUnordered::new();
-        for chunk in (self.start_block..self.end_block)
+        for chunk in &(self.start_block..self.end_block)
             .into_iter()
-            .chunks(chunk_size)
+            .chunks(chunk_size as usize)
         {
-            handles.push(ctx.task_executor.spawn(async move {
+            let chunk = chunk.collect::<Vec<_>>();
+            let spawner = ctx.task_executor.clone();
+
+            handles.push(spawner.spawn(async move {
                 for i in chunk {
                     let _ = parser.execute(i).await;
                 }
