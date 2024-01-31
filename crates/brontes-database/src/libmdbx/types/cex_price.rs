a99538a023cd4d043be20b8f8412d99c5d9eeee1
Will Smith <williamqsmith11@gmail.com>
2024-01-30 22:01:16 -0500

    tmp

--- crates/brontes-database/src/libmdbx/types/cex_price.rs
+++ crates/brontes-database/src/libmdbx/types/cex_price.rs
@@ -10,6 +10,7 @@ use brontes_types::{
     },
     pair::Pair,
 };
+use itertools::Itertools;
 use redefined::{Redefined, RedefinedConvert};
 
 #[derive(
@@ -17,17 +18,18 @@ use redefined::{Redefined, RedefinedConvert};
 )]
 #[redefined(CexPriceMap)]
 #[redefined_attr(
-    to_source = "CexPriceMap(self.map.to_source())",
+    to_source = "CexPriceMap(self.map.into_iter().collect::<HashMap<_,_>>().to_source())",
     from_source = "LibmdbxCexPriceMap::new(src.0)"
 )]
 #[archive(check_bytes)]
 pub struct LibmdbxCexPriceMap {
-    pub map: HashMap<CexExchange, HashMap<Redefined_Pair, LibmdbxCexQuote>>,
+    pub map: Vec<(CexExchange, HashMap<Redefined_Pair, LibmdbxCexQuote>)>,
 }
 
 impl LibmdbxCexPriceMap {
     fn new(map: HashMap<CexExchange, HashMap<Pair, CexQuote>>) -> Self {
-        Self { map: HashMap::from_source(map) }
+        let srd = HashMap::from_source(map);
+        Self { map: srd.into_iter().collect_vec() }
     }
 }
 
@@ -54,45 +56,5 @@ pub struct LibmdbxCexQuote {
 impl PartialEq for LibmdbxCexQuote {
     fn eq(&self, other: &Self) -> bool {
         self.clone().to_source().eq(&other.clone().to_source())
-    }
-}
-
-#[cfg(test)]
-pub mod test {
-    use std::collections::HashMap;
-
-    use brontes_types::{
-        constants::{USDC_ADDRESS, USDT_ADDRESS, WETH_ADDRESS},
-        db::cex::{CexExchange, CexQuote},
-        pair::Pair,
-    };
-    use itertools::Itertools;
-    use redefined::RedefinedConvert;
-    use rkyv::Deserialize;
-    use zstd::zstd_safe::WriteBuf;
-
-    use super::LibmdbxCexPriceMap;
-    use crate::libmdbx::types::cex_price::ArchivedLibmdbxCexPriceMap;
-
-    #[test]
-    fn test_encode_decode() {
-        let mut map = HashMap::new();
-        let mut inner = HashMap::new();
-        let quote = CexQuote { ..Default::default() };
-        inner.insert(Pair(WETH_ADDRESS, USDC_ADDRESS), quote.clone());
-
-        inner.insert(Pair(USDC_ADDRESS, USDT_ADDRESS), quote);
-        map.insert(CexExchange::Kucoin, inner);
-
-        let copied = LibmdbxCexPriceMap::from_source(brontes_types::db::cex::CexPriceMap(map));
-        let ser = rkyv::to_bytes::<_, 255>(&copied).unwrap();
-        println!("serialized this");
-        let bytes = ser.into_vec();
-
-        let res: &ArchivedLibmdbxCexPriceMap =
-            rkyv::check_archived_root::<LibmdbxCexPriceMap>(bytes.as_slice()).unwrap();
-        let this: LibmdbxCexPriceMap = res.deserialize(&mut rkyv::Infallible).unwrap();
-
-        println!("deser this {:#?}", this);
     }
 }
