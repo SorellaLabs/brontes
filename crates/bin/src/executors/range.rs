f84bc48dbf7e2016d6eea2a72d46dd783acc03e2
Will Smith <williamqsmith11@gmail.com>
2024-01-31 01:00:30 -0500

    is
    Former-commit-id: 331dc9c739318456dcd650b48a0bc076b126fbb7

--- crates/bin/src/executors/range.rs
+++ crates/bin/src/executors/range.rs
@@ -177,7 +177,7 @@ impl<'db, T: TracingProvider + Clone, DB: LibmdbxReader + LibmdbxWriter>
             let block_number = header.number;
             let mut tree = classifier.build_block_tree(traces, header).await;
             if block_number < START_OF_CHAINBOUND_MEMPOOL_DATA {
-                return (tree, meta);
+                return (tree, meta)
             }
             tree.label_private_txes(&meta);
             (tree, meta)
@@ -186,7 +186,7 @@ impl<'db, T: TracingProvider + Clone, DB: LibmdbxReader + LibmdbxWriter>
 
     fn start_next_block(&mut self) {
         let parser = self.parser.execute(self.current_block);
-        let Some(meta) = self
+        let Ok(meta) = self
             .libmdbx
             .get_metadata_no_dex_price(self.current_block) else {
                 error!(?self.current_block, "failed to load metadata for block");
