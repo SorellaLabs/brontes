011586d49c8d095a6935e68ceabc33b0859bec18
Will Smith <williamqsmith11@gmail.com>
2024-02-01 00:55:22 -0500

    finish

--- crates/brontes-types/src/tree/tx_info.rs
+++ crates/brontes-types/src/tree/tx_info.rs
@@ -2,14 +2,16 @@ use alloy_primitives::{Address, TxHash};
 
 use crate::GasDetails;
 
+#[derive(Debug, Clone, Copy)]
 pub struct TxInfo {
-    pub block_number: u64,
-    pub tx_index:     u64,
-    pub eoa:          Address,
-    pub mev_contract: Address,
-    pub tx_hash:      TxHash,
-    pub gas_details:  GasDetails,
-    pub is_classifed: bool,
+    pub block_number:    u64,
+    pub tx_index:        u64,
+    pub eoa:             Address,
+    pub mev_contract:    Address,
+    pub tx_hash:         TxHash,
+    pub gas_details:     GasDetails,
+    pub is_classifed:    bool,
+    pub is_cex_dex_call: bool,
 }
 
 impl TxInfo {
@@ -21,7 +23,21 @@ impl TxInfo {
         tx_hash: TxHash,
         gas_details: GasDetails,
         is_classifed: bool,
+        is_cex_dex_call: bool,
     ) -> Self {
-        Self { tx_index, block_number, mev_contract, eoa, tx_hash, is_classifed, gas_details }
+        Self {
+            tx_index,
+            block_number,
+            mev_contract,
+            eoa,
+            tx_hash,
+            is_classifed,
+            gas_details,
+            is_cex_dex_call,
+        }
+    }
+
+    pub fn split_to_storage_info(self) -> (TxHash, GasDetails) {
+        (self.tx_hash, self.gas_details)
     }
 }
