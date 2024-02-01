50f960bdaf6836842febc09a8f389fe033645a16
Will Smith <williamqsmith11@gmail.com>
2024-02-01 07:24:08 -0500

    wip
    Former-commit-id: 7a1adddf526ab7140b51da4d465563fa401bb928

--- crates/brontes-pricing/src/graphs/all_pair_graph.rs
+++ crates/brontes-pricing/src/graphs/all_pair_graph.rs
@@ -254,7 +254,7 @@ impl AllPairGraph {
             |node| node == end_idx,
             |node0, node1| (*node0, *node1),
             5,
-            25_000,
+            5_000,
         )
         .into_iter()
         .map(|(nodes, _)| {
