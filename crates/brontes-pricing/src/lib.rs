6b9e958a0ff825cffce630f78824a1b5c64ac410
Will Smith <williamqsmith11@gmail.com>
2024-02-01 13:46:19 -0500

    fix
    Former-commit-id: 6e4bf5d906f9ae04f6c4fd9a3c0d6f73b9717a67

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -62,7 +62,7 @@ use protocols::lazy::{LazyExchangeLoader, LazyResult, LoadResult};
 pub use protocols::{Protocol, *};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use tokio::sync::mpsc::UnboundedReceiver;
-use tracing::{error};
+use tracing::error;
 use types::{DexPriceMsg, DiscoveredPool, PoolUpdate};
 
 use crate::types::PoolState;
