3c5c5a9c9baee921a286b48e26993fd13c272053
Will Smith <williamqsmith11@gmail.com>
2024-02-02 06:11:38 -0500

    finish
    Former-commit-id: cbd8a5c036e7fc3817558dc3dc9a63458f1b1130

--- crates/bin/src/executors/shared/metadata.rs
+++ crates/bin/src/executors/shared/metadata.rs
@@ -1,7 +1,9 @@
-use std::collections::VecDeque;
+use core::panic;
+use std::{collections::VecDeque, pin::Pin, task::Poll};
 
 use brontes_core::{LibmdbxReader, LibmdbxWriter};
 use brontes_database::{clickhouse::Clickhouse, libmdbx::types::dex_price};
+use brontes_pricing::{types::DexPriceMsg, BrontesBatchPricer, GraphManager};
 use brontes_types::{
     db::{
         dex::DexQuotes,
@@ -12,26 +14,59 @@ use brontes_types::{
     BlockTree,
 };
 use eyre::eyre;
-use futures::{Stream, StreamExt};
+use futures::{stream::FuturesOrdered, Future, Stream, StreamExt};
+use reth_tasks::TaskExecutor;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tracing::error;
 
+use super::dex_pricing::WaitingForPricerFuture;
 use crate::executors::dex_pricing::WaitingForPricerFuture;
 
 /// deals with all cases on how we get and finalize our metadata
 pub struct MetadataFetcher<T: TracingProvider, DB: LibmdbxWriter + LibmdbxReader> {
-    clickhouse:        Option<&'static Clickhouse>,
-    dex_pricer_stream: Option<WaitingForPricerFuture<T, DB>>,
+    clickhouse:         Option<&'static Clickhouse>,
+    dex_pricer_stream:  Option<WaitingForPricerFuture<T, DB>>,
     /// we will drain this in the case we aren't running a dex pricer to avoid
     /// being terrible on memory
-    no_price_chan:     Option<UnboundedReceiver<DexPriceMsg>>,
+    no_price_chan:      Option<UnboundedReceiver<DexPriceMsg>>,
+    clickhouse_futures: FuturesOrdered<
+        Pin<Box<dyn Future<Output = (u64, BlockTree<Actions>, MetadataNoDex)> + Send + Sync>>,
+    >,
+
+    result_buf: VecDeque<(BlockTree<Actions>, MetadataCombined)>,
 }
 
-impl<T: TracingProvider, DB: LibmdbxWriter + LibmdbxReader> MetadataFinalizer<T, DB> {
+impl<T: TracingProvider, DB: LibmdbxWriter + LibmdbxReader> MetadataFetcher<T, DB> {
     pub fn new(
         clickhouse: Option<&'static Clickhouse>,
         dex_pricer_stream: Option<WaitingForPricerFuture<T, DB>>,
         no_price_chan: Option<UnboundedReceiver<DexPriceMsg>>,
     ) -> Self {
-        todo!()
+        Self {
+            clickhouse,
+            dex_pricer_stream,
+            no_price_chan,
+            clickhouse_futures: FuturesOrdered::new(),
+            result_buf: VecDeque::new(),
+        }
+    }
+
+    pub fn running_pricing(&self) -> bool {
+        self.dex_pricer_stream.is_some()
+    }
+
+    pub fn get_price_channel(&mut self) -> Option<UnboundedReceiver<DexPriceMsg>> {
+        self.no_price_chan.take()
+    }
+
+    pub fn into_tip_mode(
+        &mut self,
+        pricer: BrontesBatchPricer<T, DB>,
+        clickhouse: &'static Clickhouse,
+        exector: TaskExecutor,
+    ) {
+        self.clickhouse = Some(clickhouse);
+        self.dex_pricer_stream = Some(WaitingForPricerFuture::new(pricer, exector));
     }
 
     fn clear_no_price_chan(&mut self) {
@@ -40,30 +75,53 @@ impl<T: TracingProvider, DB: LibmdbxWriter + LibmdbxReader> MetadataFinalizer<T,
         }
     }
 
-    pub async fn load_metadata_for_tree(
-        &mut self,
-        block: u64,
-        tree: BlockTree<Actions>,
-        libmdbx: &'static DB,
-    ) -> eyre::Result<(BlockTree<Actions>, MetadataCombined)> {
+    pub fn load_metadata_for_tree(&mut self, tree: BlockTree<Actions>, libmdbx: &'static DB) {
+        let block = tree.header.number;
         // clear price channel
         self.clear_no_price_chan();
         // pull directly from libmdbx
         if self.dex_pricer_stream.is_none() && self.clickhouse.is_none() {
             let meta = libmdbx.get_metadata(block)?;
-            return Ok((tree, meta))
-
+            self.result_buf.push_back((tree, meta));
         // need to pull the metadata from clickhouse
         } else if let Some(clickhouse) = self.clickhouse {
-            let meta = clickhouse.get_metadata(block).await;
-            return Ok((tree, meta))
+            let future = Box::pin(async move {
+                let meta = clickhouse.get_metadata(block).await;
+                (block, tree, meta)
+            });
+            self.clickhouse_futures.push_back(future);
         // don't need to pull from clickhouse, means we are running pricing
         } else if let Some(pricer) = self.dex_pricer_stream.as_mut() {
             let meta = libmdbx.get_metadata_no_dex(block)?;
             pricer.add_pending_inspection(block, tree, meta);
-            return Ok(pricer.next().await)
         } else {
-            return Err(eyre!("metadata fetcher is not setup correctly"))
+            panic!("metadata fetcher not setup properly")
         }
+    }
+}
+
+impl<T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter> Stream for MetadataFetcher<T, DB> {
+    type Item = (BlockTree<Actions>, MetadataCombined);
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        self.clear_no_price_chan();
+
+        if let Some(res) = self.result_buf.pop_front() {
+            return Poll::Ready(Some(res))
+        }
+        if let Some(pricer) = self.dex_pricer_stream.as_mut() {
+            while let Poll::Ready(Some((block, tree, meta))) =
+                self.clickhouse_futures.poll_next_unpin(cx)
+            {
+                pricer.add_pending_inspection(block, tree, meta)
+            }
+
+            return pricer.poll_next_unpin(cx)
+        }
+
+        return std::task::Poll::Pending
     }
 }
--- crates/bin/src/executors/shared/state_collector.rs
+++ crates/bin/src/executors/shared/state_collector.rs
@@ -1,30 +1,115 @@
+use std::{
+    pin::Pin,
+    sync::{
+        atomic::{AtomicBool, Ordering::SeqCst},
+        Arc,
+    },
+    task::Poll,
+};
+
 use brontes_classifier::Classifier;
 use brontes_core::{decoding::Parser, LibmdbxReader, LibmdbxWriter};
+use brontes_database::clickhouse::Clickhouse;
+use brontes_pricing::{types::DexPriceMsg, BrontesBatchPricer, GraphManager};
 use brontes_types::{
     db::metadata::MetadataCombined, normalized_actions::Actions, traits::TracingProvider, BlockTree,
 };
 use eyre::eyre;
+use futures::{executor, stream::Buffered, Stream, StreamExt};
+use reth_tasks::TaskExecutor;
 use tracing::info;
 
 use super::metadata::MetadataFetcher;
 
-pub async fn collect_all_state<T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter>(
-    block: u64,
-    db: &'static DB,
+type CollectionFut<'a> =
+    Pin<Box<dyn Future<Output = eyre::Result<BlockTree<Actions>>> + Send + 'a>>;
+
+pub struct StateCollector<T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter> {
+    mark_as_finished: Arc<AtomicBool>,
     metadata_fetcher: MetadataFetcher<T, DB>,
-    parser: &'static Parser<'static, T, DB>,
-    classifier: &'static Classifier<'static, T, DB>,
-) -> eyre::Result<(MetadataFetcher<T, DB>, BlockTree<Actions>, MetadataCombined)> {
-    let (traces, header) = parser
-        .execute(block)
-        .await?
-        .ok_or_else(|| eyre!("no traces for block {block}"))?;
+    classifier:       &'static Classifier<'static, T, DB>,
+    parser:           &'static Parser<'static, T, DB>,
+    db:               &'static DB,
 
-    info!("Got {} traces + header", traces.len());
-    let tree = classifier.build_block_tree(traces, header).await;
-    let (tree, meta) = metadata_fetcher
-        .load_metadata_for_tree(block, tree, db)
-        .await?;
+    collection_future: Option<CollectionFut<'static>>,
+}
 
-    Ok((metadata_fetcher, meta, tree))
+impl<T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter> StateCollector<T, DB> {
+    pub fn new(
+        mark_as_finished: Arc<AtomicBool>,
+        metadata_fetcher: MetadataFetcher<T, DB>,
+        classifier: &'static Classifier<'static, T, DB>,
+        parser: &'static Parser<'static, T, DB>,
+        db: &'static DB,
+    ) -> Self {
+        Self { mark_as_finished, metadata_fetcher, classifier, parser, db, collection_future: None }
+    }
+
+    pub fn is_running_pricing(&self) -> bool {
+        self.metadata_fetcher.running_pricing()
+    }
+
+    pub fn get_shutdown(&self) -> Arc<AtomicBool> {
+        self.mark_as_finished.clone()
+    }
+
+    pub fn get_price_channel(&mut self) -> Option<UnboundedReceiver<DexPriceMsg>> {
+        self.metadata_fetcher.get_price_channel()
+    }
+
+    pub fn is_collecting_state(&self) -> bool {
+        self.collection_future.is_some()
+    }
+
+    pub fn into_tip_mode(
+        &mut self,
+        pricer: BrontesBatchPricer<T, DB>,
+        clickhouse: &'static Clickhouse,
+        executor: TaskExecutor,
+    ) {
+        self.metadata_fetcher
+            .into_tip_mode(pricer, clickhouse, executor)
+    }
+
+    pub fn fetch_state_for(&mut self, block: u64) {
+        self.collection_future = Some(Box::pin(async move {
+            let (traces, header) = parser
+                .execute(block)
+                .await?
+                .ok_or_else(|| eyre!("no traces for block {block}"))?;
+
+            info!("Got {} traces + header", traces.len());
+            Ok(classifier.build_block_tree(traces, header).await)
+        }));
+    }
+
+    pub fn range_finished(&self) {
+        self.mark_as_finished.store(true, SeqCst);
+    }
+}
+
+impl<T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter> Stream for StateCollector<T, DB> {
+    type Item = (BlockTree<Actions>, MetadataCombined);
+
+    fn poll_next(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        if let Some(collection_future) = self.collection_future.take() {
+            match collection_future.poll_unpin() {
+                Poll::Ready(Ok(tree)) => {
+                    self.metadata_fetcher.load_metadata_for_tree(tree, self.db);
+                }
+                Poll::Ready(Err(e)) => {
+                    tracing::error!(error = e, "state collector");
+                    return Poll::Ready(None)
+                }
+                Poll::Pending => {
+                    self.collection_future = Some(collection_future);
+                }
+            }
+        }
+
+        self.metadata_fetcher.poll_next_unpin(cx)
+    }
 }
