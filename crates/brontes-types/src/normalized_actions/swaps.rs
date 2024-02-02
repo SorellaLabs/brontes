b9680ee64c20b7de2482d10fa318ee999b98921d
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-02-01 22:05:35 -0500

    debug
    Former-commit-id: 152f3be03e3e42ad538cffcc42a8d2011ebbcde0

--- crates/brontes-types/src/normalized_actions/swaps.rs
+++ crates/brontes-types/src/normalized_actions/swaps.rs
@@ -55,8 +55,7 @@ impl NormalizedSwap {
     /// Calculates the rate for a given DEX swap
 
     pub fn swap_rate(&self) -> Rational {
-        // Choose the calculation method based on your standard representation
-        &self.amount_in / &self.amount_out
+        &self.amount_out / &self.amount_in
     }
 }
 
