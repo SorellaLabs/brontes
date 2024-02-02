d656a3f460a1a5f13f36d0b35fddb716d2bc4398
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-02-02 18:03:34 -0500

    remove unecessary function

--- crates/brontes-types/src/mev/bundle/data.rs
+++ crates/brontes-types/src/mev/bundle/data.rs
@@ -12,7 +12,7 @@ use crate::mev::{
 };
 #[allow(unused_imports)]
 use crate::{
-    display::utils::{display_sandwich, print_mev_type_header},
+    display::utils::display_sandwich,
     normalized_actions::{NormalizedBurn, NormalizedLiquidation, NormalizedMint, NormalizedSwap},
     serde_primitives::vec_fixed_string,
     GasDetails,
