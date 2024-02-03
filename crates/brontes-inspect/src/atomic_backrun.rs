eb74ac8b76714959ac5867a312c675a11f453bd0
Will Smith <williamqsmith11@gmail.com>
2024-02-03 09:01:42 -0500

    less false
    Former-commit-id: 16bca274d66004b157ac0c44dff7890d5e1e95cb

--- crates/brontes-inspect/src/atomic_backrun.rs
+++ crates/brontes-inspect/src/atomic_backrun.rs
@@ -121,13 +121,12 @@ impl<DB: LibmdbxReader> AtomicBackrunInspector<'_, DB> {
             return None
         } else if swaps.len() == 2 {
             let start = swaps[0].token_in.address;
-            let mid = swaps[0].token_out.address;
-            let mid1 = swaps[1].token_in.address;
             let end = swaps[1].token_out.address;
 
-            if !(start == end && mid == mid1) {
+            if start != end  {
                 return None
             }
+
         } else {
             let mut address_to_tokens: HashMap<Address, Vec<Address>> = HashMap::new();
             swaps.iter().for_each(|swap| {
