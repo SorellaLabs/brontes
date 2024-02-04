fc20599bc87a011ff814180afb2d5141e11f1e2e
Will Smith <williamqsmith11@gmail.com>
2024-02-04 14:39:18 -0500

    hopefullyfix
    Former-commit-id: 079dde3c69f4b2bd9af266bf16f9a1d6c7beb93e

--- crates/brontes-inspect/build.rs
+++ crates/brontes-inspect/build.rs
@@ -4,7 +4,6 @@ use std::{env, path};
 use alloy_primitives::Address;
 use brontes_database::libmdbx::{LibmdbxReadWriter, LibmdbxWriter};
 use brontes_types::{db::token_info::TokenInfoWithAddress, Protocol};
-use serde::Deserialize;
 use toml::Table;
 
 const CONFIG_FILE_NAME: &str = "manual_inserts.toml";
@@ -13,26 +12,12 @@ fn main() {
     insert_manually_defined_entries()
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Config {
-    pub protocol_name: Protocol,
-    pub pools:         Vec<ProtocolTable>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct ProtocolTable {
-    init_block: u64,
-    token_info: Vec<TokenInfoWithAddress>,
-}
-
 fn insert_manually_defined_entries() {
     // don't run on local
     dotenv::dotenv().unwrap();
     let Ok(brontes_db_endpoint) = env::var("BRONTES_DB_PATH") else { return };
 
-    let Ok(libmdbx) = LibmdbxReadWriter::init_db(brontes_db_endpoint, None) else {
-        return 
-    };
+    let Ok(libmdbx) = LibmdbxReadWriter::init_db(brontes_db_endpoint, None) else { return };
 
     let mut workspace_dir = workspace_dir();
     workspace_dir.push(CONFIG_FILE_NAME);
@@ -45,22 +30,24 @@ fn insert_manually_defined_entries() {
         let protocol: Protocol = protocol.parse().unwrap();
         for (address, table) in inner.as_table().unwrap() {
             let token_addr: Address = address.parse().unwrap();
+            let init_block = table.get("init_block").unwrap().as_integer().unwrap() as u64;
 
-            let table: ProtocolTable = toml::from_str(&table.to_string()).unwrap();
+            let table: Vec<TokenInfoWithAddress> =
+                toml::from_str(&table.get("token_info").unwrap().to_string()).unwrap();
 
-            for t_info in &table.token_info {
+            for t_info in &table {
                 libmdbx
                     .write_token_info(t_info.address, t_info.decimals, t_info.symbol.clone())
                     .unwrap();
             }
 
-            if table.token_info.len() < 2 {
+            if table.len() < 2 {
                 panic!("Config entry missing token info");
             }
 
-            let token_addrs = [table.token_info[0].address, table.token_info[1].address];
+            let token_addrs = [table[0].address, table[1].address];
             libmdbx
-                .insert_pool(table.init_block, token_addr, token_addrs, protocol)
+                .insert_pool(init_block, token_addr, token_addrs, protocol)
                 .unwrap();
             // not reaching here
         }
