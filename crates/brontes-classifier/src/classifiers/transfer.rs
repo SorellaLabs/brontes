e983ed4828ec2b8478dd8aa7d538ca8a89825933
Will Smith <williamqsmith11@gmail.com>
2024-02-04 17:22:57 -0500

    show transfer on amount in

---
+++ crates/brontes-classifier/src/classifiers/transfer.rs
@@ -0,0 +1,29 @@
+use alloy_primitives::{Address, Bytes};
+use alloy_sol_types::SolCall;
+use brontes_types::{
+    db::traits::LibmdbxReader, normalized_actions::NormalizedTransfer, ToScaledRational,
+};
+
+alloy_sol_macro::sol!(
+    function transfer(address, uint) returns(bool);
+);
+
+pub fn try_decode_transfer<DB: LibmdbxReader>(
+    idx: u64,
+    calldata: Bytes,
+    from: Address,
+    to: Address,
+    token: Address,
+    db: &DB,
+) -> Option<NormalizedTransfer> {
+    let res = transferCall::abi_decode(&calldata, false).ok()?;
+    let token_info = db.try_get_token_info(token).ok()??;
+
+    Some(NormalizedTransfer {
+        amount: res._1.to_scaled_rational(token_info.decimals),
+        token: token_info,
+        to,
+        from,
+        trace_index: idx,
+    })
+}
