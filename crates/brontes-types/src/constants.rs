c3f448041640e44810b99b9691908f8a08dc1723
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-02-03 22:28:08 -0500

    changed defaults

--- crates/brontes-types/src/constants.rs
+++ crates/brontes-types/src/constants.rs
@@ -1,5 +1,7 @@
 use alloy_primitives::{hex, Address, Bytes};
 
+pub const USDT_ADDRESS_STRING: &str = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+
 pub const WETH_ADDRESS: Address = Address::new(hex!("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"));
 pub const USDT_ADDRESS: Address = Address::new(hex!("dAC17F958D2ee523a2206206994597C13D831ec7"));
 pub const USDC_ADDRESS: Address = Address::new(hex!("A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"));
