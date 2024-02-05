a333355e662904cd9c97fa1459cb36ec965747e8
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-02-05 18:11:00 -0500

    wip, initial test
    Former-commit-id: 934e247cb3a70aba4d0f297cac3d3bd1c58101f7

--- crates/brontes-types/src/constants.rs
+++ crates/brontes-types/src/constants.rs
@@ -36,8 +36,8 @@ pub const SCP_MAIN_CEX_DEX_BOT: Address =
 pub const EXECUTE_FFS_YO: Bytes = Bytes::from_static(&[0x78, 0xe1, 0x11, 0xf6]);
 
 pub const EURO_STABLES: [&str; 2] = [
-    "EURS", // STASIS EURO
     "EURT", // Tether Euro
+    "EURS", // STASIS EURO
 ];
 
 pub const GOLD_STABLES: [&str; 2] = [
@@ -49,27 +49,27 @@ pub const USD_STABLES: [&str; 25] = [
     "USDT",    // Tether
     "USDC",    // USD Coin
     "DAI",     // Dai
-    "FDUSD",   // First Digital USD
     "TUSD",    // TrueUSD
-    "USDD",    // His excellency USD
     "FRAX",    // Frax
     "USDP",    // Pax Dollar
+    "BUSD",    // Binance USD
+    "MIM",     // Magic Internet Money
+    "GUSD",    // Gemini Dollar
+    "DOLA",    // Dola USD Stablecoin
+    "CRVUSD",  // Curve USD
+    "FDUSD",   // First Digital USD
+    "USDD",    // His excellency USD
     "PYUSD",   // PaypalUSD
     "USTC",    // TerraUSD Classic
     "ALUSD",   // Alchemix USD
     "USDE",    // Ethena USD
     "LUSD",    // Liquity USD
     "MKUSD",   // Prisma USD
-    "CRVUSD",  // Curve USD
-    "BUSD",    // Binance USD
-    "MIM",     // Magic Internet Money
     "SUSD",    // sUSD
     "HAY",     // Hay
     "BEAN",    // Bean
-    "GUSD",    // Gemini Dollar
     "GHO",     // Aave
     "USX",     // dForce USD
-    "DOLA",    // Dola USD Stablecoin
     "MIMATIC", // MAI (Mimatic)
 ];
 
@@ -82,4 +82,22 @@ pub fn is_euro_stable(symbol: &str) -> bool {
 
 pub fn is_gold_stable(symbol: &str) -> bool {
     GOLD_STABLES.contains(&symbol)
+}
+
+pub fn get_stable_type(symbol: &str) -> Option<StableType> {
+    if USD_STABLES.contains(&symbol) {
+        Some(StableType::USD)
+    } else if EURO_STABLES.contains(&symbol) {
+        Some(StableType::EURO)
+    } else if GOLD_STABLES.contains(&symbol) {
+        Some(StableType::GOLD)
+    } else {
+        None
+    }
+}
+
+pub enum StableType {
+    USD,
+    EURO,
+    GOLD,
 }
