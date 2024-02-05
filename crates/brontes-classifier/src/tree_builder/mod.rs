3c9176005ebc341352a849822d0b25aa3bd4fd7b
Will Smith <williamqsmith11@gmail.com>
2024-02-04 19:15:34 -0500

    log

--- crates/brontes-classifier/src/tree_builder/mod.rs
+++ crates/brontes-classifier/src/tree_builder/mod.rs
@@ -367,7 +367,8 @@ impl<'db, T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter> Classifier<'db,
                     let log_am = amount.to_scaled_rational(decimals);
 
                     if log_am != transfer.amount {
-                        tracing::info!("tax token");
+
+                        tracing::info!(?transfer, "tax token");
                         let transferred_amount = min(&log_am, &transfer.amount).clone();
                         let fee = (&log_am - &transfer.amount).abs();
                         transfer.amount = transferred_amount;
