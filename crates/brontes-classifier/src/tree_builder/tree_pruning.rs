a2f6e4f35d6bd78b49fbbad573030e3a13bde468
Will Smith <williamqsmith11@gmail.com>
2024-02-04 22:10:43 -0500

    test

--- crates/brontes-classifier/src/tree_builder/tree_pruning.rs
+++ crates/brontes-classifier/src/tree_builder/tree_pruning.rs
@@ -256,6 +256,7 @@ mod test {
             hex!("8ea5ea6de313e466483f863071461992b3ea3278e037513b0ad9b6a29a4429c1").into(),
             |node| (node.data.is_swap(), node.inner.iter().any(|n| n.data.is_swap())),
         );
+        tracing::info!("{:#?}", swaps);
         assert!(swaps.len() == 6, "didn't filter tax token");
     }
 }
