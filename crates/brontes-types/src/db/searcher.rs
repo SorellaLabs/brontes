0af3098e3f15637ac03bbf2cd9804020f9b78575
lagrange.eth <lagrange@sorellacapital.com>
2024-02-05 14:38:16 -0500

    added searcher, builder, and address metadata tables
    Former-commit-id: 8c380606d43a56d3af4ec98f62f08c1337d14357

---
+++ crates/brontes-types/src/db/searcher.rs
@@ -0,0 +1,33 @@
+use redefined::{self_convert_redefined, Redefined};
+use rkyv::{Archive, Deserialize as rDeserialize, Serialize as rSerialize};
+use serde::{self, Deserialize, Serialize};
+use sorella_db_databases::{clickhouse, clickhouse::Row};
+
+use super::builder::BuilderInfo;
+use crate::{
+    db::builder::BuilderInfoRedefined, implement_table_value_codecs_with_zc, mev::MevType,
+};
+
+#[derive(Debug, Default, Row, PartialEq, Clone, Serialize, Deserialize, Redefined)]
+#[redefined_attr(derive(Debug, PartialEq, Clone, Serialize, rSerialize, rDeserialize, Archive))]
+pub struct SearcherInfo {
+    #[redefined(same_fields)]
+    pub fund:        Option<Fund>,
+    pub pnl:         f64,
+    #[redefined(same_fields)]
+    pub mev:         Vec<MevType>,
+    pub builder:     Option<BuilderInfo>,
+    pub last_active: u64,
+}
+
+implement_table_value_codecs_with_zc!(SearcherInfoRedefined);
+
+#[derive(
+    Debug, Default, PartialEq, Clone, Serialize, Deserialize, rSerialize, rDeserialize, Archive,
+)]
+pub enum Fund {
+    #[default]
+    None,
+}
+
+self_convert_redefined!(Fund);
