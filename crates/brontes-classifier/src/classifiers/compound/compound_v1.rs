c8a46876161abafa35c30c42af070bcbcee85698
1010adigupta <1010adigupta@gmail.com>
2024-02-06 18:59:50 +0530

    fixes and fmt

--- crates/brontes-classifier/src/classifiers/compound/compound_v1.rs
+++ crates/brontes-classifier/src/classifiers/compound/compound_v1.rs
@@ -1,26 +1,30 @@
-use alloy_primitives::{Address, b256, U256};
+use alloy_primitives::{b256, Address};
 use brontes_macros::action_impl;
 use brontes_pricing::Protocol;
-use brontes_types::normalized_actions:: NormalizedLiquidation;
+use brontes_types::{normalized_actions::NormalizedLiquidation, utils::ToScaledRational};
 use malachite::{num::basic::traits::Zero, Rational};
 
 action_impl!(
     Protocol::CompoundV2,
     crate::CompoundV2CEther::liquidateBorrowCall,
     Liquidation,
-    [LiquidationEvent],
+    [..LiquidateBorrow],
     call_data: true,
+    logs: true,
     |trace_index,
     _from_address: Address,
     target_address: Address,
     msg_sender: Address,
     call_data: liquidateBorrowCall,
+    log_data: CompoundV2liquidateBorrowCallLogs,
     db_tx: &DB | {
+        let logs = log_data.LiquidateBorrow_field;
         let debt_asset: Address = Address::from_word(b256!("0000000000000000000000004Ddc2D193948926D02f9B1fE9e1daa0718270ED5"));
         let debt_info = db_tx.try_get_token_info(debt_asset).ok()??;
         let collateral_info = db_tx.try_get_token_info(call_data.cTokenCollateral).ok()??;
 
-        let covered_debt = msg_value.to_scaled_rational(debt_info.decimals);
+        let covered_debt = logs.repayAmount
+        .to_scaled_rational(debt_info.decimals);
 
         return Some(NormalizedLiquidation {
             protocol: Protocol::CompoundV2,
@@ -36,4 +40,3 @@ action_impl!(
         })
     }
 );
-
