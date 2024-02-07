72ff0e3b81c9765e64c70cf21aacc9ec99fbd226
lagrange.eth <lagrange@sorellacapital.com>
2024-02-07 16:41:18 -0500

    fixed
    Former-commit-id: f2a5ff42cbd2f12fa72e5b440a1464e3ddf72ae0

--- crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT 
     toString(p.address) AS address, 
-    (arrayMap(x -> toString(x), p.tokens), p.init_block, d.classifier_name) AS tokens
+    (arrayMap(x -> toString(x), p.tokens), p.init_block, CAST(d.classifier_name, 'String')) AS tokens
 FROM ethereum.pools p
 RIGHT JOIN brontes.protocol_details d ON p.address = d.address
 WHERE classifier_name IS NOT NULL
