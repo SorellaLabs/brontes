cace8ad586a03e8c97a51b9fdc8107b1d5a52e4a
lagrange.eth <lagrange@sorellacapital.com>
2024-02-07 16:38:30 -0500

    fixed

--- crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -11,3 +11,16 @@ WHERE classifier_name IS NOT NULL
 
 
 
+
+
+
+
+
+
+SELECT DISTINCT 
+    toString(p.address) AS address, 
+    (arrayMap(x -> toString(x), p.tokens), p.init_block, d.classifier_name) AS tokens
+FROM ethereum.pools p
+RIGHT JOIN brontes.protocol_details d ON p.address = d.address
+WHERE p.init_block = 16349269
+AND classifier_name IS NOT NULL
