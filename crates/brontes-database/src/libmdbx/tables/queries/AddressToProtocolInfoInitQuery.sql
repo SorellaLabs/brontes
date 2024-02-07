9c0959ecabb5453301287ffd70fe60f5cc6bc12a
Will Smith <williamqsmith11@gmail.com>
2024-02-07 15:48:05 -0500

    move sql
    Former-commit-id: b86b150d5c370d49297ba75abb23af5d0f7efc6a

---
+++ crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT 
+    toString(address) AS address, 
+    arrayMap(x -> toString(x), tokens), init_block, classifier_name) AS tokens
+FROM ethereum.pools 
+LEFT JOIN brontes.protocol_details
+on ethereum.pools.address = brontes.protocol_details.address
+HAVING classifier_name IS NOT NULL AND classifier_name != ''
