97e5f1ce133a94a7ecde0fe023591babb9b9783a
Will Smith <williamqsmith11@gmail.com>
2024-02-08 15:04:09 -0500

    lfg

--- crates/brontes-classifier/src/tree_builder/mod.rs
+++ crates/brontes-classifier/src/tree_builder/mod.rs
@@ -192,7 +192,7 @@ impl<'db, T: TracingProvider, DB: LibmdbxReader + LibmdbxWriter> Classifier<'db,
         )
         .await
         .into_iter()
-        .filter_map(|f| f)
+        .flatten()
         .collect_vec()
     }
 
@@ -455,7 +455,7 @@ pub mod test {
         for i in &swap {
             if let Actions::Transfer(t) = i {
                 if swaps.get(&t.token).map(|i| i.contains(&t.amount)) == Some(true) {
-                    assert!(false, "found a transfer that was part of a swap");
+                    panic!("found a transfer that was part of a swap");
                 }
             }
         }
