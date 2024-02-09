e8cd8ecd5f9260efceb7a2c48c84d50d64a46bc6
vanbeethoven.eth <103021596+0xvanbeethoven@users.noreply.github.com>
2024-02-09 18:43:33 -0500

    Ludwig/batch classifier (#372)
    * added eth transfer classification
    
    * fixed batch test
    
    * added uni x to config
    
    * try to fix
    
    * test
    
    * test + clippy
    
    * test
    
    * Fixed print messages & native eth Token info
    
    * test weth to usdt batch
    
    * clippy + fmt
    
    * full uni v3 tests
    
    * added pancake swap v3 test

--- crates/brontes-classifier/src/classifiers/sushiswap/sushiswap_v2.rs
+++ crates/brontes-classifier/src/classifiers/sushiswap/sushiswap_v2.rs
@@ -92,7 +92,7 @@ action_impl!(
             recipient: call_data.to,
             from: info.from_address,
             trace_index: info.trace_idx,
-            to: info.target_address,
+            pool: info.target_address,
             token: vec![t0_info, t1_info],
             amount: vec![am0, am1],
         })
@@ -123,10 +123,10 @@ action_impl!(
 
         Ok(NormalizedBurn {
             protocol: Protocol::SushiSwapV2,
-            recipient: call_data.to,
-            to: info.target_address,
             trace_index: info.trace_idx,
             from: info.from_address,
+            recipient: call_data.to,
+            pool: info.target_address,
             token: vec![t0_info, t1_info],
             amount: vec![am0, am1],
         })
