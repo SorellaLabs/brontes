705c314d85c1b096afca81fee7714ef296367163
Will Smith <williamqsmith11@gmail.com>
2024-02-09 12:25:02 -0500

    wip

--- crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT 
     toString(p.address) AS address, 
     (arrayMap(x -> toString(x), p.tokens), p.init_block, CAST(d.classifier_name, 'String')) AS tokens
-FROM ethereum.pools p
+FROM ethereum.pools2 p
 RIGHT JOIN brontes.protocol_details d ON p.address = d.address
 WHERE classifier_name IS NOT NULL
 
