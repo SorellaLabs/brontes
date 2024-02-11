81b42447458e54b91c4890c3bb09ef45d205b860
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2024-02-11 12:31:21 -0500

    fix: unreachable err (#382)
    * pancake v3 discovery
    
    * wip
    
    * test
    
    * moe log
    
    * wip
    
    * test
    
    * fix abi
    
    * fmt + fix
    
    * happy curve
    
    * fix: leave comments for unreachable
    
    * wip
    
    * test
    
    * tracing
    
    * no hyper logs
    
    * fix
    
    * fix
    
    * test
    
    * more
    
    * wip
    
    * wip
    
    * wip
    
    * fmt + fix

--- crates/brontes-inspect/src/jit.rs
+++ crates/brontes-inspect/src/jit.rs
@@ -68,8 +68,10 @@ impl<DB: LibmdbxReader> Inspector for JitInspector<'_, DB> {
                             })
                         })
                         .collect::<Vec<Vec<Actions>>>();
+                    tracing::debug!(?frontrun_tx, ?backrun_tx, "checking if jit");
 
                     if searcher_actions.is_empty() {
+                        tracing::debug!("no searcher actions found");
                         return None
                     }
 
@@ -84,6 +86,7 @@ impl<DB: LibmdbxReader> Inspector for JitInspector<'_, DB> {
                         .filter(|d| !d.is_revert())
                         .any(|d| mev_executor_contract == d.get_to_address())
                     {
+                        tracing::debug!("victim address is same as mev executor contract");
                         return None
                     }
 
@@ -93,7 +96,7 @@ impl<DB: LibmdbxReader> Inspector for JitInspector<'_, DB> {
                             tree.collect(*victim, |node| TreeSearchArgs {
                                 collect_current_node:  node.data.is_swap(),
                                 child_node_to_collect: node
-                                    .subactions
+                                    .get_all_sub_actions()
                                     .iter()
                                     .any(|action| action.is_swap()),
                             })
@@ -101,6 +104,7 @@ impl<DB: LibmdbxReader> Inspector for JitInspector<'_, DB> {
                         .collect_vec();
 
                     if victim_actions.iter().any(|inner| inner.is_empty()) {
+                        tracing::debug!("no victim actions found");
                         return None
                     }
 
@@ -153,6 +157,7 @@ impl<DB: LibmdbxReader> JitInspector<'_, DB> {
         let fee_collect = collect.into_iter().flatten().collect::<Vec<_>>();
 
         if mints.is_empty() || burns.is_empty() {
+            tracing::debug!("missing mints & burns");
             return None
         }
 
@@ -360,7 +365,11 @@ impl<DB: LibmdbxReader> JitInspector<'_, DB> {
             .filter_map(|(token, amount)| {
                 Some(
                     self.inner
-                        .get_dex_usd_price(idx, PriceAt::After, token, metadata.clone())?
+                        .get_dex_usd_price(idx, PriceAt::After, token, metadata.clone())
+                        .or_else(|| {
+                            tracing::debug!(?token, "failed to get price for token");
+                            None
+                        })?
                         * amount,
                 )
             })
