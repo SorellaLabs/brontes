22d9662fdc6a44d29ac6733715eb88be64937d58
vanbeethoven.eth <103021596+0xvanbeethoven@users.noreply.github.com>
2024-02-10 19:21:29 -0500

    Ludwig/new classifier (#378)
    * wip
    
    * minor edits
    
    * added pancake v2 & merged duox display improvement
    
    * renamed decoder to discovery
    
    * fmt
    
    ---------
    
    Co-authored-by: duoxehyon <jagannthkn@gmail.com>
    Former-commit-id: 2263eb738dd7e1d966c83f4ec714f8572e91cdb5

--- crates/brontes-classifier/src/classifiers/pancakeswap/discovery.rs
+++ crates/brontes-classifier/src/classifiers/pancakeswap/discovery.rs
@@ -3,7 +3,24 @@ use brontes_macros::discovery_impl;
 use brontes_pricing::Protocol;
 
 discovery_impl!(
-    PancakeSwapV3Decoder,
+    PancakeSwapV2Discovery,
+    crate::UniswapV2Factory::createPairCall,
+    0x1097053Fd2ea711dad45caCcc45EfF7548fCB362,
+    |deployed_address: Address, trace_index: u64, call_data: createPairCall, _| async move {
+        let token_a = call_data.tokenA;
+        let token_b = call_data.tokenB;
+
+        vec![NormalizedNewPool {
+            pool_address: deployed_address,
+            trace_index,
+            protocol: Protocol::SushiSwapV2,
+            tokens: vec![token_a, token_b],
+        }]
+    }
+);
+
+discovery_impl!(
+    PancakeSwapV3Discovery,
     crate::UniswapV3Factory::createPoolCall,
     0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865,
     |deployed_address: Address, trace_index: u64, call_data: createPoolCall, _| async move {
@@ -18,4 +35,3 @@ discovery_impl!(
         }]
     }
 );
-// Add v2 : 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73
