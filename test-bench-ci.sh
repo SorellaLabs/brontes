bd932a7911829ccdca960d8235e75ba77b5c589b
Will Smith <williamqsmith11@gmail.com>
2024-02-13 12:18:15 -0500

    fix

--- test-bench-ci.sh
+++ test-bench-ci.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-write_lock() {
-  echo $1 > .ci_lock
-}
-read_lock() {
-  lock=`cat .ci_lock`
-}
-
 run_tests() {
   exec | 
     git pull
@@ -28,14 +21,6 @@ run_benchmarks() {
     git checkout main
 }
 
-# simple lock to ensure only one ci can be running at once
-read_lock
-while [ $lock -eq 1 ]; do
-  read_lock
-done
-
-write_lock 1
 run_tests
 run_benchmarks
-write_lock 0
 
