e94346ee355df02aebf3eb6ce337e6df03cb9c6d
Will Smith <williamqsmith11@gmail.com>
2024-02-13 11:49:20 -0500

    test-bench-script

---
+++ test-bench-ci.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+function run_tests() {
+  exec git pull
+  exec git checkout $1
+  exec git pull
+  exec rustup default nightly
+  exec cargo +nightly test
+  exec git checkout main
+}
+
+function run_benchmarks() {
+  exec rm -rf /home/data/brontes-test/*
+  exec git pull
+  exec git checkout $1
+  exec git pull
+  exec rustup default nightly
+  exec cargo +nightly bench 
+  exec git checkout main
+}
+
+# simple lock to ensure only one ci can be running at once
+while [ $CI_RUNNING -eq "TRUE" ] 
+do
+done
+
+export CI_RUNNING="TRUE"
+run_tests()
+run_benchmarks()
+export CI_RUNNING="FALSE"
+
+
