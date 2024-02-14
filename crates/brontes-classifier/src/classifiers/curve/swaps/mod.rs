6dbd58d30e18019656b1f27b2ab4e297c2705a2b
lagrange.eth <lagrange@sorellacapital.com>
2024-02-13 19:13:33 -0500

    more testing
    Former-commit-id: 6c43aec723af830dac1914627f5b769fa4f5277e

--- crates/brontes-classifier/src/classifiers/curve/swaps/mod.rs
+++ crates/brontes-classifier/src/classifiers/curve/swaps/mod.rs
@@ -4,14 +4,8 @@ pub use base::*;
 mod v1_meta;
 pub use v1_meta::*;
 
-mod v1_meta_impl;
-pub use v1_meta_impl::*;
-
 mod v2_meta;
 pub use v2_meta::*;
-
-mod v2_meta_impl;
-pub use v2_meta_impl::*;
 
 mod v2_plain;
 pub use v2_plain::*;
