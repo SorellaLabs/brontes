ba3eaf805311f6da4562878d6f05e000a68d60e0
Will Smith <williamqsmith11@gmail.com>
2024-02-15 11:43:51 -0500

    fix db query

--- crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT 
-    toString(p.address) AS address, 
+    toString(d.address) AS address, 
     (arrayMap(x -> toString(x), p.tokens), p.init_block, CAST(d.classifier_name, 'String'), CAST(p.curve_lp_token, 'Nullable(String)')) AS tokens
 FROM ethereum.pools p
 RIGHT JOIN brontes.protocol_details d ON p.address = d.address
