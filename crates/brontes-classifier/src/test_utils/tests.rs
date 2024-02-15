5b58ec91e08da8b354fb9776a5c6e7a6f3faf468
lagrange.eth <lagrange@sorellacapital.com>
2024-02-14 19:46:52 -0500

    made dbs sync + send

--- crates/brontes-classifier/src/test_utils/tests.rs
+++ crates/brontes-classifier/src/test_utils/tests.rs
@@ -256,6 +256,7 @@ impl ClassifierTestUtils {
             if let Some((p_block, pricing)) = pricer.next().await {
                 self.libmdbx
                     .write_dex_quotes(p_block, Some(pricing.clone()))
+                    .await
                     .unwrap();
                 Some(pricing)
             } else {
@@ -412,6 +413,7 @@ impl ClassifierTestUtils {
                 // because we have pricing for full block. we store it
                 self.libmdbx
                     .write_dex_quotes(p_block, Some(pricing.clone()))
+                    .await
                     .unwrap();
                 Some(pricing)
             } else {
