cfa39accb034386f97953dad5e15d2f35ea11351
lagrange.eth <lagrange@sorellacapital.com>
2024-02-14 19:55:28 -0500

    made dbs sync + send

--- crates/brontes-types/src/mev/liquidation.rs
+++ crates/brontes-types/src/mev/liquidation.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 
 use ::serde::ser::{SerializeStruct, Serializer};
-use clickhouse::{fixed_string::FixedString, DbRow};
+use clickhouse::{self, fixed_string::FixedString, DbRow, Row};
 use redefined::Redefined;
 use reth_primitives::B256;
 use rkyv::{Archive, Deserialize as rDeserialize, Serialize as rSerialize};
