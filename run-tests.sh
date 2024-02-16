09107e923cb34692fd0f5561176687924f5a96e9
Will Smith <williamqsmith11@gmail.com>
2024-02-16 13:19:31 -0500

    wip

--- run-tests.sh
+++ run-tests.sh
@@ -1,39 +1,35 @@
 #!/bin/sh
 
 setup() {
-  mkdir -p /home/data/brontes-ci/$2
+  mkdir -p /home/data/brontes-ci/$1
   cp /home/brontes-ci/.env .
-  echo "BRONTES_DB_PATH=/home/data/brontes-ci/$2" >> .env 
-  echo "BRONTES_TST_DB_PATH=/home/data/brontes-ci/$2" >> .env 
+  echo "BRONTES_DB_PATH=/home/data/brontes-ci/$1" >> .env 
+  echo "BRONTES_TST_DB_PATH=/home/data/brontes-ci/$1" >> .env 
   
 }
 
 # deletes repo and test db
 teardown() {
   # delete db
-  rm -rf /home/data/brontes-ci/$2
+  rm -rf /home/data/brontes-ci/$1
   # delete folder
-  rm -rf /home/brontes-ci/$2
+  rm -rf /home/brontes-ci/$1
 }
 
-setup()
+setup
 
-git pull
-git checkout $1
-git pull
 rustup default nightly
 # we put these in different folders so that if you're on a branch and change these, they will run the branch version
-if [ $3 -eq "it" ]; then 
-  if ./it.sh $1; then : ; else exit; fi
+if [ $2 -eq "it" ]; then 
+  if ./it.sh; then : ; else exit; fi
 fi 
 
-if [ $3 -eq "test" ]; then 
-  if ./test.sh $1; then : ; else exit; fi
+if [ $2 -eq "test" ]; then 
+  if ./test.sh; then : ; else exit; fi
 fi
 
-if [ $3 -eq "bench" ]; then 
-  if ./bench.sh $1; then : ; else exit; fi
+if [ $2 -eq "bench" ]; then 
+  if ./bench.sh; then : ; else exit; fi
 fi 
 
-teardown()
-
+teardown
