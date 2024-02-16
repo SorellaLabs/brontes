f08ab4d5fcb8aef1df84a878ec37d7111ba6820d
Will Smith <williamqsmith11@gmail.com>
2024-02-16 13:04:12 -0500

    fix

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
+
+setup() {
+  mkdir /home/data/brontes-ci/$2
+  cp ../.env .env
+}
+
+# deletes repo and test db
+teardown() {
+  rm -rf /home/data/brontes-ci/$2
+  cd ..
+  rm -rf $2
+}
+
+setup()
+
 git pull
 git checkout $1
 git pull
 rustup default nightly
 # we put these in different folders so that if you're on a branch and change these, they will run the branch version
-if ./it.sh $1; then : ; else exit; fi
-if ./test.sh $1; then : ; else exit; fi
-if ./bench.sh $1; then : ; else exit; fi
-git checkout main
+if [ $3 -eq "it" ]; then 
+  if ./it.sh $1; then : ; else exit; fi
+fi 
+
+if [ $3 -eq "test" ]; then 
+  if ./test.sh $1; then : ; else exit; fi
+fi
+
+if [ $3 -eq "bench" ]; then 
+  if ./bench.sh $1; then : ; else exit; fi
+fi 
+
+teardown()
+
