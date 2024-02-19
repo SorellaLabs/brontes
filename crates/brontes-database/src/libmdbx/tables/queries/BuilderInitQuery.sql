8282cbe73d89fd70ef708cc31cbb2655ec442ee6
lagrange.eth <lagrange@sorellacapital.com>
2024-02-19 15:29:00 -0500

    builder info fix

--- crates/brontes-database/src/libmdbx/tables/queries/BuilderInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/BuilderInitQuery.sql
@@ -1,11 +1,13 @@
 SELECT
     toString(address) AS address,
-    CAST(Null, 'Nullable(String)') AS name,
-    CAST(Null, 'Nullable(String)') AS fund,
+    CAST(NULL, 'Nullable(String)') AS name,
+    CAST(NULL, 'Nullable(String)') AS fund,
     CAST([], 'Array(String)') AS pub_keys,
     CAST([], 'Array(String)') AS searchers_eoas,
     CAST([], 'Array(String)') AS searchers_contracts,
-    CAST(Null, 'Nullable(String)') AS ultrasound_relay_collateral_address
-FROM ethereum.builders
-WHERE pub_key != '' AND pub_key IS NOT NULL AND valid = 1
-GROUP BY address, name
+    CAST(NULL, 'Nullable(String)') AS ultrasound_relay_collateral_address
+FROM eth_analytics.builder_meta
+WHERE (pub_key != '') AND (pub_key IS NOT NULL)
+GROUP BY
+    address,
+    name
