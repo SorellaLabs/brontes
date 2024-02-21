161bdcd766659208a53c4adb8cb0a1b089228013
Will Smith <williamqsmith11@gmail.com>
2024-02-21 17:54:35 -0500

    no price storing

--- crates/brontes-classifier/src/test_utils/tests.rs
+++ crates/brontes-classifier/src/test_utils/tests.rs
@@ -382,12 +382,12 @@ impl ClassifierTestUtils {
             // triggers close
             drop(classifier);
 
-            if let Some((p_block, pricing)) = pricer.next().await {
+            if let Some((_p_block, pricing)) = pricer.next().await {
                 // because we have pricing for full block. we store it
-                self.libmdbx
-                    .write_dex_quotes(p_block, Some(pricing.clone()))
-                    .await
-                    .unwrap();
+                // self.libmdbx
+                //     .write_dex_quotes(p_block, Some(pricing.clone()))
+                //     .await
+                //     .unwrap();
                 Some(pricing)
             } else {
                 return Err(ClassifierTestUtilsError::DexPricingError);
