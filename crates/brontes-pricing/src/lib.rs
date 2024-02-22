61d0c4ed81fea57496e998b789e951a6091b9cae
Will Smith <williamqsmith11@gmail.com>
2024-02-22 17:53:38 -0500

    wip

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -1110,9 +1110,9 @@ pub mod test {
 
                     let diff = (pre_max - pre_min) / pre_max * Rational::from(100);
 
-                    if diff > Rational::from_signeds(1, 10) {
+                    if diff > Rational::from_signeds(5, 10) {
                         panic!(
-                            "{:?} pre state had a max diff that was more than 0.1% got: {}",
+                            "{:?} pre state had a max diff that was more than 0.5% got: {}",
                             pair,
                             diff.to_float()
                         );
@@ -1123,9 +1123,9 @@ pub mod test {
 
                     let diff = (post_max - post_min) / post_max * Rational::from(100);
 
-                    if diff > Rational::from_signeds(1, 10) {
+                    if diff > Rational::from_signeds(5, 10) {
                         panic!(
-                            "{:?} post state had a max diff that was more than 0.1% got: {}",
+                            "{:?} post state had a max diff that was more than 0.5% got: {}",
                             pair,
                             diff.to_float()
                         );
