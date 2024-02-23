55b38209290b14a26b856e5599a15910a5978db8
Will Smith <williamqsmith11@gmail.com>
2024-02-23 16:02:21 -0500

    wip

--- crates/brontes-pricing/benches/state.rs
+++ crates/brontes-pricing/benches/state.rs
@@ -10,7 +10,7 @@ pub fn bench_v3_price_requests(c: &mut Criterion) {
     let bencher = BrontesPricingBencher::new(USDC_ADDRESS);
     bencher
         .bench_pool_state_price(
-            "uni v3 weth usdc",
+            "uni v3 weth usdc price calc",
             hex!("9a772018fbd77fcd2d25657e5c547baff3fd7d16").into(),
             18500018,
             Pair(WBTC_ADDRESS, USDC_ADDRESS),
@@ -24,7 +24,7 @@ pub fn bench_v3_state_loads(c: &mut Criterion) {
     let bencher = BrontesPricingBencher::new(USDC_ADDRESS);
     bencher
         .bench_pool_state_loads(
-            "uni v3 weth usdc",
+            "uni v3 weth usdc state load",
             hex!("9a772018fbd77fcd2d25657e5c547baff3fd7d16").into(),
             18500018,
             Pair(WBTC_ADDRESS, USDC_ADDRESS),
@@ -38,7 +38,7 @@ pub fn bench_v2_price_requests(c: &mut Criterion) {
     let bencher = BrontesPricingBencher::new(USDC_ADDRESS);
     bencher
         .bench_pool_state_price(
-            "uni v2 wbtc eth",
+            "uni v2 wbtc eth price calc",
             hex!("bb2b8038a1640196fbe3e38816f3e67cba72d940").into(),
             18500018,
             Pair(WBTC_ADDRESS, ETH_ADDRESS),
@@ -52,7 +52,7 @@ pub fn bench_v2_state_loads(c: &mut Criterion) {
     let bencher = BrontesPricingBencher::new(USDC_ADDRESS);
     bencher
         .bench_pool_state_loads(
-            "uni v2 wbtc eth",
+            "uni v2 wbtc eth state load",
             hex!("bb2b8038a1640196fbe3e38816f3e67cba72d940").into(),
             18500018,
             Pair(WBTC_ADDRESS, ETH_ADDRESS),
