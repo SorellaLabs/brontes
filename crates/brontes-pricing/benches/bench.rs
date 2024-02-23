ed746b2140b2e0d4110322309a99e2b419852260
Will Smith <williamqsmith11@gmail.com>
2024-02-23 16:00:13 -0500

    wip

--- crates/brontes-pricing/benches/bench.rs
+++ crates/brontes-pricing/benches/bench.rs
@@ -2,8 +2,8 @@ mod it;
 mod state;
 use criterion::{criterion_group, criterion_main};
 
-criterion_group!(it_runs, it::bench_block_pricing);
+// criterion_group!(it_runs, it::bench_block_pricing);
 criterion_group!(v3, state::bench_v3_price_requests, state::bench_v3_state_loads);
 criterion_group!(v2, state::bench_v2_price_requests, state::bench_v2_state_loads);
 
-criterion_main!(it_runs, v2, v3);
+criterion_main!( v2, v3);
