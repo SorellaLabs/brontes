c4d0654b5bb7c53edb7ba5b06d526c1a7ed5a183
lagrange.eth <lagrange@sorellacapital.com>
2024-02-25 08:47:16 -0500

    testing

--- crates/brontes-database/src/libmdbx/tables/queries/DexPriceInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/DexPriceInitQuery.sql
@@ -3,4 +3,3 @@ SELECT
     tx_idx,
     quote
 FROM brontes_api.dex_price_mapping
-WHERE block_number >= ? AND block_number < ?
--- crates/brontes-database/src/libmdbx/tables/queries/PoolCreationBlocksInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/PoolCreationBlocksInitQuery.sql
@@ -2,4 +2,3 @@ SELECT
     block_number,
     pools
 FROM brontes_api.pool_creation_block
-WHERE block_number >= ? AND block_number < ?
