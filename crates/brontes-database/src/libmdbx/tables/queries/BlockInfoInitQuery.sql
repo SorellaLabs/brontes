77e887d550af8dd98b7a90fa1c7fb0ff6868482c
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2024-02-25 13:25:10 -0500

    rename table (#472)
    Former-commit-id: fd9e62ff16dd5cd7b3308ffb4fb14ad34ff63ac4

--- crates/brontes-database/src/libmdbx/tables/queries/BlockInfoInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/BlockInfoInitQuery.sql
@@ -7,5 +7,5 @@ SELECT
     CAST(proposer_fee_recipient, ' Nullable(String)') AS proposer_fee_recipient,
     CAST(proposer_mev_reward, ' Nullable(UInt128)') AS proposer_mev_reward,
     CAST(private_flow, 'Array(String)') AS private_flow
-FROM brontes.metadata_util
-WHERE metadata_util.block_hash IS NOT NULL AND block_number >= ? AND block_number < ? 
+FROM brontes.block_info
+WHERE block_info.block_hash IS NOT NULL AND block_number >= ? AND block_number < ? 
