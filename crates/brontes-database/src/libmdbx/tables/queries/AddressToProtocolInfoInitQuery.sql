fa0c23fa67ea5e9d0c6572d34eec2c0802965a38
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-02-25 17:43:15 -0500

    ported init queries to api

--- crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -1,6 +1,4 @@
 SELECT DISTINCT 
-    toString(d.address) AS address, 
-    (arrayMap(x -> toString(x), p.tokens), p.init_block, CAST(d.classifier_name, 'String'), CAST(p.curve_lp_token, 'Nullable(String)')) AS tokens
-FROM ethereum.pools p
-INNER JOIN brontes.protocol_details d ON p.address = d.address
-WHERE classifier_name IS NOT NULL
+    address, 
+    tokens
+FROM brontes_api.address_to_protocol
