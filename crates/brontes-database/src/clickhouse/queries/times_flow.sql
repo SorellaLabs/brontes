1756d9d948d52f353b1b6fd44c0e365cf15a38db
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2024-02-25 20:02:26 -0500

    feat: wire up clickhouse api (#465)
    * feat: wire up clickhouse api
    
    * updated reth
    
    * updated reth
    
    * block tx-trace
    
    * here
    
    * testing initing from brontes_api
    
    * changed db tables
    
    * testing
    
    * testing
    
    * here
    
    * here
    
    * testing db stuff
    
    * testing db stuff
    
    * testing db stuff
    
    * testing db stuff
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * main
    
    * main
    
    * wip
    
    * wip
    
    * wip
    
    * move wip
    
    * wip
    
    * wip
    
    * fix init of client
    
    * wip
    
    * try
    
    * log
    
    * wip
    
    * log
    
    * wip
    
    * wip
    
    * meta test
    
    * wip
    
    * auto load env
    
    * auto tracing init
    
    * wip
    
    * more logo
    
    * set http endpoints
    
    * wip
    
    * wip
    
    * dot env
    
    * wip
    
    * try
    
    * wip
    
    * wip
    
    * no printout
    
    * toml
    
    * tyr
    
    * fmt
    
    * try
    
    * wip
    
    * try
    
    * wip
    
    * fix
    
    * no move
    
    * wip
    
    * wip
    
    * try
    
    * wi
    
    * try
    
    * try
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * fmt
    
    * wip
    
    ---------
    
    Co-authored-by: lagrange.eth <lagrange@sorellacapital.com>
    Former-commit-id: a10ecc6452a2ebdfd732f4c3d49dbd26385ab110

--- crates/brontes-database/src/clickhouse/queries/times_flow.sql
+++ crates/brontes-database/src/clickhouse/queries/times_flow.sql
@@ -1,58 +1,28 @@
-WITH
-    ? AS block_number,
-    relay_block AS (
-        SELECT
-            block_number AS block_num,
-            toString(blocks.block_hash) AS block_hash,
-            relays.timestamp AS relay_time,
-            toString(relays.proposer_fee_recipient) AS proposer_addr,
-            relays.value AS proposer_reward
-        FROM ethereum.relays
-        INNER JOIN ethereum.blocks AS blocks ON blocks.block_number = block_number AND blocks.block_hash = ethereum.relays.block_hash AND blocks.valid = 1
-        WHERE (relays.block_number = block_number) 
-    ),
-    relay_p2p AS (
-        SELECT
-            any(relay_block.block_hash) AS block_hash,
-            any(relay_block.relay_time) AS relay_time,
-            toUInt64(round(max(cb.timestamp) / 1000)) AS p2p_time,
-            any(relay_block.proposer_addr) AS proposer_addr,
-            any(relay_block.proposer_reward) AS proposer_reward
-        FROM relay_block
-        INNER JOIN ethereum.block_observations AS cb ON cb.block_number = relay_block.block_num 
-        WHERE cb.block_hash = relay_block.block_hash
-    ),
-    private_flow AS
-    (
-        SELECT groupArray(toString(a.tx_hash)) AS flow
-        FROM
-        (
-            SELECT DISTINCT arrayJoin(transaction_hashes) AS tx_hash
-            FROM ethereum.blocks
-            WHERE (blocks.block_number = block_number) AND (valid = 1)
-        ) AS a
-        LEFT JOIN
-        (
-            SELECT a.tx_hash AS tx_hash
-            FROM ethereum.unique_mempool AS um
-            INNER JOIN
-            (
-                SELECT DISTINCT arrayJoin(transaction_hashes) AS tx_hash
-                FROM ethereum.blocks
-                WHERE (blocks.block_number = block_number) AND (valid = 1)
-            ) AS a ON a.tx_hash = um.tx_hash
-        ) AS pub ON a.tx_hash = pub.tx_hash
-        WHERE (pub.tx_hash = '') OR (pub.tx_hash IS NULL)
-    )
-SELECT 
-    block_number,
-    toString(relay_p2p.block_hash) AS block_hash,
-    relay_p2p.relay_time AS relay_time,
-    relay_p2p.p2p_time AS p2p_time,
-    toString(relay_p2p.proposer_addr) AS proposer_addr,
-    relay_p2p.proposer_reward AS proposer_reward,
-    private_flow.flow
-FROM relay_p2p, private_flow
-
-
-
+WITH most_recent AS (
+    SELECT
+        distinct(address)
+    FROM ethereum.addresses
+    WHERE 
+        entity_name IS NOT NULL OR
+        nametag IS NOT NULL
+)
+SELECT
+    toString(a.address) AS address,
+    a.entity_name,
+    a.nametag,
+    a.labels,
+    CASE 
+        WHEN a.arkham_type IS NOT NULL AND a.etherscan_type IS NOT NULL THEN a.arkham_type
+        WHEN a.arkham_type IS NULL AND a.etherscan_type IS NOT NULL THEN a.etherscan_type
+        WHEN a.arkham_type IS NOT NULL AND a.etherscan_type IS NULL THEN a.arkham_type
+        ELSE NULL
+    END AS type,
+    (a.verified_contract, CAST(c.creator_addr, 'Nullable(String)'), a.reputation) AS contract_info,
+    a.ens,
+    (a.twitter, a.twitter_followers, a.website_url, a.crunchbase, a.linkedin) AS socials
+FROM ethereum.addresses AS a
+INNER JOIN most_recent AS mr ON a.address = mr.address
+LEFT JOIN ethereum.contracts AS c ON a.address = c.address
+WHERE 
+    a.entity_name IS NOT NULL OR
+    a.nametag IS NOT NULL
