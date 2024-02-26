442d76269f22e2379b1b7b6c78debdb10ae0e201
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2024-02-25 20:02:26 -0500

    feat: wire up clickhouse api (#465)
    * feat: wire up clickhouse api
    
    * updated reth
    
    * updated reth
    
    * block tx-trace
    
    * here
    
    * testing initing from brontes_api
    
    * changed db tables
    
    * testing
    
    * testing
    
    * here
    
    * here
    
    * testing db stuff
    
    * testing db stuff
    
    * testing db stuff
    
    * testing db stuff
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * main
    
    * main
    
    * wip
    
    * wip
    
    * wip
    
    * move wip
    
    * wip
    
    * wip
    
    * fix init of client
    
    * wip
    
    * try
    
    * log
    
    * wip
    
    * log
    
    * wip
    
    * wip
    
    * meta test
    
    * wip
    
    * auto load env
    
    * auto tracing init
    
    * wip
    
    * more logo
    
    * set http endpoints
    
    * wip
    
    * wip
    
    * dot env
    
    * wip
    
    * try
    
    * wip
    
    * wip
    
    * no printout
    
    * toml
    
    * tyr
    
    * fmt
    
    * try
    
    * wip
    
    * try
    
    * wip
    
    * fix
    
    * no move
    
    * wip
    
    * wip
    
    * try
    
    * wi
    
    * try
    
    * try
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * fmt
    
    * wip
    
    ---------
    
    Co-authored-by: lagrange.eth <lagrange@sorellacapital.com>
    Former-commit-id: 137c94d876ae8852fb8d18dda0d2c89529c5037d

--- crates/brontes-macros/src/libmdbx_test.rs
+++ crates/brontes-macros/src/libmdbx_test.rs
@@ -41,6 +41,8 @@ pub fn parse(item: ItemFn, attr: TokenStream) -> syn::Result<TokenStream> {
         #vis
         #sig
         {
+            dotenv::dotenv().expect("failed to load env");
+            ::brontes_core::test_utils::init_tracing();
             std::thread::spawn(move || {
             tokio::runtime::Builder::new_multi_thread()
                 .enable_all()
