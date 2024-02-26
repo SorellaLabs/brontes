f5c947b1aae8b8321a6b74de16609e76fab83943
Will Smith <williamqsmith11@gmail.com>
2024-02-26 13:24:01 -0500

    feat: action cmp tests

--- crates/brontes-types/src/normalized_actions/utils.rs
+++ crates/brontes-types/src/normalized_actions/utils.rs
@@ -8,8 +8,12 @@ pub trait ActionCmp<O> {
 
 impl ActionCmp<NormalizedTransfer> for NormalizedSwap {
     fn is_superior_action(&self, transfer: &NormalizedTransfer) -> bool {
-        (transfer.amount == self.amount_in || (&transfer.amount + &transfer.fee) == self.amount_out)
-            && (transfer.to == self.pool || transfer.from == self.pool)
+        (&transfer.amount + &transfer.fee == self.amount_in
+            && transfer.to == self.pool
+            && self.from == transfer.from)
+            || (transfer.amount == self.amount_out
+                && transfer.from == self.pool
+                && self.recipient == transfer.to)
     }
 }
 
@@ -34,5 +38,40 @@ impl ActionCmp<NormalizedTransfer> for NormalizedCollect {
         }
 
         false
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use alloy_primitives::hex;
+    use brontes_classifier::test_utils::ClassifierTestUtils;
+
+    use crate::{
+        normalized_actions::{transfer, Actions},
+        ActionIter, BlockTree, TreeCollect, TreeFilter, TreeSearchBuilder,
+    };
+
+    #[brontes_macros::test]
+    async fn test_swap_transfer_dedup() {
+        let utils = ClassifierTestUtils::new().await;
+        let tx = hex!("c6b9e1c5e5478defaae7b2cdb8aeaf22cc16bec599cb5fdad470429919dd8f70").into();
+        let tree: BlockTree<Actions> = utils.build_tree_tx(tx).await.unwrap();
+        let call = TreeSearchBuilder::default().with_action(Actions::is_transfer);
+
+        let transfers = tree.collect_action_filter(tx, call.clone(), Actions::try_transfer);
+
+        assert_eq!(transfers.len(), 3, "{:#?}", transfers);
+
+        let deduped_transfers = tree
+            .collect_tx_deduping(
+                &tx,
+                call.clone(),
+                (Actions::try_swap_dedup(),),
+                (Actions::try_transfer_dedup(),),
+            )
+            .into_iter()
+            .collect_action_vec(Actions::try_transfer);
+
+        assert_eq!(deduped_transfers.len(), 1, "{:#?}", deduped_transfers);
     }
 }
