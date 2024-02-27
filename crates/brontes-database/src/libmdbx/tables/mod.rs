e13cd354ec3b66a979e15b5dd2e3fe4e6351e5a5
lagrange.eth <lagrange@sorellacapital.com>
2024-02-27 03:51:58 -0600

    changed chunk sizing

--- crates/brontes-database/src/libmdbx/tables/mod.rs
+++ crates/brontes-database/src/libmdbx/tables/mod.rs
@@ -456,7 +456,7 @@ compressed_table!(
         compressed_value: CexPriceMapRedefined
         },
         Init {
-            init_size: Some(100_000),
+            init_size: Some(50_000),
             init_method: Clickhouse,
             http_endpoint: Some("cex-price")
         },
@@ -570,7 +570,7 @@ compressed_table!(
             compressed_value: TxTracesInnerRedefined
         },
         Init {
-            init_size: Some(100_000),
+            init_size: Some(50_000),
             init_method: Clickhouse,
             http_endpoint: Some("tx-traces")
         },
