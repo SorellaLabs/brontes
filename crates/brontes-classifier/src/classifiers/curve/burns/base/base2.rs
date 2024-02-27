62bb54fcb6c664a5bd7cebb73520ca556ca2c941
lagrange.eth <lagrange@sorellacapital.com>
2024-02-27 06:47:49 -0600

    better init

--- crates/brontes-classifier/src/classifiers/curve/burns/base/base2.rs
+++ crates/brontes-classifier/src/classifiers/curve/burns/base/base2.rs
@@ -86,7 +86,7 @@ action_impl!(
     Protocol::CurveBasePool2,
     crate::CurveBase2::remove_liquidity_one_coinCall,
     Burn,
-    [RemoveLiquidityOne],
+    [..RemoveLiquidityOne],
     logs: true,
     call_data: true,
     |
