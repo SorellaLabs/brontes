21272d02aff81d31949a05b7a4f3e8e09d53b2e4
lagrange.eth <lagrange@sorellacapital.com>
2024-02-28 06:28:58 -0700

    more inits

--- crates/brontes-database/src/libmdbx/initialize.rs
+++ crates/brontes-database/src/libmdbx/initialize.rs
@@ -427,14 +427,14 @@ mod tests {
             .unwrap();
 
         // TokenDecimals
-        // TokenDecimals::test_initialized_data(clickhouse, libmdbx, None)
-        //     .await
-        //     .unwrap();
+        TokenDecimals::test_initialized_data(clickhouse, libmdbx, None)
+            .await
+            .unwrap();
 
-        // // AddressToProtocol
-        // AddressToProtocolInfo::test_initialized_data(clickhouse, libmdbx, None)
-        //     .await
-        //     .unwrap();
+        // AddressToProtocol
+        AddressToProtocolInfo::test_initialized_data(clickhouse, libmdbx, None)
+            .await
+            .unwrap();
 
         // // CexPrice
         // CexPrice::test_initialized_data(clickhouse, libmdbx, Some(block_range))
@@ -444,23 +444,23 @@ mod tests {
         //     .await
         //     .unwrap();
 
-        // // Metadata
-        // BlockInfo::test_initialized_data(clickhouse, libmdbx, Some(block_range))
-        //     .await
-        //     .unwrap();
-        // BlockInfo::test_initialized_arbitrary_data(clickhouse, libmdbx,
-        // arbitrary_set)     .await
-        //     .unwrap();
+        // Metadata
+        BlockInfo::test_initialized_data(clickhouse, libmdbx, Some(block_range))
+            .await
+            .unwrap();
+        BlockInfo::test_initialized_arbitrary_data(clickhouse, libmdbx, arbitrary_set)
+            .await
+            .unwrap();
 
-        // // PoolCreationBlocks
-        // PoolCreationBlocks::test_initialized_data(clickhouse, libmdbx, None)
-        //     .await
-        //     .unwrap();
+        // PoolCreationBlocks
+        PoolCreationBlocks::test_initialized_data(clickhouse, libmdbx, None)
+            .await
+            .unwrap();
 
-        // // Builder
-        // Builder::test_initialized_data(clickhouse, libmdbx, None)
-        //     .await
-        //     .unwrap();
+        // Builder
+        Builder::test_initialized_data(clickhouse, libmdbx, None)
+            .await
+            .unwrap();
 
         // AddressMeta
         AddressMeta::test_initialized_data(clickhouse, libmdbx, None)
