42182821fec68618deaa0e8cfa651be693356bd5
lagrange.eth <lagrange@sorellacapital.com>
2024-02-28 06:31:44 -0700

    more inits

--- crates/brontes-database/src/libmdbx/initialize.rs
+++ crates/brontes-database/src/libmdbx/initialize.rs
@@ -436,13 +436,13 @@ mod tests {
             .await
             .unwrap();
 
-        // // CexPrice
-        // CexPrice::test_initialized_data(clickhouse, libmdbx, Some(block_range))
-        //     .await
-        //     .unwrap();
-        // CexPrice::test_initialized_arbitrary_data(clickhouse, libmdbx, arbitrary_set)
-        //     .await
-        //     .unwrap();
+        // CexPrice
+        CexPrice::test_initialized_data(clickhouse, libmdbx, Some(block_range))
+            .await
+            .unwrap();
+        CexPrice::test_initialized_arbitrary_data(clickhouse, libmdbx, arbitrary_set)
+            .await
+            .unwrap();
 
         // Metadata
         BlockInfo::test_initialized_data(clickhouse, libmdbx, Some(block_range))
