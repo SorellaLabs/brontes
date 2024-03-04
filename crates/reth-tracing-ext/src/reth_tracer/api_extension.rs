3e58c28088044aee4eddc0edd85974b771d7f2f8
1010adigupta <1010adigupta@gmail.com>
2024-03-04 15:54:45 +0530

    api extension setup

--- crates/reth-tracing-ext/src/reth_tracer/api_extension.rs
+++ crates/reth-tracing-ext/src/reth_tracer/api_extension.rs
@@ -1,140 +1,84 @@
-
-
-
-/// Our custom cli args extension that adds one flag to reth default CLI.
+use revm_inspectors::tracing::TracingInspectorConfig;
+use reth_rpc_types::TransactionInfo;
+use reth_primitives::BlockId;
+use reth_provider::StateProviderBox;
+use reth_revm::database::StateProviderDatabase;
+use inspector::BrontesTracingInspector;
+use jsonrpsee::proc_macros::rpc;
+use reth_rpc::eth::error::EthResult;
+use revm::{
+    db::CacheDB,
+    primitives::{
+        ExecutionResult, State
+    }
+};
+use super::inspector;
+/// Custom cli args extension that adds one flag to reth default CLI, this will enable brontes cli extensions and allow installation of brontes inspectors
 #[derive(Debug, Clone, Copy, Default, clap::Args)]
-struct RethCliTxpoolExt {
-    /// CLI flag to enable the txpool extension namespace
+struct RethCliBrontesExt {
+    /// CLI flag to enable the brontesExt extension namespace
     #[arg(long)]
     pub enable_ext: bool,
 }
 
-/// trait interface for a custom rpc namespace: `txpool`
+/// trait interface for a custom rpc namespace: `brontesExt`
 ///
 /// This defines an additional namespace where all methods are configured as trait functions.
-#[cfg_attr(not(test), rpc(server, namespace = "txpoolExt"))]
-#[cfg_attr(test, rpc(server, client, namespace = "txpoolExt"))]
-pub trait TxpoolExtApi {
-    /// Returns the number of transactions in the pool.
-    #[method(name = "transactionCount")]
-    async fn get_txn_trace() -> RpcResult<usize>;
+#[cfg_attr(not(test), rpc(server, namespace = "brontesExt"))]
+#[cfg_attr(test, rpc(server, client, namespace = "brontesExt"))]
+pub trait BrontesExtApi {
+    /// gets all transaction traces
+    #[method(name = "getTransactionTraces")]
+    async fn get_txn_traces<F, R>(
+        &self,
+        block_id: BlockId,
+        f: F,
+    ) -> EthResult<Option<Vec<R>>>
+    where
+        // This is the callback that's invoked for each transaction with
+        F: for<'a> Fn(
+                TransactionInfo,
+                BrontesTracingInspector,
+                ExecutionResult,
+                &'a State,
+                &'a CacheDB<StateProviderDatabase<StateProviderBox>>,
+            ) -> EthResult<R>
+            + Send
+            + 'static,
+        R: Send + 'static;
+    
 }
 
-/// The type that implements the `txpool` rpc namespace trait
-pub struct TxpoolExt<Pool> {
-    pool: Pool,
-}
+impl <F, R>BrontesExtApiServer for BrontesTracingInspector
 
-impl<Pool> TxpoolExtApiServer for TxpoolExt<Pool>
-where
-    Pool: TransactionPool + Clone + 'static,
 {
-    fn transaction_count(&self) -> RpcResult<usize> {
-        Ok(self.pool.pool_size().total)
-    }
-}
-
-async fn trace_block_with<F, R>(
-    &self,
-    block_id: BlockId,
-    config: TracingInspectorConfig,
-    f: F,
-) -> EthResult<Option<Vec<R>>>
-where
-    // This is the callback that's invoked for each transaction with
-    F: for<'a> Fn(
-            TransactionInfo,
-            TracingInspector,
-            ExecutionResult,
-            &'a State,
-            &'a CacheDB<StateProviderDatabase<StateProviderBox>>,
-        ) -> EthResult<R>
-        + Send
-        + 'static,
-    R: Send + 'static,
-{
-    self.trace_block_until(block_id, None, config, f).await
-}
-
-async fn trace_block_until<F, R>(
-    &self,
-    block_id: BlockId,
-    highest_index: Option<u64>,
-    config: TracingInspectorConfig,
-    f: F,
-) -> EthResult<Option<Vec<R>>>
-where
-    F: for<'a> Fn(
-            TransactionInfo,
-            TracingInspector,
-            ExecutionResult,
-            &'a State,
-            &'a CacheDB<StateProviderDatabase<StateProviderBox>>,
-        ) -> EthResult<R>
-        + Send
-        + 'static,
-    R: Send + 'static,
-{
-    let ((cfg, block_env, _), block) =
-        futures::try_join!(self.evm_env_at(block_id), self.block_with_senders(block_id))?;
-
-    let Some(block) = block else { return Ok(None) };
-
-    // replay all transactions of the block
-    self.spawn_tracing_task_with(move |this| {
-        // we need to get the state of the parent block because we're replaying this block on
-        // top of its parent block's state
-        let state_at = block.parent_hash;
-        let block_hash = block.hash();
-
-        let block_number = block_env.number.saturating_to::<u64>();
-        let base_fee = block_env.basefee.saturating_to::<u64>();
-
-        // prepare transactions, we do everything upfront to reduce time spent with open state
-        let max_transactions =
-            highest_index.map_or(block.body.len(), |highest| highest as usize);
-        let mut results = Vec::with_capacity(max_transactions);
-
-        let mut transactions = block
-            .into_transactions_ecrecovered()
-            .take(max_transactions)
-            .enumerate()
-            .map(|(idx, tx)| {
-                let tx_info = TransactionInfo {
-                    hash: Some(tx.hash()),
-                    index: Some(idx as u64),
-                    block_hash: Some(block_hash),
-                    block_number: Some(block_number),
-                    base_fee: Some(base_fee),
-                };
-                let tx_env = tx_env_with_recovered(&tx);
-                (tx_info, tx_env)
-            })
-            .peekable();
-
-        // now get the state
-        let state = this.state_at(state_at.into())?;
-        let mut db = CacheDB::new(StateProviderDatabase::new(state));
-
-        while let Some((tx_info, tx)) = transactions.next() {
-            let env = EnvWithHandlerCfg::new_with_cfg_env(cfg.clone(), block_env.clone(), tx);
-
-            let mut inspector = TracingInspector::new(config);
-            let (res, _) = inspect(&mut db, env, &mut inspector)?;
-            let ResultAndState { result, state } = res;
-            results.push(f(tx_info, inspector, result, &state, &db)?);
-
-            // need to apply the state changes of this transaction before executing the
-            // next transaction
-            if transactions.peek().is_some() {
-                // need to apply the state changes of this transaction before executing
-                // the next transaction
-                db.commit(state)
-            }
+    async fn get_txn_traces(
+        &self,
+        block_id: BlockId,
+        f: F,
+    ) -> EthResult<Option<Vec<R>>>
+    where
+        // This is the callback that's invoked for each transaction with
+        F: for<'a> Fn(
+                TransactionInfo,
+                BrontesTracingInspector,
+                ExecutionResult,
+                &'a State,
+                &'a CacheDB<StateProviderDatabase<StateProviderBox>>,
+            ) -> EthResult<R>
+            + Send
+            + 'static,
+        R: Send + 'static, {
+            let config = TracingInspectorConfig {
+                record_logs:              true,
+                record_steps:             false,
+                record_state_diff:        false,
+                record_stack_snapshots:   reth_revm::tracing::StackSnapshotType::None,
+                record_memory_snapshots:  false,
+                record_call_return_data:  true,
+                exclude_precompile_calls: true,
+            };
+               unimplemented!()
         }
-
-        Ok(results)
-    })
-    .await
-    .map(Some)
-}
+        
+    }
