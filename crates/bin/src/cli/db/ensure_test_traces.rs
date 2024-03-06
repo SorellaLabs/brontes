1f4ef70ad16a3cf0685e1764b7e0133eebdf6f57
Will Smith <williamqsmith11@gmail.com>
2024-03-06 17:34:28 -0500

    wip

---
+++ crates/bin/src/cli/db/ensure_test_traces.rs
@@ -0,0 +1,60 @@
+use std::{env, path::Path};
+
+use brontes_core::decoding::Parser as DParser;
+use brontes_metrics::PoirotMetricsListener;
+use brontes_types::unordered_buffer_map::BrontesStreamExt;
+use clap::Parser;
+use futures::StreamExt;
+use tokio::sync::mpsc::unbounded_channel;
+
+use crate::{
+    cli::{determine_max_tasks, get_env_vars, get_tracing_provider, load_database, static_object},
+    runner::CliContext,
+};
+
+#[derive(Debug, Parser)]
+pub struct TestTraceArgs {
+    #[arg(long, short, value_delimiter = ',')]
+    pub blocks: Vec<u64>,
+}
+
+impl TestTraceArgs {
+    pub async fn execute(self, ctx: CliContext) -> eyre::Result<()> {
+        let db_path = get_env_vars()?;
+
+        let max_tasks = determine_max_tasks(None) * 2;
+        let (metrics_tx, metrics_rx) = unbounded_channel();
+
+        let metrics_listener = PoirotMetricsListener::new(metrics_rx);
+        ctx.task_executor
+            .spawn_critical("metrics", metrics_listener);
+
+        let brontes_db_endpoint = env::var("BRONTES_DB_PATH").expect(
+            "No
+        BRONTES_DB_PATH in .env",
+        );
+
+        let libmdbx = static_object(load_database(brontes_db_endpoint)?);
+
+        let tracer =
+            get_tracing_provider(Path::new(&db_path), max_tasks, ctx.task_executor.clone());
+
+        let parser = static_object(DParser::new(metrics_tx, libmdbx, tracer.clone()).await);
+
+        futures::stream::iter(blocks.into_iter())
+            .unordered_buffer_map(100, |i| {
+                if i % 5000 == 0 {
+                    tracing::info!(
+                        "tracing {:.2}% done",
+                        (i - self.start_block) as f64 / amount * 100.0
+                    );
+                }
+                parser.execute(i)
+            })
+            .map(|_res| ())
+            .collect::<Vec<_>>()
+            .await;
+
+        Ok(())
+    }
+}
