9d261218eb2ba17a7248775d5df3fa5ce8b50934
Will Smith <williamqsmith11@gmail.com>
2024-03-09 16:28:28 -0500

    fix queries

--- crates/brontes-database/src/clickhouse/queries/block_info.sql
+++ crates/brontes-database/src/clickhouse/queries/block_info.sql
@@ -1,11 +1,11 @@
 SELECT
     block_number,
-    CAST(block_hash, 'String') AS block_hash,
-    CAST(block_timestamp, 'UInt64') AS block_timestamp,
-    CAST(relay_timestamp, ' Nullable(UInt64)') AS relay_timestamp,
-    CAST(p2p_timestamp, ' Nullable(UInt64)') AS p2p_timestamp,
-    CAST(proposer_fee_recipient, ' Nullable(String)') AS proposer_fee_recipient,
-    CAST(proposer_mev_reward, ' Nullable(UInt128)') AS proposer_mev_reward,
-    CAST(private_flow, 'Array(String)') AS private_flow
-FROM brontes.metadata_util
-WHERE block_hash IS NOT NULL AND block_number = ? 
+    block_hash,
+    block_timestamp,
+    relay_timestamp,
+    p2p_timestamp,
+    proposer_fee_recipient,
+    proposer_mev_reward,
+    private_flow
+FROM brontes_api.block_info
+WHERE block_info.block_hash IS NOT NULL AND block_number = ? 
