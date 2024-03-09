c57149936f97d9fdab0df72aa6e48d53814270bb
Will Smith <williamqsmith11@gmail.com>
2024-03-09 15:34:24 -0500

    rip here

--- crates/brontes-core/src/test_utils/mod.rs
+++ crates/brontes-core/src/test_utils/mod.rs
@@ -48,7 +48,6 @@ pub struct TraceLoader {
 impl TraceLoader {
     pub async fn new() -> Self {
         let handle = tokio::runtime::Handle::current();
-        init_threadpools(10);
         let libmdbx = get_db_handle(handle.clone()).await;
 
         let (a, b) = unbounded_channel();
@@ -334,6 +333,7 @@ pub async fn get_db_handle(handle: Handle) -> &'static LibmdbxReadWriter {
     *DB_HANDLE
         .get_or_init(|| async {
             let _ = dotenv::dotenv();
+            init_threadpools(10);
             init_tracing();
             let brontes_db_endpoint =
                 env::var("BRONTES_TEST_DB_PATH").expect("No BRONTES_TEST_DB_PATH in .env");
