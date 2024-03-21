682928aaa656e3181f1eeb2dd7c053c7357593dd
Will Smith <williamqsmith11@gmail.com>
2024-03-20 20:55:26 -0400

    wip

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -282,7 +282,7 @@ impl<T: TracingProvider, DB: DBWriter + LibmdbxReader> BrontesBatchPricer<T, DB>
         let block = msg.block;
 
         let Some(pool_pair) = msg.get_pair(self.quote_asset) else {
-            debug!(?addr, "failed to get pair for pool");
+            info!(?addr, "failed to get pair for pool");
             return;
         };
 
@@ -324,21 +324,21 @@ impl<T: TracingProvider, DB: DBWriter + LibmdbxReader> BrontesBatchPricer<T, DB>
         let pair1 = Pair(pool_pair.1, self.quote_asset);
 
         let Some(price0_pre) = self.get_dex_price(pair0, pool_pair) else {
-            info!(?pair0, "no price for token");
+            debug!(?pair0, "no price for token");
             return;
         };
         let Some(price1_pre) = self.get_dex_price(pair1, flipped_pool) else {
-            info!(?pair1, "no price for token");
+            debug!(?pair1, "no price for token");
             return;
         };
         self.graph_manager.update_state(addr, msg);
 
         let Some(price0_post) = self.get_dex_price(pair0, pool_pair) else {
-            info!(?pair0, "no price for token");
+            debug!(?pair0, "no price for token");
             return;
         };
         let Some(price1_post) = self.get_dex_price(pair1, flipped_pool) else {
-            info!(?pair1, "no price for token");
+            debug!(?pair1, "no price for token");
             return;
         };
 
