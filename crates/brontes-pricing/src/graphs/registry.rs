120a32d25d85da63a32f84fdb3141c76420a4051
Will Smith <williamqsmith11@gmail.com>
2024-03-21 22:36:14 -0400

    wip

--- crates/brontes-pricing/src/graphs/registry.rs
+++ crates/brontes-pricing/src/graphs/registry.rs
@@ -99,6 +99,7 @@ impl SubGraphRegistry {
     ) -> Option<Rational> {
         let (next, complete_pair, default_price) =
             self.get_price_once(unordered_pair, goes_through, edge_state)?;
+        tracing::info!(?unordered_pair, ?goes_through, ?default_price);
 
         next.and_then(|next| Some(self.get_price_all(next, edge_state)? * &default_price))
             .map(|price| {
