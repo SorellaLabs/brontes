07d510316d603c56cf953a9e071cdddf04c8284b
Will Smith <williamqsmith11@gmail.com>
2024-03-25 16:32:03 -0400

    wip

--- crates/brontes-pricing/benches/bench.rs
+++ crates/brontes-pricing/benches/bench.rs
@@ -8,7 +8,7 @@ criterion_group!(
     targets =
         it::bench_block_pricing,
         it::bench_block_pricing_after_5_blocks,
-        it::bench_block_pricing_after_10_blocks
+        it::bench_block_pricing_after_10_blocks,
         it::bench_block_pricing_after_20_blocks
 );
 criterion_group!(v3, state::bench_v3_price_requests, state::bench_v3_state_loads);
