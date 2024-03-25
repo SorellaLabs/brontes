01f15927e71924d82cbba65233ce31ddb78b684f
Will Smith <williamqsmith11@gmail.com>
2024-03-25 14:06:40 -0400

    add thread override

--- crates/brontes-types/src/test_limiter.rs
+++ crates/brontes-types/src/test_limiter.rs
@@ -2,23 +2,29 @@
 //! This is done so that we can limit the amount of cpu usage of the
 //! tests to ensure we aren't nuking peoples computers / ci servers
 
-use std::sync::Mutex;
+use std::{str::FromStr, sync::Mutex};
 
 use once_cell::sync::OnceCell;
 
-const MAX_TEST_THREADS: usize = 12;
-
+/// Can be set using the env TEST_THREADS=<amount here>;
+static MAX_TEST_THREADS: OnceCell<usize> = OnceCell::new();
 static RUNNING_INFO: OnceCell<Mutex<(usize, usize)>> = OnceCell::new();
 
 /// Continuously tries to fetch the thread count lock
 pub fn wait_for_tests<F: Fn() -> () + std::panic::RefUnwindSafe>(threads: usize, test_fn: F) {
+    let max_threads = MAX_TEST_THREADS.get_or_init(|| {
+        std::env::var("TEST_THREADS")
+            .map(|s| usize::from_str(&s).unwrap_or(12))
+            .unwrap_or(12)
+    });
+
     RUNNING_INFO.get_or_init(|| Mutex::new((0, 0)));
     let ri = RUNNING_INFO.get().unwrap();
 
     // wait until we have available resources to run the test
     loop {
         if let Ok(mut lock) = ri.try_lock() {
-            if lock.0 + threads <= MAX_TEST_THREADS || lock.1 == 0 {
+            if lock.0 + threads <= *max_threads || lock.1 == 0 {
                 lock.0 += threads;
                 lock.1 += 1;
                 break
