968bc1d9d0311b5bdc8fa411107162136e2d850a
Will Smith <williamqsmith11@gmail.com>
2024-03-25 16:29:07 -0400

    add 20 block and half sample size

--- crates/brontes-pricing/benches/it.rs
+++ crates/brontes-pricing/benches/it.rs
@@ -15,7 +15,15 @@ pub fn bench_block_pricing_after_5_blocks(c: &mut Criterion) {
         .bench_pricing_post_init("pricing after 5 blocks, start = 18500018", 18500018, 5, c)
         .unwrap();
 }
+
 pub fn bench_block_pricing_after_10_blocks(c: &mut Criterion) {
+    let bencher = BrontesPricingBencher::new(USDC_ADDRESS);
+    bencher
+        .bench_pricing_post_init("pricing after 10 blocks, start = 18500018", 18500018, 10, c)
+        .unwrap();
+}
+
+pub fn bench_block_pricing_after_20_blocks(c: &mut Criterion) {
     let bencher = BrontesPricingBencher::new(USDC_ADDRESS);
     bencher
         .bench_pricing_post_init("pricing after 10 blocks, start = 18500018", 18500018, 10, c)
