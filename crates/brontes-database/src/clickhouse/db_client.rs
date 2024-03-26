f9f911955c901396deeb28bd1017511a12836cbe
Will Smith <williamqsmith11@gmail.com>
2024-03-26 19:09:17 -0400

    fix

--- crates/brontes-database/src/clickhouse/db_client.rs
+++ crates/brontes-database/src/clickhouse/db_client.rs
@@ -51,9 +51,10 @@ impl Clickhouse {
     }
 
     pub async fn max_traced_block(&self) -> eyre::Result<u64> {
+        Ok(
         self.client
             .query_one::<u64, _>("select max(block_number) from brontes_api.tx_traces", &())
-            .await
+            .await?)
     }
 
     // inserts
