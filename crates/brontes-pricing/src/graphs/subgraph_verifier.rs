4612938d95b7943089e6c547c97a3ce44908a2bd
Will Smith <williamqsmith11@gmail.com>
2024-03-28 19:30:06 -0400

    fix

--- crates/brontes-pricing/src/graphs/subgraph_verifier.rs
+++ crates/brontes-pricing/src/graphs/subgraph_verifier.rs
@@ -356,14 +356,8 @@ impl SubgraphVerifier {
                             ?pair,
                             ?extends_to,
                             extensions = ex.len(),
-                            "connected with \n {:#?} \n {:#?}",
+                            "connected with \n {:#?}",
                             ex.iter()
-                                .filter(|f| ignored.contains(f))
-                                .map(|i| state.highest_liq_for_pair(*i))
-                                .collect_vec(),
-                            ignored
-                                .iter()
-                                .filter(|i| ex.contains(i))
                                 .map(|i| state.highest_liq_for_pair(*i))
                                 .collect_vec(),
                         );
