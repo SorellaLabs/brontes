699d87eef27d7ab24f6a6bbdfbf4ccad9f9238d5
Will Smith <williamqsmith11@gmail.com>
2024-03-29 18:48:46 -0400

    more info

--- crates/brontes-pricing/src/graphs/subgraph.rs
+++ crates/brontes-pricing/src/graphs/subgraph.rs
@@ -589,7 +589,7 @@ impl PairSubGraph {
         let mut visit_next = VecDeque::new();
 
         let Some(start) = self.token_to_index.get(&start) else {
-            error!(?start, ?start_price,?self.pair, ?self.complete_pair, ?self.must_go_through, "no start node for bfs with price");
+            error!(?start, ?start_price,?self.pair, ?self.complete_pair, ?self.must_go_through, ?self.extends_to, "no start node for bfs with price");
             return R::default();
         };
 
