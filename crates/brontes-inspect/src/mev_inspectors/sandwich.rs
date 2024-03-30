459923e7d708672d9288742e750bde79fd4839f2
Will Smith <williamqsmith11@gmail.com>
2024-03-30 15:46:15 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/sandwich.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich.rs
@@ -188,6 +188,8 @@ impl<DB: LibmdbxReader> SandwichInspector<'_, DB> {
         mut victim_actions: Vec<Vec<(Vec<NormalizedSwap>, Vec<NormalizedTransfer>)>>,
     ) -> Option<Bundle> {
         tracing::debug!("sando victim sets {}", victim_actions.len());
+        tracing::info!(?searcher_actions, ?victim_info);
+
         let back_run_actions = searcher_actions.pop()?;
         let back_run_swaps = back_run_actions
             .clone()
