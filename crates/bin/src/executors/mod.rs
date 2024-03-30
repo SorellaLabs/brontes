bca896b4692f71b6044bed115f816881a51e9fd3
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-03-30 16:37:12 -0400

    test steady tick

--- crates/bin/src/executors/mod.rs
+++ crates/bin/src/executors/mod.rs
@@ -344,8 +344,8 @@ impl<T: TracingProvider, DB: LibmdbxInit, CH: ClickhouseHandle, P: Processor>
             let progress_bar = ProgressBar::with_draw_target(
                 Some(total_blocks),
                 ProgressDrawTarget::stderr_with_hz(1),
-            )
-            .enable_steady_tick(Duration::from_millis(1));
+            );
+            progress_bar.enable_steady_tick(Duration::from_millis(1));
             progress_bar.set_style(
                 ProgressStyle::with_template(
                     "{msg}\n[{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} blocks \
