be20503117ba2298f186b99275be88ba5da45932
Will Smith <williamqsmith11@gmail.com>
2024-03-30 16:24:19 -0400

    p

--- crates/brontes-inspect/src/mev_inspectors/sandwich.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich.rs
@@ -370,9 +370,11 @@ impl<DB: LibmdbxReader> SandwichInspector<'_, DB> {
 
             tracing::info!(
                 front_run_am = chunk_front_run_swaps.len(),
-                front_run_swaps= chunk_front_run_swaps.iter().flatten().count(),
+                front_run_swaps = chunk_front_run_swaps.iter().flatten().count(),
                 back_run_aswaps = chunk_back_run_swaps.len()
             );
+
+            tracing::info!("{:#?}", chunk_victim_actions);
 
             let front_run_pools = chunk_front_run_swaps
                 .iter()
