bd2c4b79054d9432b52287288d4ef73be8b8ece3
lagrange.eth <lagrange@sorellacapital.com>
2024-04-01 20:08:40 -0400

    testing pricing standard

--- crates/brontes-types/src/db/raw_cex_quotes.rs
+++ crates/brontes-types/src/db/raw_cex_quotes.rs
@@ -72,7 +72,7 @@ impl CexQuotesConverter {
     pub fn convert_to_prices(self) -> Vec<(u64, CexPriceMap)> {
         let mut block_num_map = HashMap::new();
 
-        println!("\nSELF: {:?}\n", self);
+        // println!("\nSELF: {:?}\n", self);
 
         self.quotes
             .into_par_iter()
