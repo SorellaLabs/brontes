c5d3a19154cc4466b36635e72ba5dffec22be5ef
lagrange.eth <lagrange@sorellacapital.com>
2024-04-01 20:35:23 -0400

    testing pricing standard

--- crates/brontes-database/src/clickhouse/queries/cex_symbols.sql
+++ crates/brontes-database/src/clickhouse/queries/cex_symbols.sql
@@ -1,6 +1,6 @@
 SELECT
     s.exchange AS exchange,
-    (upper(replaceAll(replaceAll(replaceAll(s.pair, '/', ''), '-', ''), '_', ''))) AS symbol_pair,
+    upper(replaceAll(replaceAll(replaceAll(s.pair, '/', ''), '-', ''), '_', '')) AS symbol_pair,
     (toString(et1.address), toString(et2.address)) AS address_pair
 FROM cex.symbols s 
 INNER JOIN ethereum.tokens AS et2 ON (upper(et2.symbol) = upper(s.quote_asset) OR concat('W', upper(s.quote_asset)) = upper(et2.symbol))
