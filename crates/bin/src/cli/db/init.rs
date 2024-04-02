be65f2eb757c7703647e535abd27762b52a36244
Will Smith <williamqsmith11@gmail.com>
2024-04-02 15:30:51 -0400

    wip

--- crates/bin/src/cli/db/init.rs
+++ crates/bin/src/cli/db/init.rs
@@ -4,6 +4,7 @@ use brontes_database::{libmdbx::LibmdbxInit, Tables};
 use brontes_types::init_threadpools;
 use clap::Parser;
 use indicatif::MultiProgress;
+use itertools::Itertools;
 
 use crate::{
     cli::{get_env_vars, get_tracing_provider, load_clickhouse, load_database, static_object},
@@ -51,6 +52,14 @@ impl Init {
 
         let tracer = Arc::new(get_tracing_provider(Path::new(&db_path), 10, task_executor.clone()));
 
+        let multi = MultiProgress::default();
+        let tables = Arc::new(
+            Tables::ALL
+                .into_iter()
+                .map(|table| (table, table.build_init_state_progress_bar(&multi)))
+                .collect_vec(),
+        );
+
         if self.init_libmdbx {
             // currently inits all tables
             let range = if let (Some(start), Some(end)) = (self.start_block, self.end_block) {
@@ -78,8 +87,7 @@ impl Init {
                                 .as_slice(),
                             false,
                             range,
-                            bar,
-                            0
+                            tables,
                         )
                         .await
                         .unwrap();
