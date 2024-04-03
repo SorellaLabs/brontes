1219a0121b5b6fd46414500608bc1ebce17fdace
Will Smith <williamqsmith11@gmail.com>
2024-04-03 18:54:47 -0400

    wip

--- crates/brontes-database/src/clickhouse/db_client.rs
+++ crates/brontes-database/src/clickhouse/db_client.rs
@@ -811,7 +811,7 @@ mod tests {
         cex_dex(database).await;
         mev_block(database).await;
         dex_price_mapping(database).await;
-        builder_stats(database).await;
+        // builder_stats(database).await;
         // searcher_stats(database).await;
         token_info(database).await;
         searcher_info(database).await;
