600228dc06c7ea9a9d30e91f75458c06ecc5bdd1
Will Smith <williamqsmith11@gmail.com>
2024-04-03 17:03:21 -0400

    spam

--- crates/brontes-types/src/db/raw_cex_trades.rs
+++ crates/brontes-types/src/db/raw_cex_trades.rs
@@ -105,6 +105,7 @@ impl CexTradesConverter {
                                 trade.price = 1.0 / trade.price;
                                 symbol.address_pair.flip()
                             };
+                            tracing::info!(?pair, ?trade.price, ?trade.amount);
 
                             exchange_symbol_map
                                 .entry(pair)
