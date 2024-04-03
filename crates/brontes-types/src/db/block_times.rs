fc7a634ce0546b6c9913d4678d5770e55add1468
lagrange.eth <lagrange@sorellacapital.com>
2024-04-03 18:48:44 -0400

    updated timestamp
    Former-commit-id: 50effc5b5fa29905aff35d60644d26698f9503f8

--- crates/brontes-types/src/db/block_times.rs
+++ crates/brontes-types/src/db/block_times.rs
@@ -27,8 +27,8 @@ pub struct CexBlockTimes {
 impl CexBlockTimes {
     pub fn add_time_window(value: BlockTimes, time_window: (u64, u64)) -> Self {
         Self {
-            start_timestamp: value.timestamp - time_window.0 * 1000,
-            end_timestamp:   value.timestamp + time_window.1 * 1000,
+            start_timestamp: value.timestamp - time_window.0 * 1000000,
+            end_timestamp:   value.timestamp + time_window.1 * 1000000,
             block_number:    value.block_number,
         }
     }
