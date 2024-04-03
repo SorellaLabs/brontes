d1ab88a76405af6c7aaea7f4c90e51d92bf14a5c
lagrange.eth <lagrange@sorellacapital.com>
2024-04-03 16:37:48 -0400

    fixed ordering issue with quotes
    Former-commit-id: d54c2330df7e241c28c87d344e870a6bfcbbb139

--- crates/brontes-types/src/db/raw_cex_quotes.rs
+++ crates/brontes-types/src/db/raw_cex_quotes.rs
@@ -102,7 +102,7 @@ impl CexQuotesConverter {
                                 .get(&(quote.exchange, quote.symbol.clone()))
                                 .unwrap();
                             exchange_symbol_map
-                                .entry(&symbol.address_pair.ordered())
+                                .entry(&symbol.address_pair)
                                 .or_insert(Vec::new())
                                 .push(quote);
                         });
