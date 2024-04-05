0c86fe51421128ea5d8ee0d3d68899251950816d
Will Smith <williamqsmith11@gmail.com>
2024-04-05 15:39:47 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
+++ crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
@@ -134,14 +134,14 @@ impl<DB: LibmdbxReader> CexDexMarkoutInspector<'_, DB> {
         metadata: &Metadata,
     ) -> Option<PossibleCexDexLeg> {
         // token in price
-        let pair = Pair(swap.token_out.address, swap.token_in.address);
+        let pair = Pair(swap.token_in.address, swap.token_out.address);
         tracing::info!(?pair, "fetching price");
 
         let (maker_price, taker_price) = metadata.cex_trades.as_ref()?.get_price(
             &self.cex_exchanges,
             &pair,
             // we always are buying amount in on cex
-            &swap.amount_out,
+            &swap.amount_in,
             // add lookup
             None,
         )?;
