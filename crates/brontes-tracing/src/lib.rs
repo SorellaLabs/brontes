18583c501d214501de2d90bbdccb97820d6a9c3b
ungaro <alp.guneysel@gmail.com>
2024-04-05 12:40:41 -0400

    add tui tracing support
    Former-commit-id: 20512ad3814a30a9df3b4a1a458e23f5f982f13b

--- crates/brontes-tracing/src/lib.rs
+++ crates/brontes-tracing/src/lib.rs
@@ -1,30 +1,31 @@
-use std::fmt::Display;
-
 use tracing::Subscriber;
-use tracing_subscriber::{
-    prelude::__tracing_subscriber_SubscriberExt, registry::LookupSpan, Registry,
-};
+use tracing_subscriber::{filter::Directive, prelude::*, registry::LookupSpan, *};
 
 /// A boxed tracing Layer.
 pub type BoxedLayer<S> = Box<dyn Layer<S> + Send + Sync>;
 
 /// Initializes a new [Subscriber] based on the given layers.
-pub fn init(layers: Vec<BoxedLayer<Registry>>) {
-    let _ = tracing_subscriber::registry().with(layers).try_init();
+pub fn init(layers: Vec<BoxedLayer<Registry>>,tui:bool) {
+    if tui {
+        let _ = tracing_subscriber::registry()
+        .with(tui_logger::tracing_subscriber_layer())
+        .try_init();
+    }else{
+        let _ = tracing_subscriber::registry()
+        .with(layers)
+        .try_init();
+    }
+
 }
-
-use tracing_subscriber::{layer::Layer, util::SubscriberInitExt, EnvFilter};
-
-pub fn stdout<S>(default_directive: impl Display) -> BoxedLayer<S>
+/// Builds a new tracing layer that writes to stdout.
+pub fn stdout<S>(directive: impl Into<Directive>) -> BoxedLayer<S>
 where
     S: Subscriber,
     for<'a> S: LookupSpan<'a>,
 {
     let filter = EnvFilter::builder()
-        .with_default_directive(default_directive.to_string().parse().unwrap())
-        .from_env_lossy()
-        .add_directive("hyper::proto::h1=off".parse().unwrap())
-        .add_directive("libmdbx=off".parse().unwrap());
+        .with_default_directive(directive.into())
+        .from_env_lossy();
 
     tracing_subscriber::fmt::layer()
         .with_ansi(true)
