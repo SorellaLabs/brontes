cf95d8b793fa9bc4e2cb046b9670c73341bb17b2
Will Smith <williamqsmith11@gmail.com>
2024-04-07 18:27:35 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/sandwich.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich.rs
@@ -332,6 +332,7 @@ impl<DB: LibmdbxReader> SandwichInspector<'_, DB> {
             MevType::Sandwich,
             !has_dex_price,
         );
+        tracing::debug!("{:#?}", header);
 
         let victim_swaps = victim_swaps
             .into_iter()
--- crates/brontes-pricing/src/graphs/yens.rs
+++ crates/brontes-pricing/src/graphs/yens.rs
@@ -7,10 +7,8 @@ use std::{
     time::{Duration, SystemTime},
 };
 
-use brontes_types::{pair::Pair, FastHashMap, FastHashSet, FastHasher};
-use dashmap::DashSet;
+use brontes_types::{pair::Pair, FastHashMap, FastHashSet};
 use pathfinding::num_traits::Zero;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 pub use crate::graphs::dijkstras::*;
 
@@ -146,7 +144,7 @@ where
     // A vector containing our paths.
     let mut routes = vec![Path { nodes: n, weights: e, cost: c }];
 
-    let visited = DashSet::with_hasher(FastHasher::new());
+    let mut visited = FastHashSet::default();
     let iter_k = k.unwrap_or(usize::MAX);
 
     // A min-heap to store our lowest-cost route candidate
