8432e469ed7d89333a88d08bfe785eab7eff06ef
Will Smith <williamqsmith11@gmail.com>
2024-04-06 22:31:19 -0400

    wip
    Former-commit-id: fa4dd92c4e024947cff28ebf933ef6c8afe2662c

--- crates/brontes-core/src/test_utils/mod.rs
+++ crates/brontes-core/src/test_utils/mod.rs
@@ -419,7 +419,7 @@ pub fn init_tracing() {
     let directive: Directive = format!("{verbosity_level}").parse().unwrap();
     let layers = vec![brontes_tracing::stdout(directive)];
 
-    brontes_tracing::init(layers, false);
+    brontes_tracing::init(layers);
 }
 
 #[cfg(feature = "local-reth")]
