cf707ff81453fe7761793977b25f01273696d9cd
Will Smith <williamqsmith11@gmail.com>
2024-04-07 16:55:00 -0400

    keys

--- crates/bin/src/executors/shared/dex_pricing.rs
+++ crates/bin/src/executors/shared/dex_pricing.rs
@@ -89,7 +89,7 @@ impl<T: TracingProvider, DB: DBWriter + LibmdbxReader + Unpin> Stream
 
             if let Some((block, prices)) = inner {
                 info!(target:"brontes","Generated dex prices for block: {}", block);
-                info!("{:#?}", self.pending_trees);
+                info!("{:#?}", self.pending_trees.keys());
 
                 let Some((mut tree, meta)) = self.pending_trees.remove(&block) else {
                     return Poll::Ready(None);
