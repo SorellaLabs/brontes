52e768cca828f0b20a0d65e3ddff9b9ec0bde766
ungaro <alp.guneysel@gmail.com>
2024-04-08 17:06:40 -0400

    more cleaning, removing warnings
    Former-commit-id: 56825d2ac764dccd02c246f45dd3ed2e75327969

--- crates/bin/src/tui/theme.rs
+++ crates/bin/src/tui/theme.rs
@@ -1,14 +1,14 @@
 use ratatui::prelude::*;
 
 pub struct Theme {
-    pub root:              Style,
-    pub content:           Style,
+    //pub root:              Style,
+    //pub content:           Style,
     pub app_title:         Style,
     pub tabs:              Style,
     pub tabs_selected:     Style,
-    pub borders:           Style,
-    pub description:       Style,
-    pub description_title: Style,
+    //pub borders:           Style,
+    //pub description:       Style,
+    //pub description_title: Style,
     pub key_binding:       KeyBinding,
 }
 
@@ -18,8 +18,8 @@ pub struct KeyBinding {
 }
 
 pub const THEME: Theme = Theme {
-    root:              Style::new().bg(DARK_BLUE),
-    content:           Style::new().bg(DARK_BLUE).fg(LIGHT_GRAY),
+    //root:              Style::new().bg(DARK_BLUE),
+    //content:           Style::new().bg(DARK_BLUE).fg(LIGHT_GRAY),
     app_title:         Style::new()
         .fg(WHITE)
         .bg(DARK_BLUE)
@@ -30,9 +30,9 @@ pub const THEME: Theme = Theme {
         .bg(DARK_BLUE)
         .add_modifier(Modifier::BOLD)
         .add_modifier(Modifier::REVERSED),
-    borders:           Style::new().fg(LIGHT_GRAY),
-    description:       Style::new().fg(LIGHT_GRAY).bg(DARK_BLUE),
-    description_title: Style::new().fg(LIGHT_GRAY).add_modifier(Modifier::BOLD),
+    //borders:           Style::new().fg(LIGHT_GRAY),
+    //description:       Style::new().fg(LIGHT_GRAY).bg(DARK_BLUE),
+    //description_title: Style::new().fg(LIGHT_GRAY).add_modifier(Modifier::BOLD),
 
     key_binding: KeyBinding {
         key:         Style::new().fg(BLACK).bg(DARK_GRAY),
@@ -41,17 +41,16 @@ pub const THEME: Theme = Theme {
 };
 
 const DARK_BLUE: Color = Color::Rgb(16, 24, 48);
-
-
 const WHITE: Color = Color::Indexed(255); // not really white, often #eeeeee
+const BLACK: Color = Color::Indexed(232); // not really black, often #080808
+const DARK_GRAY: Color = Color::Indexed(238);
+const MID_GRAY: Color = Color::Indexed(244);
+const LIGHT_GRAY: Color = Color::Indexed(250);
 
-// Not used currently, leaving for reference.
+// Not used currently, leaving for reference. might add later
+//
 //const LIGHT_BLUE: Color = Color::Rgb(64, 96, 192);
 //const LIGHT_YELLOW: Color = Color::Rgb(192, 192, 96);
 //const LIGHT_GREEN: Color = Color::Rgb(64, 192, 96);
 //const LIGHT_RED: Color = Color::Rgb(192, 96, 96);
 //const RED: Color = Color::Indexed(160);
-const BLACK: Color = Color::Indexed(232); // not really black, often #080808
-const DARK_GRAY: Color = Color::Indexed(238);
-const MID_GRAY: Color = Color::Indexed(244);
-const LIGHT_GRAY: Color = Color::Indexed(250);
