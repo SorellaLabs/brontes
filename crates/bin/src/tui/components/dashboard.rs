e15f42a0663e25fdb36d33b746c13a8b18aed93d
ungaro <alp.guneysel@gmail.com>
2024-04-08 21:48:07 -0400

    remove tui events from main application loop
    Former-commit-id: c44a972cc7e2ddc6d8867006414714a0fb960e5e

--- crates/bin/src/tui/components/dashboard.rs
+++ crates/bin/src/tui/components/dashboard.rs
@@ -111,7 +111,6 @@ impl Dashboard {
                 Some(i) => {
                     let mevblocks_guard: std::sync::MutexGuard<'_, Vec<Bundle>> =
                         self.mev_bundles.lock().unwrap();
-                    // info!("i  - len: {} {}", i, mevblocks_guard.len());
 
                     if mevblocks_guard.len() > 0 {
                         if i >= mevblocks_guard.len() - 1 {
