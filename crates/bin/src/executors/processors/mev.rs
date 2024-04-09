9432d4b55a3c4e0849fd49ace7721b233b55402e
ungaro <alp.guneysel@gmail.com>
2024-04-09 17:37:04 -0400

    fix processor
    Former-commit-id: 6ccb5d8e218fbd41c3eeda3088c1186da40db180

--- crates/bin/src/executors/processors/mev.rs
+++ crates/bin/src/executors/processors/mev.rs
@@ -49,7 +49,7 @@ impl Processor for MevProcessor {
     ) {
         let ComposerResults { block_details, mev_details, possible_mev_txes: _ } = execute_on!(
             target = inspect,
-            compose_mev_results(inspectors, tree.clone(), metadata.clone(), tui_tx.clone())
+            compose_mev_results(inspectors, tree.clone(), metadata.clone())
         );
 
         if let Err(e) = db
