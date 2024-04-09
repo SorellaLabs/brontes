c0e825def6250f551cf9d654c2f964224aa931eb
ungaro <alp.guneysel@gmail.com>
2024-04-09 16:45:07 -0400

    revert
    Former-commit-id: ac19a988148faa56d154b75be37cc1b30f91e405

--- crates/bin/src/tui/utils.rs
+++ crates/bin/src/tui/utils.rs
@@ -19,17 +19,30 @@ pub fn get_config_dir() -> PathBuf {
 macro_rules! get_symbols_from_transaction_accounting {
     ($data:expr) => {{
         use brontes_types::hasher::FastHashSet;
+
         use brontes_types::db::token_info::TokenInfoWithAddress;
 
+        let mut token_info_with_addresses: Vec<TokenInfoWithAddress> = Vec::new();
+        for transaction in $data {
+            for address_delta in &transaction.address_deltas {
+                for token_delta in &address_delta.token_deltas {
+                    token_info_with_addresses.push(token_delta.token.clone());
+                }
+            }
+        }
         let mut symbols = FastHashSet::default();
-        $data.iter()
-            .flat_map(|transaction| &transaction.address_deltas)
-            .flat_map(|address_delta| &address_delta.token_deltas)
-            .for_each(|token_delta| {
-                symbols.insert(token_delta.token.inner.symbol.clone());
-            });
-        
-        let unique_symbols = symbols.into_iter().collect::<Vec<String>>().join(", ");
-        unique_symbols
+        let unique_symbols: Vec<String> = token_info_with_addresses
+            .iter()
+            .filter_map(|x| {
+                let symbol = x.inner.symbol.to_string();
+                if symbols.insert(symbol.clone()) {
+                    Some(symbol)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        unique_symbols.join(", ")
     }};
 }
