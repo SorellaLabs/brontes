1eb49df339dd38ae08c9367e09da6e059ed269ed
Will Smith <williamqsmith11@gmail.com>
2024-04-12 12:48:55 -0400

    fmt

--- crates/brontes-inspect/src/mev_inspectors/sandwich.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich.rs
@@ -638,7 +638,7 @@ impl<DB: LibmdbxReader> SandwichInspector<'_, DB> {
 
             // if we had more than 50% victims, then we say this was valid. This
             // wiggle room is to deal with unknowns
-            if (was_victims as f64) / (amount as f64) < 0.5  || !has_sandwich {
+            if (was_victims as f64) / (amount as f64) < 0.5 || !has_sandwich {
                 return false
             }
         }
