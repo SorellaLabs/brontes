99e21c171f3ddefcb670403465a2f3dc2cfaef22
Will Smith <williamqsmith11@gmail.com>
2024-04-13 16:51:51 -0400

    fmt

--- crates/brontes-classifier/src/classifiers/maker/dss_flash.rs
+++ crates/brontes-classifier/src/classifiers/maker/dss_flash.rs
@@ -10,7 +10,10 @@ action_impl!(
     [FlashLoan],
     call_data: true,
     logs: true,
-    |call_info: CallInfo, call_data: flashLoanCall, log_data: MakerDssFlashFlashLoanCallLogs, db_tx: &DB| {
+    |call_info: CallInfo,
+    call_data: flashLoanCall,
+    log_data: MakerDssFlashFlashLoanCallLogs,
+    db_tx: &DB| {
         let logs = log_data.flash_loan_field?;
 
         let token = db_tx.try_fetch_token_info(call_data.token)?;
@@ -26,7 +29,7 @@ action_impl!(
             assets: vec![token],
             amounts: vec![amount],
 
-            // Empty 
+            // Empty
             aave_mode: None,
             child_actions: vec![],
             repayments: vec![],
@@ -42,7 +45,8 @@ mod tests {
     use alloy_primitives::{hex, Address, B256};
     use brontes_classifier::test_utils::ClassifierTestUtils;
     use brontes_types::{
-        db::token_info::{TokenInfo, TokenInfoWithAddress}, normalized_actions::{Actions, NormalizedTransfer},
+        db::token_info::{TokenInfo, TokenInfoWithAddress},
+        normalized_actions::{Actions, NormalizedTransfer},
         TreeSearchBuilder,
     };
     use reth_primitives::U256;
@@ -54,7 +58,7 @@ mod tests {
         let classifier_utils = ClassifierTestUtils::new().await;
         let flashloan_tx =
             B256::from(hex!("8e2d6af376182807f0671f1504767c7723c49921344ce4f5799d8ba2d30d014c"));
-        
+
         let dai = TokenInfoWithAddress {
             address: Address::new(hex!("6b175474e89094c44da98b954eedeac495271d0f")),
             inner:   TokenInfo { decimals: 18, symbol: "DAI".to_string() },
@@ -67,24 +71,18 @@ mod tests {
             pool:              Address::new(hex!("60744434d6339a6b27d73d9eda62b6f66a0a04fa")),
             receiver_contract: Address::new(hex!("1aecea38b8626eeb3748234343cff427268dd487")),
             assets:            vec![dai.clone()],
-            amounts:           vec![U256::from_str("100000000")
-                .unwrap()
-                .to_scaled_rational(0)],
+            amounts:           vec![U256::from_str("100000000").unwrap().to_scaled_rational(0)],
             aave_mode:         None,
             // Ignore child actions as we only need to focus on pruning necessary nodes.
             child_actions:     vec![],
-            repayments:        vec![
-                NormalizedTransfer { 
-                    trace_index: 238, 
-                    from: Address::new(hex!("1aecea38b8626eeb3748234343cff427268dd487")), 
-                    to: Address::new(hex!("60744434d6339a6b27d73d9eda62b6f66a0a04fa")), 
-                    token: dai, 
-                    amount: U256::from_str("100000000")
-                        .unwrap()
-                        .to_scaled_rational(0), 
-                    fee: U256::ZERO.to_scaled_rational(0),
-                },
-            ],
+            repayments:        vec![NormalizedTransfer {
+                trace_index: 238,
+                from:        Address::new(hex!("1aecea38b8626eeb3748234343cff427268dd487")),
+                to:          Address::new(hex!("60744434d6339a6b27d73d9eda62b6f66a0a04fa")),
+                token:       dai,
+                amount:      U256::from_str("100000000").unwrap().to_scaled_rational(0),
+                fee:         U256::ZERO.to_scaled_rational(0),
+            }],
             fees_paid:         vec![],
             msg_value:         U256::ZERO,
         });
@@ -98,6 +96,6 @@ mod tests {
                 &["child_actions"],
             )
             .await
-            .unwrap(); 
+            .unwrap();
     }
 }
