1bdd39feb98d29eacf64c61f1327348d03ebb7e2
Will Smith <williamqsmith11@gmail.com>
2024-04-13 19:51:50 -0400

    wip

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -69,7 +69,7 @@ use types::{DexPriceMsg, PoolUpdate};
 use crate::types::PoolState;
 /// max movement of price in the block before its considered invalid.
 /// currently 20%
-const MAX_BLOCK_MOVEMENT: Rational = Rational::const_from_unsigneds(2, 10);
+const MAX_BLOCK_MOVEMENT: Rational = Rational::const_from_unsigneds(6, 10);
 
 /// # Brontes Batch Pricer
 ///
