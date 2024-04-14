f7bb657e799d3402895238fc315211a9df9099f7
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-04-14 19:26:18 -0400

    test

--- crates/brontes-inspect/src/mev_inspectors/cex_dex.rs
+++ crates/brontes-inspect/src/mev_inspectors/cex_dex.rs
@@ -227,8 +227,8 @@ impl<DB: LibmdbxReader> CexDexInspector<'_, DB> {
         if smaller * Rational::from(3) < larger {
             tracing::error!(
                 "\n\x1b[1;35mDetected significant price delta for direct pair for {} - {} on {}:\x1b[0m\n\
-                 - \x1b[1;36mDEX Swap Rate:\x1b[0m {:.4}\n\
-                 - \x1b[1;36mCEX Price:\x1b[0m {:.4}\n\
+                 - \x1b[1;36mDEX Swap Rate:\x1b[0m {:.7}\n\
+                 - \x1b[1;36mCEX Price:\x1b[0m {:.7}\n\
                  - Token Contracts:\n\
                    * Token In: https://etherscan.io/address/{}\n\
                    * Token Out: https://etherscan.io/address/{}",
