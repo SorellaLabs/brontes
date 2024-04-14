4b881b485c206be1092e4af1b78c16a562bc033e
lagrange.eth <joseph@sorellalabs.xyz>
2024-04-14 16:47:11 -0400

    added verbosity (#606)
    Former-commit-id: e5b0eaa2a37c1ac4d4bfd2bfb89078f73cb2cf4f

---
+++ crates/bin/src/cli/misc.rs
@@ -0,0 +1,42 @@
+use clap::{command, ArgAction, Args};
+use tracing::{level_filters::LevelFilter, Level};
+use tracing_subscriber::filter::Directive;
+
+/// The verbosity settings for the cli.
+#[derive(Debug, Copy, Clone, Args)]
+#[command(next_help_heading = "Display")]
+pub struct Verbosity {
+    /// Set the minimum log level.
+    ///
+    /// -v      Errors
+    /// -vv     Warnings
+    /// -vvv    Info
+    /// -vvvv   Debug
+    /// -vvvvv  Traces (warning: very verbose!)
+    #[clap(short, long, action = ArgAction::Count, global = true, default_value_t = 3, verbatim_doc_comment, help_heading = "Display")]
+    verbosity: u8,
+
+    /// Silence all log output.
+    #[clap(long, alias = "silent", short = 'q', global = true, help_heading = "Display")]
+    quiet: bool,
+}
+
+impl Verbosity {
+    /// Get the corresponding [Directive] for the given verbosity, or none if
+    /// the verbosity corresponds to silent.
+    pub fn directive(&self) -> Directive {
+        if self.quiet {
+            LevelFilter::OFF.into()
+        } else {
+            let level = match self.verbosity - 1 {
+                0 => Level::ERROR,
+                1 => Level::WARN,
+                2 => Level::INFO,
+                3 => Level::DEBUG,
+                _ => Level::TRACE,
+            };
+
+            format!("{level}").parse().unwrap()
+        }
+    }
+}
