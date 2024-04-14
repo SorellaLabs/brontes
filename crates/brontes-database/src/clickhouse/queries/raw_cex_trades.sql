19471b003a995fb2de3b8dc350ed34980307b83b
lagrange.eth <lagrange@sorellacapital.com>
2024-04-14 17:05:47 -0400

    debugging trades
    Former-commit-id: 981e1db6b10a4fa83f34119e26ee704e27fde828

--- crates/brontes-database/src/clickhouse/queries/raw_cex_trades.sql
+++ crates/brontes-database/src/clickhouse/queries/raw_cex_trades.sql
@@ -7,14 +7,3 @@ SELECT
     if (side == 'buy', amount, multiply(divide(1, price), amount)) AS amount
 FROM cex.normalized_trades 
 WHERE timestamp >= ? AND timestamp < ?
-
-
-
-
-
-
-SELECT *
-FROM cex.normalized_trades
-WHERE exchange = 'okex'
-AND timestamp >= (1701543815-6) * 1000000
-AND timestamp < (1701543815+6) * 1000000
