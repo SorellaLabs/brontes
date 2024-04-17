faa917ec365be9688066cb56c3baa3214ed4653a
lagrange.eth <lagrange@sorellacapital.com>
2024-04-16 20:12:53 -0400

    merged main

--- crates/brontes-database/src/clickhouse/db_client.rs
+++ crates/brontes-database/src/clickhouse/db_client.rs
@@ -147,7 +147,7 @@ impl Clickhouse {
                     BundleData::Unknown(s) => tx.send(vec![s.into()])?,
                 };
 
-                Ok(())
+                Ok::<eyre::Result<()>>(())
             })?;
         }
 
