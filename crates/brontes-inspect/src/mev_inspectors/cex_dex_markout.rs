7af6e2611db3aef6527241a9a6687b07bce16f3a
Will Smith <williamqsmith11@gmail.com>
2024-04-20 15:52:12 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
+++ crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
@@ -281,7 +281,7 @@ impl<DB: LibmdbxReader> CexDexMarkoutInspector<'_, DB> {
             timestamp: metadata.block_timestamp,
             price_maker: (cex_quote.0.clone(), cex_quote.0.clone()),
             price_taker: (cex_quote.1.clone(), cex_quote.1.clone()),
-            amount: (Rational::ZERO, Rational::ZERO),
+            amount: (Rational::ONE, Rational::ONE),
             token0: Address::ZERO,
             exchange,
         };
