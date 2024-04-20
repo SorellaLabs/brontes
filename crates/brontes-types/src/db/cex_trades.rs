3cd3b81460d9d1b6f5b0e51ed60aa3a37c5f8fe5
Will Smith <williamqsmith11@gmail.com>
2024-04-19 20:22:10 -0400

    wip

--- crates/brontes-types/src/db/cex_trades.rs
+++ crates/brontes-types/src/db/cex_trades.rs
@@ -1,3 +1,4 @@
+use core::f64;
 use std::{cmp::max, f64::consts::E, fmt::Display, marker::PhantomData};
 
 use alloy_primitives::Address;
@@ -45,6 +46,225 @@ impl Display for ExchangePrice {
 
 type MakerTaker = (ExchangePrice, ExchangePrice);
 type RedefinedTradeMapVec = Vec<(PairRedefined, Vec<CexTradesRedefined>)>;
+
+const PRE_DECAY: f64 = -0.5;
+const POST_DECAY: f64 = -0.2;
+
+// trades sorted by time-stamp with the index to block time-stamp closest to the
+// block_number
+pub struct CexTradeMap2(pub FastHashMap<CexExchange, FastHashMap<Pair, (usize, Vec<CexTrades>)>>);
+
+impl CexTradeMap2 {
+    fn get_vwam_price_via_intermediary(
+        &self,
+        exchanges: &[CexExchange],
+        pair: &Pair,
+        volume: &Rational,
+        timestamp: u128,
+    ) -> Option<(Rational, Rational)> {
+        self.calculate_intermediary_addresses(exchanges, pair)
+            .into_par_iter()
+            .filter_map(|intermediary| {
+                // usdc / bnb 0.004668534080298786price
+                let pair0 = Pair(pair.0, intermediary);
+                // bnb / eth 0.1298price
+                let pair1 = Pair(intermediary, pair.1);
+                // check if we have a path
+                let mut has_pair0 = false;
+                let mut has_pair1 = false;
+
+                for (_, trades) in self.0.iter().filter(|(ex, _)| exchanges.contains(ex)) {
+                    has_pair0 |= trades.contains_key(&pair0);
+                    has_pair1 |= trades.contains_key(&pair1);
+
+                    if has_pair1 && has_pair0 {
+                        break
+                    }
+                }
+
+                if !(has_pair0 && has_pair1) {
+                    return None
+                }
+
+                tracing::debug!(?pair, ?intermediary, "trying via intermediary");
+                let (i, res) =
+                    (intermediary, self.get_vwam_price(exchanges, pair0, volume, timestamp)?);
+                let new_vol = volume * &res.0;
+                let pair1_v = self.get_vwam_price(exchanges, pair1, &new_vol, timestamp)?;
+
+                let maker = res.0 * pair1_v.0;
+                let taker = res.1 * pair1_v.1;
+                Some((maker, taker))
+            })
+            .max_by_key(|a| a.0)
+    }
+
+    fn get_vwam_price(
+        &self,
+        exchanges: &[CexExchange],
+        pair: Pair,
+        vol: &Rational,
+        timestamp: u128,
+    ) -> Option<(Rational, Rational)> {
+        let (ptrs, trades): (FastHashMap<CexExchange, (usize, usize)>, Vec<(CexExchange, _)>) =
+            self.0
+                .iter()
+                .filter(|(e, _)| exchanges.contains(e))
+                .filter_map(|(exchange, trades)| Some((*exchange, trades.get(&pair)?)))
+                .map(|(ex, (idx, trades))| ((ex, (idx + 1, *idx)), (ex, trades)))
+                .unzip();
+
+        let mut walker =
+            PairTradeWalker::new(trades, ptrs, timestamp - 500_000, timestamp + 2_000_000);
+
+        let mut vxp_maker = Rational::ZERO;
+        let mut vxp_taker = Rational::ZERO;
+        let mut trade_volume = Rational::ZERO;
+
+        while trade_volume.le(vol) {
+            for trade in walker.get_trades_for_window() {
+                let (m_fee, t_fee) = trade.get().exchange.fees();
+                let weight = calcuate_weight(timestamp as u64, trade.get().timestamp as u64);
+
+                vxp_maker +=
+                    (&trade.get().price * (Rational::ONE - m_fee)) * &trade.get().amount * &weight;
+                vxp_taker +=
+                    (&trade.get().price * (Rational::ONE - t_fee)) * &trade.get().amount * &weight;
+                trade_volume += &trade.get().amount * weight;
+            }
+
+            if walker.get_min_time_delta(timestamp) >= 3_000_000
+                || walker.get_max_time_delta(timestamp) >= 5_000_000
+            {
+                break
+            }
+
+            let min_expand =
+                if walker.get_max_time_delta(timestamp) >= 3_000_000 { 100_000 } else { 0 };
+
+            walker.expand_time_bounds(min_expand, 100_000);
+        }
+
+        if &trade_volume < vol {
+            return None
+        }
+
+        Some((vxp_maker, vxp_taker))
+    }
+
+    fn calculate_intermediary_addresses(
+        &self,
+        exchanges: &[CexExchange],
+        pair: &Pair,
+    ) -> FastHashSet<Address> {
+        self.0
+            .par_iter()
+            .filter(|(k, _)| exchanges.contains(k))
+            .flat_map(|(_, pairs)| {
+                pairs
+                    .keys()
+                    .filter_map(|trade_pair| {
+                        if trade_pair.ordered() == pair.ordered() {
+                            return None
+                        }
+
+                        (trade_pair.0 == pair.0)
+                            .then_some(trade_pair.1)
+                            .or_else(|| (trade_pair.1 == pair.1).then_some(trade_pair.0))
+                    })
+                    .collect_vec()
+            })
+            .collect::<FastHashSet<_>>()
+    }
+}
+
+fn calcuate_weight(block_time: u64, trade_time: u64) -> Rational {
+    let pre = trade_time < block_time;
+
+    Rational::try_from_float_simplest(if pre {
+        E.powf(PRE_DECAY * (block_time - trade_time) as f64)
+    } else {
+        E.powf(POST_DECAY * (trade_time - block_time) as f64)
+    })
+    .unwrap()
+}
+
+pub struct PairTradeWalker<'a> {
+    min_timestamp: u128,
+    max_timestamp: u128,
+    exchange_ptrs: FastHashMap<CexExchange, (usize, usize)>,
+    trades:        Vec<(CexExchange, &'a Vec<CexTrades>)>,
+}
+
+impl<'a> PairTradeWalker<'a> {
+    pub fn new(
+        trades: Vec<(CexExchange, &'a Vec<CexTrades>)>,
+        exchange_ptrs: FastHashMap<CexExchange, (usize, usize)>,
+        min_timestamp: u128,
+        max_timestamp: u128,
+    ) -> Self {
+        Self { max_timestamp, min_timestamp, trades, exchange_ptrs }
+    }
+
+    fn get_min_time_delta(&self, timestamp: u128) -> u128 {
+        timestamp - self.min_timestamp
+    }
+
+    fn get_max_time_delta(&self, timestamp: u128) -> u128 {
+        self.max_timestamp - timestamp
+    }
+
+    fn expand_time_bounds(&mut self, min: u128, max: u128) {
+        self.min_timestamp -= min;
+        self.max_timestamp += max;
+    }
+
+    fn get_trades_for_window(&mut self) -> Vec<CexTradePtr<'a>> {
+        let mut trade_res: Vec<CexTradePtr<'a>> = Vec::with_capacity(420);
+
+        for (exchange, trades) in &self.trades {
+            let Some((lower_idx, upper_idx)) = self.exchange_ptrs.get_mut(&exchange) else {
+                continue
+            };
+
+            // add lower
+            if *lower_idx > 0 {
+                loop {
+                    let next_trade = &trades[*lower_idx - 1];
+                    if next_trade.timestamp >= self.min_timestamp {
+                        trade_res.push(CexTradePtr::new(next_trade));
+                        *lower_idx -= 1;
+                    } else {
+                        break
+                    }
+
+                    if *lower_idx == 0 {
+                        break
+                    }
+                }
+            }
+
+            let max = trades.len() - 1;
+            if *upper_idx < max {
+                loop {
+                    let next_trade = &trades[*upper_idx + 1];
+                    if next_trade.timestamp <= self.max_timestamp {
+                        trade_res.push(CexTradePtr::new(next_trade));
+                        *upper_idx += 1;
+                    } else {
+                        break
+                    }
+
+                    if *upper_idx == max {
+                        break
+                    }
+                }
+            }
+        }
+
+        trade_res
+    }
+}
 
 // cex trades are sorted from lowest fill price to highest fill price
 #[derive(Debug, Default, Clone, Row, PartialEq, Eq, Serialize)]
@@ -557,17 +777,19 @@ impl CexTradeMap {
 ))]
 pub struct CexTrades {
     #[redefined(same_fields)]
-    pub exchange: CexExchange,
-    pub price:    Rational,
-    pub amount:   Rational,
+    pub exchange:  CexExchange,
+    pub timestamp: u128,
+    pub price:     Rational,
+    pub amount:    Rational,
 }
 
 impl From<RawCexTrades> for CexTrades {
     fn from(value: RawCexTrades) -> Self {
         Self {
-            exchange: value.exchange,
-            price:    Rational::try_from_float_simplest(value.price).unwrap(),
-            amount:   Rational::try_from_float_simplest(value.amount).unwrap(),
+            exchange:  value.exchange,
+            price:     Rational::try_from_float_simplest(value.price).unwrap(),
+            amount:    Rational::try_from_float_simplest(value.amount).unwrap(),
+            timestamp: value.timestamp as u128,
         }
     }
 }
