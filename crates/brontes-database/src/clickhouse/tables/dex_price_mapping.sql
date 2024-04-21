ab4f823e6e556818f34740f3577162338fb42d82
Will Smith <williamqsmith11@gmail.com>
2024-04-21 15:26:43 -0400

    wip

--- crates/brontes-database/src/clickhouse/tables/dex_price_mapping.sql
+++ crates/brontes-database/src/clickhouse/tables/dex_price_mapping.sql
@@ -3,7 +3,7 @@ CREATE TABLE brontes.dex_price_mapping ON CLUSTER eth_cluster0
     `block_number` UInt64,
     `tx_idx` UInt64,
     `quote` Array(Tuple(Tuple(String, String), Tuple(Tuple(Array(UInt64), Array(UInt64)), Tuple(Array(UInt64), Array(UInt64)), Tuple(String, String), bool))),
-    `last_updated` UInt64
+    `last_updated` UInt64 DEFAULT now()
 )
 ENGINE = ReplicatedReplacingMergeTree('/clickhouse/eth_cluster0/tables/all/brontes/dex_price_mapping', '{replica}', `last_updated`)
 PRIMARY KEY (`block_number`, `tx_idx`)
