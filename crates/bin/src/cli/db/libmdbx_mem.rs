86b18d08bb7081f0f4282c1b4aca79d5a59b8a99
Will Smith <williamqsmith11@gmail.com>
2024-04-26 13:26:54 -0400

    wip

---
+++ crates/bin/src/cli/db/libmdbx_mem.rs
@@ -0,0 +1,60 @@
+use std::path::Path;
+
+use brontes_core::{decoding::Parser as DParser, LibmdbxReader};
+use brontes_database::{
+    clickhouse::cex_config::CexDownloadConfig,
+    libmdbx::{cursor::CompressedCursor, Libmdbx},
+    CompressedTable, IntoTableKey, Tables,
+};
+use brontes_inspect::Inspectors;
+use brontes_metrics::PoirotMetricsListener;
+use brontes_types::{constants::USDT_ADDRESS_STRING, db::cex::CexExchange, init_threadpools};
+use clap::Parser;
+use itertools::Itertools;
+use reth_db::mdbx::RO;
+use reth_interfaces::db::DatabaseErrorInfo;
+use tokio::sync::mpsc::unbounded_channel;
+
+use crate::{
+    banner,
+    cli::{
+        get_tracing_provider, init_inspectors,
+        utils::{determine_max_tasks, get_env_vars, load_clickhouse, load_database, static_object},
+    },
+    runner::CliContext,
+    BrontesRunConfig, MevProcessor,
+};
+#[derive(Debug, Parser)]
+pub struct LMem {
+    #[arg(long, short)]
+    pub start: u64,
+    #[arg(long, short)]
+    pub end:   u64,
+}
+
+impl LMem {
+    pub async fn execute(self, brontes_db_endpoint: String, ctx: CliContext) -> eyre::Result<()> {
+        let libmdbx = static_object(load_database(&ctx.task_executor, brontes_db_endpoint)?);
+
+        let mut set = vec![];
+        for block_range in (self.start..self.end)
+            .chunks(1000)
+            .into_iter()
+            .map(|f| f.collect_vec())
+        {
+            set.push(ctx.task_executor.spawn_critical("test_mem", async move {
+                for block in block_range {
+                    let _ = libmdbx.load_trace(block);
+                    let _ = libmdbx.get_dex_quotes(block);
+                    let _ = libmdbx.get_metadata(block);
+                }
+            }));
+        }
+
+        for s in set {
+            s.await?;
+        }
+
+        Ok(())
+    }
+}
