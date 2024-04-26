2199b583388afdc62318b558a3b535577c627959
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2024-04-26 16:57:49 -0400

    fix: libmdbx memory issue (#629)
    * wip
    
    * wip
    
    * wip
    
    * update
    
    * wip
    
    * wip
    
    * wi
    
    * no
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fmt
    
    * more
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * update
    
    * wip
    
    * fix
    
    * new redefined
    
    Former-commit-id: 83b59deab7371f9d2b1a655e71c992dcb5f87f69

--- crates/reth-tracing-ext/src/reth_tracer/config.rs
+++ crates/reth-tracing-ext/src/reth_tracer/config.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-use alloy_rpc_trace_types::{geth::GethDefaultTracingOptions, parity::TraceType};
+use alloy_rpc_types_trace::{geth::GethDefaultTracingOptions, parity::TraceType};
 
 /// Gives guidance to the [TracingInspector](crate::tracing::TracingInspector).
 ///
