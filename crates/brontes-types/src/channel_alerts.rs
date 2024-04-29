2017ac313a3d3fdc5b3ec1f9f166a911fd864f07
Will Smith <williamqsmith11@gmail.com>
2024-04-29 17:57:50 -0400

    yap

--- crates/brontes-types/src/channel_alerts.rs
+++ crates/brontes-types/src/channel_alerts.rs
@@ -1,3 +1,5 @@
+use std::task::{Context, Poll};
+
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 
 pub struct UnboundedYapperReceiver<T> {
@@ -12,7 +14,17 @@ impl<T> UnboundedYapperReceiver<T> {
         Self { chan, yap_count, name }
     }
 
-    fn recv(&mut self) -> Result<T, tokio::sync::mpsc::error::TryRecvError> {
+    pub fn poll_recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
+        let len = self.chan.len();
+        if len > self.yap_count {
+            let mb = (std::mem::size_of::<T>() * len) / 1_000_000;
+            tracing::warn!(chan=%self.name, mb_usage=mb, "unbounded channel is above threshold");
+        }
+
+        self.chan.poll_recv(cx)
+    }
+
+    pub fn recv(&mut self) -> Result<T, tokio::sync::mpsc::error::TryRecvError> {
         let len = self.chan.len();
         if len > self.yap_count {
             let mb = (std::mem::size_of::<T>() * len) / 1_000_000;
@@ -20,9 +32,5 @@ impl<T> UnboundedYapperReceiver<T> {
         }
 
         self.chan.try_recv()
-    }
-
-    fn should_yap(&self) -> bool {
-        self.chan.len() > self.yap_count
     }
 }
