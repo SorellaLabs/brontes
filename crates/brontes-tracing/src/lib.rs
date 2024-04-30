00818404ab1d8ef05a00c3754bf8920632965dde
Will Smith <williamqsmith11@gmail.com>
2024-04-30 18:37:53 -0400

    add libmdbx dir

--- crates/brontes-tracing/src/lib.rs
+++ crates/brontes-tracing/src/lib.rs
@@ -23,8 +23,7 @@ where
     let filter = EnvFilter::builder()
         .with_default_directive(default_directive.to_string().parse().unwrap())
         .from_env_lossy()
-        .add_directive("hyper::proto::h1=off".parse().unwrap())
-        .add_directive("libmdbx=off".parse().unwrap());
+        .add_directive("hyper::proto::h1=off".parse().unwrap());
 
     tracing_subscriber::fmt::layer()
         .with_ansi(true)
