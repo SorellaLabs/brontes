8ee9b5329bdc36e71261c8be7fd27c37772f6cac
vanbeethoven.eth <103021596+0xvanbeethoven@users.noreply.github.com>
2024-05-01 18:15:10 -0400

    Ludwig/libmdx settings (#638)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * yap
    
    * wip
    
    * wip
    
    * wip
    
    * pray
    
    * fix: not removing subgraph state on finish
    
    * yer
    
    * yer
    
    * wip
    
    * yer
    
    * wip
    
    * fmt
    
    * wip
    
    * wip
    
    * wip
    
    * wpi
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * yer
    
    * fix: pricing
    
    * fix: remove files
    
    * wip
    
    * wip
    
    * wip
    
    * feat: move to borrowed
    
    * wip
    
    * wop
    
    * wi
    
    * pub
    
    * some
    
    * ip
    
    * put
    
    * fix
    
    * wip
    
    * try
    
    * fix
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * wip
    
    * add libmdbx dir
    
    * wip
    
    * ensure no writo
    
    * test
    
    * test
    
    * test
    
    * wip
    
    * wip checkpoint
    
    * finally ported reth libmbx to local to optimise env
    
    * testing new env settings
    
    * test without write map
    
    * test
    
    * merged main
    
    ---------
    
    Co-authored-by: Will Smith <williamqsmith11@gmail.com>
    Co-authored-by: root <root@ip-172-21-136-18.ec2.internal>

---
+++ crates/brontes-database/libmdbx-rs/src/lib.rs
@@ -0,0 +1,75 @@
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![allow(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
+pub use crate::{
+    codec::*,
+    cursor::{Cursor, Iter, IterDup},
+    database::Database,
+    environment::{
+        Environment, EnvironmentBuilder, EnvironmentKind, Geometry, HandleSlowReadersCallback,
+        HandleSlowReadersReturnCode, Info, PageSize, Stat,
+    },
+    error::{Error, Result},
+    flags::*,
+    transaction::{CommitLatency, Transaction, TransactionKind, RO, RW},
+};
+pub mod ffi {
+    pub use ffi::{MDBX_dbi as DBI, MDBX_log_level_t as LogLevel};
+}
+
+#[cfg(feature = "read-tx-timeouts")]
+pub use crate::environment::read_transactions::MaxReadTransactionDuration;
+
+mod codec;
+mod cursor;
+mod database;
+mod environment;
+mod error;
+mod flags;
+mod transaction;
+mod txn_manager;
+
+#[cfg(test)]
+mod test_utils {
+    use byteorder::{ByteOrder, LittleEndian};
+    use tempfile::tempdir;
+
+    use super::*;
+
+    /// Regression test for https://github.com/danburkert/lmdb-rs/issues/21.
+    /// This test reliably segfaults when run against lmbdb compiled with opt
+    /// level -O3 and newer GCC compilers.
+    #[test]
+    fn issue_21_regression() {
+        const HEIGHT_KEY: [u8; 1] = [0];
+
+        let dir = tempdir().unwrap();
+
+        let env = {
+            let mut builder = Environment::builder();
+            builder.set_max_dbs(2);
+            builder
+                .set_geometry(Geometry { size: Some(1_000_000..1_000_000), ..Default::default() });
+            builder.open(dir.path()).expect("open mdbx env")
+        };
+
+        for height in 0..1000 {
+            let mut value = [0u8; 8];
+            LittleEndian::write_u64(&mut value, height);
+            let tx = env.begin_rw_txn().expect("begin_rw_txn");
+            let index = tx
+                .create_db(None, DatabaseFlags::DUP_SORT)
+                .expect("open index db");
+            tx.put(index.dbi(), HEIGHT_KEY, value, WriteFlags::empty())
+                .expect("tx.put");
+            tx.commit().expect("tx.commit");
+        }
+    }
+}
