df0fad42dfcb872b6f7e2319d14c5d824968f1c0
Will Smith <williamqsmith11@gmail.com>
2024-05-02 16:06:39 -0400

    wip

--- crates/bin/src/executors/range.rs
+++ crates/bin/src/executors/range.rs
@@ -76,12 +76,12 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter, CH: ClickhouseHandle, P: 
 
     fn on_price_finish(&mut self, tree: BlockTree<Action>, meta: Metadata) {
         debug!(target:"brontes","Completed DEX pricing");
-        self.insert_futures.push(Box::pin(P::process_results(
-            self.libmdbx,
-            self.inspectors,
-            tree,
-            meta,
-        )));
+        // self.insert_futures.push(Box::pin(P::process_results(
+        //     self.libmdbx,
+        //     self.inspectors,
+        //     tree,
+        //     meta,
+        // )));
     }
 }
 
