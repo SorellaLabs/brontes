23590ce9fb42a1e55392e5a2edc53e495447be2c
Will Smith <williamqsmith11@gmail.com>
2024-05-02 18:50:59 -0400

    wip

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -1175,6 +1175,12 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter + Unpin> Stream
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
+        {
+            cx.waker().wake_by_ref();
+            while self.update_rx.poll_recv(cx).is_ready() {}
+            return Poll::Pending
+        }
+
         if let Some(new_prices) = self.poll_state_processing(cx) {
             return new_prices
         }
