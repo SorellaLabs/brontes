1fa80e135eeefeb2fd01a8c49325eb9b2337c710
Will Smith <williamqsmith11@gmail.com>
2024-05-03 15:57:14 -0400

    wip

--- crates/bin/src/executors/range.rs
+++ crates/bin/src/executors/range.rs
@@ -128,6 +128,7 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter, CH: ClickhouseHandle, P: 
             && self.insert_futures.is_empty()
             && !self.collector.is_collecting_state()
         {
+            tracing::info!(rem=?self.insert_futures.len());
             cx.waker().wake_by_ref();
             self.collector.range_finished();
         }
