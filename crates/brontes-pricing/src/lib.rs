b0c4c8c1ca3d53ab2bbe0cf0fa44f320bf3ba07a
Will Smith <williamqsmith11@gmail.com>
2024-05-03 15:08:59 -0400

    wip

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -1136,7 +1136,7 @@ impl<T: TracingProvider, DB: DBWriter + LibmdbxReader> BrontesBatchPricer<T, DB>
         self.graph_manager
             .prune_dead_subgraphs(self.completed_block);
 
-        tracing::info!(?block, "on close");
+        tracing::info!(?block, ?self.current_block, "on close");
         self.should_return().then_some((block, res))
     }
 
