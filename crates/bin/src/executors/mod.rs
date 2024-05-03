e1c2fd998fd649e6d6566f8c45fb724d1629db34
Will Smith <williamqsmith11@gmail.com>
2024-05-03 14:50:58 -0400

    wip

--- crates/bin/src/executors/mod.rs
+++ crates/bin/src/executors/mod.rs
@@ -441,7 +441,7 @@ impl Future for Brontes {
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        while self.futures.poll_next_unpin(cx).is_ready() {}
+        while let Poll::Ready(Some(_)) = self.futures.poll_next_unpin(cx) {}
         if self.futures.is_empty() {
             return Poll::Ready(())
         }
