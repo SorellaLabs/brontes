bed9eb0f7d04abb83d3cfcfbf5cb242e76784a19
Will Smith <williamqsmith11@gmail.com>
2024-05-04 10:16:18 -0400

    fix: always poll for now
    Former-commit-id: 8bccd73d327c14a775025ee7e355eace02da51a5

--- crates/bin/src/executors/range.rs
+++ crates/bin/src/executors/range.rs
@@ -97,7 +97,6 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter, CH: ClickhouseHandle, P: 
             && self.insert_futures.len() < 5
             && self.current_block != self.end_block
         {
-            cx.waker().wake_by_ref();
             let block = self.current_block;
             self.collector.fetch_state_for(block);
             self.current_block += 1;
@@ -124,10 +123,11 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter, CH: ClickhouseHandle, P: 
             && self.insert_futures.is_empty()
             && !self.collector.is_collecting_state()
         {
-            cx.waker().wake_by_ref();
             self.collector.range_finished();
         }
 
+        // for now fine
+        cx.waker().wake_by_ref();
         Poll::Pending
     }
 }
