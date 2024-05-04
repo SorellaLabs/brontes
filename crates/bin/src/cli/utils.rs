7ddf6d56adb840ace3da6376ae8a6dfaabe6c028
Will Smith <williamqsmith11@gmail.com>
2024-05-04 15:10:50 -0400

    wip

--- crates/bin/src/cli/utils.rs
+++ crates/bin/src/cli/utils.rs
@@ -162,7 +162,7 @@ fn spawn_db_writer_thread(
     executor.spawn_critical_with_graceful_shutdown_signal(
         "clickhouse insert process",
         |shutdown| async move {
-            let clickhouse_writer = ClickhouseBuffered::new(UnboundedYapperReceiver::new(buffered_rx, 1500, "clickhouse buffered".to_string()), 100);
+            let clickhouse_writer = ClickhouseBuffered::new(UnboundedYapperReceiver::new(buffered_rx, 1500, "clickhouse buffered".to_string()), 5_000);
             pin_mut!(clickhouse_writer, shutdown);
 
             let mut graceful_guard = None;
