01d04c91a27c8862741a5e255bba9d4d5029fe20
Will Smith <williamqsmith11@gmail.com>
2024-05-04 17:11:38 -0400

    wip

--- crates/brontes-macros/src/action_classifier/closure_dispatch.rs
+++ crates/brontes-macros/src/action_classifier/closure_dispatch.rs
@@ -31,27 +31,27 @@ impl ToTokens for ClosureDispatch {
         let log_data = self.logs.then_some(quote!(log_data,)).unwrap_or_default();
 
         tokens.extend(quote!(
-                let fixed_fields = call_info.get_fixed_fields();
-                let result: ::eyre::Result<_> = (#closure)
-                (
-                    fixed_fields,
-                    #call_data
-                    #return_data
-                    #log_data
-                    db_tx
-                );
+            let fixed_fields = call_info.get_fixed_fields();
+            let result: ::eyre::Result<_> = (#closure)
+            (
+                fixed_fields,
+                #call_data
+                #return_data
+                #log_data
+                db_tx
+            );
 
-                // metrics
-                if result.is_err() {
-                            let protocol= db_tx.get_protocol(call_info.target_address)
-                                .ok()?;
+            // metrics
+            if result.is_err() {
+                        let protocol= db_tx.get_protocol(call_info.target_address)
+                            ?;
 
-        crate::CLASSIFICATION_METRICS.get_or_init(|| ClassificationMetrics::default())
-            .bad_protocol_classification(protocol);
-                }
+            crate::CLASSIFICATION_METRICS.get_or_init(|| ClassificationMetrics::default())
+                .bad_protocol_classification(protocol);
+            }
 
 
-                let result = result?;
-            ))
+            let result = result?;
+        ))
     }
 }
