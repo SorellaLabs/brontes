86ad3ab7b808b2f9ae2127dfa599e339824e997c
Will Smith <williamqsmith11@gmail.com>
2024-05-05 11:10:32 -0400

    wip

--- crates/bin/src/executors/shared/metadata.rs
+++ crates/bin/src/executors/shared/metadata.rs
@@ -178,6 +178,8 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter, CH: ClickhouseHandle> Str
             if bn == self.block {
                 self.block += 1;
                 return Poll::Ready(Some(res))
+            } else {
+                tracing::info!(?bn, ?self.block);
             }
             self.result_buf.push_front(res);
         }
