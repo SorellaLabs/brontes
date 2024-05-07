7d4fad5cd81159fcd1cee884e69d8a402c3cbdd8
Will Smith <williamqsmith11@gmail.com>
2024-05-07 19:53:29 -0400

    f

--- crates/bin/src/executors/shared/state_collector.rs
+++ crates/bin/src/executors/shared/state_collector.rs
@@ -99,8 +99,9 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter, CH: ClickhouseHandle>
 
     pub fn fetch_state_for(&mut self, block: u64, id: usize, metrics: GlobalRangeMetrics) {
         let metrics_c = metrics.clone();
+        let b = block;
         let execute_fut =
-            Box::pin(metrics_c.block_tracing(id, || self.parser.execute(block))) as ParserFuture;
+            Box::pin(metrics_c.block_tracing(id, || self.parser.execute(b))) as ParserFuture;
 
         let generate_pricing = self.metadata_fetcher.generate_dex_pricing(block, self.db);
         self.collection_future = Some(Box::pin(
