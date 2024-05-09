8c89608a5eeeba727d8cb87f966eb66e981bb572
Will Smith <williamqsmith11@gmail.com>
2024-05-09 13:46:56 -0400

    fix

--- crates/bin/src/cli/db/export.rs
+++ crates/bin/src/cli/db/export.rs
@@ -30,7 +30,7 @@ pub struct Export {
 
 impl Export {
     pub async fn execute(self, brontes_db_endpoint: String, ctx: CliContext) -> eyre::Result<()> {
-        let libmdbx = static_object(load_libmdbx(brontes_db_endpoint, &ctx.task_executor)?);
+        let libmdbx = static_object(load_libmdbx(&ctx.task_executor, brontes_db_endpoint)?);
         let exporter =
             Arc::new(ParquetExporter::new(self.start_block, self.end_block, self.path, libmdbx));
 
