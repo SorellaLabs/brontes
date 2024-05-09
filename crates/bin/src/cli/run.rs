81e5701c70c24b0e5a6939540c0fb84a11733bf9
Will Smith <williamqsmith11@gmail.com>
2024-05-09 15:25:09 -0400

    wip

--- crates/bin/src/cli/run.rs
+++ crates/bin/src/cli/run.rs
@@ -161,7 +161,9 @@ impl RunArgs {
                     tracing::error!(%e);
                     e
                 }) {
+                    tracing::error!("awaiting brontes");
                     brontes.await;
+                    tracing::error!("brontes ret");
                 }
             });
 
