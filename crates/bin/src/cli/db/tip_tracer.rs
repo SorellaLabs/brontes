73015d3822f6fb790be216b7e3f4a10c2d1e68ef
Will Smith <williamqsmith11@gmail.com>
2024-05-10 19:25:32 -0400

    cl

--- crates/bin/src/cli/db/tip_tracer.rs
+++ crates/bin/src/cli/db/tip_tracer.rs
@@ -57,10 +57,7 @@ impl TipTraceArgs {
         let catchup = ctx.task_executor.spawn_critical("catchup", async move {
             futures::stream::iter(start_block..=end_block)
                 .unordered_buffer_map(100, |i| parser.trace_for_clickhouse(i))
-                .map(|res| {
-                    drop(res);
-                    return
-                })
+                .map(|res| ())
                 .collect::<Vec<_>>()
                 .await;
         });
