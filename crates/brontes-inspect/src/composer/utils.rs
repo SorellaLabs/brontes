a39f282a29d5c1185736496a111e88e62f95630d
lagrange.eth <jnoorchashm37@sorellabs.xyz>
2024-05-16 19:20:38 -0400

    serde
    Former-commit-id: 1137aeb471d112520f5e72755c805d06c5c225dc

--- crates/brontes-inspect/src/composer/utils.rs
+++ crates/brontes-inspect/src/composer/utils.rs
@@ -21,7 +21,7 @@ pub(crate) fn build_mev_header(
     let (cumulative_mev_priority_fee_paid, cumulative_mev_profit_usd, total_mev_bribe) =
         orchestra_data.iter().fold(
             (0u128, 0f64, 0u128),
-            |(total_fee_paid, total_profit_usd, total_mev_bribe)| {
+            |(total_fee_paid, total_profit_usd, mev_bribe), bundle| {
                 let fee_paid = bundle.data.total_priority_fee_paid(
                     tree.header.base_fee_per_gas.unwrap_or_default() as u128,
                 );
@@ -31,7 +31,7 @@ pub(crate) fn build_mev_header(
                 (
                     total_fee_paid + fee_paid,
                     total_profit_usd + profit_usd,
-                    total_mev_bribe + bundle.data.bribe(),
+                    mev_bribe + bundle.data.bribe(),
                 )
             },
         );
