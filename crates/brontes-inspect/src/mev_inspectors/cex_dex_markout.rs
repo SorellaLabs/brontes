fbf17e7e050e6051be6f0b727f1e7bb53e103209
Will Smith <44074786+Will-Smith11@users.noreply.github.com>
2024-05-17 14:49:37 -0400

    fix: sandwich deduping (#657)
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * ensure proper across all
    
    * nicer
    
    * wip
    
    * wip
    
    * fix
    
    * wip
    
    * naming
    
    * wip
    
    * fix
    
    * wip
    
    * avoid clickhouse writing errors
    
    * yapper yap less
    
    Former-commit-id: 0926a2e7bfbbf1b4d3dab6317542984c23d1b4aa

--- crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
+++ crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
@@ -347,6 +347,7 @@ impl<DB: LibmdbxReader> CexDexMarkoutInspector<'_, DB> {
     ) -> Vec<(Option<MakerTakerWindowVWAP>, Option<MakerTaker>)> {
         dex_swaps
             .iter()
+            .filter(|swap| swap.amount_out != Rational::ZERO)
             .map(|swap| {
                 let pair = Pair(swap.token_in.address, swap.token_out.address);
 
@@ -363,7 +364,7 @@ impl<DB: LibmdbxReader> CexDexMarkoutInspector<'_, DB> {
                                 .calculate_time_window_vwam(
                                     &self.cex_exchanges,
                                     pair,
-                                    &swap.amount_in,
+                                    &swap.amount_out,
                                     metadata.microseconds_block_timestamp(),
                                 )
                         })
@@ -1013,7 +1014,7 @@ mod tests {
 
         let config = InspectorTxRunConfig::new(Inspectors::CexDexMarkout)
             .with_mev_tx_hashes(vec![tx])
-            .with_expected_profit_usd(8958.161)
+            .with_expected_profit_usd(8958.161528605704)
             .with_gas_paid_usd(79748.18);
 
         inspector_util.run_inspector(config, None).await.unwrap();
--- crates/brontes-types/src/channel_alerts.rs
+++ crates/brontes-types/src/channel_alerts.rs
@@ -18,7 +18,9 @@ impl<T> UnboundedYapperReceiver<T> {
         let len = self.chan.len();
         if len > self.yap_count {
             let mb = (std::mem::size_of::<T>() * len) / 1_000_000;
-            tracing::warn!(chan=%self.name,pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            if len % 100 == 0 {
+                tracing::warn!(chan=%self.name,pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            }
         }
 
         self.chan.blocking_recv()
@@ -28,7 +30,9 @@ impl<T> UnboundedYapperReceiver<T> {
         let len = self.chan.len();
         if len > self.yap_count {
             let mb = (std::mem::size_of::<T>() * len) / 1_000_000;
-            tracing::warn!(chan=%self.name,pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            if len % 100 == 0 {
+                tracing::warn!(chan=%self.name,pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            }
         }
 
         self.chan.poll_recv(cx)
@@ -38,7 +42,9 @@ impl<T> UnboundedYapperReceiver<T> {
         let len = self.chan.len();
         if len > self.yap_count {
             let mb = (std::mem::size_of::<T>() * len) / 1_000_000;
-            tracing::warn!(chan=%self.name, pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            if len % 100 == 0 {
+                tracing::warn!(chan=%self.name,pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            }
         }
 
         self.chan.recv().await
@@ -52,7 +58,9 @@ impl<T> UnboundedYapperReceiver<T> {
         let len = self.chan.len();
         if len > self.yap_count {
             let mb = (std::mem::size_of::<T>() * len) / 1_000_000;
-            tracing::warn!(chan=%self.name, pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            if len % 100 == 0 {
+                tracing::warn!(chan=%self.name,pending=len, mb_usage=mb, "unbounded channel is above threshold");
+            }
         }
 
         self.chan.try_recv()
