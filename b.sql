9139c646aeb7ecda757744adb743b1f279cb9625
Will Smith <williamqsmith11@gmail.com>
2024-05-20 18:03:27 -0400

    remove

--- b.sql
+++
@@ -1,8 +0,0 @@
-with (
-  select block_timestamp * 1000000 from ethereum.blocks where block_number = 18264692
-) as h
-select count() from 
-cex.normalized_trades 
-where timestamp >= h - 5000000 
-  and timestamp < h + 8000000 
-  and upper(replaceAll(replaceAll(replaceAll(symbol, '/', ''), '-', ''), '_', '')) = 'PERLUSDT'
