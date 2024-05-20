88bea7c21dfe2f3779549ea21831fe9065930330
Will Smith <williamqsmith11@gmail.com>
2024-05-20 17:22:46 -0400

    finally

--- crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
+++ crates/brontes-inspect/src/mev_inspectors/cex_dex_markout.rs
@@ -320,10 +320,7 @@ impl<DB: LibmdbxReader> CexDexMarkoutInspector<'_, DB> {
             );
 
             return None
-        } else {
-            trace!("got valid prices");
         }
-
         // A positive delta indicates potential profit from buying on DEX
         // and selling on CEX.
         let maker_delta = &cex_quote.0 - swap.swap_rate();
@@ -465,7 +462,6 @@ impl<DB: LibmdbxReader> CexDexMarkoutInspector<'_, DB> {
                         .get_metrics()
                         .inspect(|m| m.missing_cex_pair(pair));
                 }
-                trace!(?window, ?other);
 
                 (window, other)
             })
