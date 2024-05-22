01db687eb7eab68d558bfd07899abf1711702cd5
Will Smith <williamqsmith11@gmail.com>
2024-05-22 12:47:13 -0400

    wip

--- crates/brontes-database/brontes-db/src/libmdbx/initialize.rs
+++ crates/brontes-database/brontes-db/src/libmdbx/initialize.rs
@@ -706,7 +706,7 @@ mod tests {
 
         initializer.initialize_full_range_tables().await.unwrap();
 
-        let tables = [Tables::TxTraces, Tables::BlockInfo];
+        let tables = [Tables::BlockInfo];
 
         let multi = MultiProgress::default();
         let tables_cnt = Arc::new(
