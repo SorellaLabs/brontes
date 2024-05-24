d9f2b5cccf648bc04576cdb71f1c9e62f0dc9020
Will Smith <williamqsmith11@gmail.com>
2024-05-24 17:40:39 -0400

    wip

--- crates/brontes-types/src/db/block_analysis.rs
+++ crates/brontes-types/src/db/block_analysis.rs
@@ -29,55 +29,51 @@ pub struct BlockAnalysis {
     pub all_total_revenue:         f64,
     pub all_average_profit_margin: f64,
 
-    pub all_top_searcher_rev:         f64,
-    pub all_top_searcher_rev_addr:    Address,
-    pub all_top_searcher_profit:      f64,
-    pub all_top_searcher_profit_addr: Address,
+    pub all_top_searcher_rev:         Option<f64>,
+    pub all_top_searcher_rev_addr:    Option<Address>,
+    pub all_top_searcher_profit:      Option<f64>,
+    pub all_top_searcher_profit_addr: Option<Address>,
     pub all_searchers:                u64,
 
-    pub all_top_fund_rev:       f64,
-    pub all_top_fund_rev_id:    Fund,
-    pub all_top_fund_profit:    f64,
-    pub all_top_fund_profit_id: Fund,
+    pub all_top_fund_rev:       Option<f64>,
+    pub all_top_fund_rev_id:    Option<Fund>,
+    pub all_top_fund_profit:    Option<f64>,
+    pub all_top_fund_profit_id: Option<Fund>,
     pub all_fund_count:         u64,
 
-    pub all_most_arbed_pool_profit_address: Address,
-    pub all_most_arbed_pool_profit:         f64,
+    pub all_most_arbed_pool_address: Address,
+    pub all_most_arbed_pool_profit:  f64,
+    pub all_most_arbed_pool_revenue: f64,
 
-    pub all_most_arbed_pool_revenue_address: Address,
-    pub all_most_arbed_pool_revenue:         f64,
-
-    pub all_most_arbed_pair_profit_address: Pair,
-    pub all_most_arbed_pair_profit:         f64,
-
-    pub all_most_arbed_pair_revenue_address: Pair,
-    pub all_most_arbed_pair_revenue:         f64,
+    pub all_most_arbed_pair_address: Pair,
+    pub all_most_arbed_pair_profit:  f64,
+    pub all_most_arbed_pair_revenue: f64,
 
     // atomic
-    pub atomic_total_profit: f64,
-    pub atomic_total_revenue: f64,
-    pub atomic_average_profit_margin: f64,
-    pub atomic_top_searcher_rev: f64,
-    pub atomic_top_searcher_rev_addr: Address,
-    pub atomic_top_searcher_profit: f64,
+    pub atomic_total_profit:             f64,
+    pub atomic_total_revenue:            f64,
+    pub atomic_average_profit_margin:    f64,
+    pub atomic_top_searcher_rev:         f64,
+    pub atomic_top_searcher_rev_addr:    Address,
+    pub atomic_top_searcher_profit:      f64,
     pub atomic_top_searcher_profit_addr: Address,
-    pub atomic_searchers: u64,
-    pub atomic_top_fund_rev: f64,
-    pub atomic_top_fund_rev_id: Fund,
-    pub atomic_top_fund_profit: f64,
-    pub atomic_top_fund_profit_id: Fund,
-    pub atomic_fund_count: u64,
-    pub atomic_most_arbed_pool_profit_address: Address,
-    pub atomic_most_arbed_pool_profit: f64,
-    pub atomic_most_arbed_pool_revenue_address: Address,
+    pub atomic_searchers:                u64,
+    pub atomic_top_fund_rev:             f64,
+    pub atomic_top_fund_rev_id:          Fund,
+    pub atomic_top_fund_profit:          f64,
+    pub atomic_top_fund_profit_id:       Fund,
+    pub atomic_fund_count:               u64,
+
+    pub atomic_most_arbed_pool_address: Address,
+    pub atomic_most_arbed_pool_profit:  f64,
     pub atomic_most_arbed_pool_revenue: f64,
-    pub atomic_most_arbed_pair_profit_address: Pair,
-    pub atomic_most_arbed_pair_profit: f64,
-    pub atomic_most_arbed_pair_revenue_address: Pair,
+
+    pub atomic_most_arbed_pair_address: Pair,
+    pub atomic_most_arbed_pair_profit:  f64,
     pub atomic_most_arbed_pair_revenue: f64,
-    pub atomic_most_arbed_dex_profit_address: Protocol,
-    pub atomic_most_arbed_dex_profit: f64,
-    pub atomic_most_arbed_dex_revenue_address: Protocol,
+
+    pub atomic_most_arbed_dex_address: Protocol,
+    pub atomic_most_arbed_dex_profit:  f64,
     pub atomic_most_arbed_dex_revenue: f64,
 
     // sandwich
@@ -200,37 +196,67 @@ pub struct BlockAnalysis {
 
 impl BlockAnalysis {
     pub fn new<DB: LibmdbxReader>(block: &MevBlock, bundles: &[Bundle], db: DB) -> Self {
-        todo!()
+        let (all_profit_addr, all_profit_am) =
+            Self::top_searcher_by_profit(|b| b != MevType::SearcherTx, bundles).unzip();
+        let (all_rev_addr, all_rev_am) =
+            Self::top_searcher_by_rev(|b| b != MevType::SearcherTx, bundles).unzip();
+
+        let (fund_rev, fund_rev_am) =
+            Self::top_fund_by_type_rev(|b| b != MevType::SearcherTx, bundles, db).unzip();
+        let (fund_profit, fund_profit_am) =
+            Self::top_fund_by_type_rev(|b| b != MevType::SearcherTx, bundles, db).unzip();
+
+        Self {
+            block_number:                 block.block_number,
+            all_total_profit:             Self::total_profit_by_type(
+                |f| f != MevType::SearcherTx,
+                bundles,
+            ),
+            all_total_revenue:            Self::total_revenue_by_type(
+                |f| f != MevType::SearcherTx,
+                bundles,
+            ),
+            all_average_profit_margin:    Self::average_profit_margin(
+                |f| f != MevType::SearcherTx,
+                bundles,
+            ),
+            all_searchers:                Self::unique(|b| b != MevType::SearcherTx, bundles),
+            all_top_searcher_rev:         all_rev_am,
+            all_top_searcher_rev_addr:    all_rev_addr,
+            all_top_searcher_profit_addr: all_profit_addr,
+            all_top_searcher_profit:      all_profit_am,
+            all_top_fund_rev_id:          fund_rev,
+            all_top_fund_rev:             fund_rev_am,
+            all_top_fund_profit:          fund_profit_am,
+            all_top_fund_profit_id:       fund_profit,
+        }
     }
 
-    fn total_revenue_by_type(mev_type: MevType, bundles: &[Bundle]) -> f64 {
+    fn total_revenue_by_type(mev_type: impl Fn(MevType) -> bool, bundles: &[Bundle]) -> f64 {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .map(|s| s.header.profit_usd + s.header.bribe_usd)
             .sum::<f64>()
     }
 
-    fn total_profit_by_type(mev_type: MevType, bundles: &[Bundle]) -> f64 {
+    fn total_profit_by_type(mev_type: impl Fn(MevType) -> bool, bundles: &[Bundle]) -> f64 {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .map(|s| s.header.profit_usd)
             .sum::<f64>()
     }
 
     fn top_fund_by_type_profit<DB: LibmdbxReader>(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         db: &DB,
     ) -> Option<(Fund, f64)> {
         bundles
             .iter()
+            .filter(|b| mev_type(b))
             .filter_map(|b| {
-                if b.data.mev_type() != mev_type {
-                    return None
-                }
-
                 let Some(eoa) = db.try_fetch_searcher_eoa_info(b.header.eoa).unwrap() else {
                     return None
                 };
@@ -249,17 +275,14 @@ impl BlockAnalysis {
     }
 
     fn top_fund_by_type_rev<DB: LibmdbxReader>(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         db: &DB,
     ) -> Option<(Fund, f64)> {
         bundles
             .iter()
+            .filter(|b| mev_type(b))
             .filter_map(|b| {
-                if b.data.mev_type() != mev_type {
-                    return None
-                }
-
                 let Some(eoa) = db.try_fetch_searcher_eoa_info(b.header.eoa).unwrap() else {
                     return None
                 };
@@ -277,18 +300,24 @@ impl BlockAnalysis {
             .max_by(|a, b| a.1.total_cmp(&b.1))
     }
 
-    fn top_searcher_by_profit(mev_type: MevType, bundles: &[Bundle]) -> Option<(Address, f64)> {
+    fn top_searcher_by_profit(
+        mev_type: impl Fn(MevType) -> bool,
+        bundles: &[Bundle],
+    ) -> Option<(Address, f64)> {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .max_by(|a, b| a.header.profit_usd.total_cmp(&b.header.profit_usd))
             .map(|r| (r.header.eoa, r.header.profit_usd))
     }
 
-    fn top_searcher_by_rev(mev_type: MevType, bundles: &[Bundle]) -> Option<(Address, f64)> {
+    fn top_searcher_by_rev(
+        mev_type: impl Fn(MevType) -> bool,
+        bundles: &[Bundle],
+    ) -> Option<(Address, f64)> {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .max_by(|a, b| {
                 (a.header.profit_usd + a.header.bribe_usd)
                     .total_cmp(&(b.header.profit_usd + b.header.bribe_usd))
@@ -297,7 +326,7 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_pool_profit(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Address>,
     ) -> Option<(Address, f64)> {
@@ -305,7 +334,7 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_pool_rev(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Address>,
     ) -> Option<(Address, f64)> {
@@ -313,7 +342,7 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_pair_profit(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Pair>,
     ) -> Option<(Pair, f64)> {
@@ -321,7 +350,7 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_pair_rev(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Pair>,
     ) -> Option<(Pair, f64)> {
@@ -329,7 +358,7 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_dex_profit(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Protocol>,
     ) -> Option<(Protocol, f64)> {
@@ -337,24 +366,27 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_dex_rev(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Protocol>,
     ) -> Option<(Protocol, f64)> {
         Self::most_transacted_rev(mev_type, bundles, f)
     }
 
-    fn average_profit_margin(mev_type: MevType, bundles: &[Bundle]) -> Option<f64> {
+    fn average_profit_margin(
+        mev_type: impl Fn(MevType) -> bool,
+        bundles: &[Bundle],
+    ) -> Option<f64> {
         Some(
             bundles
                 .iter()
-                .filter(|b| b.data.mev_type() == mev_type && b.header.bribe_usd != 0.0)
+                .filter(|b| mev_type(b.data.mev_type()) && b.header.bribe_usd != 0.0)
                 .map(|s| s.header.profit_usd / s.header.bribe_usd)
                 .sum::<f64>()
                 / Some(
                     bundles
                         .iter()
-                        .filter(|b| b.data.mev_type() == mev_type && b.header.bribe_usd != 0.0)
+                        .filter(|b| mev_type(b.data.mev_type()) && b.header.bribe_usd != 0.0)
                         .count(),
                 )
                 .filter(|value| *value != 0)
@@ -362,23 +394,23 @@ impl BlockAnalysis {
         )
     }
 
-    fn unique(mev_type: MevType, bundles: &[Bundle]) -> u64 {
+    fn unique(mev_type: fn(MevType) -> bool, bundles: &[Bundle]) -> u64 {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .map(|b| b.header.eoa)
             .unique()
             .count() as u64
     }
 
     fn most_transacted_profit<Ty: Copy + Hash + Eq>(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Ty>,
     ) -> Option<(Ty, f64)> {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .flat_map(|b| {
                 let res = f(&b.data);
                 let mut merged = Vec::with_capacity(res.len());
@@ -394,13 +426,13 @@ impl BlockAnalysis {
     }
 
     fn most_transacted_rev<Ty: Copy + Hash + Eq>(
-        mev_type: MevType,
+        mev_type: impl Fn(MevType) -> bool,
         bundles: &[Bundle],
         f: impl Fn(&BundleData) -> Vec<Ty>,
     ) -> Option<(Ty, f64)> {
         bundles
             .iter()
-            .filter(|b| b.data.mev_type() == mev_type)
+            .filter(|b| mev_type(b.data.mev_type()))
             .flat_map(|b| {
                 let res = f(&b.data);
                 let mut merged = Vec::with_capacity(res.len());
