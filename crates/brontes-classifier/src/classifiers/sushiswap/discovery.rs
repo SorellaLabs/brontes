4bd6219b80a6c0a61dc20cecd80335d772e9aa41
Timothy Snyder <timsnyder@sorellalabs.xyz>
2024-06-04 15:40:24 -0400

    add sushiswap test
    Former-commit-id: c6d0dccec28a58690578397431b0a23f857909ce

--- crates/brontes-classifier/src/classifiers/sushiswap/discovery.rs
+++ crates/brontes-classifier/src/classifiers/sushiswap/discovery.rs
@@ -35,3 +35,43 @@ discovery_impl!(
         }]
     }
 );
+
+#[cfg(test)]
+mod tests {
+    use alloy_primitives::{hex, Address, B256};
+    use brontes_types::{normalized_actions::pool::NormalizedNewPool, Protocol};
+
+    use crate::test_utils::ClassifierTestUtils;
+
+    #[brontes_macros::test]
+    async fn test_sushiswap_v2_discovery() {
+        let utils = ClassifierTestUtils::new().await;
+        let tx =
+            B256::new(hex!("d0acb944bf0f45dddc92e73376825a6395a3badf82f86283fa0b3ac5139a46eb"));
+
+        let eq_create = NormalizedNewPool {
+            trace_index:  1,
+            protocol:     Protocol::BalancerV1,
+            pool_address: Address::new(hex!("4c5be0fea74c33455f81c85561146bdaf09633da")),
+            tokens:       vec![
+                hex!("189564397643D9e6173A002f1BA98da7d40a0FA6").into(),
+                hex!("A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48").into(),
+            ],
+        };
+
+        utils
+            .test_discovery_classification(
+                tx,
+                Address::new(hex!("4c5be0fea74c33455f81c85561146bdaf09633da")),
+                |mut pool| {
+                    assert_eq!(pool.len(), 1);
+                    let pool = pool.remove(0);
+                    assert_eq!(pool.protocol, eq_create.protocol);
+                    assert_eq!(pool.pool_address, eq_create.pool_address);
+                    assert_eq!(pool.tokens, eq_create.tokens);
+                },
+            )
+            .await
+            .unwrap();
+    }
+}
