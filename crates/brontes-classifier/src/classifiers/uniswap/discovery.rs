70c7e68cc7dbb912376a4d120e5cc81c7238bab0
Timothy Snyder <timsnyder@sorellalabs.xyz>
2024-06-04 14:35:15 -0400

    add uniswap test

--- crates/brontes-classifier/src/classifiers/uniswap/discovery.rs
+++ crates/brontes-classifier/src/classifiers/uniswap/discovery.rs
@@ -42,28 +42,6 @@ mod tests {
 
     use crate::test_utils::ClassifierTestUtils;
 
-    // #[brontes_macros::test]
-    // async fn test_uniswap_v2_discovery() {
-    //     let classifier_utils = ClassifierTestUtils::new().await;
-    //     let uniswap_v2_discovery =
-    //         B256::from(hex!("16bba367585045f6c87ec2beca8243575d7a5891f58c1af5e70bc45de4d3e347"));
-
-    //     let eq_action = Action::NewPool(NormalizedNewPool {
-    //         trace_index:  1,
-    //         protocol:     Protocol::UniswapV2,
-    //         pool_address: hex!("5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f").into(),
-    //         tokens:       vec![
-    //             hex!("52c6889677E514BDD0f09E32003C15B33E88DccE").into(),
-    //             hex!("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").into(),
-    //         ],
-    //     });
-    //     let search = TreeSearchBuilder::default().with_action(Action::is_new_pool);
-
-    //     classifier_utils
-    //         .contains_action(uniswap_v2_discovery, 0, eq_action, search)
-    //         .await
-    //         .unwrap();
-    // }
     #[brontes_macros::test]
     async fn test_uniswap_v2_discovery() {
         let utils = ClassifierTestUtils::new().await;
@@ -96,4 +74,35 @@ mod tests {
             .unwrap();
     }
 
+    #[brontes_macros::test]
+    async fn test_uniswap_v2_discovery() {
+        let utils = ClassifierTestUtils::new().await;
+        let tx =
+            B256::new(hex!("06c8ae6cc8705d3c6c8da07f2cb14af08ce981788ef237dcd204992ad207ddf1"));
+
+        let eq_create = NormalizedNewPool {
+            trace_index:  1,
+            protocol:     Protocol::UniswapV2,
+            pool_address: Address::new(hex!("1F98431c8aD98523631AE4a59f267346ea31F984")),
+            tokens:       vec![
+                hex!("edB357b55BC2DA1882B629EaDD3DF06202092d69").into(),
+                hex!("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2").into(),
+            ],
+        };
+
+        utils
+            .test_discovery_classification(
+                tx,
+                Address::new(hex!("1F98431c8aD98523631AE4a59f267346ea31F984")),
+                |mut pool| {
+                    assert_eq!(pool.len(), 1);
+                    let pool = pool.remove(0);
+                    assert_eq!(pool.protocol, eq_create.protocol);
+                    assert_eq!(pool.pool_address, eq_create.pool_address);
+                    assert_eq!(pool.tokens, eq_create.tokens);
+                },
+            )
+            .await
+            .unwrap();
+    }
 }
