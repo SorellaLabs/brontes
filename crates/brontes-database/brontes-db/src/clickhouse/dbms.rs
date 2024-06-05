6859381d3d97b516ced2c5f2511f616ed2498df8
Timothy Snyder <timsnyder@sorellalabs.xyz>
2024-06-05 09:35:13 -0400

    address clippy
    Former-commit-id: baaa523edb6eece4ec03301772e1c2473ac7f625

--- crates/brontes-database/brontes-db/src/clickhouse/dbms.rs
+++ crates/brontes-database/brontes-db/src/clickhouse/dbms.rs
@@ -11,6 +11,7 @@ use db_interfaces::{clickhouse_dbms, remote_clickhouse_table};
 
 // TODO(timsnyder): Annotate and/or fix the `clickhouse_dbms` macro and remove
 // annotation here.
+#[allow(unused_attributes)]
 #[allow(clippy::manual_async_fn)]
 clickhouse_dbms!(
     BrontesClickhouseTables,
