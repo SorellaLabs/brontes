80a43ee9b38696573fa6e886d532cf9fc6deb3aa
Will Smith <williamqsmith11@gmail.com>
2024-06-06 18:18:13 -0400

    wip

--- crates/brontes-database/brontes-db/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/brontes-db/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -1,5 +1,5 @@
-select DISTINCT
-  address,
-  tokens
-from ethereum.pools
+SELECT
+    address,
+    (tokens, init_block, concat(protocol, protocol_subtype), curve_lp_token) AS tokens
+FROM ethereum.pools
 
