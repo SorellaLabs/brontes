ae5c4d470358bd387d23e00090c75084fe84af67
Timothy Snyder <timsnyder@sorellalabs.xyz>
2024-06-06 14:16:22 -0400

    add metapoolv2 tests
    Former-commit-id: c3b2c36fe9a62786da76120a6fc7d17b87241061

--- crates/brontes-classifier/src/classifiers/curve/discovery.rs
+++ crates/brontes-classifier/src/classifiers/curve/discovery.rs
@@ -195,40 +195,6 @@ mod tests {
     }
 
     #[brontes_macros::test]
-    async fn test_curve_v2_metapool_discovery() {
-        let utils = ClassifierTestUtils::new().await;
-        let tx =
-            B256::new(hex!("eb2401547e1318c4f643df05c0151908aca499d2e4f9488f0cdc4a94ac3fef6c"));
-
-        let eq_create = NormalizedNewPool {
-            trace_index:  1,
-            protocol:     Protocol::CurveV2MetaPool,
-            pool_address: Address::new(hex!("b28d406ed20e2768a74884e875ca50dee441da9a")),
-            tokens:       vec![
-                hex!("6b175474e89094c44da98b954eedeac495271d0f").into(),
-                hex!("a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48").into(),
-                hex!("dac17f958d2ee523a2206206994597c13d831ec7").into(),
-                hex!("8191DC3053Fe4564c17694cB203663d3C07B8960").into(),
-            ],
-        };
-
-        utils
-            .test_discovery_classification(
-                tx,
-                Address::new(hex!("b28d406ed20e2768a74884e875ca50dee441da9a")),
-                |mut pool| {
-                    assert_eq!(pool.len(), 1);
-                    let pool = pool.remove(0);
-                    assert_eq!(pool.protocol, eq_create.protocol);
-                    assert_eq!(pool.pool_address, eq_create.pool_address);
-                    assert_eq!(pool.tokens, eq_create.tokens);
-                },
-            )
-            .await
-            .unwrap();
-    }
-
-    #[brontes_macros::test]
     async fn test_curve_v2_metapool_plainpool1_discovery() {
         verify_discovery(
             hex!("6f9223d991fa3620d7295f5c7e96581bbbfcd6eb03054ebd85ed3b1d06472217").into(), 
@@ -272,7 +238,7 @@ mod tests {
     async fn test_curve_v2_metapool_metapool1_discovery() {
         verify_discovery(
             hex!("11dfcfa281837030ac8c994828fe174fdd75cfa8a66971b4b84fb38a1bb08597").into(), 
-            Protocol::CurveV2PlainPool, 
+            Protocol::CurveV2MetaPool, 
             hex!("6d0bd8365e2fcd0c2acf7d218f629a319b6c9d47").into(),
             vec![
                 hex!("fd8e70e83E399307db3978D3F34B060a06792c36").into(),
@@ -284,7 +250,7 @@ mod tests {
     async fn test_curve_v2_metapool_metapool2_discovery() {
         verify_discovery(
             hex!("59814dc53b4d415b68662433f6eea167ae64370432283598b5314b81a4801abb").into(), 
-            Protocol::CurveV2PlainPool, 
+            Protocol::CurveV2MetaPool, 
             hex!("e60986759872393a8360a4a7abeab3a6e0ba7848").into(),
             vec![
                 hex!("466a756E9A7401B5e2444a3fCB3c2C12FBEa0a54").into(),
