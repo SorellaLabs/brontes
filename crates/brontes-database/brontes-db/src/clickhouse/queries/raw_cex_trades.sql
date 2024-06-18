11d2a89dcdd9aad7f0a2dc4364712a1d17f2593a
Will Smith <williamqsmith11@gmail.com>
2024-06-18 15:15:42 -0400

    try

--- crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_trades.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_trades.sql
@@ -9,15 +9,15 @@
   FROM cex.normalized_trades 
   where timestamp >= ? AND timestamp < ?
 
-  UNION ALL
-
-  SELECT
-      exchange,
-      'Maker' as trade_type,
-      upper(replaceAll(replaceAll(replaceAll(symbol, '/', ''), '-', ''), '_', '')) AS symbol,
-      timestamp,
-      if(side == 'sell', 'buy', 'sell') as side,
-      if (side == 'sell', price, divide(1, price)) AS price,
-      if (side == 'sell', amount, multiply(divide(1, price), amount)) AS amount
-  FROM cex.normalized_trades 
-  where timestamp >= ? AND timestamp < ?
+  -- UNION ALL
+  --
+  -- SELECT
+  --     exchange,
+  --     'Maker' as trade_type,
+  --     upper(replaceAll(replaceAll(replaceAll(symbol, '/', ''), '-', ''), '_', '')) AS symbol,
+  --     timestamp,
+  --     if(side == 'sell', 'buy', 'sell') as side,
+  --     if (side == 'sell', price, divide(1, price)) AS price,
+  --     if (side == 'sell', amount, multiply(divide(1, price), amount)) AS amount
+  -- FROM cex.normalized_trades 
+  -- where timestamp >= ? AND timestamp < ?
