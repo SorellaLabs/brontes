b23c7b518f604d7b650544455c24a7f6e9d85c1e
Will Smith <williamqsmith11@gmail.com>
2024-06-18 15:18:14 -0400

    wip

--- crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_trades.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_trades.sql
@@ -4,8 +4,8 @@
       upper(replaceAll(replaceAll(replaceAll(symbol, '/', ''), '-', ''), '_', '')) AS symbol,
       timestamp,
       side,
-      if (side == 'sell', price, divide(1, price)) AS price,
-      if (side == 'sell', amount, multiply(divide(1, price), amount)) AS amount
+      if (side == 'buy', price, divide(1, price)) AS price,
+      if (side == 'buy', amount, multiply(divide(1, price), amount)) AS amount
   FROM cex.normalized_trades 
   where timestamp >= ? AND timestamp < ?
 
