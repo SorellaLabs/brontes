495935dc7011aa0ce628f9e54412eac9127d127b
Will Smith <williamqsmith11@gmail.com>
2024-06-20 17:07:13 -0400

    wip

--- scripts/export-db.sh
+++ scripts/export-db.sh
@@ -12,3 +12,6 @@ wc -c ~/brontes-db-latest.tar.gz  | awk '{print $1}' | tr -d '\n' > ~/db-size.tx
 rclone copy ~/brontes-db-latest.tar.gz r2:brontes-db-latest/ --s3-upload-cutoff=100M --s3-chunk-size=100M
 # upload byte-count to r2
 rclone copy ~/db-size.txt r2:brontes-db-latest/
+
+# cleanup tmp folder
+rm -rf ~/brontes-db-latest
