17f6733d373f28fef25c4ee289cd96111a72bf5a
Will Smith <williamqsmith11@gmail.com>
2024-06-21 11:38:18 -0400

    reset cahnges

--- crates/brontes-types/src/db/cex/trades/time_window_vwam.rs
+++ crates/brontes-types/src/db/cex/trades/time_window_vwam.rs
@@ -7,7 +7,10 @@ use std::{
 use alloy_primitives::{Address, FixedBytes};
 use itertools::Itertools;
 use malachite::{
-    num::basic::traits::{One, Zero},
+    num::{
+        arithmetic::traits::Reciprocal,
+        basic::traits::{One, Zero},
+    },
     Rational,
 };
 use tracing::trace;
@@ -229,7 +232,7 @@ impl<'a> TimeWindowTrades<'a> {
                     bypass_intermediary_vol = true;
                 }
 
-                let new_vol = volume / &res.0.global_exchange_price.clone();
+                let new_vol = volume / &res.0.global_exchange_price.clone().reciprocal();
                 let pair1_v = self.get_vwap_price(
                     config,
                     exchanges,
