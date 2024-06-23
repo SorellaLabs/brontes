4c88ace29b0a818756c513a26f1faade4ff9f414
Will Smith <williamqsmith11@gmail.com>
2024-06-23 18:22:16 -0400

    wip

--- crates/bin/src/executors/mod.rs
+++ crates/bin/src/executors/mod.rs
@@ -358,7 +358,11 @@ impl<T: TracingProvider, DB: LibmdbxInit, CH: ClickhouseHandle, P: Processor>
         if let Some(end_block) = self.end_block {
             (true, end_block)
         } else {
+            #[cfg(feature = "local-reth")]
             let chain_tip = self.parser.get_latest_block_number().unwrap();
+            #[cfg(not(feature = "local-reth"))]
+            let chain_tip = self.parser.get_latest_block_number().await.unwrap();
+
             (false, chain_tip - self.back_from_tip)
         }
     }
