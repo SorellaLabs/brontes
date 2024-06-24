15eda224ccef13bbbf068503ef87ca2a939ce528
Will Smith <williamqsmith11@gmail.com>
2024-06-24 18:46:52 -0400

    wip

--- crates/brontes-types/src/db/cex/trades/time_window_vwam.rs
+++ crates/brontes-types/src/db/cex/trades/time_window_vwam.rs
@@ -164,7 +164,7 @@ impl<'a> TimeWindowTrades<'a> {
                 config, exchanges, pair, volume, timestamp, bypass_vol, dex_swap, tx_hash,
             )
             .or_else(|| {
-                tracing::info!(?tx_hash, "getting intermediary");
+                tracing::info!(?pair, ?tx_hash, "getting intermediary");
                 self.get_vwap_price_via_intermediary(
                     config, exchanges, &pair, volume, timestamp, bypass_vol, dex_swap, tx_hash,
                 )
