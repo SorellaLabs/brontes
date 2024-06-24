5ea1486b9fb64712ad51de0aa7c08cb5585d53a9
Will Smith <williamqsmith11@gmail.com>
2024-06-24 11:58:49 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/shared_utils.rs
+++ crates/brontes-inspect/src/mev_inspectors/shared_utils.rs
@@ -344,7 +344,6 @@ impl<DB: LibmdbxReader> SharedInspectorUtils<'_, DB> {
         mev_addresses: FastHashSet<Address>,
         deltas: &AddressDeltas,
         metadata: Arc<Metadata>,
-        eth_value: U256,
     ) -> Option<Rational> {
         let mut usd_deltas = FastHashMap::default();
 
