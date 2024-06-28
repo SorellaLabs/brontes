85ce2854c0d63938480562fce89572fbf7099b03
Will Smith <williamqsmith11@gmail.com>
2024-06-28 13:37:15 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/liquidations.rs
+++ crates/brontes-inspect/src/mev_inspectors/liquidations.rs
@@ -90,7 +90,7 @@ impl<DB: LibmdbxReader> LiquidationInspector<'_, DB> {
         let deltas = actions
             .into_iter()
             .chain(info.get_total_eth_value().iter().cloned().map(Action::from))
-            .filter(|a| a.is_eth_transfer() || a.is_transfer() || a.is_liquidation())
+            .filter(|a| a.is_eth_transfer() || a.is_transfer() )
             .account_for_actions();
 
         let (rev, mut has_dex_price) = if let Some(rev) = self.utils.get_deltas_usd(
