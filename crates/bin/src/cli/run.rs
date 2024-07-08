65080b16d5e267a3a008544b2f5211240af32832
vanbeethoven.eth <103021596+0xvanbeethoven@users.noreply.github.com>
2024-07-08 19:01:17 -0400

    Ludwig/c d docs (#731)
    * cex-dex-docs WIP
    
    * dex-wip

--- crates/bin/src/cli/run.rs
+++ crates/bin/src/cli/run.rs
@@ -46,11 +46,11 @@ pub struct RunArgs {
     pub inspectors: Option<Vec<Inspectors>>,
     /// The sliding time window (BEFORE) for cex prices or trades relative to
     /// the block timestamp
-    #[arg(long = "tw-before", short = 'b', default_value = if cfg!(feature = "cex-dex-markout") { "5.0" } else { "0.5" })]
+    #[arg(long = "tw-before", short = 'b', default_value = if cfg!(feature = "cex-dex-quotes") { "0.5" } else { "5.0" })]
     pub time_window_before: f64,
     /// The sliding time window (AFTER) for cex prices or trades relative to the
     /// block timestamp
-    #[arg(long = "tw-after", short = 'a', default_value = if cfg!(feature = "cex-dex-markout") { "8.0" } else { "2.0" })]
+    #[arg(long = "tw-after", short = 'a', default_value = if cfg!(feature = "cex-dex-quotes") { "2.0" } else { "8.0" })]
     pub time_window_after: f64,
     /// The time window (BEFORE) for cex prices or trades relative to
     /// the block timestamp for fully optimistic calculations
