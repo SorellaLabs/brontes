a650a12937e44601fdced72f5a69ea3d4569a1ee
Will Smith <williamqsmith11@gmail.com>
2024-07-17 13:38:04 -0400

    fixo

--- crates/brontes-database/brontes-db/src/clickhouse/queries/crit_init_tables.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/queries/crit_init_tables.sql
@@ -3,10 +3,10 @@ with
     select cast(count(), 'UInt64') as pool_creation from ( select init_block from ethereum.pools group by init_block )
   ),
   address_to_protocol as (
-    select cast(count(), 'UInt64') as address_to_protocol from ethereum.pools 
+    select cast(count(distinct address), 'UInt64') as address_to_protocol from ethereum.pools 
   ),
   tokens as (
-    select cast(count(), 'UInt64') as tokens from brontes.token_info
+    select cast(count(distinct address), 'UInt64') as tokens from brontes.token_info
   ),
   builder as (
     select cast(count(), 'UInt64') as builder from brontes_api.builder_info
