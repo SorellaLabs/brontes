fe68aefc1bdff45cf5580c89d4d27182f5b8c841
Will Smith <williamqsmith11@gmail.com>
2024-07-25 15:09:33 -0400

    speed

--- crates/brontes-pricing/benches/bench.rs
+++ crates/brontes-pricing/benches/bench.rs
@@ -6,10 +6,10 @@ criterion_group!(
     name = it_runs;
     config = Criterion::default().significance_level(0.1).noise_threshold(0.05).sample_size(10);
     targets =
+    it::bench_block_pricing,
         it::bench_block_pricing_after_5_blocks,
         it::bench_block_pricing_after_10_blocks,
         it::bench_block_pricing_after_20_blocks,
-        it::bench_block_pricing,
 );
 criterion_group!(v3, state::bench_v3_price_requests, state::bench_v3_state_loads);
 criterion_group!(v2, state::bench_v2_price_requests, state::bench_v2_state_loads);
