8ff257ccc34c02c5575f842bac2c567619ff7dba
Will Smith <williamqsmith11@gmail.com>
2024-07-26 13:32:03 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/sandwich/mod.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich/mod.rs
@@ -1461,4 +1461,21 @@ mod tests {
 
         inspector_util.assert_no_mev(config).await.unwrap();
     }
+
+    #[brontes_macros::test]
+    async fn beaver_double_cex_dex_false_positive() {
+        let inspector_util = InspectorTestUtils::new(USDT_ADDRESS, 1.0).await;
+
+        let config = InspectorTxRunConfig::new(Inspectors::Sandwich)
+            .with_dex_prices()
+            .with_mev_tx_hashes(vec![
+                hex!("abcc6968cd2a072b20f5e2d25d80d7ad6957efa999079c511a278dd6eb9095d6").into(),
+                hex!("a79536b1257d96b03f53ff9e0017176704535a19353beb006179f4f9f9ef69aa").into(),
+                hex!("435470d1f5e2494525f556d03303e6a1e3622777b6b718cbb77abf9d6bd0ebdb").into(),
+            ])
+            .with_expected_profit_usd(2711.18)
+            .with_gas_paid_usd(3129.59);
+
+        inspector_util.run_inspector(config, None).await.unwrap();
+    }
 }
