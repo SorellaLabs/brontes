5a577828bfcd9a4fc962e31c4613997e031c0cf2
Will Smith <williamqsmith11@gmail.com>
2024-07-29 18:01:32 -0400

    a

--- crates/bin/src/executors/shared/metadata_loader.rs
+++ crates/bin/src/executors/shared/metadata_loader.rs
@@ -140,8 +140,9 @@ impl<T: TracingProvider, CH: ClickhouseHandle> MetadataLoader<T, CH> {
             return self.cex_window_data.cex_trade_map()
         }
 
-        let last_block = self.cex_window_data.get_last_end_block_loaded();
+        let last_block = self.cex_window_data.get_last_end_block_loaded() + 1;
         let window = self.cex_window_data.get_window_lookahead();
+
         let offsets = (window / 12) as u64;
 
         for block in last_block..last_block + offsets {
