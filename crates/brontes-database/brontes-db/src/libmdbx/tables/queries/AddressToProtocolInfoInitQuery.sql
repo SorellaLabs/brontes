577338c45709f2b06cd0c5046f31141324d0f153
Joseph Noorchashm <joseph@sorellalabs.xyz>
2024-07-30 01:13:16 -0400

    Fix/analysis data (#757)
    * avg profit margin
    
    * updated names

--- crates/brontes-database/brontes-db/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
+++ crates/brontes-database/brontes-db/src/libmdbx/tables/queries/AddressToProtocolInfoInitQuery.sql
@@ -1,8 +1,8 @@
 SELECT
     cast(address,'String') as address,
-    cast((tokens, init_block, concat(protocol, protocol_subtype), curve_lp_token),
-      'Tuple(Array(String),UInt64,String,Nullable(String))')
-      AS tokens
+    cast(CASE
+      WHEN protocol = 'Curve.fi' AND protocol_subtype = 'Base' THEN (tokens, init_block, concat(protocol, protocol_subtype, toString(length(tokens))), curve_lp_token)
+      ELSE (tokens, init_block, concat(protocol, protocol_subtype), curve_lp_token)
+    END, 'Tuple(Array(String),UInt64,String,Nullable(String))') AS tokens
 FROM ethereum.pools 
 WHERE length(pools.tokens) >= 2
-
