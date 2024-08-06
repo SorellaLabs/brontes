b838b8044c698d84401c2b1dc59b93d02d4e4665
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2024-08-06 01:06:06 -0400

    joe please fix inserts

--- crates/brontes-database/brontes-db/src/clickhouse/tables/cex_dex.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/tables/cex_dex.sql
@@ -92,6 +92,6 @@ CREATE TABLE mev.cex_dex ON CLUSTER eth_cluster0
     ),
     `run_id` UInt64
 )
-ENGINE = ReplicatedReplacingMergeTree('/clickhouse/eth_cluster0/tables/all/mev/cex_dex', '{replica}', `run_id')
+ENGINE = ReplicatedReplacingMergeTree('/clickhouse/eth_cluster0/tables/{shard}/mev/cex_dex', '{replica}', run_id)
 PRIMARY KEY (block_number, tx_hash)
 ORDER BY (block_number, tx_hash)
