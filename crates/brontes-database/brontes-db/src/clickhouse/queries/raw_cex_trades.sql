6878c922883dd42f857a16b8f8056835e8f63ea0
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-08-09 11:44:49 -0400

    ordered

--- crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_trades.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_trades.sql
@@ -1,10 +1,11 @@
-  SELECT
-      c.exchange as exchange,
-      'Taker' as trade_type,
-      upper(replaceAll(replaceAll(replaceAll(c.symbol, '/', ''), '-', ''), '_', '')) AS symbol,
-      c.timestamp as timestamp,
-      c.side as side,
-      c.price AS price,
-      c.amount AS amount
-  FROM cex.normalized_trades as c 
-  where c.timestamp >= ? AND c.timestamp < ?
+SELECT
+    c.exchange as exchange,
+    'Taker' as trade_type,
+    upper(replaceAll(replaceAll(replaceAll(c.symbol, '/', ''), '-', ''), '_', '')) AS symbol,
+    c.timestamp as timestamp,
+    c.side as side,
+    c.price AS price,
+    c.amount AS amount
+FROM cex.normalized_trades as c 
+where c.timestamp >= ? AND c.timestamp < ?
+ORDER BY c.timestamp
