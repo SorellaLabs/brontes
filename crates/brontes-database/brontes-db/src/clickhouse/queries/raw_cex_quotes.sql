d5b46e1fb345e48dc10b555ba204fc5e6551eace
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-08-09 12:10:51 -0400

    updated query for faster performance

--- crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_quotes.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/queries/raw_cex_quotes.sql
@@ -1,11 +1,26 @@
+WITH
+    ? AS start_time,
+    ? AS end_time,
+    grouped_time AS (
+        SELECT
+            c.exchange as exchange,
+            upper(replaceAll(replaceAll(replaceAll(c.symbol, '/', ''), '-', ''), '_', '')) AS symbol,
+            toDateTime(c.timestamp / 1000000, 'UTC') AS timestamp_sec,
+            max(c.timestamp) as timestamp,
+            argMax(c.ask_amount, c.timestamp) as ask_amount,
+            argMax(c.ask_price, c.timestamp) as ask_price,
+            argMax(c.bid_price, c.timestamp) as bid_price,
+            argMax(c.bid_amount, c.timestamp) as bid_amount
+        FROM cex.normalized_quotes as c
+        WHERE c.timestamp >= start_time AND c.timestamp < end_time
+        GROUP BY exchange, symbol, timestamp_sec
+    )
 SELECT
-    c.exchange as exchange,
-    upper(replaceAll(replaceAll(replaceAll(c.symbol, '/', ''), '-', ''), '_', '')) AS symbol,
-    c.timestamp as timestamp,
-    c.ask_amount as ask_amount,
-    c.ask_price as ask_price,
-    c.bid_price as bid_price,
-    c.bid_amount as bid_amount
-FROM cex.normalized_quotes as c
-WHERE c.timestamp >= ? AND c.timestamp < ?
-ORDER BY c.timestamp
+    exchange,
+    symbol,
+    timestamp,
+    ask_amount,
+    ask_price,
+    bid_price,
+    bid_amount
+FROM grouped_time
