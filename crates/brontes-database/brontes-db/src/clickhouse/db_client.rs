bd73add5749431b17c5eae5958d7ab192b82874f
Tim Snyder <timsnyder@sorellalabs.xyz>
2024-08-09 09:50:26 -0400

    fix up tests

--- crates/brontes-database/brontes-db/src/clickhouse/db_client.rs
+++ crates/brontes-database/brontes-db/src/clickhouse/db_client.rs
@@ -817,7 +817,7 @@ mod tests {
             ..CexDex::default()
         };
 
-        db.insert_one::<MevCex_Dex>(&DbDataWithRunId<CexDex>(case0, 0)).await.unwrap();
+        db.insert_one::<MevCex_Dex>(&DbDataWithRunId::new_with_run_id(case0, 0)).await.unwrap();
 
         let case1 = CexDex {
             swaps: vec![swap.clone()],
