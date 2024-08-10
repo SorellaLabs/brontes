4763de2f6a8c3697a19dde7559d00903c2b6e080
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-08-10 18:31:07 -0400

    table fix

--- crates/brontes-database/brontes-db/src/clickhouse/queries/most_volume_pair_exchange.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/queries/most_volume_pair_exchange.sql
@@ -4,7 +4,7 @@ with ranked_symbols as (
     symbol,
     exchange,
     ROW_NUMBER() OVER (PARTITION BY symbol ORDER BY sum_volume DESC) as rn
-    from cex.ranked_symbol_exchange
+    from cex.trading_volume_by_month
     where month >= toStartOfMonth(toDateTime(? / 1000000) - INTERVAL 1 MONTH) and month <= toStartOfMonth(toDateTime(? / 1000000) - INTERVAL 1 MONTH)
 )
 SELECT symbol, exchange, toUnixTimestamp(month) * 1000000 as timestamp
