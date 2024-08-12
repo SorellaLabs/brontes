4b17153d78e4fd22ca1b14c8dfce6725cd19d94a
Will Smith <williamqsmith11@gmail.com>
2024-08-12 14:21:56 -0400

    wip

--- crates/brontes-inspect/src/mev_inspectors/sandwich/mod.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich/mod.rs
@@ -1362,8 +1362,8 @@ mod tests {
                 USDT_ADDRESS,
                 USDC_ADDRESS,
             ])
-            .with_gas_paid_usd(702.36)
-            .with_expected_profit_usd(110.2);
+            .with_gas_paid_usd(700.36)
+            .with_expected_profit_usd(112.2);
 
         inspector_util.run_inspector(config, None).await.unwrap();
     }
