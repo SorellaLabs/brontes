0877c1c5a4146698534a9d988a1d4b9b9cda720f
Will Smith <williamqsmith11@gmail.com>
2024-08-13 17:57:07 -0400

    wip

--- crates/brontes-types/src/db/initialized_state.rs
+++ crates/brontes-types/src/db/initialized_state.rs
@@ -3,12 +3,21 @@ use serde::{Deserialize, Serialize};
 
 use crate::implement_table_value_codecs_with_zc;
 
-pub const META_FLAG: u8 = 0b1;
-pub const CEX_QUOTES_FLAG: u8 = 0b10;
-pub const CEX_TRADES_FLAG: u8 = 0b100;
-pub const TRACE_FLAG: u8 = 0b1000;
-pub const DEX_PRICE_FLAG: u8 = 0b10000;
-pub const SKIP_FLAG: u8 = 0b100000;
+/// All these flags are the shift amount of the avaiablilities
+pub const META_FLAG: u16 = 0;
+pub const CEX_QUOTES_FLAG: u16 = 2;
+pub const CEX_TRADES_FLAG: u16 = 4;
+pub const TRACE_FLAG: u16 = 6;
+pub const DEX_PRICE_FLAG: u16 = 8;
+
+/// Data not present, availability unknown
+pub const DATA_NOT_PRESENT_UNKNOWN: u16 = 0b00;
+///  Data not present and not available
+pub const DATA_NOT_PRESENT_NOT_AVAILABLE: u16 = 0b01;
+/// Data not present but available (i.e., confirmed empty, not present in
+/// clickhouse)
+pub const DATA_NOT_PRESENT_BUT_AVAILABLE: u16 = 0b10;
+pub const DATA_PRESENT: u16 = 0b11;
 
 #[derive(
     Debug,
@@ -27,10 +36,7 @@ pub const SKIP_FLAG: u8 = 0b100000;
 #[repr(transparent)]
 /// InitializedState allows for us to mark up to 8 fields in
 /// the database as initialized
-/// there keys are as followed,
-/// [0, 0, should_skip, has_dex_price, has_traces,
-/// has_cex_trades,has_cex_quotes, has_meta]
-pub struct InitializedStateMeta(u8);
+pub struct InitializedStateMeta(u16);
 
 impl InitializedStateMeta {
     pub fn new(
@@ -41,10 +47,7 @@ impl InitializedStateMeta {
         has_cex_trades: bool,
         has_meta: bool,
     ) -> Self {
-        let mut this = 0u8;
-        if should_skip {
-            this |= SKIP_FLAG;
-        }
+        let mut this = 0u16;
         if has_dex_price {
             this |= DEX_PRICE_FLAG
         }
@@ -65,22 +68,24 @@ impl InitializedStateMeta {
     }
 
     #[inline(always)]
-    pub fn set(&mut self, this: u8) {
+    pub fn set(&mut self, this: u16) {
         self.0 |= this
     }
 
     #[inline(always)]
     pub fn should_ignore(&self) -> bool {
-        self.0 & SKIP_FLAG != 0
+        // self.0 & SKIP_FLAG != 0
+        //kjkj
+        false
     }
 
     #[inline(always)]
-    pub fn is_initialized(&self, flag: u8) -> bool {
+    pub fn is_initialized(&self, flag: u16) -> bool {
         (self.0 & flag) == flag
     }
 
     #[inline(always)]
-    pub fn apply_reset_key(&mut self, flag: u8) {
+    pub fn apply_reset_key(&mut self, flag: u16) {
         if self.is_initialized(flag) {
             self.0 ^= flag;
         }
