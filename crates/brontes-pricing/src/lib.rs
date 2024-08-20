4f5678321a175b9119ba6b26b499374d4bbb4c22
Will Smith <williamqsmith11@gmail.com>
2024-08-19 22:31:35 -0400

    w

--- crates/brontes-pricing/src/lib.rs
+++ crates/brontes-pricing/src/lib.rs
@@ -134,31 +134,6 @@ impl<T: TracingProvider> BrontesBatchPricer<T> {
         metrics: Option<DexPricingMetrics>,
         executor: BrontesTaskExecutor,
     ) -> Self {
-        #[cfg(feature = "deadlock_detection")]
-        {
-            // only for #[cfg]
-            use std::{thread, time::Duration};
-
-            use parking_lot::deadlock;
-
-            // Create a background thread which checks for deadlocks every 10s
-            thread::spawn(move || loop {
-                thread::sleep(Duration::from_secs(10));
-                let deadlocks = deadlock::check_deadlock();
-                if deadlocks.is_empty() {
-                    continue
-                }
-
-                println!("{} deadlocks detected", deadlocks.len());
-                for (i, threads) in deadlocks.iter().enumerate() {
-                    println!("Deadlock #{}", i);
-                    for t in threads {
-                        println!("Thread Id {:#?}", t.thread_id());
-                        println!("{:#?}", t.backtrace());
-                    }
-                }
-            });
-        } // only for #[cfg]
         Self {
             range_id,
             finished,
