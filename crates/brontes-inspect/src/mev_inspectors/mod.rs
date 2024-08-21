4d37639d70dd13a436f624398c73f7e9d70cf8b5
Will Smith <williamqsmith11@gmail.com>
2024-08-21 17:26:16 -0400

    fix: proper sign

--- crates/brontes-inspect/src/mev_inspectors/mod.rs
+++ crates/brontes-inspect/src/mev_inspectors/mod.rs
@@ -10,4 +10,4 @@ pub mod shared_utils;
 use malachite::Rational;
 
 pub(crate) const MAX_PROFIT: Rational = Rational::const_from_unsigned(15_000_000);
-pub(crate) const MIN_PROFIT: Rational = Rational::const_from_unsigned(15_000_000);
+pub(crate) const MIN_PROFIT: Rational = Rational::const_from_signed(-15_000_000);
