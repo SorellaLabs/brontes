0efabfe9cf428a0187f1491d6b489788c7ccd024
Will Smith <williamqsmith11@gmail.com>
2024-08-24 19:45:27 -0400

    fix: use or else

--- crates/bin/src/executors/mod.rs
+++ crates/bin/src/executors/mod.rs
@@ -253,7 +253,7 @@ impl<T: TracingProvider, DB: LibmdbxInit, CH: ClickhouseHandle, P: Processor>
                 let start_block = if *from_db_tip {
                     self.libmdbx
                         .get_most_recent_block()
-                        .unwrap_or(start_block.unwrap())
+                        .unwrap_or_else(|| start_block.unwrap())
                 } else {
                     start_block.unwrap()
                 };
