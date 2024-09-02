a1e3141be2533446d4213a3d050bbc0eb586d91e
Will Smith <williamqsmith11@gmail.com>
2024-09-02 17:56:04 -0400

    add total bar for gobal stats

--- crates/brontes-database/brontes-db/src/libmdbx/db_utils/libmdbx_merger.rs
+++ crates/brontes-database/brontes-db/src/libmdbx/db_utils/libmdbx_merger.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use brontes_types::BrontesTaskExecutor;
 use fs_extra::dir::get_dir_content;
-use indicatif::MultiProgress;
+use indicatif::{MultiProgress, ProgressBar, ProgressDrawTarget, ProgressState, ProgressStyle};
 use rayon::iter::*;
 
 use crate::{libmdbx::LibmdbxReadWriter, move_tables_to_partition, *};
@@ -15,6 +15,9 @@ pub fn merge_libmdbx_dbs(
     let files = get_dir_content(partition_db_folder)?;
     let multi = MultiProgress::default();
     // we can par this due to the single reader and not have any read locks.
+    let directory_count = files.directories.len() as u64;
+    let total_progress_bar = total_merge_bar(&multi, directory_count);
+
     files
         .directories
         .par_iter()
@@ -37,7 +40,25 @@ pub fn merge_libmdbx_dbs(
             TokenDecimals,
             DexPrice
             );
+            total_progress_bar.inc(1);
 
             eyre::Ok(())
         })
+}
+
+pub fn total_merge_bar(mutli_bar: &MultiProgress, count: u64) -> ProgressBar {
+    let progress_bar =
+        ProgressBar::with_draw_target(Some(count), ProgressDrawTarget::stderr_with_hz(50));
+    progress_bar.set_style(
+        ProgressStyle::with_template(
+            "{msg}\n[{elapsed_precise}] [{wide_bar:.green/red}] {pos}/{len} ({percent}%)",
+        )
+        .unwrap()
+        .progress_chars("#>-")
+        .with_key("percent", |state: &ProgressState, f: &mut dyn std::fmt::Write| {
+            write!(f, "{:.1}", state.fraction() * 100.0).unwrap()
+        }),
+    );
+    progress_bar.set_message("Total Databases Merged");
+    mutli_bar.add(progress_bar)
 }
