e34e0f12ffbef0d8dd099ddfa91c9342c33e05e8
Will Smith <williamqsmith11@gmail.com>
2024-09-17 18:23:38 -0500

    fmt

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -1,9 +1,9 @@
 use std::{env, error::Error, time::Duration};
 
 use brontes_tracing::BoxedLayer;
-use tracing_subscriber::{Layer, Registry};
 use log_report_layer::TelegramConfig;
 use tracing::Level;
+use tracing_subscriber::{Layer, Registry};
 
 #[cfg(all(feature = "jemalloc", unix))]
 #[global_allocator]
