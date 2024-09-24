5316c305c1485fe443d0eaefb3524cdec74fd8e8
Will Smith <williamqsmith11@gmail.com>
2024-09-23 20:49:04 -0500

    w

--- crates/brontes-inspect/src/mev_inspectors/shared_utils.rs
+++ crates/brontes-inspect/src/mev_inspectors/shared_utils.rs
@@ -617,11 +617,13 @@ impl<DB: LibmdbxReader> SharedInspectorUtils<'_, DB> {
 
                         let pct = if effective_price > dex_pricing_rate {
                             if effective_price == Rational::ZERO {
+                                tracing::info!("zero price");
                                 return None
                             }
                             (&effective_price - &dex_pricing_rate) / &effective_price
                         } else {
                             if dex_pricing_rate == Rational::ZERO {
+                                tracing::info!("zero price");
                                 return None
                             }
                             (&dex_pricing_rate - &effective_price) / &dex_pricing_rate
