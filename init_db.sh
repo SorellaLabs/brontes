42ab93a5cfcd80a5a8c5324cac1b045d91518ed3
Jinsuk Park <totorovirus@gmail.com>
2025-04-08 17:29:24 +0900

    add init_db.sh script for initializing the clickhouse

---
+++ init_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USER="brontes"
+
+echo "Creating Databases..."
+clickhouse-client --query="CREATE DATABASE IF NOT EXISTS brontes; CREATE DATABASE IF NOT EXISTS brontes_api; CREATE DATABASE IF NOT EXISTS ethereum; CREATE DATABASE IF NOT EXISTS mev;" --user $USER
+
+echo "Initializing brontes tables..."
+for sql_file in ./crates/brontes-database/brontes-db/src/clickhouse/tables/*.sql; do
+    echo "Running $sql_file..."
+    clickhouse-client --user $USER --multiquery < "$sql_file"
+done
