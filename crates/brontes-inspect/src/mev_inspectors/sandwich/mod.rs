208b43c662d270ddcb1f38a8e58a3979a98f384c
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2025-04-09 18:55:04 -0400

    testing

--- crates/brontes-inspect/src/mev_inspectors/sandwich/mod.rs
+++ crates/brontes-inspect/src/mev_inspectors/sandwich/mod.rs
@@ -292,7 +292,7 @@ impl<DB: LibmdbxReader> SandwichInspector<'_, DB> {
             .map(|g| g.gas_paid())
             .sum::<u128>();
 
-        println!("Backrun gas details:", backrun_info.gas_details);
+        println!("Backrun gas details: {}", backrun_info.gas_details);
 
         println!(
             "Frontrun Txees: {} Backrun tx: {} bribe_usd: {} Eth price: {} Quote Token: {} Number 
