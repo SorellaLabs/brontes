c91855469600400de24e6ea87c3780f2a7c3ec4e
Jinsuk Park <totorovirus@gmail.com>
2025-04-18 18:49:49 +0900

    add password prompt to init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -2,6 +2,10 @@
 
 USER="brontes"
 
+# Get password securely
+read -s -p "Enter ClickHouse password: " PASSWORD
+echo
+
 echo "Creating Databases..."
 clickhouse-client --query="
     CREATE DATABASE IF NOT EXISTS brontes;
@@ -9,10 +13,10 @@ clickhouse-client --query="
     CREATE DATABASE IF NOT EXISTS ethereum;
     CREATE DATABASE IF NOT EXISTS mev;
     CREATE DATABASE IF NOT EXISTS cex;
-" --user $USER --password
+" --user $USER --password "$PASSWORD"
 
 echo "Initializing brontes tables..."
 for sql_file in ./crates/brontes-database/brontes-db/src/clickhouse/tables/*.sql; do
     echo "Running $sql_file..."
-    clickhouse-client --user $USER --multiquery --password < "$sql_file"
+    clickhouse-client --user $USER --multiquery --password "$PASSWORD" < "$sql_file"
 done
