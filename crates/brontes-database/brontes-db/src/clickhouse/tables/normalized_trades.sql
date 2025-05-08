96f0eebfe6cf6711d4fc2ed4903f577f0fdf40b4
Jinsuk Park <totorovirus@gmail.com>
2025-05-08 21:22:45 +0900

    use FixedString(5) for the side of the trade in the normalized_trades table

--- crates/brontes-database/brontes-db/src/clickhouse/tables/normalized_trades.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/tables/normalized_trades.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cex.normalized_trades (
     `symbol` LowCardinality(FixedString(16)),           -- Trading pair symbol (e.g., "BTC/USDT")
     `exchange` LowCardinality(FixedString(16)),         -- Exchange name
-    `side` LowCardinality(String),            -- Side of the trade (e.g., "buy", "sell")
+    `side` LowCardinality(FixedString(5)),            -- Side of the trade (e.g., "buy", "sell")
     `timestamp` UInt64,        -- Unix timestamp in microseconds
     `amount` Float64,          -- Trade amount/volume
     `price` Float64            -- Trade price
