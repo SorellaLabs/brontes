44ea06d60e04daf0cd9fd259d2bd00df14cc8041
Jinsuk Park <totorovirus@gmail.com>
2025-05-08 22:48:17 +0900

    add trading_pairs.sql column trading_type

--- crates/brontes-database/brontes-db/src/clickhouse/tables/trading_pairs.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/tables/trading_pairs.sql
@@ -1,5 +1,6 @@
 CREATE TABLE cex.trading_pairs (
     exchange LowCardinality(String),         -- Exchange name
+    trading_type LowCardinality(String),     -- Trading type
     pair LowCardinality(String),             -- Trading pair identifier
     base_asset LowCardinality(String),       -- Symbol of base asset
     quote_asset LowCardinality(String)       -- Symbol of quote asset
