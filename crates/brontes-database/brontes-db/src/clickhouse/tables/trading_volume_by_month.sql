f8f6f33b9b607f72f9d3a5b88285def595cdd4dd
Jinsuk Park <totorovirus@gmail.com>
2025-05-09 17:13:00 +0900

    define trading_volume_by_month table and mv

---
+++ crates/brontes-database/brontes-db/src/clickhouse/tables/trading_volume_by_month.sql
@@ -0,0 +1,20 @@
+CREATE TABLE cex.trading_volume_by_month (
+    `month`       Date,
+    `symbol`      LowCardinality(FixedString(16)),
+    `exchange`    LowCardinality(FixedString(16)),
+    `sum_volume`  Float64
+)
+ENGINE = SummingMergeTree()
+PARTITION BY toYYYYMM(month)
+ORDER BY (month, symbol, exchange);
+
+CREATE MATERIALIZED VIEW cex.mv_trading_volume_by_month
+  TO cex.trading_volume_by_month
+AS
+SELECT
+  -- convert your microsecond ts into the first day of its month
+  toDate( toStartOfMonth( toDateTime(timestamp/1000000) ) ) AS month,
+  symbol,
+  exchange,
+  amount       AS sum_volume
+FROM cex.normalized_trades;
