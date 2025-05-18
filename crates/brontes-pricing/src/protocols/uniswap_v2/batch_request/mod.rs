0f6c1b0e5627790b9524282b6990c523e598c83a
marsclimber0109 <marsclimber0109@gmail.com>
2025-05-19 01:46:16 +0900

    chore: update abi

--- crates/brontes-pricing/src/protocols/uniswap_v2/batch_request/mod.rs
+++ crates/brontes-pricing/src/protocols/uniswap_v2/batch_request/mod.rs
@@ -12,7 +12,7 @@ use crate::errors::AmmError;
 
 sol!(
     IGetUniswapV2PoolDataBatchRequest,
-    "./src/protocols/uniswap_v2/batch_request/GetUniswapV2PoolDataBatchRequestABI.json"
+    "./src/protocols/uniswap_v2/batch_request/GetUniswapV2PoolDataBatchRequest.json"
 );
 
 sol!(
