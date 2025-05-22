e151dd9e51a0a31ca10c36960c9fc012ccdcc4b7
Jinsuk Park <totorovirus@gmail.com>
2025-05-22 18:35:13 +0900

    Update CamelotV2 to use the new state updater

--- crates/brontes-pricing/src/protocols/mod.rs
+++ crates/brontes-pricing/src/protocols/mod.rs
@@ -52,6 +52,7 @@ impl LoadState for Protocol {
                 | Self::SushiSwapV3
                 | Self::PancakeSwapV2
                 | Self::PancakeSwapV3
+                | Self::CamelotV2
         )
     }
 
@@ -64,7 +65,7 @@ impl LoadState for Protocol {
         fp: PairWithFirstPoolHop,
     ) -> Result<PoolFetchSuccess, PoolFetchError> {
         match self {
-            Self::UniswapV2 | Self::SushiSwapV2 | Self::PancakeSwapV2 => {
+            Self::UniswapV2 | Self::SushiSwapV2 | Self::PancakeSwapV2 | Self::CamelotV2 => {
                 let (pool, res) = if let Ok(pool) =
                     UniswapV2Pool::new_load_on_block(address, provider.clone(), block_number - 1)
                         .await
