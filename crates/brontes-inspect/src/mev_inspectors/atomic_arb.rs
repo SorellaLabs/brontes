606da2c847cebe6317861ff6537efc71a955dfa5
Jinsuk Park <totorovirus@gmail.com>
2025-06-03 22:53:21 +0900

    log profit > 100.0

--- crates/brontes-inspect/src/mev_inspectors/atomic_arb.rs
+++ crates/brontes-inspect/src/mev_inspectors/atomic_arb.rs
@@ -240,8 +240,8 @@ impl<DB: LibmdbxReader> AtomicArbInspector<'_, DB> {
 
         let profit = profit.to_float();
 
-        if profit < 0.0 {
-            tracing::error!(?info, ?header, ?profit, "negative profit");
+        if profit > 100.0 {
+            tracing::error!(?info, ?header.tx_hash, ?profit, "negative profit");
         }
 
         self.utils.get_profit_metrics().inspect(|m| {
